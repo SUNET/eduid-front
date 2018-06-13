b0175d7d2978e3a9e321b1c6e268d32db593bc7e
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-06-13 14:07:08 +0200

    test footer component

---
+++ src/tests/Footer-test.js
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import expect from "expect";
+
+import FooterContainer from "containers/Footer";
+import { setupComponent } from "tests/Main-test";
+
+
+describe("Footer Component", () => {
+
+    const state = {
+        main: {
+          is_app_loaded: true,
+          available_languages: {
+            en: 'English',
+            sv: 'Svenska'
+          }
+        },
+        intl: {
+          locale: 'en'
+        }
+    };
+
+    it("Renders the footer component", () => {
+
+        const wrapper = setupComponent(<FooterContainer />, state),
+              span = wrapper.find('span.langselector'),
+              link = wrapper.find('span.langselector').find('a');
+
+        expect(span.length).toEqual(2);
+        expect(link.length).toEqual(1);
+        expect(link.text()).toEqual('Svenska');
+    });
+});
+
