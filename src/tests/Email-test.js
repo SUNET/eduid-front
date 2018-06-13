c33450cc76d321863dbb66d339c6ed91d8d9d4fd
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-06-13 10:31:19 +0200

    email component tests

--- src/tests/Email-test.js
+++ src/tests/Email-test.js
@@ -9,10 +9,25 @@ import { setupComponent } from "tests/Main-test";
 describe("Email Component", () => {
 
     it("Renders the email component", () => {
-        const wrapper = setupComponent(<EmailContainer />),
-              splash = wrapper.find('input#email-input');
+        const wrapper = setupComponent(<EmailContainer />, {main: {window_size: 'lg'}}),
+              input = wrapper.find('input#email-input'),
+              button = wrapper.find('button#email-button'),
+              faIcon = wrapper.find('FontAwesomeIcon');
 
-        expect(splash.length).toEqual(1);
+        expect(input.length).toEqual(1);
+        expect(button.length).toEqual(1);
+        expect(faIcon.length).toEqual(1);
+    });
+
+    it("Renders the small email component", () => {
+        const wrapper = setupComponent(<EmailContainer />, {main: {window_size: 'xs'}}),
+              input = wrapper.find('input#email-input'),
+              button = wrapper.find('button#email-button'),
+              faIcon = wrapper.find('FontAwesomeIcon');
+
+        expect(input.length).toEqual(1);
+        expect(button.length).toEqual(1);
+        expect(faIcon.length).toEqual(0);
     });
 });
 
