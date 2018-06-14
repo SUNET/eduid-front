07820e323affda57cab3d7580d5056e6cb31d2ed
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-06-14 10:05:25 +0200

    main actions tests

--- src/tests/Main-test.js
+++ src/tests/Main-test.js
@@ -119,3 +119,73 @@ describe("Main Component", () => {
         expect(routes.length).toEqual(7);
     });
 });
+
+describe("Main Actions", () => {
+
+    it("Should get code status ", () => {
+        const expectedAction = {
+            type: actions.GET_CODE_STATUS,
+            payload: {
+                code: 'dummy code'               
+            }
+        };
+        expect(actions.getCodeStatus('dummy code')).toEqual(expectedAction);
+    });
+
+    it("Should signal the app has loaded", () => {
+        const expectedAction = {
+            type: actions.APP_LOADED,
+        };
+        expect(actions.appLoaded()).toEqual(expectedAction);
+    });
+
+    it("Should timeout the resizing", () => {
+      const expectedAction = {
+          type: actions.RESIZE_TIMEOUT,
+          payload: {
+              resize_timeout: 'dummy timeout'
+          }
+      };
+      expect(actions.resizeTimeout('dummy timeout')).toEqual(expectedAction);
+    });
+
+    it("Should resize the app", () => {
+      const expectedAction = {
+          type: actions.RESIZE_WINDOW,
+          payload: {
+              window_size: 'dummy size'
+          }
+      };
+      expect(actions.resizeWindow(true)).toEqual(expectedAction);
+    });
+
+    it("Should get the config", () => {
+        const expectedAction = {
+            type: actions.GET_SIGNUP_CONFIG,
+        };
+        expect(actions.getConfig()).toEqual(expectedAction);
+    });
+
+    it("Should fail when trying to get the config", () => {
+        const err = new Error('Get config error');
+        const expectedAction = {
+            type: actions.GET_SIGNUP_CONFIG_FAIL,
+            error: true,
+            payload: {
+                error: err,
+                message: err
+            }
+        };
+        expect(actions.getConfigFail(err)).toEqual(expectedAction);
+    });
+
+    it("Should store a new csrf token", () => {
+        const expectedAction = {
+            type: actions.NEW_CSRF_TOKEN,
+            payload: {
+                csrf_token: 'dummy token'
+            }
+        };
+        expect(actions.newCsrfToken('dummy token')).toEqual(expectedAction);
+    });
+});
