f8a51b6d37303c013aff9f8e03518624b1838b0f
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-06-18 09:17:39 +0200

    test captcha saga

--- src/tests/Captcha-test.js
+++ src/tests/Captcha-test.js
@@ -3,6 +3,7 @@ import mock from 'jest-mock';
 import React from 'react';
 import expect from "expect";
 import fetchMock from 'fetch-mock';
+import { put, call, select } from "redux-saga/effects";
 
 import { setupComponent, fakeStore, getState } from "tests/Main-test";
 import CaptchaContainer from "containers/Captcha";
@@ -105,5 +106,49 @@ describe("Test captcha Container", () => {
         const numCalls = dispatch.mock.calls.length;
         wrapper.find('EduIDButton#send-captcha-button').props().onClick();
         expect(dispatch.mock.calls.length).toEqual(numCalls + 1);
+    });
+});
+
+import { sendCaptcha, requestSendCaptcha } from "sagas/Captcha";
+
+describe("Async actions for captcha", () => {
+
+    it("Tests the send captcha saga", () => {
+
+        const state = getState({
+            main: {
+                csrf_token: "dummy-token",
+            },
+            email: {
+                email: "dummy@example.com",
+                tou_accepted: true
+            },
+            captcha: {
+                captcha_verification: "dummy response"
+            }
+        });
+        const generator = sendCaptcha();
+        let next = generator.next();
+
+        const data = {
+                 email: 'dummy@example.com',
+                 recaptcha_response: 'dummy response',
+                 csrf_token: 'dummy-token',
+                 tou_accepted: true
+               };
+        const resp = generator.next(state);
+        expect(resp.value).toEqual(call(requestSendCaptcha, data));
+
+        const action = {
+          type: actions.POST_SIGNUP_TRYCAPTCHA_SUCCESS,
+          payload: {
+            csrf_token: 'csrf-token',
+          }
+        }
+        next = generator.next(action);
+        expect(next.value.PUT.action.type).toEqual('NEW_CSRF_TOKEN');
+        next = generator.next();
+        delete(action.payload.csrf_token);
+        expect(next.value).toEqual(put(action));
     });
 });
