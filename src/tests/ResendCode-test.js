d4b14b5a4666a370d052d74c405941b0b45d701b
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-06-18 11:07:40 +0200

    test resend-code saga

--- src/tests/ResendCode-test.js
+++ src/tests/ResendCode-test.js
@@ -1,10 +1,12 @@
 
 import React from 'react';
 import expect from "expect";
+import { put, call, select } from "redux-saga/effects";
 
 import { setupComponent, fakeStore, getState } from "tests/Main-test";
 import ResendCodeContainer from "containers/ResendCode";
 import * as actions from "actions/ResendCode";
+import { resendCode, requestResendCode } from "sagas/ResendCode";
 
 
 describe("ResendCode Component", () => {
@@ -58,5 +60,42 @@ describe("Test Resend code Container", () => {
         const numCalls = dispatch.mock.calls.length;
         wrapper.find('EduIDButton#resend-button').props().onClick();
         expect(dispatch.mock.calls.length).toEqual(numCalls + 1);
+    });
+});
+
+describe("Resend code async actions", () => {
+
+    it("Tests the request config saga", () => {
+
+        const state = getState({
+            email: {
+                email: 'dummy@example.com'
+            },
+            main: {
+                csrf_token: "dummy-token"
+            }
+        });
+        const url = SIGNUP_SERVICE_URL + 'resend-verification';
+        const generator = resendCode();
+        let next = generator.next();
+
+        const data = {
+            email: 'dummy@example.com',
+            csrf_token: 'dummy-token'
+        };
+        const resp = generator.next(state);
+        expect(resp.value).toEqual(call(requestResendCode, data));
+
+        const action = {
+            type: actions.POST_SIGNUP_RESEND_VERIFICATION_SUCCESS,
+            payload: {
+                csrf_token: 'csrf-token',
+            }
+        }
+        next = generator.next(action);
+        expect(next.value.PUT.action.type).toEqual('NEW_CSRF_TOKEN');
+        next = generator.next();
+        delete(action.payload.csrf_token);
+        expect(next.value).toEqual(put(action));
     });
 });
