8bab7ae800dbf71ceb2c2145f214c065b595840a
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-07-17 11:30:40 +0200

    moving the js code for the plugins to the eduid-front repo

---
+++ plugins/eduid_action.tou/index.js
@@ -0,0 +1,24 @@
+
+try {
+    require("entry-points/plugin-common");
+}
+catch (error) {
+    console.log('Problem loading polyfills, ', error);
+}
+
+import React from "react";
+
+import init_plugin from "init-plugin";
+import * as actions from "actions/ActionWrapper";
+import MainContainer from "./component";
+import App from "./store";
+import rootSaga from "./root-saga";
+
+
+init_plugin(
+    App,
+    rootSaga,
+    document.getElementById('root'),
+    <MainContainer />,
+    actions.getConfig()
+);
---
+++ plugins/eduid_action.tou/root-saga.js
@@ -0,0 +1,44 @@
+
+import { takeLatest, takeEvery } from 'redux-saga';
+import { call, put, select } from "redux-saga/effects";
+
+import defaultSaga from "plugins-saga";
+import * as actions from "actions/ActionWrapper";
+import { postRequest, checkStatus, putCsrfToken } from "sagas/common";
+
+
+export function requestPostAcceptTOU (data) {
+    const url = 'post-action';
+    return window.fetch(url, {
+        ...postRequest,
+        body: JSON.stringify(data)
+    })
+    .then(checkStatus)
+    .then(response => response.json())
+}
+
+export function* postAcceptTOU () {
+    try {
+        const state = yield select(state => state),
+            data = {
+                accept: true,
+                version: state.plugin.version,
+                csrf_token: state.main.csrf_token,
+            };
+        const resp = yield call(requestPostAcceptTOU, data);
+        yield put(putCsrfToken(resp));
+        yield put(resp);
+    } catch(error) {
+        yield put(actions.postActionFail(error.toString()));
+    }
+}
+
+function* rootSaga() {
+    yield [
+        ...defaultSaga,
+        takeLatest(actions.POST_ACTIONS_ACTION, postAcceptTOU),
+    ];
+}
+
+export default rootSaga;
+
