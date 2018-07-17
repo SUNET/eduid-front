8d2ce1b3ab5fac91e082bd5d5264c2505700c03b
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-07-17 08:27:05 +0200

    add the skeleton for tghe js side of an actions plugin

---
+++ plugin-skel/component.js
@@ -0,0 +1,44 @@
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import i18n from 'i18n-messages';
+import { appFetching, postAction } from "actions/ActionWrapper";
+import ActionWrapperContainer from "containers/ActionWrapper";
+
+import './style.scss';
+
+
+class Main extends Component {
+
+    render () {
+
+        return (
+            <ActionWrapperContainer>
+              // UI for the action
+            </ActionWrapperContainer>
+        );
+    }
+}
+
+Main.propTypes = {
+}
+
+const mapStateToProps = (state, props) => {
+    return {
+    }
+};
+
+const mapDispatchToProps = (dispatch, props) => {
+    return {
+    }
+};
+
+const MainContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Main);
+
+export default i18n(MainContainer);
+
---
+++ plugin-skel/store.js
@@ -0,0 +1,34 @@
+
+import { combineReducers } from 'redux';
+import { intlReducer } from 'react-intl-redux'
+import { routerReducer } from 'react-router-redux';
+
+import actionWrapperReducer from 'reducers/ActionWrapper';
+import notificationsReducer from 'reducers/Notifications';
+
+import * as actions from "actions/ActionWrapper";
+
+// see the config params in eduid-developer/etcd/conf.yaml
+const actionData = {
+    // central state particular for this plugin, kept under state.plugin
+};
+
+export const actionReducer = (state=actionData, action) => {
+  switch (action.type) {
+    // modify the central state according to the particular actions defined in
+    // this plugin.
+    default:
+      return state;
+  }
+};
+
+
+const App = combineReducers({
+    router: routerReducer,
+    intl: intlReducer,
+    main: actionWrapperReducer,
+    plugin: actionReducer,
+    notifications: notificationsReducer
+});
+
+export default App;
