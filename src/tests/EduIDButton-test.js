623db975af79f701d72b202cce6cfbc7bb1edc3c
Enrique Pérez Arnaud <enrique@cazalla.net>
2019-01-10 11:19:07 +0100

    test eduID buttons

--- src/tests/EduIDButton-test.js
+++ src/tests/EduIDButton-test.js
@@ -3,45 +3,46 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import expect, { createSpy } from "expect";
 import EduIDButton from 'components/EduIDButton'
+import FetchingContext from 'components/FetchingContext';
+import ReactTestUtils from 'react-dom/test-utils';
 
 
 function setupComponent(spinning=false) {
-  const props = {
-    spinning: spinning
-  }
+    const compState = {
+        fetching: spinning
+    }
 
-  const wrapper = shallow(<EduIDButton {...props} />)
+    const wrapper = mount(
+        <FetchingContext.Provider value={compState}>
+            <EduIDButton />
+        </FetchingContext.Provider>
+    );
 
-  return {
-    props,
-    wrapper
-  }
+    return wrapper;
 }
 
 describe("EduIDButton Component should mount", () => {
 
   it("Renders without spinner", () => {
-    const { wrapper, props } = setupComponent(),
+    const wrapper = setupComponent(),
           button = wrapper.find('Button'),
           divEl = button.find('div.spin-holder'),
-          glyph = divEl.find('Glyphicon');
+          icon = divEl.find('FontAwesomeIcon');
 
-    expect(button.hasClass('has-spinner')).toBe(true);
-    expect(button.hasClass('active')).toBe(false);
+    expect(button.hasClass('has-spinner')).toBe(false);
     expect(divEl.length).toBe(0);
-    expect(glyph.length).toBe(0);
+    expect(icon.length).toBe(0);
   });
 
   it("Renders with spinner", () => {
-    const { wrapper, props } = setupComponent(true),
+    const wrapper = setupComponent(true),
           button = wrapper.find('Button'),
           divEl = button.find('div.spin-holder'),
-          glyph = divEl.find('Glyphicon');
+          icon = divEl.find('FontAwesomeIcon');
 
     expect(button.hasClass('has-spinner')).toBe(true);
-    expect(button.hasClass('active')).toBe(true);
     expect(divEl.hasClass('spin-holder')).toBe(true);
-    expect(glyph.hasClass('spinner')).toBe(true);
+    expect(icon.length).toBe(1);
   });
 });
 
