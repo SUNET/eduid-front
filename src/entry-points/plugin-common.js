1392b0d1c299410b0348fb3163a5a7c678ceb2d1
Nathalie Jonsson <njons@sunet.se>
2019-05-02 10:49:59 +0200

    prettier formatting code

--- src/entry-points/plugin-common.js
+++ src/entry-points/plugin-common.js
@@ -1,54 +1,52 @@
-
 import "babel-polyfill";
 
 // Polyfill for Element.closest for IE9+
 // see https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
 
 if (!Element.prototype.matches)
-      Element.prototype.matches = Element.prototype.msMatchesSelector || 
-                                    Element.prototype.webkitMatchesSelector;
+  Element.prototype.matches =
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.webkitMatchesSelector;
 
 if (!Element.prototype.closest)
-      Element.prototype.closest = function(s) {
-                var el = this;
-                if (!document.documentElement.contains(el)) return null;
-                do {
-                              if (el.matches(s)) return el;
-                              el = el.parentElement || el.parentNode;
-                          } while (el !== null); 
-                return null;
-            };
+  Element.prototype.closest = function(s) {
+    var el = this;
+    if (!document.documentElement.contains(el)) return null;
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null);
+    return null;
+  };
 
 // end Polyfill
 
 // URL.searchParams polyfill
 
-window.URLSearchParams = require('url-search-params');
+window.URLSearchParams = require("url-search-params");
 
 // End polyfill
 
-import { addLocaleData } from 'react-intl';
+import { addLocaleData } from "react-intl";
 
 const locales = [];
 
 //AVAILABLE_LANGUAGES.forEach((lang) => {
-    //const locale = require('react-intl/locale-data/' + lang[0]);
-    //addLocaleData([...locale]);
+//const locale = require('react-intl/locale-data/' + lang[0]);
+//addLocaleData([...locale]);
 //});
 
-
-//require.ensure([], () => {  
-    //AVAILABLE_LANGUAGES.forEach((lang) => {
-        //const locale = require('react-intl/locale-data/' + lang[0]);
-        //addLocaleData([...locale]);
-    //});
+//require.ensure([], () => {
+//AVAILABLE_LANGUAGES.forEach((lang) => {
+//const locale = require('react-intl/locale-data/' + lang[0]);
+//addLocaleData([...locale]);
+//});
 //});
 
 // XXX TODO load locales dynamically based on AVAILABLE_LANGUAGES.
 // None of the above commented techniques work
 
-import en from 'react-intl/locale-data/en';
-import sv from 'react-intl/locale-data/sv';
+import en from "react-intl/locale-data/en";
+import sv from "react-intl/locale-data/sv";
 
-addLocaleData([...en, ...sv])
-
+addLocaleData([...en, ...sv]);
