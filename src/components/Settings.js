1957576277f4ea35bee15c0999671ca3d2988028
Nathalie Jonsson <njons@sunet.se>
2019-05-02 14:17:39 +0200

    displays old dashboard upon click of Settings button

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -52,212 +52,217 @@ class Settings extends Component {
   // }
 
   render() {
-    // let contentElem = "";
+    let contentElem = "";
 
-    // const routesElem = notsElem => (
-    //   <div className="tab-content col-md-9 col-md-offset-1 col-sm-12">
-    //     <div className="tab-pane active">
-    //       {notsElem}
-    //       <Route
-    //         exact
-    //         path="/profile/"
-    //         component={() => <Redirect to="/profile/personaldata" />}
-    //       />
-    //       <Route
-    //         path="/profile/personaldata"
-    //         component={PersonalDataContainer}
-    //       />
-    //       <Route path="/profile/nins" component={NinsContainer} />
-    //       <Route path="/profile/emails" component={EmailsContainer} />
-    //       <Route path="/profile/phones" component={MobileContainer} />
-    //       <Route
-    //         path="/profile/accountlinking"
-    //         component={AccountLinkingContainer}
-    //       />
-    //       <Route path="/profile/security" component={SecurityContainer} />
-    //       <Route path="/profile/chpass" component={ChangePasswordContainer} />
-    //     </div>
-    //   </div>
-    // );
+    const routesElem = notsElem => (
+      <div className="tab-content col-md-9 col-md-offset-1 col-sm-12">
+        <div className="tab-pane active">
+          {notsElem}
+          <Route
+            exact
+            path="/profile/"
+            component={() => <Redirect to="/profile/personaldata" />}
+          />
+          <Route
+            path="/profile/personaldata"
+            component={PersonalDataContainer}
+          />
+          <Route path="/profile/nins" component={NinsContainer} />
+          <Route path="/profile/emails" component={EmailsContainer} />
+          <Route path="/profile/phones" component={MobileContainer} />
+          <Route
+            path="/profile/accountlinking"
+            component={AccountLinkingContainer}
+          />
+          <Route path="/profile/security" component={SecurityContainer} />
+          <Route path="/profile/chpass" component={ChangePasswordContainer} />
+        </div>
+      </div>
+    );
 
     if (this.props.show !== true) {
       return <div />;
     }
-    // if (this.props.show_sidebar) {
-    //   const size = this.props.window_size,
-    //     sm = size === "sm",
-    //     tabs = [
-    //       {
-    //         id: "personaldata",
-    //         label: sm ? "main.personal_data_label_sm" : "main.personal_data"
-    //       },
-    //       { id: "nins", label: sm ? "main.nins_label_sm" : "main.nins" },
-    //       { id: "emails", label: sm ? "main.emails_label_sm" : "main.emails" },
-    //       { id: "phones", label: sm ? "main.phones_label_sm" : "main.phones" },
-    //       {
-    //         id: "accountlinking",
-    //         label: sm ? "main.account_linking_label_sm" : "main.account_linking"
-    //       },
-    //       {
-    //         id: "security",
-    //         label: sm ? "main.security_label_sm" : "main.security"
-    //       }
-    //     ],
-    //     profElem = (
-    //       <div className="col-sm-12">
-    //         <ul className="nav nav-stacked nav-tabs navbar-nav">
-    //           <ProfileFilledContainer />
-    //           <li id="profile-menu-eppn-li">
-    //             <div className="profile-menu-eppn">
-    //               <p className="eppn-text-muted">
-    //                 {this.props.l10n("main.eduid_id")}: {this.props.eppn}
-    //               </p>
-    //             </div>
-    //           </li>
-    //         </ul>
-    //       </div>
-    //     ),
-    //     tabsElems = classes => {
-    //       return tabs.map((tab, index) => {
-    //         return (
-    //           <li className="nav-item" key={index}>
-    //             <NavLink
-    //               className={classes}
-    //               activeClassName="active"
-    //               onClick={() => this.setState({ openTabs: false })}
-    //               to={`/profile/${tab.id}`}
-    //               id={`${tab.id}-router-link`}
-    //             >
-    //               {this.props.l10n(tab.label)}
-    //             </NavLink>
-    //           </li>
-    //         );
-    //       });
-    //     };
-    //   if (size === "xl" || size === "lg" || size === "md") {
-    //     contentElem = (
-    //       <div className="row">
-    //         <div className="col-md-3">
-    //           <div className="profile-head">
-    //             <h3>{this.props.l10n("main.profile_title")}</h3>
-    //             <PendingActionsContainer history={history} />
-    //           </div>
+    if (this.props.show_sidebar) {
+      const size = this.props.window_size,
+        sm = size === "sm",
+        tabs = [
+          {
+            id: "personaldata",
+            label: sm ? "main.personal_data_label_sm" : "main.personal_data"
+          },
+          { id: "nins", label: sm ? "main.nins_label_sm" : "main.nins" },
+          { id: "emails", label: sm ? "main.emails_label_sm" : "main.emails" },
+          { id: "phones", label: sm ? "main.phones_label_sm" : "main.phones" },
+          {
+            id: "accountlinking",
+            label: sm ? "main.account_linking_label_sm" : "main.account_linking"
+          },
+          {
+            id: "security",
+            label: sm ? "main.security_label_sm" : "main.security"
+          }
+        ],
+        profElem = (
+          <div className="col-sm-12">
+            <ul className="nav nav-stacked nav-tabs navbar-nav">
+              <ProfileFilledContainer />
+              <li id="profile-menu-eppn-li">
+                <div className="profile-menu-eppn">
+                  <p className="eppn-text-muted">
+                    {/* {this.props.l10n("main.eduid_id")}: {this.props.eppn} */}
+                    main.eduid_id: eppn
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        ),
+        tabsElems = classes => {
+          return tabs.map((tab, index) => {
+            return (
+              <li className="nav-item" key={index}>
+                <NavLink
+                  className={classes}
+                  activeClassName="active"
+                  onClick={() => this.setState({ openTabs: false })}
+                  to={`/profile/${tab.id}`}
+                  id={`${tab.id}-router-link`}
+                >
+                  {/* {this.props.l10n(tab.label)} */}
+                  tab.label
+                </NavLink>
+              </li>
+            );
+          });
+        };
+      if (size === "xl" || size === "lg" || size === "md") {
+        contentElem = (
+          <div className="row">
+            <div className="col-md-3">
+              <div className="profile-head">
+                {/* <h3>{this.props.l10n("main.profile_title")}</h3> */}
+                <h3>profile title</h3>
+                <PendingActionsContainer history={history} />
+              </div>
 
-    //           <div
-    //             className="tabs-left"
-    //             role="navigation"
-    //             id="profile-menu-large"
-    //           >
-    //             <ul className="nav nav-tabs nav-stacked">
-    //               {tabsElems("main-nav-tabs nav-link")}
-    //               <ProfileFilledContainer />
-    //               <li id="profile-menu-eppn-li">
-    //                 <div className="profile-menu-eppn">
-    //                   <p className="eppn-text-muted">
-    //                     {this.props.l10n("main.eduid_id")}: {this.props.eppn}
-    //                   </p>
-    //                 </div>
-    //               </li>
-    //             </ul>
-    //           </div>
-    //         </div>
-    //         {routesElem(<NotificationsContainer />)}
-    //       </div>
-    //     );
-    //   } else if (size === "sm") {
-    //     contentElem = [
-    //       <div className="profile-head">
-    //         <div className="row">
-    //           <div className="col-sm-4">
-    //             <h3>{this.props.l10n("main.profile_title")}</h3>
-    //           </div>
-    //           <div className="col-sm-1" />
-    //           <div className="col-sm-7">
-    //             <NotificationsContainer />
-    //           </div>
-    //         </div>
-    //         <div className="row">
-    //           <div className="col-sm-1" />
-    //           <div className="col-sm-11">
-    //             <PendingActionsContainer history={history} />
-    //           </div>
-    //         </div>
-    //       </div>,
-    //       <div className="row">
-    //         <div className="col-sm-12">
-    //           <nav
-    //             id="profile-menu-small"
-    //             className="navbar navbar-light col-sm-10 col-sm-offset-2"
-    //             role="navigation"
-    //           >
-    //             <ul className="nav nav-tabs">
-    //               {tabsElems("main-nav-tabs nav-link")}
-    //             </ul>
-    //           </nav>
-    //         </div>
-    //         {routesElem("")}
-    //         {profElem}
-    //       </div>
-    //     ];
-    // } else if (size === "xs") {
-    //   contentElem = [
-    //     <div className="profile-head">
-    //       <div className="row">
-    //         <div className="col-xs-4 profile-head">
-    //           <h3>{this.props.l10n("main.profile_title")}</h3>
-    //         </div>
-    //         <div className="col-xs-1" />
-    //         <div className="col-xs-7 profile-head">
-    //           <NotificationsContainer />
-    //         </div>
-    //       </div>
-    //       <div className="row">
-    //         <div className="col-xs-1" />
-    //         <div className="col-xs-11 pending-actions-container">
-    //           <PendingActionsContainer history={history} />
-    //         </div>
-    //       </div>
-    //     </div>,
-    //     <div className="row">
-    //       <div id="col-xs-12 profile-navbar-xs">
-    //         <nav
-    //           id="profile-menu-small"
-    //           className="navbar navbar-light"
-    //           role="navigation"
-    //         >
-    //           <a className="navbar-brand" href="#">
-    //             {this.props.l10n("main.menu")}
-    //           </a>
-    //           <button
-    //             className="navbar-toggler"
-    //             type="button"
-    //             onClick={() =>
-    //               this.setState({ openTabs: !this.state.openTabs })
-    //             }
-    //           >
-    //             <span className="navbar-toggler-icon" />
-    //           </button>
-    //         </nav>
-    //       </div>
-    //       <Collapse
-    //         id="eduid-menu-items-xs"
-    //         isOpen={this.state.openTabs}
-    //         className="text-center"
-    //       >
-    //         <nav className="navbar navbar-light bg-light">
-    //           <ul className="nav nav-stacked nav-tabs navbar-nav">
-    //             {tabsElems("main-nav-tabs tab-link btn-block")}
-    //           </ul>
-    //         </nav>
-    //       </Collapse>
-    //       {routesElem("")}
-    //       {profElem}
-    //     </div>
-    //   ];
-    // }
-    // } else {
-    //   contentElem = routesElem(<NotificationsContainer />);
-    // }
+              <div
+                className="tabs-left"
+                role="navigation"
+                id="profile-menu-large"
+              >
+                <ul className="nav nav-tabs nav-stacked">
+                  {tabsElems("main-nav-tabs nav-link")}
+                  <ProfileFilledContainer />
+                  <li id="profile-menu-eppn-li">
+                    <div className="profile-menu-eppn">
+                      <p className="eppn-text-muted">
+                        {/* {this.props.l10n("main.eduid_id")}: {this.props.eppn} */}
+                        eppn-text-muted: eppn
+                      </p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {routesElem(<NotificationsContainer />)}
+          </div>
+        );
+      }
+      // } else if (size === "sm") {
+      //   contentElem = [
+      //     <div className="profile-head">
+      //       <div className="row">
+      //         <div className="col-sm-4">
+      //           <h3>{this.props.l10n("main.profile_title")}</h3>
+      //         </div>
+      //         <div className="col-sm-1" />
+      //         <div className="col-sm-7">
+      //           <NotificationsContainer />
+      //         </div>
+      //       </div>
+      //       <div className="row">
+      //         <div className="col-sm-1" />
+      //         <div className="col-sm-11">
+      //           <PendingActionsContainer history={history} />
+      //         </div>
+      //       </div>
+      //     </div>,
+      //     <div className="row">
+      //       <div className="col-sm-12">
+      //         <nav
+      //           id="profile-menu-small"
+      //           className="navbar navbar-light col-sm-10 col-sm-offset-2"
+      //           role="navigation"
+      //         >
+      //           <ul className="nav nav-tabs">
+      //             {tabsElems("main-nav-tabs nav-link")}
+      //           </ul>
+      //         </nav>
+      //       </div>
+      //       {routesElem("")}
+      //       {profElem}
+      //     </div>
+      //   ];
+      // } else if (size === "xs") {
+      //   contentElem = [
+      //     <div className="profile-head">
+      //       <div className="row">
+      //         <div className="col-xs-4 profile-head">
+      //           <h3>{this.props.l10n("main.profile_title")}</h3>
+      //         </div>
+      //         <div className="col-xs-1" />
+      //         <div className="col-xs-7 profile-head">
+      //           <NotificationsContainer />
+      //         </div>
+      //       </div>
+      //       <div className="row">
+      //         <div className="col-xs-1" />
+      //         <div className="col-xs-11 pending-actions-container">
+      //           <PendingActionsContainer history={history} />
+      //         </div>
+      //       </div>
+      //     </div>,
+      //     <div className="row">
+      //       <div id="col-xs-12 profile-navbar-xs">
+      //         <nav
+      //           id="profile-menu-small"
+      //           className="navbar navbar-light"
+      //           role="navigation"
+      //         >
+      //           <a className="navbar-brand" href="#">
+      //             {this.props.l10n("main.menu")}
+      //           </a>
+      //           <button
+      //             className="navbar-toggler"
+      //             type="button"
+      //             onClick={() =>
+      //               this.setState({ openTabs: !this.state.openTabs })
+      //             }
+      //           >
+      //             <span className="navbar-toggler-icon" />
+      //           </button>
+      //         </nav>
+      //       </div>
+      //       <Collapse
+      //         id="eduid-menu-items-xs"
+      //         isOpen={this.state.openTabs}
+      //         className="text-center"
+      //       >
+      //         <nav className="navbar navbar-light bg-light">
+      //           <ul className="nav nav-stacked nav-tabs navbar-nav">
+      //             {tabsElems("main-nav-tabs tab-link btn-block")}
+      //           </ul>
+      //         </nav>
+      //       </Collapse>
+      //       {routesElem("")}
+      //       {profElem}
+      //     </div>
+      //   ];
+      // }
+    } else {
+      contentElem = routesElem(<NotificationsContainer />);
+    }
 
     return (
       <FetchingContext.Provider value={this.state}>
@@ -271,7 +276,7 @@ class Settings extends Component {
                 id="profile-content-area"
               >
                 <h1>This is settings component</h1>
-                {/* {contentElem} */}
+                {contentElem}
               </div>
             </div>
           </ConnectedRouter>
