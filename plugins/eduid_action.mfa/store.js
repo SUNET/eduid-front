74e4b8ecb86ed3fff7e2f3780a43ebb5c0e3204f
Nathalie Jonsson <njons@sunet.se>
2019-05-02 10:45:30 +0200

    prettier formatting code

--- plugins/eduid_action.mfa/store.js
+++ plugins/eduid_action.mfa/store.js
@@ -1,38 +1,36 @@
+import { combineReducers } from "redux";
+import { intlReducer } from "react-intl-redux";
+import { routerReducer } from "react-router-redux";
 
-import { combineReducers } from 'redux';
-import { intlReducer } from 'react-intl-redux'
-import { routerReducer } from 'react-router-redux';
-
-import actionWrapperReducer from 'reducers/ActionWrapper';
-import notificationsReducer from 'reducers/Notifications';
+import actionWrapperReducer from "reducers/ActionWrapper";
+import notificationsReducer from "reducers/Notifications";
 
 import * as actions from "actions/ActionWrapper";
 import { WEBAUTHN_CREDS_GOT } from "./component";
 
 // see the config params in eduid-developer/etcd/conf.yaml
 const actionData = {
-    webauthn_assertion: null
+  webauthn_assertion: null
 };
 
-export const actionReducer = (state=actionData, action) => {
+export const actionReducer = (state = actionData, action) => {
   switch (action.type) {
     case WEBAUTHN_CREDS_GOT:
       return {
-          ...state,
-          webauthn_assertion: action.payload
+        ...state,
+        webauthn_assertion: action.payload
       };
     default:
       return state;
   }
 };
 
-
 const App = combineReducers({
-    router: routerReducer,
-    intl: intlReducer,
-    config: actionWrapperReducer,
-    plugin: actionReducer,
-    notifications: notificationsReducer
+  router: routerReducer,
+  intl: intlReducer,
+  config: actionWrapperReducer,
+  plugin: actionReducer,
+  notifications: notificationsReducer
 });
 
 export default App;
--- plugins/eduid_action.mfa/test.js
+++ plugins/eduid_action.mfa/test.js
@@ -1,8 +1,7 @@
-
-const mock = require('jest-mock');
-import React from 'react';
-import { Provider } from 'react-intl-redux';
-import { mount } from 'enzyme';
+const mock = require("jest-mock");
+import React from "react";
+import { Provider } from "react-intl-redux";
+import { mount } from "enzyme";
 import expect from "expect";
 import { put, call, select } from "redux-saga/effects";
 
@@ -13,124 +12,140 @@ import * as actions from "actions/ActionWrapper";
 import { postCompleteWebauthn, requestCompleteWebauthn } from "./root-saga";
 
 const pluginState = {
-    webauthn_assertion: {}
+  webauthn_assertion: {}
 };
 
 const setupComponent = genSetupComponent(pluginState);
 
 describe("Some Component", () => {
+  it("Renders the splash screen", () => {
+    const wrapper = setupComponent({
+        component: <MainContainer />,
+        overrides: { config: { is_app_loaded: false } }
+      }),
+      splash = wrapper.find("div#eduid-splash-screen");
 
-    it("Renders the splash screen", () => {
-        const wrapper = setupComponent({component: <MainContainer />,
-                                        overrides: {config: {is_app_loaded: false}}}),
-              splash = wrapper.find('div#eduid-splash-screen');
+    expect(splash.length).toEqual(1);
+  });
 
-        expect(splash.length).toEqual(1);
+  it("Renders", () => {
+    const wrapper = setupComponent({ component: <MainContainer /> }),
+      splash = wrapper.find("div#eduid-splash-screen"),
+      title = wrapper.find("div.webauthn-title"),
+      subtitle = wrapper.find("div.webauthn-subtitle"),
+      animation = wrapper.find("div.key-animation"),
+      fallback = wrapper.find("div#mfa-try-another-way");
+
+    expect(splash.length).toEqual(0);
+    expect(title.length).toEqual(1);
+    expect(subtitle.length).toEqual(1);
+    expect(animation.length).toEqual(1);
+    expect(title.text()).toEqual("Two-factor authentication");
+    expect(fallback.length).toEqual(1);
+  });
+
+  it("Renders no webauthn", () => {
+    const creds = window.navigator.credentials;
+    Object.defineProperty(window.navigator, "credentials", {
+      value: undefined,
+      configurable: true
     });
 
-    it("Renders", () => {
-        const wrapper = setupComponent({component: <MainContainer />}),
-              splash = wrapper.find('div#eduid-splash-screen'),
-              title = wrapper.find('div.webauthn-title'),
-              subtitle = wrapper.find('div.webauthn-subtitle'),
-              animation = wrapper.find('div.key-animation'),
-              fallback = wrapper.find('div#mfa-try-another-way');
+    const wrapper = setupComponent({ component: <MainContainer /> }),
+      splash = wrapper.find("div#eduid-splash-screen"),
+      title = wrapper.find("div.webauthn-title"),
+      fallback = wrapper.find("div#mfa-try-another-way"),
+      animation = wrapper.find("div.key-animation");
 
-        expect(splash.length).toEqual(0);
-        expect(title.length).toEqual(1);
-        expect(subtitle.length).toEqual(1);
-        expect(animation.length).toEqual(1);
-        expect(title.text()).toEqual('Two-factor authentication');
-        expect(fallback.length).toEqual(1);
+    expect(splash.length).toEqual(0);
+    expect(title.length).toEqual(1);
+    expect(fallback.length).toEqual(1);
+    expect(animation.length).toEqual(0);
+    expect(title.text()).toEqual("No support for Security Keys");
+
+    Object.defineProperty(window.navigator, "credentials", {
+      value: creds,
+      configurable: true
     });
-
-    it("Renders no webauthn", () => {
-        const creds = window.navigator.credentials;
-        Object.defineProperty(window.navigator, 'credentials', {value: undefined, configurable: true});
-
-        const wrapper = setupComponent({component: <MainContainer />}),
-              splash = wrapper.find('div#eduid-splash-screen'),
-              title = wrapper.find('div.webauthn-title'),
-              fallback = wrapper.find('div#mfa-try-another-way'),
-              animation = wrapper.find('div.key-animation');
-
-        expect(splash.length).toEqual(0);
-        expect(title.length).toEqual(1);
-        expect(fallback.length).toEqual(1);
-        expect(animation.length).toEqual(0);
-        expect(title.text()).toEqual('No support for Security Keys');
-
-        Object.defineProperty(window.navigator, 'credentials', {value: creds, configurable: true});
-    });
+  });
 });
 
 describe("Some action reducer", () => {
+  const mockState = {
+    webauthn_assertion: {}
+  };
 
-    const mockState = {
-        webauthn_assertion: {}
-    };
-
-    it("Receives plugin config loaded action", () => {
-        expect(
-            actionReducer(
-                mockState,
-                {
-                    type: "WEBAUTHN_CREDS_GOT",
-                    payload: {
-                        dummy: 'assertion'
-                    }
-                }
-            )
-        ).toEqual(
-          {
-              ...mockState,
-              webauthn_assertion: {dummy: 'assertion'},
-          }
-        );
+  it("Receives plugin config loaded action", () => {
+    expect(
+      actionReducer(mockState, {
+        type: "WEBAUTHN_CREDS_GOT",
+        payload: {
+          dummy: "assertion"
+        }
+      })
+    ).toEqual({
+      ...mockState,
+      webauthn_assertion: { dummy: "assertion" }
     });
+  });
 });
 
 describe("Some plugin async actions", () => {
-
-    it("Tests post webauthn response saga", () => {
-
-        const assertion = {
-                  rawId: 'dummy-id',
-                  response: {
-                      authenticatorData: new ArrayBuffer('dummy authn data'),
-                      clientDataJSON: new ArrayBuffer('dummy json'),
-                      signature: new ArrayBuffer('dummy signature')
-                  }
-              },
-              state = getState({
-                  config: {
-                      csrf_token: 'dummy-token',
-                      webauthn_options: '',
-                  },
-                  plugin: {
-                      webauthn_assertion: assertion
-                  }
-              });
-        const data = {
-            csrf_token: state.config.csrf_token,
-            credentialId: btoa(String.fromCharCode.apply(null, new Uint8Array(assertion.rawId))),
-            authenticatorData: btoa(String.fromCharCode.apply(null, new Uint8Array(assertion.response.authenticatorData))),
-            clientDataJSON: btoa(String.fromCharCode.apply(null, new Uint8Array(assertion.response.clientDataJSON))),
-            signature: btoa(String.fromCharCode.apply(null, new Uint8Array(assertion.response.signature)))
-        };
-        const generator = postCompleteWebauthn();
-        generator.next();
-        let resp = generator.next(state);
-        expect(resp.value).toEqual(call(requestCompleteWebauthn, data));
-        const action = {
-            type: actions.POST_ACTIONS_ACTION_SUCCESS,
-            payload: {
-                csrf_token: 'csrf-token'
-            }
-        };
-        generator.next(action);
-        delete action.payload.csrf_token;
-        resp = generator.next();
-        expect(resp.value).toEqual(put(action));
-    });
+  it("Tests post webauthn response saga", () => {
+    const assertion = {
+        rawId: "dummy-id",
+        response: {
+          authenticatorData: new ArrayBuffer("dummy authn data"),
+          clientDataJSON: new ArrayBuffer("dummy json"),
+          signature: new ArrayBuffer("dummy signature")
+        }
+      },
+      state = getState({
+        config: {
+          csrf_token: "dummy-token",
+          webauthn_options: ""
+        },
+        plugin: {
+          webauthn_assertion: assertion
+        }
+      });
+    const data = {
+      csrf_token: state.config.csrf_token,
+      credentialId: btoa(
+        String.fromCharCode.apply(null, new Uint8Array(assertion.rawId))
+      ),
+      authenticatorData: btoa(
+        String.fromCharCode.apply(
+          null,
+          new Uint8Array(assertion.response.authenticatorData)
+        )
+      ),
+      clientDataJSON: btoa(
+        String.fromCharCode.apply(
+          null,
+          new Uint8Array(assertion.response.clientDataJSON)
+        )
+      ),
+      signature: btoa(
+        String.fromCharCode.apply(
+          null,
+          new Uint8Array(assertion.response.signature)
+        )
+      )
+    };
+    const generator = postCompleteWebauthn();
+    generator.next();
+    let resp = generator.next(state);
+    expect(resp.value).toEqual(call(requestCompleteWebauthn, data));
+    const action = {
+      type: actions.POST_ACTIONS_ACTION_SUCCESS,
+      payload: {
+        csrf_token: "csrf-token"
+      }
+    };
+    generator.next(action);
+    delete action.payload.csrf_token;
+    resp = generator.next();
+    expect(resp.value).toEqual(put(action));
+  });
 });
