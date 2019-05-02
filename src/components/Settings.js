7b7dfa8469ce8a94d5ca71aafebbb232fd2507b9
Nathalie Jonsson <njons@sunet.se>
2019-05-02 16:36:53 +0200

    refactors settings page by removing all of the tab sidebar elements (profElem, tabsElem and contentElem) until only routes rendering the components holdig user data remain

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -52,151 +52,133 @@ class Settings extends Component {
   // }
 
   render() {
-    let contentElem = "";
+    // let contentElem = "";
 
-    const routesElem = notsElem => (
-      <div className="tab-content col-md-9 col-md-offset-1 col-sm-12">
-        <div className="tab-pane active">
-          {notsElem}
-          <Route
-            exact
-            path="/profile/settings/"
-            component={() => <Redirect to="/profile/settings/personaldata" />}
-          />
-          <Route
-            path="/profile/settings/personaldata"
-            component={PersonalDataContainer}
-          />
-          <Route path="/profile/settings/nins" component={NinsContainer} />
-          <Route path="/profile/settings/emails" component={EmailsContainer} />
-          <Route path="/profile/settings/phones" component={MobileContainer} />
-          <Route
-            path="/profile/settings/accountlinking"
-            component={AccountLinkingContainer}
-          />
-          <Route
-            path="/profile/settings/security"
-            component={SecurityContainer}
-          />
-          <Route
-            path="/profile/settings/chpass"
-            component={ChangePasswordContainer}
-          />
-        </div>
-      </div>
-    );
+    // const routesElem = notsElem => (
+
+    // );
 
     // if (this.props.show !== true) {
     //   return <div />;
     // }
-    if (this.props.show_sidebar) {
-      const size = this.props.window_size,
-        sm = size === "sm",
-        tabs = [
-          {
-            id: "personaldata",
-            label: sm ? "main.personal_data_label_sm" : "main.personal_data"
-          },
-          { id: "nins", label: sm ? "main.nins_label_sm" : "main.nins" },
-          { id: "emails", label: sm ? "main.emails_label_sm" : "main.emails" },
-          { id: "phones", label: sm ? "main.phones_label_sm" : "main.phones" },
-          {
-            id: "accountlinking",
-            label: sm ? "main.account_linking_label_sm" : "main.account_linking"
-          },
-          {
-            id: "security",
-            label: sm ? "main.security_label_sm" : "main.security"
-          }
-        ],
-        profElem = (
-          <div className="col-sm-12">
-            <ul className="nav nav-stacked nav-tabs navbar-nav">
-              <ProfileFilledContainer />
-              <li id="profile-menu-eppn-li">
-                <div className="profile-menu-eppn">
-                  <p className="eppn-text-muted">
-                    {/* {this.props.l10n("main.eduid_id")}: {this.props.eppn} */}
-                    main.eduid_id: eppn
-                  </p>
-                </div>
-              </li>
-            </ul>
-          </div>
-        ),
-        tabsElems = classes => {
-          return tabs.map((tab, index) => {
-            return (
-              <li className="nav-item" key={index}>
-                <NavLink
-                  className={classes}
-                  activeClassName="active"
-                  onClick={() => this.setState({ openTabs: false })}
-                  to={`/profile/${tab.id}`}
-                  id={`${tab.id}-router-link`}
-                >
-                  {/* {this.props.l10n(tab.label)} */}
-                  tab.label
-                </NavLink>
-              </li>
-            );
-          });
-        };
-      if (size === "xl" || size === "lg" || size === "md") {
-        contentElem = (
-          <div className="row">
-            <div className="col-md-3">
-              <div className="profile-head">
-                {/* <h3>{this.props.l10n("main.profile_title")}</h3> */}
-                <h3>profile title</h3>
-                <PendingActionsContainer history={history} />
-              </div>
+    // if (this.props.show_sidebar) {
+    // const size = this.props.window_size,
+    //   sm = size === "sm",
+    // tabs = [
+    //   {
+    //     id: "personaldata",
+    //     label: sm ? "main.personal_data_label_sm" : "main.personal_data"
+    //   },
+    //   { id: "nins", label: sm ? "main.nins_label_sm" : "main.nins" },
+    //   { id: "emails", label: sm ? "main.emails_label_sm" : "main.emails" },
+    //   { id: "phones", label: sm ? "main.phones_label_sm" : "main.phones" },
+    //   {
+    //     id: "accountlinking",
+    //     label: sm ? "main.account_linking_label_sm" : "main.account_linking"
+    //   },
+    //   {
+    //     id: "security",
+    //     label: sm ? "main.security_label_sm" : "main.security"
+    //   }
+    // ],
+    // profElem = (
+    //   <div className="col-sm-12">
+    //     <ul className="nav nav-stacked nav-tabs navbar-nav">
+    //       <ProfileFilledContainer />
+    //       <li id="profile-menu-eppn-li">
+    //         <div className="profile-menu-eppn">
+    //           <p className="eppn-text-muted">
+    //             {/* {this.props.l10n("main.eduid_id")}: {this.props.eppn} */}
+    //             main.eduid_id: eppn
+    //           </p>
+    //         </div>
+    //       </li>
+    //     </ul>
+    //   </div>
+    // ),
+    // tabsElems = classes => {
+    //   return tabs.map((tab, index) => {
+    //     return (
+    //       <li className="nav-item" key={index}>
+    //         <NavLink
+    //           className={classes}
+    //           activeClassName="active"
+    //           onClick={() => this.setState({ openTabs: false })}
+    //           to={`/profile/${tab.id}`}
+    //           id={`${tab.id}-router-link`}
+    //         >
+    //           {/* {this.props.l10n(tab.label)} */}
+    //           tab.label
+    //         </NavLink>
+    //       </li>
+    //     );
+    //   });
+    // };
+    // if (size === "xl" || size === "lg" || size === "md") {
+    //   contentElem = (
 
-              <div
-                className="tabs-left"
-                role="navigation"
-                id="profile-menu-large"
-              >
-                <ul className="nav nav-tabs nav-stacked">
-                  {tabsElems("main-nav-tabs nav-link")}
-                  <ProfileFilledContainer />
-                  <li id="profile-menu-eppn-li">
-                    <div className="profile-menu-eppn">
-                      <p className="eppn-text-muted">
-                        {/* {this.props.l10n("main.eduid_id")}: {this.props.eppn} */}
-                        eppn-text-muted: eppn
-                      </p>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            {routesElem(<NotificationsContainer />)}
-          </div>
-        );
-      }
-    }
+    //   );
+    // }
+    //  }
 
     // else {
     //   contentElem = routesElem(<NotificationsContainer />);
     // }
 
     return (
-      <FetchingContext.Provider value={this.state}>
+      <div>
         <SplashContainer />
         <div className="container-fluid">
-          <ConnectedRouter history={history}>
-            <div id="content-block">
-              <div
-                className="tabbable well profile-combo"
-                id="profile-content-area"
-              >
-                {contentElem}
+          <NotificationsContainer />
+
+          <div id="content-block">
+            <div
+              className="tabbable well profile-combo"
+              id="profile-content-area"
+            >
+              <div className="row">
+                <div className="tab-content col-md-9 col-md-offset-1 col-sm-12">
+                  <Route
+                    exact
+                    path="/profile/settings/"
+                    component={() => (
+                      <Redirect to="/profile/settings/personaldata" />
+                    )}
+                  />
+                  <Route
+                    path="/profile/settings/personaldata"
+                    component={PersonalDataContainer}
+                  />
+                  <Route
+                    path="/profile/settings/nins"
+                    component={NinsContainer}
+                  />
+                  <Route
+                    path="/profile/settings/emails"
+                    component={EmailsContainer}
+                  />
+                  <Route
+                    path="/profile/settings/phones"
+                    component={MobileContainer}
+                  />
+                  <Route
+                    path="/profile/settings/accountlinking"
+                    component={AccountLinkingContainer}
+                  />
+                  <Route
+                    path="/profile/settings/security"
+                    component={SecurityContainer}
+                  />
+                  <Route
+                    path="/profile/settings/chpass"
+                    component={ChangePasswordContainer}
+                  />
+                </div>
               </div>
             </div>
-          </ConnectedRouter>
+          </div>
         </div>
-      </FetchingContext.Provider>
+      </div>
     );
   }
 }
