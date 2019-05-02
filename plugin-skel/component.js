12356476be5f8793d84b52fab320a92c611df1dd
Nathalie Jonsson <njons@sunet.se>
2019-05-02 10:44:44 +0200

    prettier formatting code

--- plugin-skel/component.js
+++ plugin-skel/component.js
@@ -1,44 +1,34 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-
-import i18n from 'i18n-messages';
+import i18n from "i18n-messages";
 import { appFetching, postAction } from "actions/ActionWrapper";
 import ActionWrapperContainer from "containers/ActionWrapper";
 
-import './style.scss';
-
+import "./style.scss";
 
 class Main extends Component {
-
-    render () {
-
-        return (
-            <ActionWrapperContainer>
-              // UI for the action
-            </ActionWrapperContainer>
-        );
-    }
+  render() {
+    return (
+      <ActionWrapperContainer>// UI for the action</ActionWrapperContainer>
+    );
+  }
 }
 
-Main.propTypes = {
-}
+Main.propTypes = {};
 
 const mapStateToProps = (state, props) => {
-    return {
-    }
+  return {};
 };
 
 const mapDispatchToProps = (dispatch, props) => {
-    return {
-    }
+  return {};
 };
 
 const MainContainer = connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Main);
 
 export default i18n(MainContainer);
-
--- plugin-skel/test.js
+++ plugin-skel/test.js
@@ -1,8 +1,7 @@
-
-const mock = require('jest-mock');
-import React from 'react';
-import { Provider } from 'react-intl-redux';
-import { mount } from '@pisano/enzyme';
+const mock = require("jest-mock");
+import React from "react";
+import { Provider } from "react-intl-redux";
+import { mount } from "@pisano/enzyme";
 import expect from "expect";
 import { put, call, select } from "redux-saga/effects";
 
@@ -12,53 +11,43 @@ import { actionReducer } from "./store";
 import * as actions from "actions/ActionWrapper";
 
 const pluginState = {
-    // central state.plugin for testing
+  // central state.plugin for testing
 };
 
 const setupComponent = genSetupComponent(pluginState);
 
 describe("Some Component", () => {
+  it("Renders the splash screen", () => {
+    const wrapper = setupComponent({
+        component: <MainContainer />,
+        overrides: { main: { is_app_loaded: false } }
+      }),
+      splash = wrapper.find("div#eduid-splash-screen");
 
-    it("Renders the splash screen", () => {
-        const wrapper = setupComponent({component: <MainContainer />,
-                                        overrides: {main: {is_app_loaded: false}}}),
-              splash = wrapper.find('div#eduid-splash-screen');
+    expect(splash.length).toEqual(1);
+  });
 
-        expect(splash.length).toEqual(1);
-    });
-
-    it("Renders", () => {
-    });
+  it("Renders", () => {});
 });
 
 describe("Some action reducer", () => {
+  const mockState = {};
 
-    const mockState = {
-    };
-
-    it("Receives plugin config loaded action", () => {
-        expect(
-            actionReducer(
-                mockState,
-                {
-                    type: actions.GET_ACTIONS_CONFIG_SUCCESS,
-                    payload: {
-                        // some payload
-                    }
-                }
-            )
-        ).toEqual(
-          {
-              ...mockState,
-              // whatever the payload is reduced to
-          }
-        );
+  it("Receives plugin config loaded action", () => {
+    expect(
+      actionReducer(mockState, {
+        type: actions.GET_ACTIONS_CONFIG_SUCCESS,
+        payload: {
+          // some payload
+        }
+      })
+    ).toEqual({
+      ...mockState
+      // whatever the payload is reduced to
     });
+  });
 });
 
 describe("Some plugin async actions", () => {
-
-    it("Tests some saga", () => {
-
-    });
+  it("Tests some saga", () => {});
 });
