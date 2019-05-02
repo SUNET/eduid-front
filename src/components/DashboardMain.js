eedcf2ac72886270d3fc84735cfbea1cdcd64f08
Nathalie Jonsson <njons@sunet.se>
2019-05-02 14:53:59 +0200

    adds content and styles to new dashboard

--- src/components/DashboardMain.js
+++ src/components/DashboardMain.js
@@ -276,16 +276,65 @@ class Main extends Component {
         <div className="container-fluid">
           <HeaderContainer />
           <div id="content-block">
-            <h1>this is dashboard</h1>
-            <button type="submit" onClick={() => this.showSettings()}>
+            <button
+              id="settings-button"
+              type="submit"
+              onClick={() => this.showSettings()}
+            >
               Settings
             </button>
-            {/* <div
-                className="tabbable well profile-combo"
-                id="profile-content-area"
-              >
-                {/* {contentElem} 
-              </div> */}
+
+            <div id="dashboard-text">
+              <h1>eduID for email@email.com</h1>
+              <p>
+                {" "}
+                Welcome to your eduid account. To be able to use it you need to
+                provide some more information.
+              </p>
+              <div id="content">
+                <div id="welcome">
+                  <div id="verify-identity-prompt">
+                    <h3>
+                      {" "}
+                      You're almost done, the next step is to verify your
+                      identity{" "}
+                    </h3>
+                    <p>
+                      {" "}
+                      Choose a suitable way to verify your identity and follow
+                      the instuctions to start using eduID. You can change any
+                      of your personal information in Settings.
+                    </p>
+                    <div id="verify-identity-button">
+                      <button
+                        id="verify-button"
+                        type="submit"
+                        // onClick={() => this.showVerifyIdentity()}
+                      >
+                        {" "}
+                        I want to verify my identity
+                      </button>
+                      {/* {this.props.l10n(tab.label)} */}
+                    </div>
+                  </div>
+                  <div>
+                    <h3>Why do I need eduID?</h3>
+                    <p>
+                      eduID kan användas för inloggning inom flera olika
+                      organisationer. eduID har utvecklats för att kunna ge alla
+                      studenter en identitet online utan att kräva ett svenskt
+                      personnummer.
+                    </p>
+                    <h3>How to use eduID</h3>
+                    <p>
+                      När möjlighet finns, välj "Logga in med eduID". Du kommer
+                      att bli skickad till eduIDs login där du anger din
+                      e-postadress och ditt lösenord.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <ConnectedRouter history={history}>
             <SettingsComponent
