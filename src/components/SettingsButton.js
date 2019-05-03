9ab92e86e90e639ffe76830ff788597abce2c347
Nathalie Jonsson <njons@sunet.se>
2019-05-03 10:21:59 +0200

    creates SettingsButton component

---
+++ src/components/SettingsButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "style/base.scss";
+import "style/DashboardMain.scss";
+
+class SettingsButton extends Component {
+  render() {
+  }
+}
+
+
+export default SettingsButton;
