49adda47e3878d5799a5a1a381c972458ff16965
Nathalie Jonsson <njons@sunet.se>
2019-05-03 11:44:00 +0200

    adds nav to top of Settings component and linking to existing components holding user data

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -39,6 +39,72 @@ class Settings extends Component {
     return (
       <div id="settings">
         <NotificationsContainer />
+
+        <nav id="settings-nav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                // id="settings-personal-info"
+                to={`/profile/settings/personaldata`}
+              >
+                <h5>Personal information</h5>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                id="settings-emails"
+                to={`/profile/settings/emails`}
+              >
+                <h5>Email address</h5>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                id="settings-phone"
+                to={`/profile/settings/phones`}
+              >
+                <h5>Phone</h5>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                id="settings-nin"
+                to={`/profile/settings/nins`}
+              >
+                <h5>National identity number</h5>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                id="settings-account-linking"
+                to={`/profile/settings/accountlinking`}
+              >
+                <h5>Account linking</h5>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                id="settings-security"
+                to={`/profile/settings/security`}
+              >
+                <h5>Add Security</h5>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
         <Route
           exact
           path="/profile/settings/"
