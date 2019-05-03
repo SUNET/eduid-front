48dfcb3d786313b3d776ee681f8c4afb94d73b63
Nathalie Jonsson <njons@sunet.se>
2019-05-03 10:54:52 +0200

    creates VerifyIdentity.js component

---
+++ src/components/VerifyIdentity.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "style/base.scss";
+import "style/DashboardMain.scss";
+
+class VerifyIdentity extends Component {
+  render() {}
+}
+
+export default VerifyIdentity;
