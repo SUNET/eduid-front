8860a1930904dcc028162d07dc63625697613fa1
Nathalie Jonsson <njons@sunet.se>
2019-05-13 15:18:49 +0200

    removes commented out code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,18 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Collapse } from "reactstrap";
 
 import "style/Header.scss";
 
 class Header extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.toggle = this.toggle.bind(this);
-  //   this.state = { collapse: true };
-  // }
-
-  // toggle() {
-  //   this.setState({ collapse: !this.state.collapse });
-  // }
-
   render() {
-    // let email = this.props.email;
-    // if (email === "") {
-    //   email = this.props.l10n("main.no-email-yet");
-    // }
-
     const logout = (
       <div id="eduid-button">
         <button
           id="logout"
-          // className="button"
           onClick={this.props.handleLogout}
         >
           {this.props.l10n("header.logout")}
         </button>
-        {/* <div className="loa-big hidden-sm" id="eduid-header-loa">
-          {email} ({this.props.l10n(this.props.confirmed)})
-        </div>  */}
       </div>
     );
 
@@ -57,34 +37,6 @@ class Header extends Component {
       </ul>
     );
 
-    // if (this.props.size === "xs") {
-    //   return (
-    //     <header className="header">
-    //       <nav className="navbar navbar-dark bg-dark">
-    //         <a className="navbar-brand" href="#">
-    //           <div id="eduid-logo" />
-    //         </a>
-    //         <button
-    //           className="navbar-toggler"
-    //           type="button"
-    //           onClick={this.toggle}
-    //         >
-    //           <span className="navbar-toggler-icon" />
-    //         </button>
-    //       </nav>
-    //       <Collapse
-    //         isOpen={!this.state.collapse}
-    //         className="text-center"
-    //         id="eduid-menu"
-    //       >
-    //         <nav className="navbar navbar-dark bg-dark" id="eduid-navbar">
-    //           {links}
-    //           {buttons}
-    //         </nav>
-    //       </Collapse>
-    //     </header>
-    //   );
-    // } else {
     return (
       <header>
         <div id="eduid-logo" />
@@ -92,12 +44,10 @@ class Header extends Component {
         {logout}
       </header>
     );
-    //   }
   }
 }
 
 Header.propTypes = {
-  email: PropTypes.string,
   confirmed: PropTypes.string,
   studentsLink: PropTypes.string,
   techniciansLink: PropTypes.string,
