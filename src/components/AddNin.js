58967e5d3a7d8a8385dfbb27b8f6ae9e8eadda10
Nathalie Jonsson <njons@sunet.se>
2019-05-28 09:51:19 +0200

    renders form to take nin or added nin number to state

--- src/components/AddNin.js
+++ src/components/AddNin.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { isValid } from "redux-form";
+// import Nins from "components/Nins";
+import * as actions from "actions/Nins";
+import i18n from "i18n-messages";
 // import { Link } from "react-router-dom";
 
 import NinForm from "./NinForm";
@@ -9,37 +13,6 @@ import NinDisplay from "./NinDisplay";
 
 import "style/Nins.scss";
 
-// let NinNumber = props => {
-//   return (
-//     <div data-ninnumber={props.nins[0].number} id="nin-number-container">
-//       <p id="nin-number">{props.nins[0].number}</p>
-//     </div>
-//   );
-// };
-
-// let RemoveButton = props => {
-//   return (
-//     <EduIDButton
-//       className="btn-danger"
-//       className="btn-sm"
-//       id={"button-rm-nin-" + props.nins[0].number}
-//       onClick={props.handleDelete}
-//     >
-//       {props.l10n("nins.button_delete")}
-//     </EduIDButton>
-//   );
-// };
-
-// let VerifyButton = props => {
-//   return (
-//     <Link id="verify-button" to="/profile/verify-identity/step2">
-//       <button>
-//         <p>connect eduid to my person</p>
-//       </button>
-//     </Link>
-//   );
-// };
-
 class AddNin extends Component {
   constructor(props) {
     super(props);
@@ -47,54 +20,31 @@ class AddNin extends Component {
     this.addNin = this.addNin.bind(this);
     this.removeNin = this.removeNin.bind(this);
   }
+
   addNin(validNin) {
     this.setState((state, props) => {
       return { nin: validNin };
-    }, console.log("this is state in addNin() in AddNin:", this.state.nin));
+      },
+      () => {
+        console.log("this is the updated state in addNin():", this.state.nin);
+      }
+    );
   }
 
   removeNin() {
     console.log("you  clicked removeNin")
     this.setState((state, props) => {
       return { nin: null };
-    }, console.log("this is state in removeNin() in AddNin:", this.state.nin));
+      },
+      () => {
+        console.log("this is state in removeNin() in AddNin:", this.state.nin);
+      }
+    );
   }
+  
   render() {
-    // let ninStatus = "nonin";
-
-    // if (this.props.nins.length) {
-    //   ninStatus = "unverified";
-    //   const nins = this.props.nins.filter(nin => nin.verified);
-    //   if (nins.length === 1) {
-    //     ninStatus = "verified";
-    //     verifiedNin = nins[0].number;
-    //   }
-    // if (this.props.nins.length > 1) {
-    //   ninInput = this.props.l10n("nins.only_one_to_verify");
-    // }
-    // }
-
-    // let ninVerified = [
-    //   <div key="1" id="add-nin-number">
-    //     <NinNumber {...this.props} />
-    //     <div id="nin-buttons">
-    //       <RemoveButton {...this.props} />
-    //     </div>
-    //   </div>
-    // ];
-    if (this.props.nins.length) {
-      console.log("show number beacsue nin arraaaaayyyyyy!")
-      return <NinDisplay {...this.props} />
-    }
-
-    if (this.state.nin) {
-      console.log("is state.nin?", this.state.nin);
-      console.log("show number!");
-      console.log("these are the props (AddNin):", this.props);
-      return <NinDisplay removeNin={this.removeNin} {...this.props} />;
-    } else {
-      console.log("is state.nin?", this.state.nin);
-      console.log("show form!");
+    if (this.state.nin === null) {
+      console.log("show form! because nin is null!");
       console.log("these are the props (AddNin):", this.props);
       return (
         <div key="1" className="intro">
@@ -108,9 +58,14 @@ class AddNin extends Component {
           </div>
         </div>
       );
+    } else {
+      console.log("show number! because nin is not null!");
+      return <NinDisplay removeNin={this.removeNin} {...this.props} />;
     }
   }
 }
+
+// export default i18n(NinsContainer);
 
 // AddNin.propTypes = {
 //   nin: PropTypes.string,
