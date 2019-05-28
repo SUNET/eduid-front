4af4481fa16148e67bdc98419c930eaae576c180
Nathalie Jonsson <njons@sunet.se>
2019-05-28 15:28:48 +0200

    comments out style and text change of nin depending on verification status

--- src/components/NinDisplay.js
+++ src/components/NinDisplay.js
@@ -35,23 +35,23 @@ let RemoveButton = props => {
 class NinDisplay extends Component {
   render() {
     if (this.props.nins) {
-      if (this.props.nins[0].verified) {
-        console.log(this.props.nins.verified);
-        return (
-          <div key="1" className="intro">
-            <h3> Step 1. Add your national identity number</h3>
-            <p>Your id number has been added and connected to your person.</p>
-            <div key="1" id="add-nin-number">
-              <div key="1" id="nin-form-container">
-                <div key="1" id="add-nin-number" className="verified">
-                  <NinNumber {...this.props} />
-                  <RemoveButton {...this.props} />
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      }
+      // if (this.props.nins[0].verified) {
+      //   console.log(this.props.nins.verified);
+      //   return (
+      //     <div key="1" className="intro">
+      //       <h3> Step 1. Add your national identity number</h3>
+      //       <p>Your id number has been added and connected to your person.</p>
+      //       <div key="1" id="add-nin-number">
+      //         <div key="1" id="nin-form-container">
+      //           <div key="1" id="add-nin-number" className="verified">
+      //             <NinNumber {...this.props} />
+      //             <RemoveButton {...this.props} />
+      //           </div>
+      //         </div>
+      //       </div>
+      //     </div>
+      //   );
+      // }
       return (
         <div key="1" className="intro">
           <h3> Step 1. Add your national id number</h3>
