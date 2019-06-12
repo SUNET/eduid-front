09204e169aff907d685a9c928e76600c3fc9447b
Nathalie Jonsson <njons@sunet.se>
2019-06-12 10:59:57 +0200

    displays different styles and text depending on verification status

--- src/components/NinDisplay.js
+++ src/components/NinDisplay.js
@@ -9,62 +9,49 @@ import EduIDButton from "components/EduIDButton";
 
 import "style/Nins.scss";
 
-let NinNumber = props => {
-  return (
-    <div data-ninnumber={props.nin} id="nin-number-container">
-      <p id="nin-number">{props.nin}</p>
-    </div>
-  );
-};
-
 let RemoveButton = props => {
   console.log("these are the props in remove button:", props);
   return (
-    <EduIDButton
-      className="btn-danger btn-sm"
-      onClick={e => {
-        props.removeNin();
-        props.handleDelete(e);
-      }}
-    >
+    <EduIDButton className="btn-danger btn-sm" onClick={props.handleDelete}>
       X
-</EduIDButton>
+    </EduIDButton>
   );
 };
 
 class NinDisplay extends Component {
   render() {
-    if (this.props.nins) {
-      // if (this.props.nins[0].verified) {
-      // console.log(this.props.nins.verified);
-      // return (
-      // <div key="1" className="intro">
-      // <h3> Step 1. Add your national identity number</h3>
-      // <p>Your id number has been added and connected to your person.</p>
-      // <div key="1" id="add-nin-number">
-      // <div key="1" id="nin-form-container">
-      // <div key="1" id="add-nin-number" className="verified">
-      // <NinNumber {...this.props} />
-      // <RemoveButton {...this.props} />
-      // </div>
-      // </div>
-      // </div>
-      // </div>
-      // );
-      // }
+    if (this.props.nins[0].verified) {
+      console.log(this.props.nins[0].verified);
+      return (
+        <div key="1" className="intro">
+          <h3> Step 1. Add your national identity number</h3>
+          <p>Your id number has been added and connected to your person.</p>
+          <div key="1" id="add-nin-number">
+            <div key="1" id="nin-form-container">
+              <div key="1" id="add-nin-number" className="verified">
+                <div data-ninnumber={this.props.nin} id="nin-number-container">
+                  <p id="nin-number">{this.props.nin}</p>
+                </div>
+                <RemoveButton {...this.props} />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
       return (
         <div key="1" className="intro">
           <h3> Step 1. Add your national id number</h3>
           <p>
-            Your id number has been added,
-<Link id="verify-id-link" to="/profile/verify-identity/step2">
-              but you still need to connect it to your person
-</Link>
+            Your id number has been added, but you still need to connect it to
+            your person
           </p>
           <div key="1" id="add-nin-number">
             <div key="1" id="nin-form-container">
               <div key="1" id="add-nin-number" className="unverified">
-                <NinNumber {...this.props} />
+                <div data-ninnumber={this.props.nin} id="nin-number-container">
+                  <p id="nin-number">{this.props.nin}</p>
+                </div>
                 <RemoveButton {...this.props} />
               </div>
             </div>
@@ -89,7 +76,7 @@ const mapStateToProps = (state, props) => {
 
 const mapDispatchToProps = (dispatch, props) => {
   return {
-    handleDelete: function (e) {
+    handleDelete: function(e) {
       console.log("you're in handleDelete through ninDisplay!");
       const ninNumber = e.target.previousSibling.dataset.ninnumber;
       dispatch(actions.startRemove(ninNumber));
