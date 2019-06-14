00130de17bc506b5c73ae071ad6289ac15ad3bb7
Nathalie Jonsson <njons@sunet.se>
2019-06-14 09:04:51 +0200

    comments out vetting functionality on mobile vetting click (in attempt to adopt the same informative modal pattern as for letter vetting)

--- src/containers/LookupMobileProofing.js
+++ src/containers/LookupMobileProofing.js
@@ -1,6 +1,7 @@
 import { connect } from "react-redux";
 import { isValid } from "redux-form";
 import LookupMobileProofing from "components/LookupMobileProofing";
+import { eduidRMAllNotify } from "actions/Notifications";
 import { postLookupMobile } from "actions/LookupMobileProofing";
 import i18n from "i18n-messages";
 
@@ -13,8 +14,13 @@ const mapStateToProps = (state, props) => {
 const mapDispatchToProps = (dispatch, props) => {
   return {
     handleLookupMobile: function(e) {
-      dispatch(postLookupMobile());
+      dispatch(eduidRMAllNotify());
+      // dispatch(postLookupMobile());
     }
+    // handleLetterProofing: function (e) {
+    //   dispatch(eduidRMAllNotify());
+    //   dispatch(actions.getLetterProofingState());
+    // },
   };
 };
 
