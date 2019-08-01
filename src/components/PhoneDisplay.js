0337d48015c870068768faede1ad89a21be047c1
Nathalie Jonsson <njons@sunet.se>
2019-08-01 18:42:31 +0200

    adjusts rendering logic to always show primary number if present

--- src/components/PhoneDisplay.js
+++ src/components/PhoneDisplay.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import i18n from "i18n-messages";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "style/base.scss";
 import "style/DashboardMain.scss";
@@ -7,18 +9,19 @@ class PhoneDisplay extends Component {
   render() {
     let text = "";
     if (this.props.phones.length) {
-      if (this.props.verifiedPhone) {
+      if (this.props.primaryPhoneStatus) {
         text = [
           <p key="0" id="nin-number" className="verified">
-            {this.props.phones[0].number}
+            {this.props.primaryPhone[0].number}
+          </p>
+        ];
+      } else {
+        text = [
+          <p key="0" id="nin-number" className="no-data">
+            {this.props.l10n("profile.phone_display_unconfirmed_data")}
           </p>
         ];
       }
-      text = [
-        <p key="0" id="nin-number" className="no-data">
-          {this.props.l10n("profile.phone_display_unconfirmed_data")}
-        </p>
-      ];
     } else {
       text = [
         <p key="0" id="nin-number" className="no-data">
@@ -26,6 +29,7 @@ class PhoneDisplay extends Component {
         </p>
       ];
     }
+
     return (
       <div key="2" className="profile-card">
         <label key="0">{this.props.l10n("profile.phone_display_title")}</label>
@@ -37,4 +41,28 @@ class PhoneDisplay extends Component {
   }
 }
 
-export default PhoneDisplay;
+// export default PhoneDisplay;
+
+const mapStateToProps = (state, props) => {
+  let primaryPhoneStatus = "";
+  const primaryPhone = state.phones.phones.filter(phone => phone.primary);
+  primaryPhone.length === 1
+    ? (primaryPhoneStatus = true)
+    : (primaryPhoneStatus = false);
+  return {
+    phones: state.phones.phones,
+    primaryPhone,
+    primaryPhoneStatus
+  };
+};
+
+const mapDispatchToProps = (dispatch, props) => {
+  return {};
+};
+
+const PhoneDisplayContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PhoneDisplay);
+
+export default i18n(PhoneDisplayContainer);
