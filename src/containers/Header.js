9a8c7b6e98ff4f9f3ec107af112f45c804416071
Nathalie Jonsson <njons@sunet.se>
2019-11-06 15:14:45 +0100

    removes commented out code

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -4,33 +4,10 @@ import { startLogout } from "actions/Header";
 import i18n from "i18n-messages";
 
 const mapStateToProps = (state, props) => {
-  let email, confirmed;
-  // if (state.emails.emails.length >= 1) {
-  //   email = state.emails.emails.filter(mail => mail.primary)[0].email;
-  // } else {
-  //   email = "";
-  // }
-  // const nins = state.nins.nins.filter(nin => nin.verified);
-  // if (nins.length >= 1) {
-  //   confirmed = "main.confirmed";
-  // } else {
-  //   confirmed = "main.unconfirmed";
-  // }
-
-
+  let confirmed;
   return {
     dashboard_url: state.config.dashboard_url,
-    // students_link: state.config.students_link,
-    // technicians_link: state.config.technicians_link,
-    // staff_link: state.config.staff_link,
-    // faq_link: state.config.faq_link,
-    // email: email,
     confirmed: confirmed,
-    // studentsLink: state.config.STATIC_STUDENTS_URL,
-    // techniciansLink: state.config.STATIC_TECHNICIANS_URL,
-    // staffLink: state.config.STATIC_STAFF_URL,
-    // faqLink: state.config.STATIC_FAQ_URL,
-    // size: state.config.window_size
   };
 };
 
@@ -39,10 +16,6 @@ const mapDispatchToProps = (dispatch, props) => {
     handleLogout: function(e) {
       dispatch(startLogout());
     },
-    // gotoSignup: function (e) {
-    //   e.preventDefault();
-    //   document.location.href = "/";
-    // },
     gotoSignin: function (e) {
       e.preventDefault();
       const dataNode = e.target.closest("div"),
