683b5751a39bfaf53e7bfb9a50d62ba01b4e3bb5
Nathalie Jonsson <njons@sunet.se>
2019-11-06 15:24:36 +0100

    update test to remove HeaderAnon component

--- src/tests/Header-test.js
+++ src/tests/Header-test.js
@@ -2,7 +2,7 @@ import React from "react";
 import expect from "expect";
 import { shallow } from "../../node_modules/enzyme";
 import { IntlProvider } from "react-intl";
-import HeaderAnonContainer from "containers/HeaderAnon";
+// import HeaderAnonContainer from "containers/HeaderAnon";
 import HeaderContainer from "containers/Header";
 import { setupComponent } from "tests/SignupMain-test";
 
@@ -24,41 +24,41 @@ describe("Header Component", () => {
     expect(logo.length).toEqual(1);
   });
 
-  it("Component renders the LOGOUT button", () => {
-    const fullWrapper = setupComponent({
-      component: <HeaderContainer />
-    });
-    const button = fullWrapper.find("#logout");
-    expect(button.exists()).toEqual(true);
-    expect(button.length).toEqual(1);
-    // expect(button.text()).toContain("Logout");
-  });
+  // it("Component renders the LOGOUT button", () => {
+  //   const fullWrapper = setupComponent({
+  //     component: <HeaderContainer />
+  //   });
+  //   const button = fullWrapper.find("#logout");
+  //   expect(button.exists()).toEqual(true);
+  //   expect(button.length).toEqual(1);
+  //   // expect(button.text()).toContain("Logout");
+  // });
 });
 
-describe("HeaderAnon Component", () => {
-  it("Component does not render 'false' or 'null'", () => {
-    const wrapper = shallow(
-      <IntlProvider locale="en">
-        <HeaderAnonContainer />
-      </IntlProvider>
-    );
-    expect(wrapper.isEmptyRender()).toEqual(false);
-  });
-  it("Component renders the eduID logo", () => {
-    const wrapper = setupComponent({
-        component: <HeaderAnonContainer />
-      }),
-      logo = wrapper.find("#eduid-logo");
-    expect(logo.length).toEqual(1);
-  });
+// describe("HeaderAnon Component", () => {
+//   it("Component does not render 'false' or 'null'", () => {
+//     const wrapper = shallow(
+//       <IntlProvider locale="en">
+//         <HeaderAnonContainer />
+//       </IntlProvider>
+//     );
+//     expect(wrapper.isEmptyRender()).toEqual(false);
+//   });
+//   it("Component renders the eduID logo", () => {
+//     const wrapper = setupComponent({
+//         component: <HeaderAnonContainer />
+//       }),
+//       logo = wrapper.find("#eduid-logo");
+//     expect(logo.length).toEqual(1);
+//   });
 
-  it("Component renders the LOGIN button", () => {
-    const fullWrapper = setupComponent({
-      component: <HeaderAnonContainer />
-    });
-    const button = fullWrapper.find("#login");
-    expect(button.exists()).toEqual(true);
-    expect(button.length).toEqual(1);
-    // expect(button.text()).toContain("Log in");
-  });
-});
+//   it("Component renders the LOGIN button", () => {
+//     const fullWrapper = setupComponent({
+//       component: <HeaderAnonContainer />
+//     });
+//     const button = fullWrapper.find("#login");
+//     expect(button.exists()).toEqual(true);
+//     expect(button.length).toEqual(1);
+//     // expect(button.text()).toContain("Log in");
+//   });
+// });
