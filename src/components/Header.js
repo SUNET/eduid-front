0addd471c7c38ce42ed39ce1c7e54796b9953e37
Nathalie Jonsson <njons@sunet.se>
2019-11-06 15:08:46 +0100

    adds conditional based on URL to determine what type of button to display on Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,48 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-
 import "style/Header.scss";
 
 class Header extends Component {
   render() {
-    let login = (
-      <div data-dashboard_url={this.props.dashboard_url}>
-        <a onClick={this.props.gotoSignin}>
-          <button id="login" className="btn">
-            {this.props.l10n("header.signin")}
+    const url = location.pathname;
+    let button = "";
+    if (url.includes("register")) {
+      button = (
+        <div data-dashboard_url={this.props.dashboard_url}>
+          <a onClick={this.props.gotoSignin}>
+            <button id="login" className="btn">
+              {this.props.l10n("header.signin")}
+            </button>
+          </a>
+        </div>
+      );
+    } else if (url.includes("profile")) {
+      button = (
+        <div id="eduid-button">
+          <button id="logout" className="btn" onClick={this.props.handleLogout}>
+            {this.props.l10n("header.logout")}
           </button>
-        </a>
-      </div>
-    );
-
-    const logout = (
-      <div id="eduid-button">
-        <button id="logout" className="btn" onClick={this.props.handleLogout}>
-          {this.props.l10n("header.logout")}
-        </button>
-      </div>
-    );
+        </div>
+      );
+    } else {
+      button = <div />;
+    }
 
     return (
       <header>
         <a href="http://html.eduid.docker/">
           <div id="eduid-logo" />
         </a>
-        {logout}
+        {button}
       </header>
     );
   }
 }
 
 Header.propTypes = {
-  withButtons: PropTypes.bool,
-  size: PropTypes.string,
-  // gotoSignup: PropTypes.func,
   gotoSignin: PropTypes.func,
   l10n: PropTypes.func,
   confirmed: PropTypes.string
-  // studentsLink: PropTypes.string,
-  // techniciansLink: PropTypes.string,
-  // staffLink: PropTypes.string,
-  // faqLink: PropTypes.string
 };
 
 export default Header;
