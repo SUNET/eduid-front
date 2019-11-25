01fec36b0fcb740bc0c2c5dbaea738369e345812
enriquepablo <enrique@cazalla.net>
2019-11-25 14:23:35 +0100

    this was not added to git

---
+++ src/login/LoginMain/LoginMain_sagas.js
@@ -0,0 +1,30 @@
+import { put, call } from "redux-saga/effects";
+import { ajaxHeaders, checkStatus, getRequest } from "sagas/common";
+
+import * as actions from "login/LoginMain/LoginMain_actions";
+
+
+export function* requestLoginConfig() {
+  try {
+    console.log("Getting config from " + LOGIN_CONFIG_URL);
+    const config = yield call(fetchLoginConfig, LOGIN_CONFIG_URL);
+    yield put(config);
+    yield put(actions.appLoaded());
+  } catch (error) {
+    yield put(actions.getLoginConfigFail(error.toString()));
+  }
+}
+
+export function fetchLoginConfig(url) {
+  const request = {
+    ...getRequest,
+    redirect: "follow"
+  };
+  return window
+    .fetch(url, {
+      ...request
+    })
+    .then(checkStatus)
+    .then(response => response.json());
+}
+
