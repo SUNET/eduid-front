c3138df277bdded44969e37a8803ee16f6926cc0
enriquepablo <enrique@cazalla.net>
2020-01-24 14:12:07 +0100

    get config settings for the code in the url

--- src/login-root-saga.js
+++ src/login-root-saga.js
@@ -6,10 +6,14 @@ import { requestLoginConfig } from "login/LoginMain/LoginMain_sagas";
 import * as init_actions from "login/InitResetForm/InitResetForm_actions";
 import { initReset } from "login/InitResetForm/InitResetForm_sagas";
 
+import * as resetting_actions from "login/Resetting/Resetting_actions";
+import { getConfigFromCode } from "login/Resetting/Resetting_sagas";
+
 function* rootSaga() {
   yield [
     takeLatest(actions.GET_LOGIN_CONFIG, requestLoginConfig),
     takeLatest(init_actions.DEAL_WITH_EMAIL, initReset),
+    takeLatest(resetting_actions.CODE_FOR_CONFIG, getConfigFromCode),
   ];
 }
 
---
+++ src/login/Resetting/Resetting_sagas.js
@@ -0,0 +1,30 @@
+import { checkStatus,
+         postRequest,
+         saveData } from "sagas/common";
+
+import * as main_actions from "login/LoginMain/LoginMain_actions";
+import * as actions from "login/Resetting/Resetting_actions";
+
+
+export function requestConfigFromCode(config, data) {
+  return window
+    .fetch(PASSWORD_SERVICE_URL + "/reset/config/", {
+      ...postRequest,
+      body: JSON.stringify(data)
+    })
+    .then(checkStatus)
+    .then(response => response.json());
+}
+
+const getData = state => ({
+  code: document.location.href.split('/').reverse()[0],
+  csrf_token: state.config.csrf_token
+});
+
+export const getConfigFromCode = saveData(
+  getData,
+  "config-reset-form",
+  main_actions.appLoaded,
+  requestConfigFromCode,
+  actions.postCodeFail
+);
