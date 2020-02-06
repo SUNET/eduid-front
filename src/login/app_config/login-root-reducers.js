525f1cd146c26c11b36498edb3e18e55d8bf9c29
Nathalie Jonsson <njons@sunet.se>
2020-02-06 17:02:57 +0100

    adds app_config folder where teh files that will be configured as the app grows (root-saga and combined reducers) are stored

---
+++ src/login/app_config/login-root-reducers.js
@@ -0,0 +1,22 @@
+import { combineReducers } from "redux";
+
+import notificationsReducer from "reducers/Notifications";
+import { routerReducer } from "react-router-redux";
+import { reducer as formReducer } from "redux-form";
+import { intlReducer } from "react-intl-redux";
+
+import loginReducer from "login/components/App/App_reducer";
+// import initResetFormReducer from "login/InitResetForm/InitResetForm_reducer";
+// import resettingReducer from "login/Resetting/Resetting_reducer";
+
+const eduIDLoginApp = combineReducers({
+  config: loginReducer,
+  notifications: notificationsReducer,
+  router: routerReducer,
+  form: formReducer,
+  intl: intlReducer
+  // reset: initResetFormReducer,
+  // resetting: resettingReducer,
+});
+
+export default eduIDLoginApp;
---
+++ src/login/app_config/login-root-saga.js
@@ -0,0 +1,20 @@
+import { takeLatest } from "redux-saga/effects";
+
+import * as actions from "login/components/App/App_actions";
+import { requestLoginConfig } from "login/components/App/App_sagas";
+
+// import * as init_actions from "login/InitResetForm/InitResetForm_actions";
+// import { initReset } from "login/InitResetForm/InitResetForm_sagas";
+
+// import * as resetting_actions from "login/components/Resetting/Resetting_actions";
+// import { getConfigFromCode } from "login/components/Resetting/Resetting_sagas";
+
+function* rootSaga() {
+  yield [
+    takeLatest(actions.GET_LOGIN_CONFIG, requestLoginConfig)
+    // takeLatest(init_actions.DEAL_WITH_EMAIL, initReset),
+    // takeLatest(resetting_actions.CODE_FOR_CONFIG, getConfigFromCode),
+  ];
+}
+
+export default rootSaga;
