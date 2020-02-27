fd3815f30eb6518b873187f8956decaac37e7ba6
Nathalie Jonsson <njons@sunet.se>
2020-02-27 14:44:32 +0100

    removes console.logs

--- src/login/app_init/init_container.js
+++ src/login/app_init/init_container.js
@@ -1,22 +1,16 @@
 import initStore from "./initStore";
 import * as init_actions from "./init_actions";
-// import * as emailLinkSent_actions from "../components/LoginApp/ResetPassword/EmailLinkSent/EmailLinkSent_actions";
-// import * as app_actions from "../components/App/App_actions";
-
-// window.URLSearchParams = require("url-search-params");
 
 const init_container = () => {
   console.log("Initializing state for the login app...");
+  // Starting app with a link from an email
   const url = document.location.href.split("/").reverse();
-  console.log("url:", url);
-  // console.log("url[0]:", url[0]);
   const emailCode = url[0].split("=")[1];
-  console.log("emailCode[1]:", emailCode);
   if (url[0].includes("email_code")) {
-    // get config for app when staring from an email link
+    // pass on code get config for app and
     initStore.dispatch(init_actions.useLinkCode(emailCode));
   } else {
-    // get config for app
+    // Starting app without a link from an email
     initStore.dispatch(init_actions.getConfig());
   }
 };
