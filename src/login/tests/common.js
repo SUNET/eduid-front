ce9937f2f6fa75e37ff91b739d0d8651d6f99101
enriquepablo <enrique@cazalla.net>
2020-03-03 09:00:06 +0100

    some testing infrastructure

---
+++ src/login/tests/common.js
@@ -0,0 +1,78 @@
+const mock = require("jest-mock");
+import React from "react";
+import { Provider } from "react-intl-redux";
+import { mount } from "enzyme";
+import expect from "expect";
+
+import { Provider } from "react-intl-redux";
+import { addLocaleData } from "react-intl";
+import en from "react-intl/locale-data/en";
+import sv from "react-intl/locale-data/sv";
+
+addLocaleData([...en, ...sv]);
+
+// initial state for login app
+const fakeState = {
+  config: {
+    is_app_loaded: false,
+    csrf_token: "",
+    //is_fetching: false,
+    error: false,
+    DEBUG: true,
+    available_languages: {},
+    success_title: "",
+    success_body: ""
+  },
+  notifications: {
+    messages: [],
+    errors: []
+  },
+  reset: {
+    email: "",
+    email_sent: false
+  },
+  resetting: {
+    choose_extrasec: "",
+    extrasec_phone_index: -1
+  },
+  do_reset: {
+    new_password: "",
+    sms_code: "",
+    password_chosen_sms: false,
+    webauthn_assertion: null
+  }
+};
+
+// function that returns a state, possibly overriding some keys.
+export const getState = (overrides = {}) => {
+  const refakeState = { ...fakeState };
+  Object.getOwnPropertyNames(fakeState).forEach(propName => {
+    const overriddenProps = Object.getOwnPropertyNames(overrides);
+    if (overriddenProps.includes(propName)) {
+      refakeState[propName] = {
+        ...fakeState[propName],
+        ...overrides[propName]
+      };
+    }
+  });
+  return refakeState;
+};
+
+// Fake redux store
+export const fakeStore = state => ({
+  default: () => {},
+  dispatch: mock.fn(),
+  subscribe: mock.fn(),
+  getState: () => ({ ...state })
+});
+
+export const setupComponent = function({ component, overrides, store } = {}) {
+  if (store === undefined) {
+    if (overrides === undefined) {
+      overrides = {};
+    }
+    store = fakeStore(getState(overrides));
+  }
+  const wrapper = mount(<Provider store={store}>{component}</Provider>);
+  return wrapper;
+};
