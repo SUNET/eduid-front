eba31f9fef4a26141d10cb3034fb48caa28c638a
Nathalie Jonsson <njons@sunet.se>
2020-03-05 17:59:59 +0100

    removes unused files

--- src/login/components/LoginApp/ResetPassword/ResetPassword_action.js
+++
@@ -1,9 +0,0 @@
-// export const REDIRECT = "REDIRECT";
-
-// // this action switches is_loaded from false to true (removes spinner and shows website)
-// export function redirect() {
-//   return {
-//     type: REDIRECT
-//   };
-// }
-
