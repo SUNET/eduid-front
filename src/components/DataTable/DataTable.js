87f27a6ba6e0964569ebcc47dc1da10bfde0a50d
Nathalie Jonsson <njons@sunet.se>
2020-03-26 12:59:27 +0100

    adds import of new table file and commeints out its code

---
+++ src/components/DataTable/DataTable.js
@@ -0,0 +1,150 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import i18n from "../../login/translation/InjectIntl_HOC_factory";
+import EduIDButton from "components/EduIDButton";
+
+class DataTable extends Component {
+  render() {
+    let rows = [];
+    let key = [];
+
+    console.log("this is this.props.entries:", this.props.entries)
+
+    // if (this.props.entries) {
+    //   rows = this.props.entries.map((entry, index) => {
+    //     if (entry.number) {
+    //       key = entry.number;
+    //     } else {
+    //       key = entry.email;
+    //     }
+    //     if (entry.primary) {
+    //       return (
+    //         <tr
+    //           className="emailrow primary"
+    //           data-identifier={index}
+    //           data-object={key}
+    //           key={key}
+    //         >
+    //           <td className="identifier data">{key}</td>
+    //           <td className="non-identifier status-label">
+    //             <span className="nobutton verify-status-label">
+    //               {this.props.translate("tl.primary")}
+    //             </span>
+    //           </td>
+    //           <td className="non-identifier data-remove-button">
+    //             <EduIDButton
+    //               className="icon-button"
+    //               onClick={this.props.handleRemove}
+    //             >
+    //               <svg
+    //                 className="remove"
+    //                 width="16"
+    //                 height="16"
+    //                 viewBox="0 0 16 16"
+    //                 fill="none"
+    //                 xmlns="http://www.w3.org/2000/svg"
+    //               >
+    //                 <path d="M7 0h2v16H7z" />
+    //                 <path d="M0 9V7h16v2z" />
+    //               </svg>
+    //             </EduIDButton>
+    //           </td>
+    //         </tr>
+    //       );
+    //     } else if (entry.verified) {
+    //       return (
+    //         <tr
+    //           className="emailrow make-primary"
+    //           data-identifier={index}
+    //           data-object={key}
+    //           key={key}
+    //         >
+    //           <td className="identifier data">{key}</td>
+    //           <td className="non-identifier">
+    //             <EduIDButton
+    //               className="btn-link verify-label verify-status-label"
+    //               onClick={this.props.handleMakePrimary}
+    //             >
+    //               {this.props.translate("tl.make_primary")}
+    //             </EduIDButton>
+    //           </td>
+    //           <td className="non-identifier ">
+    //             <EduIDButton
+    //               className="icon-button"
+    //               onClick={this.props.handleRemove}
+    //             >
+    //               <svg
+    //                 className="remove"
+    //                 width="16"
+    //                 height="16"
+    //                 viewBox="0 0 16 16"
+    //                 fill="none"
+    //                 xmlns="http://www.w3.org/2000/svg"
+    //               >
+    //                 <path d="M7 0h2v16H7z" />
+    //                 <path d="M0 9V7h16v2z" />
+    //               </svg>
+    //             </EduIDButton>
+    //           </td>
+    //         </tr>
+    //       );
+    //     } else {
+    //       return (
+    //         <tr
+    //           className="emailrow make-primary"
+    //           data-identifier={index}
+    //           data-object={key}
+    //           key={key}
+    //         >
+    //           <td className="identifier data">{key}</td>
+    //           <td className="non-identifier ">
+    //             <EduIDButton
+    //               className="btn-link verify-status-label"
+    //               onClick={this.props.handleStartConfirmation}
+    //             >
+    //               {this.props.translate("tl.pending")}
+    //             </EduIDButton>
+    //           </td>
+    //           <td className="non-identifier">
+    //             <EduIDButton
+    //               className="icon-button"
+    //               onClick={this.props.handleRemove}
+    //             >
+    //               <svg
+    //                 className="remove"
+    //                 width="16"
+    //                 height="16"
+    //                 viewBox="0 0 16 16"
+    //                 fill="none"
+    //                 xmlns="http://www.w3.org/2000/svg"
+    //               >
+    //                 <path d="M7 0h2v16H7z" />
+    //                 <path d="M0 9V7h16v2z" />
+    //               </svg>
+    //             </EduIDButton>
+    //           </td>
+    //         </tr>
+    //       );
+    //     }
+    //   }, this);
+    // }
+    return (
+      <div className="table-responsive">
+        <table className="table-form">
+          <tbody>{rows}</tbody>
+          {/* <tbody>{rows}</tbody> */}
+        </table>
+      </div>
+    );
+  }
+}
+
+DataTable.propTypes = {
+  entries: PropTypes.array,
+  handleStartConfirmation: PropTypes.func,
+  handleRemove: PropTypes.func,
+  handleMakePrimary: PropTypes.func
+};
+
+export default i18n(DataTable);
--- src/components/Emails.js
+++ src/components/Emails.js
@@ -5,7 +5,7 @@ import { Field, reduxForm } from "redux-form";
 
 import TextInput from "components/EduIDTextInput";
 import EduIDButton from "components/EduIDButton";
-import TableList from "components/DataTable/TableList";
+import TableList from "./DataTable/DataTable";
 import ConfirmModal from "components/ConfirmModal";
 
 import "style/Emails.scss";
