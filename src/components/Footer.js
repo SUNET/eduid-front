daa087010357878a85264d848bf47d528be90a80
Nathalie Jonsson <njons@sunet.se>
2020-04-03 14:48:57 +0200

    moves css code from stylesheet to partial _Footer.scss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-// import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+//  import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 // import "style/Footer.scss";
 // import "style/base.scss";
 // import "style/DashboardMain.scss";
