7c6186a95ea09822dfd6dbcdeda6b065a8f71527
Nathalie Jonsson <njons@sunet.se>
2020-04-09 17:03:29 +0200

    replaces EduIDButton with a PrimaryButton, comments out import av stylesheet and makes the button dispbled while input is not valid

--- src/components/NinForm.js
+++ src/components/NinForm.js
@@ -7,8 +7,9 @@ import { Field, reduxForm } from "redux-form";
 import * as actions from "actions/Nins";
 
 import TextInput from "components/EduIDTextInput";
+import PrimaryButton from "../login/components/Buttons/ButtonPrimary";
 
-import "style/Nins.scss";
+// import "style/Nins.scss";
 
 const validate = values => {
   let value = values.nin;
@@ -38,21 +39,21 @@ const validate = values => {
 
 class NinForm extends Component {
   render() {
-    let validNin = "",
-      formButton = "";
+    // let validNin = "",
+    //   formButton = "";
 
-    if (this.props.valid) {
-      validNin = this.props.nin;
-      formButton = [
-        <button
-          className="btn settings-button"
-          onClick={this.props.addNin}
-          key="1"
-        >
-          {this.props.translate("emails.button_add")}
-        </button>
-      ];
-    }
+    // if (this.props.valid) {
+    //   validNin = this.props.nin;
+    //   formButton = [
+    //     <PrimaryButton
+    //       className="btn settings-button"
+    //       onClick={this.props.addNin}
+    //       key="1"
+    //     >
+    //       {this.props.translate("emails.button_add")}
+    //     </PrimaryButton>
+    //   ];
+    // }
 
     return (
       <div key="2" id="nin-form-container">
@@ -65,7 +66,13 @@ class NinForm extends Component {
             placeholder={this.props.translate("nins.input_placeholder")}
             helpBlock={this.props.translate("nins.input_help_text")}
           />
-          {formButton}
+          <PrimaryButton
+            disabled={!this.props.valid}
+            onClick={this.props.addNin}
+            key="1"
+          >
+            {this.props.translate("emails.button_add")}
+          </PrimaryButton>
         </Form>
       </div>
     );
