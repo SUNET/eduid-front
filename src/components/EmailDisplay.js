22547cb448ac730a0f1a3edabd898637289ffeaf
Nathalie Jonsson <njons@sunet.se>
2020-04-22 13:59:30 +0200

    aligns styles between NameDisplay and EmailDisplay

--- src/components/EmailDisplay.js
+++ src/components/EmailDisplay.js
@@ -1,32 +1,32 @@
 import React, { Component } from "react";
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "style/base.scss";
-import "style/DashboardMain.scss";
+// import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import "style/base.scss";
+// import "style/DashboardMain.scss";
 
 class EmailDisplay extends Component {
   render() {
-    let text = "";
+    let userData = "";
     if (this.props.email.length === 0) {
-      text = [
-        <p key="0" id="nin-number" className="no-data">
+      userData = [
+        <p key="0" className="display-data no-data">
           {this.props.translate("profile.email_display_no_data")}
-        </p>
+        </p>,
       ];
     } else {
-      text = [
-        <p key="0" id="nin-number" className="verified email-address">
+      userData = [
+        <p key="0" className="display-data verified">
           {this.props.email[0].email}
-        </p>
+        </p>,
       ];
     }
     return (
-      <div key="3" className="profile-card">
+      <div key="3" className="profile-grid-cell">
         <label key="0">
           {this.props.translate("profile.email_display_title")}
         </label>
-        <div key="1" id="nin-number-container">
-          {text}
-        </div>
+        {/* <div key="1" id="nin-number-container"> */}
+        {userData}
+        {/* </div> */}
       </div>
     );
   }
