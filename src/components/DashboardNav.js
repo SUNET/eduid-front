013c99033246caa2cf356d470d654f76f00a4565
Nathalie Jonsson <njons@sunet.se>
2020-04-23 14:32:14 +0200

    removes extra dashboard nav logic showing back when changing password

--- src/components/DashboardNav.js
+++ src/components/DashboardNav.js
@@ -4,46 +4,33 @@ import i18n from "../login/translation/InjectIntl_HOC_factory";
 
 class DashboardNav extends Component {
   render() {
-    const url = location.pathname;
-    if (url.includes("chpass")) {
-      return (
-        <nav id="dashboard-nav">
-          <ul>
-            <NavLink exact to={`/profile/`}>
-              <li>{this.props.translate("dashboard_nav.back")}</li>
-            </NavLink>
-          </ul>
-        </nav>
-      );
-    } else {
-      return (
-        <nav id="dashboard-nav">
-          <ul>
-            <NavLink exact activeClassName="active" to={`/profile/`}>
-              <li>{this.props.translate("dashboard_nav.profile")}</li>
-            </NavLink>
-            <NavLink
-              exact
-              activeClassName="active"
-              to={`/profile/verify-identity/`}
-            >
-              <li>
-                <span>Identity</span>
-              </li>
-            </NavLink>
-            <NavLink activeClassName="active" to={`/profile/settings/`}>
-              <li>{this.props.translate("dashboard_nav.settings")}</li>
-            </NavLink>
-            <NavLink
-              activeClassName="active"
-              to={`/profile/settings/advanced-settings`}
-            >
-              <li>{this.props.translate("dashboard_nav.advanced-settings")}</li>
-            </NavLink>
-          </ul>
-        </nav>
-      );
-    }
+    return (
+      <nav id="dashboard-nav">
+        <ul>
+          <NavLink exact activeClassName="active" to={`/profile/`}>
+            <li>{this.props.translate("dashboard_nav.profile")}</li>
+          </NavLink>
+          <NavLink
+            exact
+            activeClassName="active"
+            to={`/profile/verify-identity/`}
+          >
+            <li>
+              <span>Identity</span>
+            </li>
+          </NavLink>
+          <NavLink activeClassName="active" to={`/profile/settings/`}>
+            <li>{this.props.translate("dashboard_nav.settings")}</li>
+          </NavLink>
+          <NavLink
+            activeClassName="active"
+            to={`/profile/settings/advanced-settings`}
+          >
+            <li>{this.props.translate("dashboard_nav.advanced-settings")}</li>
+          </NavLink>
+        </ul>
+      </nav>
+    );
   }
 }
 
