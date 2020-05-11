6a22e6ba615bd6e150704b3507af6e1230ca6d4a
Nathalie Jonsson <njons@sunet.se>
2020-05-11 13:22:13 +0200

    splits out changing content (tagline text and button) into variables that change with url

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,14 @@ import PropTypes from "prop-types";
 import "../login/styles/index.scss";
 import "style/Header.scss";
 
-
 class Header extends Component {
   render() {
     const url = location.pathname;
+    let tagline = "";
     let button = "";
+ 
     if (url.includes("register")) {
+      tagline = "eduID är enklare och säkrare inloggning.";
       button = (
         <a href={this.props.dashboard_url}>
           <button id="login" className="btn">
@@ -16,7 +18,9 @@ class Header extends Component {
           </button>
         </a>
       );
+      
     } else if (url.includes("profile")) {
+      tagline = this.props.translate("dashboard.welcome");
       button = (
         <div id="eduid-button">
           <button id="logout" className="btn" onClick={this.props.handleLogout}>
@@ -29,12 +33,19 @@ class Header extends Component {
     }
 
     return (
-      <header id="header">
-        <a href={this.props.dashboard_url}>
-          <div id="eduid-logo" />
-        </a>
-        {button}
-      </header>
+      <section class="banner">
+        <header id="header">
+          <a href={this.props.dashboard_url}>
+            <div id="eduid-logo" />
+          </a>
+          {button}
+        </header>
+        <div className="vertical-content-margin">
+          <h1 className="tagline">
+            {tagline} {this.props.email}
+          </h1>
+        </div>
+      </section>
     );
   }
 }
