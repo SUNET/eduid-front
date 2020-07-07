23868bfbc44b480d62138a7bd41a8ccb3c45526b
Eunju Huss <eunju@sunet.se>
2020-07-07 17:42:31 +0200

    Replace field to RenderInput

--- src/login/components/Inputs/CustomInput.js
+++ src/login/components/Inputs/CustomInput.js
@@ -1,98 +1,68 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-
-import FormText from "reactstrap/lib/FormText";
+import React from "react";
 import FormGroup from "reactstrap/lib/FormGroup";
-import FormFeedback from "reactstrap/lib/FormFeedback";
 import Input from "reactstrap/lib/Input";
 import Label from "reactstrap/lib/Label";
 import i18n from "../../translation/InjectIntl_HOC_factory";
+
+const RenderInput = props => {
+  const {
+    input,
+    name,
+    selectOptions,
+    type,
+    disabled,
+    placeholder
+  } = props;
+  
+  if (type === "select") {
+    let options = [];
+    if (selectOptions) {
+      options = selectOptions.slice();
+    }
+    const selectedOption = options.map(option => {
+      return (
+        <option key={option[0]} value={option[0]}>
+          {option[1]}
+        </option>
+      );
+    });
+    
+    return(
+      <Input
+        type={type}
+        disabled={disabled}
+        placeholder={placeholder}
+        id={name}
+        name={name}
+        {...input}
+      >
+        {selectedOption}
+      </Input>
+
+)} else {
+  return(
+    <Input
+      type={type}
+      disabled={disabled}
+      placeholder={placeholder}
+      id={name}
+      name={name}
+      {...input}
+    />
+  )}
+}
 
 const customInput = props => {
   const {
     input,
     label,
     name,
-    meta,
-    selectOptions,
-    componentClass,
-    type,
-    translate,
-    disabled,
-    helpBlock,
-    placeholder
   } = props;
-  let valid = false,
-    invalid = false;
-  if (meta.touched || meta.submitFailed) {
-    if (meta.error) {
-      invalid = true;
-    } else {
-      valid = true;
-    }
-  }
-  const errmsg = (invalid && translate(meta.error)) || "";
-  let feedback = "",
-    help;
-  if (errmsg !== "") {
-    feedback = <span className="eduid-field-error">{errmsg}</span>;
-    help = (
-      <FormText>
-        {feedback} | {helpBlock}
-      </FormText>
-    );
-  } else {
-    help = <FormText>{helpBlock}</FormText>;
-  }
-
-  let field;
-
-  if (type === "select") {
-    let options = [];
-    if (selectOptions) {
-      options = selectOptions.slice();
-    }
-    const children = options.map(opt => {
-      return (
-        <option key={opt[0]} value={opt[0]}>
-          {opt[1]}
-        </option>
-      );
-    });
-    field = (
-      <Input
-        type={type}
-        disabled={disabled}
-        placeholder={placeholder}
-        id={name}
-        name={name}
-        valid={valid}
-        invalid={invalid}
-        {...input}
-      >
-        {children}
-      </Input>
-    );
-  } else {
-    field = (
-      <Input
-        type={type}
-        disabled={disabled}
-        placeholder={placeholder}
-        id={name}
-        name={name}
-        valid={valid}
-        invalid={invalid}
-        {...input}
-      />
-    );
-  }
 
   return (
     <FormGroup id={input.name}>
       <Label for={name}>{label && label}</Label>
-      {field}
-      {help}
+      <RenderInput {...props}/>
     </FormGroup>
   );
 };
