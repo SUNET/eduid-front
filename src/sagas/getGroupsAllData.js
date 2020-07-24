f9936e5e4e075962fa4f0d0223c838f4707a2dc1
Nathalie Jonsson <njons@sunet.se>
2020-07-24 13:44:44 +0200

    moves requests form /components folder to /sagas folder and renames all-data to both start with getGroups

--- src/sagas/getGroupsAllData.js
+++ src/sagas/getGroupsAllData.js
@@ -1,14 +1,14 @@
 // import { push } from "react-router-redux";
 import { call } from "redux-saga/effects";
-import { getData } from "../../../../../../sagas/getData";
+import { getData } from "./getData";
 // import { checkStatus, getRequest } from "../../../../../../sagas/common";
 
-export function* requestGroupsData() {
-  const url = GROUP_MGMT_URL + "/groups";
+export function* requestGroupsAllData() {
+  const url = GROUP_MGMT_URL + "/all-data";
   try {
     console.log("this is url", url);
-    const groupsReponse = yield call(getData, url);
-    console.log("This should be groupsReponse:", groupsReponse);
+    const allGroupReponse = yield call(getData, url);
+    console.log("This should be allGroupReponse:", allGroupReponse);
     // yield put(groupDataReponse);
   } catch (error) {
     console.log("groups request errored", error.message);
