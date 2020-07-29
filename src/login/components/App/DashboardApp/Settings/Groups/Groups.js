dae720a86f3d6da7885a20f473d7841f65079b9d
Nathalie Jonsson <njons@sunet.se>
2020-07-29 14:18:50 +0200

    add in old version of datatable as a mock component to recieve saga data

--- src/login/components/App/DashboardApp/Settings/Groups/Groups.js
+++ src/login/components/App/DashboardApp/Settings/Groups/Groups.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import InjectIntl from "../../../../../translation/InjectIntl_HOC_factory";
 import checkForCookie from "../../../../../app_utils/checkForCookie";
+import DataTable from "../../../../DataTable/DataTable";
 
 class Groups extends Component {
   render() {
@@ -10,6 +11,7 @@ class Groups extends Component {
     const showComponent = checkForCookie(cookieName, cookiePattern);
 
     if (showComponent) {
+      console.log("these are props:", this.props);
       return (
         <div className="namesview-form-container">
           <div className="intro">
@@ -18,6 +20,7 @@ class Groups extends Component {
               Create groups with other eduID users to allow them access to
               third-party services using eduID for login.
             </p>
+            <DataTable data={this.props.data} />
           </div>
         </div>
       );
