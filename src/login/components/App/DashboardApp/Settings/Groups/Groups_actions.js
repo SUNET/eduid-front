4f300175a11a96aa6d7ed0048d7c5b98020669e0
Nathalie Jonsson <njons@sunet.se>
2020-07-29 13:36:11 +0200

    adds failing (erro message) and succeding (data) actions for the saga response

---
+++ src/login/components/App/DashboardApp/Settings/Groups/Groups_actions.js
@@ -0,0 +1,19 @@
+// export const POST_USERDATA = "POST_USERDATA";
+export const GET_GROUP_MANAGEMENT_GROUPS_SUCCESS = "GET_GROUP_MANAGEMENT_GROUPS_SUCCESS";
+export const GET_GROUP_MANAGEMENT_GROUPS_FAIL = "GET_GROUP_MANAGEMENT_GROUPS_FAIL";
+
+export function getAllUserdata() {
+  return {
+    type: GET_ALL_USERDATA
+  };
+}
+
+export function getGropusDataFail(err) {
+  return {
+    type: GET_GROUP_MANAGEMENT_GROUPS_FAIL,
+    error: true,
+    payload: {
+      message: err
+    }
+  };
+}
