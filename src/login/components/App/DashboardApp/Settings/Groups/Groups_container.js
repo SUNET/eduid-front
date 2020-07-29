06a8af8e89361a4243510b7e215a4fa79be05836
Nathalie Jonsson <njons@sunet.se>
2020-07-29 14:17:58 +0200

    adds updated imports to container and imports container into Groups parent: Settings

--- src/login/components/App/DashboardApp/Settings/Groups/Groups_container.js
+++ src/login/components/App/DashboardApp/Settings/Groups/Groups_container.js
@@ -1,28 +1,17 @@
 import { connect } from "react-redux";
 import Groups from "./Groups";
 import { postUserdata, changeUserdata } from "actions/PersonalData";
-import i18n from "../login/translation/InjectIntl_HOC_factory";
+import i18n from "../../../../../translation/InjectIntl_HOC_factory";
 
 const mapStateToProps = (state, props) => {
-  // let langs = [];
-  // if (state.config.AVAILABLE_LANGUAGES !== undefined) {
-  //   langs = [...state.config.AVAILABLE_LANGUAGES];
-  //   // langs.unshift(["", props.translate("pd.choose-language")]);
-  // }
   return {
     data: state.groups.data,
-    // langs: langs
   };
 };
 
-// const mapDispatchToProps = (dispatch, props) => {
-//   return {
-//     handleSave: e => {
-//       e.preventDefault();
-//       dispatch(postUserdata());
-//     }
-//   };
-// };
+const mapDispatchToProps = (dispatch, props) => {
+  return {};
+};
 
 const GropusDataContainer = connect(
   mapStateToProps,
