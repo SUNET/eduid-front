fc35aa0c1248a826ab502fe4a94923d757ed23b2
Eunju Huss <eunju@sunet.se>
2020-08-26 09:03:39 +0200

    Add both of proptypes for label

--- src/login/components/Inputs/CustomInput.js
+++ src/login/components/Inputs/CustomInput.js
@@ -118,7 +118,10 @@ const customInput = (props) => {
 };
 
 customInput.propTypes = {
-  label: PropTypes.any,
+  label: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object
+  ]),
   meta: PropTypes.object,
   input: PropTypes.object,
   name: PropTypes.string,
