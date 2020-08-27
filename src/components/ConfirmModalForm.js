c90f694726d936cbe9840a1a0b55567e18823229
Eunju Huss <eunju@sunet.se>
2020-08-27 10:38:17 +0200

    Clean and remove code

--- src/components/ConfirmModalForm.js
+++ src/components/ConfirmModalForm.js
@@ -1,25 +1,12 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
-import { ButtonGroup, Form } from "reactstrap";
-import Button from "reactstrap/lib/Button";
-import Modal from "reactstrap/lib/Modal";
-import ModalHeader from "reactstrap/lib/ModalHeader";
-import ModalBody from "reactstrap/lib/ModalBody";
-import ModalFooter from "reactstrap/lib/ModalFooter";
-
+import { Form } from "reactstrap";
 import i18n from "../login/translation/InjectIntl_HOC_factory";
 import CustomInput from "../login/components/Inputs/CustomInput";
-import EduIDButton from "components/EduIDButton";
-import NotificationsContainer from "containers/Notifications";
 
 const validate = (values, props) => {
-  console.log("validate - values", values);
-  console.log("valiadate - props.inputName", props.inputName);
-  console.log("valiadate - this.props.inputName", props.inputName);
   let inputName = props.inputName;
-  console.log("valiadate - inputName", inputName);
   const errors = {};
   const code = values[inputName];
   if (!code) {
