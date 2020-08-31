0a59cdc9a742dd4f80510849078d9e0fb8ae39ae
Nathalie Jonsson <njons@sunet.se>
2020-08-31 15:49:45 +0200

    cleanup of commented out code: removal of handling API data and removal of rendering of object keys - to be moved to child component

--- src/login/components/App/DashboardApp/Settings/Groups/Groups.js
+++ src/login/components/App/DashboardApp/Settings/Groups/Groups.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import InjectIntl from "../../../../../translation/InjectIntl_HOC_factory";
 import checkForCookie from "../../../../../app_utils/checkForCookie";
-import DataTable from "../../../../DataTable/DataTable";
 
 class Groups extends Component {
   // this component should:
@@ -41,14 +40,6 @@ class Groups extends Component {
     };
 
     if (showComponent) {
-      // // just mock handling data here to render something from the api
-      // let mockData = [];
-      // let mockArray = Object.keys(this.props.data);
-      // mockData = mockArray.map((thing, i) => {
-      //   console.log("these are thing:", thing);
-      //   return <p key={i}>{thing}</p>;
-      // });
-
       return (
         <article>
           <div className="intro">
@@ -60,12 +51,10 @@ class Groups extends Component {
               Create groups with other eduID users to allow them access to
               third-party services using eduID for login.
             </p>
-            {/* {mockData} */}
             <div>
               <p>This will be the DataPanel component</p>
               <RenderWizard show={this.state.wizardCreateGroup} />
             </div>
-            {/* <DataTable data={this.props.data} /> */}
           </div>
         </article>
       );
