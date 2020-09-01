97a7379fd301999210aed459eaee043faa6bc6f5
Eunju Huss <eunju@sunet.se>
2020-09-01 10:45:26 +0200

    Adjust emails.invalid_code text

--- src/login/translation/messageIndex.js
+++ src/login/translation/messageIndex.js
@@ -42,7 +42,7 @@ export const unformattedMessages = defineMessages({
   },
   "emails.invalid_code": {
     id: "emails.invalid_code",
-    defaultMessage: `Invalid confirmation code`,
+    defaultMessage: `Invalid confirmation code, only letters and numbers allowed`,
     description: "error text for invalid confirmation code"
   },
   "emails.confirm_code_wrong_length": {
