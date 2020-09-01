db8ae58fbd00e34bd92cb0158f373dd1da84cef9
Nathalie Jonsson <njons@sunet.se>
2020-09-01 11:07:22 +0200

    moves the functions outside of render()

--- src/login/components/App/DashboardApp/Settings/Groups/Groups.js
+++ src/login/components/App/DashboardApp/Settings/Groups/Groups.js
@@ -3,6 +3,25 @@ import PropTypes from "prop-types";
 import InjectIntl from "../../../../../translation/InjectIntl_HOC_factory";
 import checkForCookie from "../../../../../app_utils/checkForCookie";
 
+const RenderWizard = (props) => {
+  return (
+    <div className={"wizard"}>
+      {props.show && <p>Create you first group wizard</p>}
+    </div>
+  );
+};
+
+const RenderCreateButton = (props) => {
+  return (
+    <a href="#">
+      {!props.show && (
+        <button className={"create-group"}>Create button</button>
+      )}
+    </a>
+  );
+};
+
+
 class Groups extends Component {
   // this component should:
   // 1. take in text to fill .intro h4 and .intro p (hardcoded at the moment)
@@ -20,25 +39,7 @@ class Groups extends Component {
     const cookieName = "show-groups";
     const cookiePattern = "";
     const showComponent = checkForCookie(cookieName, cookiePattern);
-
-    const RenderWizard = (props) => {
-      return (
-        <div className={"wizard"}>
-          {props.show && <p>Create you first group wizard</p>}
-        </div>
-      );
-    };
-
-    const RenderCreateButton = (props) => {
-      return (
-        <a href="#">
-          {!props.show && (
-            <button className={"create-group"}>Create button</button>
-          )}
-        </a>
-      );
-    };
-
+ 
     if (showComponent) {
       return (
         <article>
