281f45d1b17f22f4802b59ac689d4b444994a653
Nathalie Jonsson <njons@sunet.se>
2020-09-01 13:41:36 +0200

    adds in instructions for component

--- src/login/components/Wizard/WizardPanel.js
+++ src/login/components/Wizard/WizardPanel.js
@@ -3,6 +3,25 @@ import PropTypes from "prop-types";
 import InjectIntl from "../../translation/InjectIntl_HOC_factory";
 
 class WizardPanel extends Component {
+  // this component should:
+  // 1. Be able to handle logic of what wizard to render when
+  // - at start: 
+  //     state = {firstGroup: true}
+  //     -> render wizard
+  // - when clicking CREATE GROUP button in wizard: 
+  //     state = {firstGroup: false}
+  //     -> permanently remove wizard
+  //  - when first invite: 
+  //     state = {firstInvite: true}
+  //     -> render wizard
+  //  - when clicking ACCEPT or IGNORE button in wizard: 
+  //     state = {firstInvite: false}
+  //     -> render invite data in a table
+  // 2. Be able to inform <Groups/> about: 
+  //     state = {firstGroup: false} 
+  //     -> keeping this as local state if possible
+
+  
   render() {
     return (
       <div className="wizard">
