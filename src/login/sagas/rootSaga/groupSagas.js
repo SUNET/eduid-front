4e38db6cd630e5a5286429d5caad452d70467cbe
Nathalie Jonsson <njons@sunet.se>
2020-09-15 11:40:18 +0200

    renames getGroups to groupSaga and moves it into the groupSagas dir

--- src/login/sagas/rootSaga/groupSagas.js
+++ src/login/sagas/rootSaga/groupSagas.js
@@ -2,13 +2,13 @@ import { takeLatest } from "redux-saga/effects";
 import * as configActions from "../../../actions/DashboardConfig";
 import * as createGroupActions from "../../redux/actions/createGroupActions";
 import { allDataSaga } from "../../redux/sagas/groupSagas/allDataSaga";
- import { requestGroupsData } from "../getGroups";
+import { groupsSaga } from "../../redux/sagas/groupSagas/groupsSaga";
 import { createGroupSaga } from "../../redux/sagas/groupSagas/createGroupSaga";
-// console.log("this is requestGroupsData in groupsRootSaga", requestGroupsData);
+// console.log("this is groupsSaga in groupsSagas", groupsSaga);
 
 // connecting actions (redux) with sagas (fetch)
 const groupsSagas = [
-  // takeLatest(configActions.GET_INITIAL_USERDATA, requestGroupsData),
+  // takeLatest(configActions.GET_INITIAL_USERDATA, groupsSaga),
   takeLatest(configActions.GET_INITIAL_USERDATA, allDataSaga),
   takeLatest(createGroupActions.CREATE_GROUP, createGroupSaga),
 ];
