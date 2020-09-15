9dcf9deb3242fcfc384cbf19960c46d4e42150d4
Nathalie Jonsson <njons@sunet.se>
2020-09-15 11:26:04 +0200

    updates rootsaga with the new group saga to create groups

--- src/login/sagas/rootSaga/groupSagas.js
+++ src/login/sagas/rootSaga/groupSagas.js
@@ -2,12 +2,15 @@ import { takeLatest } from "redux-saga/effects";
 import * as configActions from "../../../actions/DashboardConfig";
 import { requestGroupsData } from "../getGroups";
 import { requestGroupsAllData } from "../getGroupsAllData";
-
+import * as createGroupActions from "../../redux/actions/createGroupActions";
+import { createGroupSaga } from "../../redux/sagas/createGroupSaga";
 // console.log("this is requestGroupsData in groupsRootSaga", requestGroupsData);
 
+// connecting actions (redux) with sagas (fetch)
 const groupsSagas = [
   // takeLatest(configActions.GET_INITIAL_USERDATA, requestGroupsData),
   takeLatest(configActions.GET_INITIAL_USERDATA, requestGroupsAllData),
+  takeLatest(createGroupActions.CREATE_GROUP, createGroupSaga),
 ];
 
 export default groupsSagas;
