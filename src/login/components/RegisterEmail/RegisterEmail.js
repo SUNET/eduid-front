a04b6a4a7c1fd16110ccc4f58f594a6389af2e91
Eunju Huss <eunju@sunet.se>
2020-09-18 10:30:56 +0200

    Move RegisterEmail to login/component/RegisterEmail

--- src/login/components/RegisterEmail/RegisterEmail.js
+++ src/login/components/RegisterEmail/RegisterEmail.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import CustomInput from "../login/components/Inputs/CustomInput";
+import CustomInput from "../Inputs/CustomInput";
 import { Field, reduxForm } from "redux-form";
 import Form from "reactstrap/lib/Form";
 import EduIDButton from "components/EduIDButton";
-import NotificationModal from "../login/components/Modals/NotificationModal";
-import { validate } from "../login/app_utils/validation/validateEmail";
+import NotificationModal from "../Modals/NotificationModal";
+import { validate } from "../../app_utils/validation/validateEmail";
 
 /* FORM */
 let EmailForm = (props) => (
