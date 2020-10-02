2767473467fef32aa5d34132341437768e731c00
Eunju Huss <eunju@sunet.se>
2020-10-02 14:26:14 +0200

    Add regexPattern file to reuse patterns

---
+++ src/login/app_utils/validation/regexPattern.js
@@ -0,0 +1,9 @@
+// Naming of regex patterns refer to matching long (UUID format) and short (10 characters) codes. This aligns with naming used in the backend
+// UUID format source from: https://en.wikipedia.org/wiki/Universally_unique_identifier#Format
+// longCodePattern is used to verify an added email address
+export const longCodePattern = /^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/;
+// shortCodePattern is used to verify an added phone number and vetting via letter and phone
+export const shortCodePattern = /^[A-Za-z0-9]{10,12}$/;
+// securityKeyPattern is used to verify user's entered descriptions for security keys
+export const securityKeyPattern = /^.{1,50}$/;
+
