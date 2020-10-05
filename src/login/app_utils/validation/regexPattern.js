7b8e11c34bccaeb0e579a8c06457d54c99ea5f0b
Eunju Huss <eunju@sunet.se>
2020-10-05 09:46:29 +0200

    Adjust longCodePattern to match UUID format

--- src/login/app_utils/validation/regexPattern.js
+++ src/login/app_utils/validation/regexPattern.js
@@ -1,7 +1,7 @@
 // Naming of regex patterns refer to matching long (UUID format) and short (10 characters) codes. This aligns with naming used in the backend
 // UUID format source from: https://en.wikipedia.org/wiki/Universally_unique_identifier#Format
 // longCodePattern is used to verify an added email address
-export const longCodePattern = /^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/;
+export const longCodePattern = /^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$/;
 // shortCodePattern is used to verify an added phone number and vetting via letter and phone
 export const shortCodePattern = /^[A-Za-z0-9]{10,12}$/;
 // securityKeyPattern is used to verify user's entered descriptions for security keys
