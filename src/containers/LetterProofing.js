d57c86afe5742ce0e9e622a0577de340af1fa122
Eunju Huss <eunju@sunet.se>
2020-10-12 12:35:54 +0200

    Adjust props disabled to ignore nin data until confirmed

--- src/containers/LetterProofing.js
+++ src/containers/LetterProofing.js
@@ -9,11 +9,11 @@ const mapStateToProps = (state, props) => {
   const confirming = state.letter_proofing.confirmingLetter;
   const valid_nin = isValid("nins")(state);
   const confirmingLetter = confirming && valid_nin;
+  let disabled = !state.nins.nins[0]
   return {
-    disabled: !valid_nin,
+    disabled: disabled,
     confirmingLetter: confirmingLetter,
     verifyingLetter: state.letter_proofing.verifyingLetter,
-    // nin: state.nins.nin,
   };
 };
 
