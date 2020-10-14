5a1c388da142d1ed5c4939ac8ce19e93fc2a09e0
Eunju Huss <eunju@sunet.se>
2020-10-14 16:04:20 +0200

    Add letter_expired

--- src/containers/LetterProofing.js
+++ src/containers/LetterProofing.js
@@ -8,21 +8,22 @@ import i18n from "../login/translation/InjectIntl_HOC_factory";
 const mapStateToProps = (state, props) => {
   const confirming = state.letter_proofing.confirmingLetter;
   const valid_nin = isValid("nins")(state);
-  const confirmingLetter = confirming && valid_nin;
+  const confirmingLetter = confirming && valid_nin || state.letter_proofing.letter_expired;
   let withoutNin = !state.nins.nins[0]
   return {
     disabled: withoutNin,
     confirmingLetter: confirmingLetter,
     verifyingLetter: state.letter_proofing.verifyingLetter,
     letter_sent: state.letter_proofing.letter_sent,
-    letter_expires: state.letter_proofing.letter_expires
+    letter_expires: state.letter_proofing.letter_expires,
+    letter_expired: state.letter_proofing.letter_expired
   };
 };
 
 const mapDispatchToProps = (dispatch, props) => {
   return {
     handleLetterProofing: function(e) {
-      dispatch(eduidRMAllNotify());
+      // dispatch(eduidRMAllNotify());
       dispatch(actions.getLetterProofingState());
     },
     confirmLetterProofing: function(e) {
