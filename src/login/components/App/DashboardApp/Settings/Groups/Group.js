b1d2b014559efdce0e9eb14f458de45f248e005c
Nathalie Jonsson <njons@sunet.se>
2020-11-06 14:30:08 +0100

    adds render of open and closed state and mock info for open state (group identifier)

--- src/login/components/App/DashboardApp/Settings/Groups/Group.js
+++ src/login/components/App/DashboardApp/Settings/Groups/Group.js
@@ -2,50 +2,30 @@ import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import i18n from "../../../../../translation/InjectIntl_HOC_factory";
 
+
 const RenderOpenGroup = (props) => {
   console.log("these are props:", props);
-  return (
+  return props.openGroup ? (
     <Fragment>
-      <div className="element-pair">
-        <button className="dropdown-open">^</button>
-        {/* <p>{props.group.display_name}</p> */}
-        <p>open group</p>
-      </div>
-      <button
-        onClick={() => {
-          props.toggleMode();
-        }}
-      >
-        edit
-      </button>
+      <p>{props.group.identifier}</p>
+
     </Fragment>
-  );
+  ) : null;
 };
 
-const RenderClosedGroup = (props) => {
-  return (
-    <Fragment>
-      <div className="element-pair">
-        <button className="dropdown-closed">^</button>
-        <p>{props.group.display_name}</p>
-      </div>
-      <button
-        onClick={() => {
-          props.toggleMode();
-        }}
-      >
-        edit
-      </button>
-    </Fragment>
+const AnimateDropdownIcon = (state) =>
+  state.openGroup ? (
+    <button className="dropdown-open">^</button>
+  ) : (
+    <button className="dropdown-closed">^</button>
   );
-};
 
 class Group extends Component {
   state = {
     openGroup: false,
   };
 
-  toggleGroupOpenClosed = (id) => {
+  toggleGroupOpenClosed = () => {
     this.setState((prevState) => {
       return {
         openGroup: !prevState.openGroup,
@@ -56,16 +36,28 @@ class Group extends Component {
   render() {
     return (
       <li
+        className="closed"
         onClick={() => {
           this.toggleGroupOpenClosed();
         }}
         key={this.props.group.identifier}
       >
-        {this.state.openGroup ? (
-          <RenderOpenGroup {...this.props} />
-        ) : (
-          <RenderClosedGroup {...this.props} />
-        )}
+        <div className="title">
+          <div className="element-pair">
+            <AnimateDropdownIcon {...this.state} />
+            <p>{this.props.group.display_name}</p>
+          </div>
+          <button
+            onClick={() => {
+              props.toggleMode();
+            }}
+          >
+            edit
+          </button>
+        </div>
+        <div>
+          <RenderOpenGroup openGroup={this.state.openGroup} {...this.props} />
+        </div>
       </li>
     );
   }
