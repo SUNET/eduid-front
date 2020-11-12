260a2d959118cd6bf064c52b9e8922da74f617fc
Nathalie Jonsson <njons@sunet.se>
2020-11-12 15:03:06 +0100

    adds in separate function to check if email addresses are undefined before extarcting username

--- src/login/components/App/DashboardApp/Settings/Groups/GroupsContainer.js
+++ src/login/components/App/DashboardApp/Settings/Groups/GroupsContainer.js
@@ -19,28 +19,34 @@ const mapStateToProps = (state, props) => {
 
   // filter out primary email address to look for username among members and owners in groups
   let emailAddresses = state.emails.emails;
-  let username = emailAddresses.filter((email, i) => {
-    if (email.primary) {
-      return email.email[0].email;
-    }
-  });
+  const primaryEmailAddress = emailAddresses.filter(
+    (email) => email.primary
+  )[0];
 
-  // create new object listing user relationship with each group
+  // email request sometimes happens after group data
+  const getUsername = () => {
+    if (primaryEmailAddress !== undefined) {
+      return primaryEmailAddress.email;
+    }
+    return null;
+  };
+
+  //create new object listing user relationship with each group
   let userGroupsAndRoles = uniqueGroups.map((group, i) => {
+    let username = getUsername();
     let membershipObj = { group: group, isMember: false, isAdmin: false };
     group.members.some((member, i) => {
       if (member.display_name === username) {
-        membershipObj.isMember = true;
+        return (membershipObj.isMember = true);
       }
     });
     group.owners.some((owner, i) => {
       if (owner.display_name === username) {
-        membershipObj.isAdmin = true;
+        return (membershipObj.isAdmin = true);
       }
     });
     return membershipObj;
   });
-  // console.log("this is userGroupsAndRoles", userGroupsAndRoles);
 
   return {
     noGroups,
