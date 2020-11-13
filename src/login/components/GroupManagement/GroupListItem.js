171bbcc0de942c7fa1874e90b997f93f06776b99
Nathalie Jonsson <njons@sunet.se>
2020-11-13 14:33:28 +0100

    changes admin to owner in list of owners

--- src/login/components/GroupManagement/GroupListItem.js
+++ src/login/components/GroupManagement/GroupListItem.js
@@ -33,7 +33,7 @@ export const RenderMemberList = (props) => {
 const RenderOpenGroup = (props) => {
   return props.openGroup ? (
     <div className="open-group">
-      <RenderAdminList {...props} />
+      <RenderOwnerList {...props} />
       <RenderMemberList {...props} />
     </div>
   ) : null;
