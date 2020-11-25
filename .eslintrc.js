61a54276cb4802dff84d43e28bb8ab7766b37494
Eunju Huss <eunju@sunet.se>
2020-11-25 14:41:22 +0100

    Remove rules for testing github action

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,6 @@ module.exports = {
         "node": true,
     },
     "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
     ],
     "parserOptions": {
     "ecmaFeatures": {
@@ -21,30 +19,6 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "no-duplicate-case":"error",
-        "no-empty":"error",
-        "no-extra-semi":"error",
-        "no-func-assign": "error",
-        "no-irregular-whitespace":"error",
-        "no-unreachable": "error",
-        "curly": "error",
-        "eqeqeq": "error",
-        "no-multi-spaces": "error",
-        "no-mixed-spaces-and-tabs": "error",
-        "no-trailing-spaces": "error",
-        "default-case": "error",
-        "no-fallthrough": "error",
-        "no-use-before-define": "error",
-        "no-redeclare": "error",
-        "brace-style": "error",
-        "prefer-const": "error",
-        "no-var": "error",
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "no-unused-vars": ["error", { "args": "all" }],
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"]
+    
     }
 };
