1573093ffe966ed7d49084e691add4e39bc5082d
Eunju Huss <eunju@sunet.se>
2020-11-25 15:13:31 +0100

    Add extends eslint, plugin react to check if test will pass with incorrect format

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,8 @@ module.exports = {
         "node": true,
     },
     "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
     ],
     "parser": "babel-eslint",
     "parserOptions": {
