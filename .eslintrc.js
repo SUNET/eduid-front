5ba6072037decdd2ed89ee8e76b53ab61e66a191
Eunju Huss <eunju@sunet.se>
2020-12-10 11:39:10 +0100

    Set up eslint extends react/recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
         "node": true,
     },
     "extends": [
-        
+        "eslint:recommended",
+        "plugin:react/recommended"
     ],
     "parser": "babel-eslint",
     "parserOptions": {
