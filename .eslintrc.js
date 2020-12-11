f827a0bc9842a37c8e14378eea4fb6a93e0b2eb7
Eunju Huss <eunju@sunet.se>
2020-12-11 10:54:44 +0100

    Add globals variables form init-config  to prevent having an error no-undef

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         "browser": true,
         "es2021": true,
         "mocha": true,
-        "node": true,
+        "node": true
     },
     "extends": [
         "eslint:recommended",
@@ -22,6 +22,20 @@ module.exports = {
         "react"
     ],
     "rules": {
-    
-    }
+        "react/prop-types": "off"
+    },
+    "globals": { // global variables from init-config, to prevent having an error "no-undef" 
+        "EDUID_COOKIE_NAME": true,
+        "TOKEN_SERVICE_URL": true,
+        "EDUID_CONFIG_URL": true,
+        "SIGNUP_CONFIG_URL": true,
+        "LOGIN_CONFIG_URL": true,
+        "PASSWORD_SERVICE_URL": true,
+        "AVAILABLE_LANGUAGES": true,
+        "LOCALIZED_MESSAGES": true,
+        "SIGNUP_SERVICE_URL": true,
+        "BASE_PATH": true,
+        "GROUP_MGMT_URL": true,
+        "ACTIONS_SERVICE_URL": true
+      }
 };
