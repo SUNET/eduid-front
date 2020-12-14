7785b4f6232c03515d01086d5e92a2994f5eb9a3
Eunju Huss <eunju@sunet.se>
2020-12-14 15:30:01 +0100

    Add extends eslint/react recommended and tunring off some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         "browser": true,
         "es2021": true,
         "mocha": true,
-        "node": true,
+        "node": true
     },
     "extends": [
         "eslint:recommended",
@@ -22,6 +22,19 @@ module.exports = {
         "react"
     ],
     "rules": {
-    
-    }
+        "no-undef": "off",
+        "react/jsx-no-undef": "off", 
+        "no-case-declarations": "off",
+        "no-useless-escape": "off",
+        "react/jsx-key": "off",
+        "react/jsx-no-duplicate-props": "off",
+        "react/no-deprecated": "off",
+        "react/jsx-no-target-blank": "off",
+        "react/prop-types": "off",
+        "react/no-string-refs": "off",
+        "no-class-assign": "off",
+        "no-prototype-builtins": "off",
+        "react/display-name": "off",
+        "no-empty": "off"
+    },
 };
