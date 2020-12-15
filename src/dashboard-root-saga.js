97b0827a128acbaae6f619db02f3c68701fe503f
Nathalie Jonsson <njons@sunet.se>
2020-12-15 09:52:42 +0100

    renames groupsSagas to groupManagementSagas

--- src/dashboard-root-saga.js
+++ src/dashboard-root-saga.js
@@ -52,7 +52,7 @@ import {
 } from "sagas/LetterProofing";
 import { requestLogout } from "sagas/Header";
 import { saveLMPNinData } from "sagas/LookupMobileProofing";
-import groupsSagas from "./login/redux/sagas/rootSaga/groupsSagas";
+import groupsSagas from "./login/redux/sagas/rootSaga/groupManagementSagas";
 
 function* configSaga() {
   yield put(configActions.getInitialUserdata());
