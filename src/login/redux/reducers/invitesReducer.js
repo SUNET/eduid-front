2ac2327176744c7f1e949c07c017c16b3bba0159
Nathalie Jonsson <njons@sunet.se>
2020-12-15 14:46:37 +0100

    removes some commented out code from invites reducer

--- src/login/redux/reducers/invitesReducer.js
+++ src/login/redux/reducers/invitesReducer.js
@@ -11,16 +11,11 @@ let invitesDataReducer = (state = invitesData, action) => {
     case createInviteActions.POST_GROUP_INVITE_INVITES_CREATE_SUCCESS:
       return {
         ...state,
-        // invitesFromMe: action.payload.outgoing,
-        // member_of: action.payload.member_of,
-        // owner_of: action.payload.owner_of,
       };
     case getOutgoingInvitesActions.GET_GROUP_INVITE_INVITES_OUTGOING_SUCCESS:
       return {
         ...state,
         invitesFromMe: action.payload.outgoing,
-        // member_of: action.payload.member_of,
-        // owner_of: action.payload.owner_of,
       };
     default:
       return state;
