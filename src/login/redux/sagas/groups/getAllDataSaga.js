f14d5450c2335ca7606f0aa9ecd52e08cd01c93f
Nathalie Jonsson <njons@sunet.se>
2020-12-17 16:51:29 +0100

    split groups and invites sagas into their own separate dirs and change names to provide standard and to better reflect what they do

--- src/login/redux/sagas/groups/getAllDataSaga.js
+++ src/login/redux/sagas/groups/getAllDataSaga.js
@@ -2,7 +2,7 @@ import { call, put } from "redux-saga/effects";
 import { getData } from "../getDataRequest";
 import * as actions from "../../actions/getAllGroupMgmtDataActions";
 
-export function* allDataSaga() {
+export function* getAllDataSaga() {
   const url = GROUP_MGMT_URL + "/all-data";
   try {
     console.log("this is url", url);
--- src/login/redux/sagas/rootSaga/groupManagementSagas.js
+++ src/login/redux/sagas/rootSaga/groupManagementSagas.js
@@ -3,20 +3,20 @@ import * as allDataActions from "../../actions/getAllGroupMgmtDataActions";
 import * as createGroupActions from "../../actions/createGroupActions";
 import * as createInviteActions from "../../actions/createInviteActions";
 import * as getOutgoingInvitesActions from "../../actions/getOutgoingInvitesActions";
-import { allDataSaga } from "../groups/allDataSaga";
+import { getAllDataSaga } from "../groups/getAllDataSaga";
 import { createGroupSaga } from "../groups/createGroupSaga";
-import { createInviteSaga } from "../groups/createInviteSaga";
-import { allOutgoingInvitesSaga } from "../groups/allOutgoingInvitesSaga";
+import { createInviteSaga } from "../invites/createInviteSaga";
+import { getAllOutgoingInvitesSaga } from "../invites/getAllOutgoingInvitesSaga";
 
 // connecting actions (redux) with sagas (fetch)
 const groupsSagas = [
-  takeLatest(allDataActions.GET_ALL_DATA, allDataSaga),
+  takeLatest(allDataActions.GET_ALL_DATA, getAllDataSaga),
   takeLatest(createGroupActions.CREATE_GROUP, createGroupSaga),
   // cretaion of an invite leads to req for all outgoing invites
   takeLatest(createInviteActions.CREATE_INVITE, createInviteSaga),
   takeLatest(
     getOutgoingInvitesActions.GET_ALL_OUTGOING_INVITES,
-    allOutgoingInvitesSaga
+    getAllOutgoingInvitesSaga
   ),
 ];
 
