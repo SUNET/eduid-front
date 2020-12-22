1d4f8f62c439b5b5f4ce5111aeda1b6f1c1adb12
Eunju Huss <eunju@sunet.se>
2020-12-22 13:14:46 +0100

    Add comments

--- src/components/LookupMobileProofing.js
+++ src/components/LookupMobileProofing.js
@@ -4,13 +4,15 @@ import NotificationModal from "../login/components/Modals/NotificationModal";
 import { HashLink } from 'react-router-hash-link';
 class LookupMobileProofing extends Component {  
   render() {
+    /* Description is the text inside the vetting button */
     let description = "";
+    /* without nin, description text will help the user to add id number */
     if(this.props.withoutNin){
       description = (
         <div className="description">
           {this.props.translate("verify-identity.vetting_explanation_add_nin")}
         </div> 
-      )
+      ) /* without phone number, description text will help the user to add phone number and the text "setting" is linked to the setting page phone number section */ 
     } else if(this.props.withoutPhoneNumber){
       description = (
         <div className="link">
@@ -22,7 +24,7 @@ class LookupMobileProofing extends Component {
             {this.props.translate("verify-identity.vetting_link_settings")}
           </HashLink>
         </div> 
-      )
+      ) /* without verified phone number, description text will help the user to confirm phone number and the text "setting" is linked to the setting page phone number section */
     } else if(this.props.notVerifiedNumber) {
       description = (
         <div className="link">
@@ -34,21 +36,21 @@ class LookupMobileProofing extends Component {
             {this.props.translate("verify-identity.vetting_link_settings")}
           </HashLink>
         </div> 
-      ); 
+      ); /* the verified phone number is not a Swedish number, description text show "only avaiable with Swedish number" */
     } else if(this.props.nonSweNumber) {
       description = (
         <div className="description">
           {this.props.translate("verify-identity.vetting_explanation_only_available_swe_number")}
         </div> 
-      ); 
+      ); /* if user verified swedish number, description text will be empty */
     } else {
       <div /> 
     }
 
     return (
       <div key="0">
-        <div key="0" className="vetting-button">
-          <button
+        <div key="0" className="vetting-button"> 
+          <button 
             disabled={this.props.withoutNin || this.props.withoutPhoneNumber|| this.props.notVerifiedNumber || this.props.nonSweNumber}
             onClick={this.props.handleShowModal}
           >
@@ -60,7 +62,8 @@ class LookupMobileProofing extends Component {
               {this.props.translate("lmp.button_text_request")}
             </div>
           </button>
-        </div>
+        </div> 
+        {/* notificationModal will only opens when user are able to verify identity by phone */}
         <NotificationModal
           modalId="mobileGenericConfirmDialog"
           title={this.props.translate("lmp.modal_confirm_title")}
