ea8345b705bf6ed0e0719f5af8204f3d102a97f4
Nathalie Jonsson <njons@sunet.se>
2021-01-18 16:07:17 +0100

    adds NameForm values to CreateGroup and a handler function to pass the data on to the create group endpoint

--- src/login/components/GroupManagement/Groups/CreateGroupContainer.js
+++ src/login/components/GroupManagement/Groups/CreateGroupContainer.js
@@ -4,21 +4,19 @@ import * as createGroupActions from "../../../redux/actions/createGroupActions";
 import i18n from "../../../translation/InjectIntl_HOC_factory";
 
 const mapStateToProps = (state) => {
-  // console.log("this is state in createGroupContainer:", state);
-  // let groupNameValues = { groupName: "" };
-  // if (state.form.groupName !== undefined) {
-  //   groupNameValues = state.form.groupName.values;
-  // }
+  let groupNameValues = { groupName: "" };
+  if (state.form.groupName !== undefined) {
+    groupNameValues = state.form.groupName.values;
+  }
   return {
-    // values: groupNameValues,
+    values: groupNameValues,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleCreateGroup: () => {
-      console.log("you're in handleCreateGroup ");
-      // dispatch(createGroupActions.createGroup("Test"));
+    handleCreateGroup: (groupName) => {
+      dispatch(createGroupActions.createGroup(groupName));
     },
   };
 };
