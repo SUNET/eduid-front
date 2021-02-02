e0b411ce75c596dfb81276c8adb55224a2882087
Nathalie Jonsson <njons@sunet.se>
2021-02-02 11:45:09 +0100

    removes delete button from InvitesListItem and adds placeholder markers for both owner and member

--- src/login/components/GroupManagement/Invites/InviteListItem.js
+++ src/login/components/GroupManagement/Invites/InviteListItem.js
@@ -1,37 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import i18n from "../../../translation/InjectIntl_HOC_factory";
-import ButtonRemoveData from "../../Buttons/ButtonRemoveData";
 
-const InviteListItem = (props) => {
-  let { membersList } = props;
-  return (
-    <Fragment>
-      {membersList.map((list) =>
-        list.map((member, i) => (
-          <li key={i}>
-            <div className="list-grid">
-              <div className="hide-overflow list-cell left-align">
-                <p>{member.email_address}</p>
+class InviteListItem extends Component {
+  render() {
+    let { membersList } = this.props;
+    return (
+      <Fragment>
+        {membersList.map((list) =>
+          list.map((member, i) => (
+            <li key={i}>
+              <div className="list-grid">
+                <div className="hide-overflow list-cell left-align">
+                  <p>{member.email_address}</p>
+                </div>
+                <div className="list-cell">X</div>
+                <div className="list-cell">X</div>
               </div>
-              <div className="list-cell">
-                <p></p>
-              </div>
-              <div className="list-cell">
-                <p>X</p>
-              </div>
-              <div className="list-cell">
-                <ButtonRemoveData
-                  className="icon-button"
-                  onClick={props.handleRemoveInvite}
-                />
-              </div>
-            </div>
-          </li>
-        ))
-      )}
-    </Fragment>
-  );
-};
+            </li>
+          ))
+        )}
+      </Fragment>
+    );
+  }
+}
 
 InviteListItem.propTypes = {};
 
