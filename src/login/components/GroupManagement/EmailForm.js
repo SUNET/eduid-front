8cb63bb97beabe6d4afb52332ae21ee6fc205cc2
Nathalie Jonsson <njons@sunet.se>
2021-02-02 09:30:24 +0100

    updates EmailForm to enable importing only input to new combined createInviteForm

--- src/login/components/GroupManagement/EmailForm.js
+++ src/login/components/GroupManagement/EmailForm.js
@@ -8,42 +8,51 @@ import EduIDButton from "../../../components/EduIDButton";
 import { validate } from "../../app_utils/validation/validateEmail";
 import i18n from "../../translation/InjectIntl_HOC_factory";
 
-let EmailForm = (props) => {
-  const { handleSubmit, invalid, translate} = props;
+const RenderSubmitButton = ({ invalid, translate }) => (
+  <EduIDButton
+    id="email-button"
+    type="submit"
+    className="settings-button"
+    disabled={invalid}
+  >
+    {translate("emails.button_add")}
+  </EduIDButton>
+);
+
+export let RenderEmailInput = ({ translate, required }) => (
+  <fieldset id="emails-form" className="tabpane">
+    <Field
+      required={required}
+      label={translate("profile.email_display_title")}
+      component={CustomInput}
+      componentClass="input"
+      type="text"
+      name="email"
+      placeholder="example@example.com"
+      helpBlock={translate("emails.input_help_text")}
+    />
+  </fieldset>
+);
+
+let EmailForm = ({ submitButton, onSubmit }) => {
+  // const { submitButton, onSubmit } = props;
   return (
-    <Form id="emailsview-form" role="form" onSubmit={handleSubmit}>
-      <fieldset id="emails-form" className="tabpane">
-        <Field
-          label={translate("profile.email_display_title")}
-          component={CustomInput}
-          componentClass="input"
-          type="text"
-          name="email"
-          placeholder="example@example.com"
-          helpBlock={translate("emails.input_help_text")}
-        />
-      </fieldset>
-      <EduIDButton
-        id="email-button"
-        type="submit"
-        className="settings-button"
-        disabled={invalid}
-      >
-        {translate("emails.button_add")}
-      </EduIDButton>
+    <Form id="emailsview-form" role="form" onSubmit={onSubmit}>
+      <RenderEmailInput {...props} />
+      {submitButton && <RenderSubmitButton {...props} />}
     </Form>
   );
 };
 
 EmailForm = reduxForm({
-  form: "emails",
+  form: "email",
   validate,
 })(EmailForm);
 
 EmailForm = connect((state) => ({
   initialValues: { email: state.emails.email },
   enableReinitialize: true,
-  // destroyOnUnmount: false,
+  touchOnChange: true,
 }))(EmailForm);
 
 EmailForm.propTypes = {
