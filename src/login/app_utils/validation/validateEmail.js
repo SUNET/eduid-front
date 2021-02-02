a5f363dfe6d94f7b5ccc5761dd575b7d7b8abd49
Nathalie Jonsson <njons@sunet.se>
2021-02-02 09:40:13 +0100

    adds error handling to email validation

--- src/login/app_utils/validation/validateEmail.js
+++ src/login/app_utils/validation/validateEmail.js
@@ -1,11 +1,13 @@
 export const validate = (values) => {
-  const errors = {},
-    email = values.email,
-    pattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  if (!email) {
-    errors.email = "required";
-  } else if (!pattern.test(email)) {
-    errors.email = "email.invalid_email";
+  const errors = {};
+  if (values !== undefined) {
+    let email = values.email;
+    const pattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!email) {
+      errors.email = "required";
+    } else if (!pattern.test(email)) {
+      errors.email = "email.invalid_email";
+    }
   }
   return errors;
 };
