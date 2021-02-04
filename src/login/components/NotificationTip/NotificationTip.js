756975df7d743790c21d0f17e2d0f9e846b092fe
Eunju Huss <eunju@sunet.se>
2021-02-04 15:55:55 +0100

    Fix indentation

--- src/login/components/NotificationTip/NotificationTip.js
+++ src/login/components/NotificationTip/NotificationTip.js
@@ -1,47 +1,46 @@
 
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
 
 const SpeechBubbleTip = (props) => {
-    let timeout;
-    const [active, setActive] = useState(false);
-  
-    const showTip = () => {
-      timeout = setTimeout(() => {
-        setActive(true);
-      }, props.delay || 100);
-    };
-  
-    const hideTip = () => {
-      clearInterval(timeout);
-      setActive(false);
-    };
-  
-    return (
-      <div
-        className={props.nins !== undefined  && props.nins[0] ? "SpeechBubbleTip-Wrapper" :  "SpeechBubbleTip-Wrapper hide"}
-        onMouseEnter={showTip}
-        onMouseLeave={hideTip}
-      >
-        {props.children}
-        {active && (
-          <div className={`SpeechBubbleTip ${props.direction || "top"}`}>
-            {props.content}
-          </div>
-        )}
-      </div>
-    );
+  let timeout;
+  const [active, setActive] = useState(false);
+
+  const showTip = () => {
+    timeout = setTimeout(() => {
+      setActive(true);
+    }, props.delay || 100);
   };
 
+  const hideTip = () => {
+    clearInterval(timeout);
+    setActive(false);
+  };
 
-function NotificationTip() {
-  const nins = useSelector(state => state.nins.nins)
-    return (
-        <SpeechBubbleTip nins={nins} direction="top">
-            <div className="notification-dot">
-                <div className="notification-dot-inner" /> 
-            </div>
-        </SpeechBubbleTip>
-        )}
+  return (
+    <div
+      className="SpeechBubbleTip-Wrapper"
+      onMouseEnter={showTip}
+      onMouseLeave={hideTip}
+    >
+      {props.children}
+      {active && (
+        <div className={`SpeechBubbleTip ${props.direction || "top"}`}>
+          {props.content}
+        </div>
+      )}
+    </div>
+  );
+};
+
+
+function NotificationTip(props) {
+  return (
+    <SpeechBubbleTip content={props.content} direction="top">
+        <div className="notification-dot">
+            <div className="notification-dot-inner" /> 
+        </div>
+    </SpeechBubbleTip>
+    )
+  }
 
 export default NotificationTip;
