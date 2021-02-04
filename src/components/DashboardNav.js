05beaf626b2fe34b73efa689976aab62749421fc
Eunju Huss <eunju@sunet.se>
2021-02-04 15:10:20 +0100

    Change class component to function

--- src/components/DashboardNav.js
+++ src/components/DashboardNav.js
@@ -1,42 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink, withRouter } from "react-router-dom";
 import NotificationTip from "../login/components/NotificationTip/NotificationTip";
 import i18n from "../login/translation/InjectIntl_HOC_factory";
 
-class DashboardNav extends Component {
-  render() {
-    return (
-      <nav id="dashboard-nav">
-        <ul>
-          <NavLink exact activeClassName="active" to={`/profile/`}>
-            <li>{this.props.translate("dashboard_nav.profile")}</li>
-          </NavLink>
-          <NavLink
-            exact
-            activeClassName="active"
-            to={`/profile/verify-identity/`}
-          >
-            <li>
-              {this.props.translate("dashboard_nav.identity")}
-              <NotificationTip />
-            </li>
-          </NavLink>
-          <NavLink activeClassName="active" to={`/profile/settings/`}>
-            <li>
-              {this.props.translate("dashboard_nav.settings")}
-              <NotificationTip />
-            </li>
-          </NavLink>
-          <NavLink
-            activeClassName="active"
-            to={`/profile/settings/advanced-settings`}
-          >
-            <li>{this.props.translate("dashboard_nav.advanced-settings")}</li>
-          </NavLink>
-        </ul>
-      </nav>
-    );
-  }
+function DashboardNav(props) {
+  return (
+    <nav id="dashboard-nav">
+      <ul>
+        <NavLink exact activeClassName="active" to={`/profile/`}>
+          <li>{props.translate("dashboard_nav.profile")}</li>
+        </NavLink>
+        <NavLink
+          exact
+          activeClassName="active"
+          to={`/profile/verify-identity/`}
+        >
+          <li>
+            {props.translate("dashboard_nav.identity")}
+            <NotificationTip />
+          </li>
+        </NavLink>
+        <NavLink activeClassName="active" to={`/profile/settings/`}>
+          <li>
+            {props.translate("dashboard_nav.settings")}
+            <NotificationTip />
+          </li>
+        </NavLink>
+        <NavLink
+          activeClassName="active"
+          to={`/profile/settings/advanced-settings`}
+        >
+          <li>{props.translate("dashboard_nav.advanced-settings")}</li>
+        </NavLink>
+      </ul>
+    </nav>
+  );
 }
 
 export default i18n(withRouter(DashboardNav));
