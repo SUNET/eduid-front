791b5d3f91959f35d9c377951d8ab99c8ae0d87a
Eunju Huss <eunju@sunet.se>
2021-02-04 12:23:00 +0100

    Add a functional compoenent for notificationTip

---
+++ src/login/components/NotificationTip/NotificationTip.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+function NotificationTip(props) {
+    return (
+        <div className="notification-dot">
+            <div className="notification-dot-inner" /> 
+        </div>
+        )}
+
+export default NotificationTip;
