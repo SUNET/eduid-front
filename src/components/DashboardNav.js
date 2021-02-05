869239234be8b67c556a98e81bb10305dee18fd3
Eunju Huss <eunju@sunet.se>
2021-02-05 09:04:57 +0100

    Add useEffect for dispach getNins

--- src/components/DashboardNav.js
+++ src/components/DashboardNav.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink, withRouter } from "react-router-dom";
 import NotificationTip from "../login/components/NotificationTip/NotificationTip";
 import i18n from "../login/translation/InjectIntl_HOC_factory";
+import * as actions from "../actions/Nins";
 
 function DashboardNav(props) {
+  const nins = useSelector(state => state.nins.nins);
+  const phones =  useSelector(state => state.phones.phones);
+
+  const dispatch = useDispatch()
+
+  useEffect(() => { 
+    dispatch(actions.getNins())
+    console.log("[dispatch]", props)
+ }, [dispatch]);
+  let tipsAtIdentity = "";
+  console.log('nins',nins)
+
+  if(nins !== undefined  && nins[0]){
+    tipsAtIdentity = <NotificationTip content={"you can verify by post"}/>
+    }else if(phones !== undefined  && phones[0]){
+      tipsAtIdentity =  <NotificationTip content={"you can verify by post, by phone"}/>
+    }else if(nins === undefined  && !nins[0]){
+      tipsAtIdentity =  <div/>}else null;
+
+  
   return (
     <nav id="dashboard-nav">
       <ul>
@@ -17,13 +39,12 @@ function DashboardNav(props) {
         >
           <li>
             {props.translate("dashboard_nav.identity")}
-            <NotificationTip />
+            {tipsAtIdentity}
           </li>
         </NavLink>
         <NavLink activeClassName="active" to={`/profile/settings/`}>
           <li>
             {props.translate("dashboard_nav.settings")}
-            <NotificationTip />
           </li>
         </NavLink>
         <NavLink
