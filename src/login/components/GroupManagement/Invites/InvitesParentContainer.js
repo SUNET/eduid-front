54a18ccaa4523b2c160962d3965d9f2ef066da2b
Nathalie Jonsson <njons@sunet.se>
2021-02-11 15:36:02 +0100

    removes any functionality that handles members or owners and leaves functionality that passes an array of the groupIds with invites to its component

--- src/login/components/GroupManagement/Invites/InvitesParentContainer.js
+++ src/login/components/GroupManagement/Invites/InvitesParentContainer.js
@@ -4,23 +4,14 @@ import * as getAllOutgoingActions from "../../../redux/actions/getOutgoingInvite
 import InvitesParent from "./InvitesParent";
 
 const mapStateToProps = (state) => {
-  let invitesArray = [];
   let groupIdsArray = [];
   if (state.invites.invitesFromMe !== undefined) {
     groupIdsArray = state.invites.invitesFromMe.map(
       (group) => group.group_identifier
     );
-    invitesArray = state.invites.invitesFromMe.map((group) => {
-      return group.member_invites.concat(group.owner_invites);
-    });
   }
-  // create new object with group_identifier (value) and memberListArray (key)
-  let groupsWithInvites = Object.assign(
-    groupIdsArray.map((groupId, i) => ({ [groupId]: invitesArray[i] }))
-  );
-
   return {
-    groupsWithInvites: groupsWithInvites,
+    groupsWithInvites: groupIdsArray,
   };
 };
 
