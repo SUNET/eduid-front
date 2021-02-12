8aa351c840d15ef906f939613ccffbddc0b99050
Nathalie Jonsson <njons@sunet.se>
2021-02-12 16:24:30 +0100

    updates test to run in karma

--- src/tests/helperFunctions/invitesByRole-test.js
+++ src/tests/helperFunctions/invitesByRole-test.js
@@ -1,4 +1,4 @@
-import expect from "jest";
+import expect from "expect";
 import invitesByRole from "../../login/app_utils/helperFunctions/invitesByRole";
 
 let noInvites = [];
@@ -38,49 +38,53 @@ let ownerInvites = [
   "both2@email.com",
 ];
 
-test("invitesByRole returns an array", () => {
-  let invitesFromMeByRole = invitesByRole(invites);
-  expect(Array.isArray(invitesFromMeByRole)).toEqual(true);
+describe("invitesByRole returns an array", () => {
+  it("with input", () => {
+    let invitesFromMeByRole = invitesByRole(invites);
+    expect(Array.isArray(invitesFromMeByRole)).toEqual(true);
+  });
+
+  it("with empty input", () => {
+    let invitesFromMeByRole = invitesByRole(noInvites);
+    expect(Array.isArray(invitesFromMeByRole)).toEqual(true);
+  });
 });
 
-test("invitesByRole returns an array, even with empty input", () => {
-  let invitesFromMeByRole = invitesByRole(noInvites);
-  expect(Array.isArray(invitesFromMeByRole)).toEqual(true);
-});
+describe("invitesByRole returns an array", () => {
+  it("with only unique email addresses", () => {
+    let memberInvitesLength = invites[0].member_invites.length;
+    let ownerInvitesLength = invites[0].owner_invites.length;
+    let invitesFromMeByRole = invitesByRole(invites);
+    expect(invitesFromMeByRole.length).toBe(8);
+    expect(invitesFromMeByRole.length).not.toBe(
+      memberInvitesLength + ownerInvitesLength
+    );
+  });
 
-test("invitesByRole returns an array with only unique email addresses", () => {
-  let memberInvitesLength = invites[0].member_invites.length;
-  let ownerInvitesLength = invites[0].owner_invites.length;
-  let invitesFromMeByRole = invitesByRole(invites);
-  expect(invitesFromMeByRole.length).toBe(8);
-  expect(invitesFromMeByRole.length).not.toBe(
-    memberInvitesLength + ownerInvitesLength
-  );
-});
+  it("sorted in alpabetical order", () => {
+    let unsortedInvites = memberInvites.concat(ownerInvites);
+    let sortedInvites = memberInvites.concat(ownerInvites).sort();
+    expect(unsortedInvites).not.toEqual(sortedInvites);
 
-test("invitesByRole returns an array sorted in alpabetical order", () => {
-  let unsortedInvites = memberInvites.concat(ownerInvites);
-  let sortedInvites = memberInvites.concat(ownerInvites).sort();
-  expect(unsortedInvites).not.toEqual(sortedInvites);
+    let firstUnsortedEmail = unsortedInvites[0];
+    let firstSortedEmail = sortedInvites[0];
+    let invitesFromMeByRole = invitesByRole(invites);
+    expect(invitesFromMeByRole[0].email).toEqual(firstSortedEmail);
+    expect(invitesFromMeByRole[0].email).not.toEqual(firstUnsortedEmail);
+  });
 
-  let firstUnsortedEmail = unsortedInvites[0];
-  let firstSortedEmail = sortedInvites[0];
-  let invitesFromMeByRole = invitesByRole(invites);
-  expect(invitesFromMeByRole[0].email).toEqual(firstSortedEmail);
-  expect(invitesFromMeByRole[0].email).not.toEqual(firstUnsortedEmail);
-});
+it("pairing invite emails with its role/s", () => {
+    let invitesFromMeByRole = invitesByRole(invites);
+    let both1Email = invitesFromMeByRole[0];
+    expect(both1Email.member).toEqual(true);
+    expect(both1Email.owner).toEqual(true);
 
-test("invitesByRole returns an array pairing invite emails with its role/s", () => {
-  let invitesFromMeByRole = invitesByRole(invites);
-  let both1Email = invitesFromMeByRole[0];
-  expect(both1Email.member).toEqual(true);
-  expect(both1Email.owner).toEqual(true);
+    let member1Email = invitesFromMeByRole[3];
+    expect(member1Email.member).toEqual(true);
+    expect(member1Email.owner).toEqual(false);
 
-  let member1Email = invitesFromMeByRole[3];
-  expect(member1Email.member).toEqual(true);
-  expect(member1Email.owner).toEqual(false);
-
-  let owner1Email = invitesFromMeByRole[6];
-  expect(owner1Email.member).toEqual(false);
-  expect(owner1Email.owner).toEqual(true);
+    let owner1Email = invitesFromMeByRole[6];
+    expect(owner1Email.member).toEqual(false);
+    expect(owner1Email.owner).toEqual(true);
+  });
 });
