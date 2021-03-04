39c578f45777fc1ed56ea65433aae97cd4d03ac6
Nathalie Jonsson <njons@sunet.se>
2021-03-04 11:22:14 +0100

    updates InvitesParent with render logic based on the navId

--- src/login/components/GroupManagement/Invites/InvitesParent.js
+++ src/login/components/GroupManagement/Invites/InvitesParent.js
@@ -2,16 +2,30 @@ import React, { Component } from "react";
 import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
 import CreateInvite from "./CreateInviteContainer";
 import InvitesList from "./InvitesList";
+import EditInvite from "../Invites/EditInvite";
 
-const InvitesParent = ({ groupsWithInvites, allInvitesFromMe, group }) => {
+const InvitesParent = ({
+  groupsWithInvites,
+  allInvitesFromMe,
+  group,
+  navId,
+}) => {
   let { identifier } = group;
   let groupHasInvites = groupsWithInvites.includes(identifier);
   return (
     <div className="invites">
-      <CreateInvite groupId={identifier} />
-      {groupHasInvites ? (
-        <InvitesList groupId={identifier} allInvitesFromMe={allInvitesFromMe} />
-      ) : null}
+      {navId === "edit-invite" ? (
+        <EditInvite />
+      ) : (
+        <CreateInvite groupId={identifier} />
+      )}
+      {groupHasInvites && (
+        <InvitesList
+          navId={navId}
+          groupId={identifier}
+          allInvitesFromMe={allInvitesFromMe}
+        />
+      )}
     </div>
   );
 };
