e8e10de972872df6c61557cf86ca348c1bbc023d
Nathalie Jonsson <njons@sunet.se>
2021-03-04 15:22:10 +0100

    adds rendering of EditRoleForm and hardcoded initial values

---
+++ src/login/components/GroupManagement/Invites/EditRolesForm.js
@@ -0,0 +1,64 @@
+import React, { Fragment, useEffect } from "react";
+import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+import Form from "reactstrap/lib/Form";
+import EduIDButton from "../../../../components/EduIDButton";
+import { RenderCheckboxInput } from "../InviteRoleCheckboxes";
+import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
+
+const validateEditInvite = (values) => {
+  // console.log("these are values in validateEditInvite", values);
+  let errors = {};
+  return errors;
+};
+
+let EditRolesForm = (props) => {
+  const { handleSubmit, pristine, checkboxNames } = props;
+
+  return (
+    <Form id={"edit-invite-form"} role="form" onSubmit={handleSubmit}>
+      {checkboxNames.map(({ name, label }, i) => {
+        return (
+          <div key={i} className="list-cell">
+            <Field
+              type="checkbox"
+              label={label}
+              id={name}
+              component={RenderCheckboxInput}
+              name={name}
+            />
+          </div>
+        );
+      })}
+    </Form>
+  );
+};
+
+EditRolesForm = reduxForm({
+  form: "editInviteRole",
+  validate: validateEditInvite,
+  enableReinitialize: true,
+  keepDirtyOnReinitialize: true,
+})(EditRolesForm);
+
+EditRolesForm = connect(() => ({
+  initialValues: {
+    "hardcodedtest@email-member": false,
+    "hardcodedtest@email-owner": true,
+    "hardcoded1test@email-member": true,
+    "hardcoded1test@email-owner": true,
+    "hardcoded2test@email-member": false,
+    "hardcoded2test@email-owner": true,
+    "hardcoded3test@email-member": true,
+    "hardcoded3test@email-owner": true,
+    "hardcoded4test@email-member": true,
+    "hardcoded4test@email-owner": false,
+    "hardcoded5test@email-member": false,
+    "hardcoded5test@email-owner": true,
+  },
+  destroyOnUnmount: false,
+}))(EditRolesForm);
+
+// CreateInviteForm.propTypes = {};
+
+export default InjectIntl(EditRolesForm);
