e43ea95b02943c310eff6f369a49d2f3d25595c4
Nathalie Jonsson <njons@sunet.se>
2021-03-04 11:30:04 +0100

    deletes EditInvite as separate component and moves it into its parent becasue it is only renedered text

--- src/login/components/GroupManagement/Invites/InvitesParent.js
+++ src/login/components/GroupManagement/Invites/InvitesParent.js
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
 import CreateInvite from "./CreateInviteContainer";
 import InvitesList from "./InvitesList";
-import EditInvite from "../Invites/EditInvite";
+
+const RenderEditInviteText = () => (
+  <div className="edit-invite">
+    <h3>Manage memberships in your group</h3>
+    <p>
+      As members, users belong to the group and as owners, they can also edit
+      the group and who belongs to it. Users can also be removed from the group.
+    </p>
+  </div>
+);
 
 const InvitesParent = ({
   groupsWithInvites,
@@ -15,7 +24,7 @@ const InvitesParent = ({
   return (
     <div className="invites">
       {navId === "edit-invite" ? (
-        <EditInvite />
+        <RenderEditInviteText />
       ) : (
         <CreateInvite groupId={identifier} />
       )}
