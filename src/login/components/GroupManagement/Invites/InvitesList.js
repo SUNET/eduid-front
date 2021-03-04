b292bc4300c5e348769defe8883ffe4ed0d5fdfa
Nathalie Jonsson <njons@sunet.se>
2021-03-04 12:02:42 +0100

    splits out RenderListItem into a separate function

--- src/login/components/GroupManagement/Invites/InvitesList.js
+++ src/login/components/GroupManagement/Invites/InvitesList.js
@@ -19,22 +19,39 @@ const RenderListHeading = ({ columnNumber }) => {
   );
 };
 
+const RenderListItems = ({ invitesForGroup, navId, columnNumber }) => {
+  let invitesFromMeByRole = invitesByRole(invitesForGroup);
+  return (
+    <div className="list-data invites">
+      <ul>
+        {invitesFromMeByRole.map((invite, i) => (
+          <InviteListItem
+            key={i}
+            invite={invite}
+            navId={navId}
+            columnNumber={columnNumber}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 const InvitesList = ({ groupId, allInvitesFromMe, navId }) => {
   let invitesForGroup = allInvitesFromMe.filter(
     (invite) => invite.group_identifier === groupId
   );
-  let invitesFromMeByRole = invitesByRole(invitesForGroup);
   let columnNumber = navId === "edit-invite" ? "four-columns" : "three-columns";
   return (
     <div className="invites-list">
       <h3>Sent invites</h3>
       <div className="list-data invites">
         <RenderListHeading columnNumber={columnNumber} />
-        <ul>
-          {invitesFromMeByRole.map((invite, i) => (
-            <InviteListItem key={i} invite={invite} />
-          ))}
-        </ul>
+        <RenderListItems
+          invitesForGroup={invitesForGroup}
+          navId={navId}
+          columnNumber={columnNumber}
+        />
       </div>
     </div>
   );
