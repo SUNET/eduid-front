1714abfcea2b36c3732712290936c0abf1a72c94
Nathalie Jonsson <njons@sunet.se>
2021-03-04 10:39:29 +0100

    clean up and refactor of components that previously requested data after removal of componentDidMount and individual handler functions and actions

--- src/login/components/GroupManagement/GroupManagement.js
+++ src/login/components/GroupManagement/GroupManagement.js
@@ -3,17 +3,15 @@ import InjectIntl from "../../translation/InjectIntl_HOC_factory";
 import CreateGroup from "./Groups/CreateGroupContainer";
 import GroupsParent from "./Groups/GroupsParent";
 
-const RenderCreateGroupButton = (props) => {
-  return (
-    <Fragment>
-      {!props.createGroup && (
-        <button className="create-group" onClick={props.handleOpenCreateGroup}>
-          create group
-        </button>
-      )}
-    </Fragment>
-  );
-};
+const RenderCreateGroupButton = ({ createGroup, handleOpenCreateGroup }) => (
+  <Fragment>
+    {!createGroup && (
+      <button className="create-group" onClick={handleOpenCreateGroup}>
+        create group
+      </button>
+    )}
+  </Fragment>
+);
 
 const RenderCreateGroupOrGroupData = (props) => {
   if (props.loading) return <p>Loading...</p>;
@@ -21,29 +19,21 @@ const RenderCreateGroupOrGroupData = (props) => {
   if (!props.hasNoGroups) return <GroupsParent {...props} />;
 };
 
-class GroupManagement extends Component {
-  componentDidMount() {
-    this.props.handleGetAllData();
-  }
-
-  render() {
-    return (
-      <article>
-        <div className="intro">
-          <div className="heading">
-            <h4>Groups</h4>
-            <RenderCreateGroupButton {...this.props} />
-          </div>
-          <p>
-            Create groups with other eduID users. What the groups are used for
-            is up to you and the local services your university provides.
-          </p>
-          <RenderCreateGroupOrGroupData {...this.props} />
-        </div>
-      </article>
-    );
-  }
-}
+const GroupManagement = (props) => (
+  <article>
+    <div className="intro">
+      <div className="heading">
+        <h4>Groups</h4>
+        <RenderCreateGroupButton {...props} />
+      </div>
+      <p>
+        Create groups with other eduID users. What the groups are used for is up
+        to you and the local services your university provides.
+      </p>
+      <RenderCreateGroupOrGroupData {...props} />
+    </div>
+  </article>
+);
 
 // GroupManagement.propTypes = {};
 
