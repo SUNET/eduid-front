1f56f453ec8cc5a23112d73d5de27c3ad81af033
Nathalie Jonsson <njons@sunet.se>
2021-03-04 11:40:01 +0100

    splits out RenderListHeading into its own function

--- src/login/components/GroupManagement/Invites/InvitesList.js
+++ src/login/components/GroupManagement/Invites/InvitesList.js
@@ -3,27 +3,32 @@ import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
 import InviteListItem from "./InviteListItem";
 import invitesByRole from "../../../app_utils/helperFunctions/invitesByRole";
 
-const InvitesList = ({ groupId, allInvitesFromMe }) => {
+const RenderListHeading = () => {
+  return (
+    <div className="list-grid" id="three-columns">
+      <div className="list-cell left-align">
+        <label>Invites</label>
+      </div>
+      <div className="list-cell">
+        <label>Owner</label>
+      </div>
+      <div className="list-cell">
+        <label>Member</label>
+      </div>
+    </div>
+  );
+};
+
+const InvitesList = ({ groupId, allInvitesFromMe, navId }) => {
   let invitesForGroup = allInvitesFromMe.filter(
     (invite) => invite.group_identifier === groupId
   );
   let invitesFromMeByRole = invitesByRole(invitesForGroup);
-
   return (
     <div className="invites-list">
       <h3>Sent invites</h3>
       <div className="list-data invites">
-        <div className="list-grid" id="three-columns">
-          <div className="list-cell left-align">
-            <label>Invites</label>
-          </div>
-          <div className="list-cell">
-            <label>Owner</label>
-          </div>
-          <div className="list-cell">
-            <label>Member</label>
-          </div>
-        </div>
+        <RenderListHeading />
         <ul>
           {invitesFromMeByRole.map((invite, i) => (
             <InviteListItem key={i} invite={invite} />
@@ -33,7 +38,6 @@ const InvitesList = ({ groupId, allInvitesFromMe }) => {
     </div>
   );
 };
-// }
 
 // InvitesList.propTypes = {};
 
