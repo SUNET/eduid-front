e86353284e3a927ca5e55b3e5694f784d13b9d10
Nathalie Jonsson <njons@sunet.se>
2021-03-12 10:27:20 +0100

    passes on disabled prop

--- src/login/components/GroupManagement/Invites/EditRolesForm.js
+++ src/login/components/GroupManagement/Invites/EditRolesForm.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { reduxForm, Field } from "redux-form";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import Form from "reactstrap/lib/Form";
 import { RenderCheckboxInput } from "../InviteRoleCheckboxes";
+import validateRoleCheckboxes from "../../../app_utils/validation/validateRoleCheckboxes";
 import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
 
-const validateEditInvite = () => {
+export const validateEditInvite = (values, props) => {
   let errors = {};
+  //console.log("values ", values);
   return errors;
 };
 
+export const disableUnchnagedInvites = (invitesFromMeByRole, updatedInvite) => {
+  return invitesFromMeByRole.map((invite) =>
+    Object.assign({
+      ...invite,
+      disabled: invite.email.startsWith(updatedInvite.email) ? false : true,
+    })
+  );
+};
+
 let EditRolesForm = (props) => {
-  const { handleSubmit, checkboxNames } = props;
+  const { handleSubmit, checkboxNames, disabled } = props;
   return (
-    <Form id={"edit-invite-form"} role="form" onSubmit={handleSubmit}>
+    <Form id={"edit-invite-form"} role="form">
       {checkboxNames.map(({ name, label }, i) => {
         return (
           <div key={i} className="list-cell">
@@ -21,6 +32,7 @@ let EditRolesForm = (props) => {
               type="checkbox"
               label={label}
               id={name}
+              disabled={disabled}
               component={RenderCheckboxInput}
               name={name}
             />
@@ -33,12 +45,14 @@ let EditRolesForm = (props) => {
 
 EditRolesForm = reduxForm({
   form: "editInviteRole",
-  validate: validateEditInvite,
   enableReinitialize: true,
   keepDirtyOnReinitialize: true,
+  touchOnChange: true,
+  //   validate: validateEditInvite,
 })(EditRolesForm);
 
-EditRolesForm = connect(() => ({
+EditRolesForm = connect((state) => ({
+  state,
   destroyOnUnmount: false,
 }))(EditRolesForm);
 
