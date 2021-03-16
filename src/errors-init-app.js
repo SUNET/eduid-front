a817fb7a6356ba0bf5d8b32eaec2f0ef4d2096f2
Eunju Huss <eunju@sunet.se>
2021-03-16 13:44:37 +0100

    Add initState for dispatching updateAvailableLanguage

--- src/errors-init-app.js
+++ src/errors-init-app.js
@@ -1,25 +1,18 @@
-/*
- * This is meant to be imported in the different entry points
- * to initialize the different components that the entry points
- * may want to render in different parts of the app.
- * Initialization involves localizing the app and providing
- * it with the redux store.
- */
-
 import React from "react";
 import ReactDOM from "react-dom";
 
 import { routerMiddleware } from "react-router-redux";
 
 import createSagaMiddleware from "redux-saga";
-import rootSaga from "./signup-root-saga";
+import rootSaga from "./errors-root-saga";
 import { createLogger } from "redux-logger";
 import { Provider } from "react-intl-redux";
 import { updateIntl } from "react-intl-redux";
 import { createStore, applyMiddleware, compose } from "redux";
-import eduIDApp from "./signup-store";
+import eduIDApp from "./errors-store";
 import notifyAndDispatch from "./notify-middleware";
 import { history } from "components/ErrorsMain";
+import { updateAvailableLanguage } from "actions/ErrorsMain";
 
 /* for redux dev tools */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -41,14 +34,21 @@ export const store = createStore(
 
 sagaMiddleware.run(rootSaga);
 
+const initState = function() {
+  store.dispatch(updateAvailableLanguage());
+};
+
 /* render app */
 const init_app = function(target, component) {
+  let action;
+  action = initState;
   const language = navigator.languages
     ? navigator.languages[0]
     : navigator.language || navigator.userLanguage;
   const supported = AVAILABLE_LANGUAGES.map(lang => lang[0]);
   if (supported.includes(language)) {
     const lang_code = language.substring(0, 2);
+    console.log("lang_code", lang_code)
     store.dispatch(
       updateIntl({
         locale: lang_code,
@@ -57,7 +57,7 @@ const init_app = function(target, component) {
     );
   }
   const app = <Provider store={store}>{component}</Provider>;
-  ReactDOM.render(app, target);
+  ReactDOM.render(app, target, action);
 };
 
 export default init_app;
