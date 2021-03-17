27b1a5b8dddda4a1091bf7238ec6600fdb124faa
Nathalie Jonsson <njons@sunet.se>
2021-03-17 11:35:22 +0100

    cleanup of EditRolesForm

--- src/login/components/GroupManagement/Invites/EditRolesForm.js
+++ src/login/components/GroupManagement/Invites/EditRolesForm.js
@@ -1,28 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { reduxForm, Field } from "redux-form";
-import { connect, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import Form from "reactstrap/lib/Form";
 import { RenderCheckboxInput } from "../InviteRoleCheckboxes";
-import validateRoleCheckboxes from "../../../app_utils/validation/validateRoleCheckboxes";
 import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
 
-export const validateEditInvite = (values, props) => {
-  let errors = {};
-  //console.log("values ", values);
-  return errors;
-};
-
-export const disableUnchnagedInvites = (invitesFromMeByRole, updatedInvite) => {
-  return invitesFromMeByRole.map((invite) =>
-    Object.assign({
-      ...invite,
-      disabled: invite.email.startsWith(updatedInvite.email) ? false : true,
-    })
-  );
-};
-
 let EditRolesForm = (props) => {
-  const { handleSubmit, checkboxNames, disabled } = props;
+  const { checkboxNames, disabled } = props;
   return (
     <Form id={"edit-invite-form"} role="form">
       {checkboxNames.map(({ name, label }, i) => {
@@ -48,11 +32,9 @@ EditRolesForm = reduxForm({
   enableReinitialize: true,
   keepDirtyOnReinitialize: true,
   touchOnChange: true,
-  //   validate: validateEditInvite,
 })(EditRolesForm);
 
-EditRolesForm = connect((state) => ({
-  state,
+EditRolesForm = connect(() => ({
   destroyOnUnmount: false,
 }))(EditRolesForm);
 
