07b12c788d6c2d437f0beb2b932fee1b4c24394d
Nathalie Jonsson <njons@sunet.se>
2021-03-17 13:23:00 +0100

    cleans up RenderListItems code and adds comments

--- src/login/components/GroupManagement/Invites/InvitesList.js
+++ src/login/components/GroupManagement/Invites/InvitesList.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { reduxForm } from "redux-form";
 import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
 import InviteListItem from "./InviteListItem";
+import createInvitesByRole from "../../../app_utils/helperFunctions/invitesByRole";
 import { createInitValues } from "../../../app_utils/helperFunctions/checkboxHelpers";
-import invitesByRole from "../../../app_utils/helperFunctions/invitesByRole";
+import disableInvitesNotInFocus from "../../../app_utils/helperFunctions/disableInvitesNotInFocus";
 
 const RenderListHeading = () => {
   const navId = useSelector((state) => state.groups.navId);
-  let columnNumber = navId === "edit-invite" ? "four-columns" : "three-columns";
+  let columnNumber = navId.includes("edit") ? "four-columns" : "three-columns";
   let headingText =
     columnNumber === "four-columns"
       ? ["Invites", "Member", "Owner", ""]
@@ -23,13 +25,28 @@ const RenderListHeading = () => {
   );
 };
 
-const RenderListItems = ({ invitesForGroup }) => {
-  const invitesFromMeByRole = invitesByRole(invitesForGroup);
-  const initialValues = createInitValues(invitesFromMeByRole);
+let RenderListItems = ({ invitesByRole, pristine, initialValues }) => {
+  // set disabled status on all invites that were not clicked
+  const updatedInvite = useSelector((state) => state.invites.updatedInvite);
+  const disabledInvitesByRole = disableInvitesNotInFocus(
+    invitesByRole,
+    updatedInvite
+  );
+  // set state if any checkbox in "edit-invite" has been clicked
+  const navId = useSelector((state) => state.groups.navId);
+  const [isClicked, setsClickedStatus] = useState(false);
+  useEffect(() => {
+    if (navId.includes("edit")) {
+      setsClickedStatus(true);
+    }
+  }, [updatedInvite]);
+  // toggle invites with or without disable status
+  let invitesArray =
+    isClicked && !pristine ? disabledInvitesByRole : invitesByRole;
   return (
     <div className="list-data invites">
       <ul>
-        {invitesFromMeByRole.map((invite, i) => (
+        {invitesArray.map((invite, i) => (
           <InviteListItem
             key={i}
             invite={invite}
@@ -41,16 +58,27 @@ const RenderListItems = ({ invitesForGroup }) => {
   );
 };
 
+RenderListItems = reduxForm({
+  form: "editInviteRole",
+  destroyOnUnmount: false,
+})(RenderListItems);
+
 const InvitesList = ({ groupId, allInvitesFromMe }) => {
   let invitesForGroup = allInvitesFromMe.filter(
     (invite) => invite.group_identifier === groupId
   );
+  // transform the data into the structures needed to render checkboxes
+  const invitesByRole = createInvitesByRole(invitesForGroup);
+  const initialValues = createInitValues(invitesByRole);
   return (
     <div className="invites-list">
       <h3>Sent invites</h3>
       <div className="list-data invites">
         <RenderListHeading />
-        <RenderListItems invitesForGroup={invitesForGroup} />
+        <RenderListItems
+          initialValues={initialValues}
+          invitesByRole={invitesByRole}
+        />
       </div>
     </div>
   );
