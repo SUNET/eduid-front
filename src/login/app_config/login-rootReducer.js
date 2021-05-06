8ba60030fc9c2e6a39c1b3cf5b3ad2c4449989b9
Eunju Huss <eunju@sunet.se>
2021-05-06 10:32:36 +0200

    Clean up

--- src/login/app_config/login-rootReducer.js
+++ src/login/app_config/login-rootReducer.js
@@ -4,31 +4,18 @@ import notificationsReducer from "../components/Notifications/Notifications_redu
 import { routerReducer } from "react-router-redux";
 import { reducer as formReducer } from "redux-form";
 import { intlReducer } from "react-intl-redux";
-
 import initReducer from "../app_init/init_reducer";
 import appReducer from "../components/App/App_reducer";
 import loginReducer from "../components/LoginApp/LoginForm/LoginForm_reducer";
-import getEmailLinkReducer from "../components/LoginApp/ResetPassword/GetEmailLink/GetEmailLink_reducer";
-// import emailLinkSentReducer from "../components/LoginApp/ResetPassword/EmailLinkSent/EmailLinkSent_reducer";
-// import resetPasswordReducer from "../components/LoginApp/ResetPassword/ResetPassword_reducer";
-
-
-// import initResetFormReducer from "login/InitResetForm/InitResetForm_reducer";
-// import resettingReducer from "login/Resetting/Resetting_reducer";
 
 const eduIDLoginApp = combineReducers({
   config: initReducer,
   app: appReducer,
   login: loginReducer,
-  getEmailLink: getEmailLinkReducer,
-  // emailLinkSent: emailLinkSentReducer,
   notifications: notificationsReducer,
   router: routerReducer,
-  // resetPassword: resetPasswordReducer,
   form: formReducer,
   intl: intlReducer
-  // reset: initResetFormReducer,
-  // resetting: resettingReducer,
 });
 
 export default eduIDLoginApp;
