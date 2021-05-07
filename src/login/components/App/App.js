3eb4fc30c4cde4d0d46b4ca1c57913b2a34b3dbc
Eunju Huss <eunju@sunet.se>
2021-05-07 13:09:24 +0200

    Remove arrary in return

--- src/login/components/App/App.js
+++ src/login/components/App/App.js
@@ -1,23 +1,20 @@
 import React, { Component, Fragment } from "react";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
-
 import Splash from "../Splash/Splash_container";
 import Header from "../Header/Header_container";
 import Notifications from "../Notifications/Notifications_container";
 import LoginApp from "../LoginApp/LoginApp";
 import Footer from "../Footer/Footer_container";
-
 import "../../styles/index.scss";
 
 export const history = createBrowserHistory();
-
 class App extends Component {
   render() {
-    return [
-      <Splash key="0" />,
-      <Fragment>
-        <Header />
+    return (
+      <>
+        <Splash />
+        <Header/>
         <section id="panel">
           <Notifications />
           <Router history={history}>
@@ -25,8 +22,8 @@ class App extends Component {
           </Router>
         </section>
         <Footer {...this.props} />
-      </Fragment>,
-    ];
+      </>
+    )
   }
 }
 
