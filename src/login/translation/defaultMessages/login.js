964ddbb7d7832b95d261c3347efe7543498d6967
Eunju Huss <eunju@sunet.se>
2021-05-10 09:09:13 +0200

    Add translation for resetpass.send-link

--- src/login/translation/defaultMessages/login.js
+++ src/login/translation/defaultMessages/login.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
 
-export const login= { 
-  // help
+export const login = { 
   "resetpass.send-link": (
     <FormattedMessage
       id="resetpass.send-link"
