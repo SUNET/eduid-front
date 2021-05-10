98719075fa93a4c08addc8d13a51ff5c901a379c
Eunju Huss <eunju@sunet.se>
2021-05-10 09:13:20 +0200

    Add postEmail saga

--- src/login/app_config/login-rootSaga.js
+++ src/login/app_config/login-rootSaga.js
@@ -1,12 +1,13 @@
 import { takeLatest } from "redux-saga/effects";
 import * as init_actions from "../app_init/init_actions";
-import { requestConfig, getResetPasswordData } from "../app_init/init_sagas";
+import { requestConfig, getResetPasswordData, postEmail } from "../app_init/init_sagas";
 import * as resetPasswordActions from "../redux/actions/resetPasswordActions";
 
 function* rootSaga() {
   yield [
     takeLatest(init_actions.GET_CONFIG, requestConfig),
     takeLatest(resetPasswordActions.GET_RESET_PASSWORD, getResetPasswordData),
+    takeLatest(resetPasswordActions.POST_RESET_PASSWORD, postEmail),
   ];
 }
 
