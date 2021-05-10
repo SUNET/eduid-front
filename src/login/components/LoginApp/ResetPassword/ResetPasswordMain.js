02de048b5f723c98193912ce50cb768633ad1f79
Eunju Huss <eunju@sunet.se>
2021-05-10 13:25:34 +0200

    Separate EmailForm from inside ResetPasswordMain

--- src/login/components/LoginApp/ResetPassword/ResetPasswordMain.js
+++ src/login/components/LoginApp/ResetPassword/ResetPasswordMain.js
@@ -2,7 +2,7 @@ import React, {useEffect} from "react";
 import { withRouter } from "react-router-dom";
 import i18n from "../../../translation/InjectIntl_HOC_factory";
 import { useSelector, useDispatch,  } from 'react-redux';
-import {getResetPassword, postResetPassword} from "../../../redux/actions/resetPasswordActions"
+import {getResetPassword, postEmail} from "../../../redux/actions/resetPasswordActions"
 import { Field, reduxForm } from "redux-form";
 import Form from "reactstrap/lib/Form";
 import CustomInput from "../../Inputs/CustomInput";
@@ -10,50 +10,61 @@ import EduIDButton from "../../../../components/EduIDButton";
 import { validate } from "../../../app_utils/validation/validateEmail";
 import { connect } from "react-redux";
 
+let EmailForm = (props) => (
+  <Form id="reset-password-form" role="form" onSubmit={props.sendLink}>
+    <Field
+      type="email"
+      name="email"
+      label={props.translate("signup.registering-input")}
+      componentClass="input"
+      id="email-input"
+      component={CustomInput}
+      translate={props.translate}
+      placeholder="example@email.com"
+    />
+    <EduIDButton
+      className="settings-button"
+      id="reset-password-button"
+      disabled={props.invalid}
+      onClick={props.sendLink}
+    >
+      {props.translate("resetpw.send-link")}
+    </EduIDButton>
+  </Form>
+);
+
+EmailForm = reduxForm({
+  form: "emailForm",
+  validate,
+})(EmailForm);
+
+EmailForm = connect(() => ({
+  enableReinitialize: true,
+  destroyOnUnmount: false,
+}))(EmailForm);
+
 const ResetPasswordMain = (props) => {
   const dispatch = useDispatch();
-  const state = useSelector(state => state.config.csrf_token);
+  const csrf_token = useSelector(state => state.config.csrf_token);
 
-  let EmailForm = () => (
-    <Form id="reset-password-form" role="form" onSubmit={props.handleEmail}>
-      <Field
-        type="email"
-        name="email"
-        label={props.translate("signup.registering-input")}
-        componentClass="input"
-        id="email-input"
-        component={CustomInput}
-        translate={props.translate}
-        placeholder="example@email.com"
-      />
-      <EduIDButton
-        className="settings-button"
-        id="register-button"
-        disabled={props.invalid}
-        onClick={()=> dispatch(postResetPassword())}
-      >
-        {props.translate("resetpass.send-link")}
-      </EduIDButton>
-    </Form>
-  );
-
-  EmailForm = reduxForm({
-    form: "emailForm",
-    validate,
-  })(EmailForm);
-
-  EmailForm = connect(() => ({
-    enableReinitialize: true,
-    destroyOnUnmount: false,
-  }))(EmailForm);
-
+  const sendLink = () => {
+    const email = document.querySelector("input[name='email']").value;
+    dispatch(postEmail(email));
+  };
+  
   useEffect( () => {
     dispatch(getResetPassword());
-  }, [state])
+  }, [csrf_token])
 
   return (
     <>
-      <EmailForm {...props} />
+      <p className="heading">{props.translate("resetpw.heading-add-email")}</p>
+      <EmailForm sendLink={sendLink} {...props} />
+      <div className="return-login-link">
+        <a href={"/login"}>
+          {props.translate("resetpw.return-login")}
+        </a>
+      </div>
     </>
   );
 }
