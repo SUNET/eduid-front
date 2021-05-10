63ab28f9c9c3723e15820d1b87fead1d2cd2e607
Eunju Huss <eunju@sunet.se>
2021-05-10 08:43:06 +0200

    Replace old header for translation

--- src/login/components/App/App.js
+++ src/login/components/App/App.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import Splash from "../Splash/Splash_container";
-import Header from "../Header/Header_container";
+import Header from "../../../containers/Header";
 import Notifications from "../Notifications/Notifications_container";
 import LoginApp from "../LoginApp/LoginApp";
 import Footer from "../Footer/Footer_container";
