9a9c496ba100c677ac9a86cd044e46d50eed0b8c
Eunju Huss <eunju@sunet.se>
2021-05-17 15:26:22 +0200

    Add takeEvery getResetPAsswordConfig after faild post reset password

--- src/login/app_config/login-rootSaga.js
+++ src/login/app_config/login-rootSaga.js
@@ -1,4 +1,4 @@
-import { takeLatest } from "redux-saga/effects";
+import { takeLatest, takeEvery } from "redux-saga/effects";
 import * as init_actions from "../app_init/init_actions";
 import { requestConfig } from "../app_init/init_sagas";
 import { getResetPasswordConfig, postEmailLink } from "../redux/sagas/resetpassword/resetPasswordSaga";
@@ -9,6 +9,7 @@ function* rootSaga() {
     takeLatest(init_actions.GET_CONFIG, requestConfig),
     takeLatest(resetPasswordActions.GET_RESET_PASSWORD, getResetPasswordConfig),
     takeLatest(resetPasswordActions.POST_RESET_PASSWORD, postEmailLink),
+    takeEvery(resetPasswordActions.POST_RESET_PASSWORD_FAIL, getResetPasswordConfig),
   ];
 }
 
