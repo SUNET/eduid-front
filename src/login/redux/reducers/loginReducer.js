0c06230ca5594e520013e1ef8971b606221bb02a
Nathalie Jonsson <njons@sunet.se>
2021-05-20 14:38:31 +0200

    container dispatches the mock action ADD_EMAIL_TO_STORE and console.log removed from reducer

--- src/login/redux/reducers/loginReducer.js
+++ src/login/redux/reducers/loginReducer.js
@@ -7,7 +7,6 @@ const loginData = {
 let loginReducer = (state = loginData, action) => {
   switch (action.type) {
     case actions.ADD_EMAIL_ADDRESS_TO_STORE:
-      // console.log("email address:", action.payload.email);
       return {
         ...state,
         email: action.payload.email,
