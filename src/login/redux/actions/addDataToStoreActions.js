deb0cfbe32a4bad24656f0677e85f502a055ab68
Nathalie Jonsson <njons@sunet.se>
2021-05-20 09:12:01 +0200

    on dispatch of ADD_COOKIE_STATUS_TO_STORE the cookie status becomes avilable in the reducer

--- src/login/redux/actions/addDataToStoreActions.js
+++ src/login/redux/actions/addDataToStoreActions.js
@@ -1,8 +1,16 @@
 export const ADD_NAVID_TO_STORE = "ADD_NAVID_TO_STORE";
+export const ADD_COOKIE_STATUS_TO_STORE = "ADD_COOKIE_STATUS_TO_STORE";
 
 export const addNavId = (navId) => ({
   type: ADD_NAVID_TO_STORE,
   payload: {
     navId: navId,
+  },
+});
+
+export const checkCookieStatus = (status) => ({
+  type: ADD_COOKIE_STATUS_TO_STORE,
+  payload: {
+    hasCookie: status,
   },
 });
