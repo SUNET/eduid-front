6de4b90ee7211c05fffd1fc98fa8dbd57bb63b75
Eunju Huss <eunju@sunet.se>
2021-05-21 09:14:06 +0200

    dispatch postEmailLink for clicking resend link

--- src/login/components/LoginApp/ResetPassword/EmailLinkSent.js
+++ src/login/components/LoginApp/ResetPassword/EmailLinkSent.js
@@ -2,15 +2,34 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import i18n from "../../../translation/InjectIntl_HOC_factory";
 import PropTypes from "prop-types";
+import { useSelector, useDispatch, } from 'react-redux';
+import { postEmailLink } from "../../../redux/actions/resetPasswordActions";
 
-function EmailLinkSent(){
+function EmailLinkSent(props){
+  const email = useSelector(state => state.resetPassword.email);
+  const dispatch = useDispatch();
+
+  const sendLink = () => {
+    dispatch(postEmailLink(email));
+  };
+
   return (
-   <h1>SUCCESS</h1>
+    <>
+      <p className="heading">
+        {props.translate("resetpw.reset-pw-initialized")}
+      </p>
+      <div id="reset-pass-display">
+        <p>{props.translate("resetpw.check-email-link")({ email: email })}</p>
+        <p>{props.translate("resetpw.resend-link")} 
+          <a className={`resend-link` } onClick={sendLink}> {props.translate("resetpw.resend-link-button")} </a>
+        </p>
+      </div>
+    </>
   ) 
 }
 
 EmailLinkSent.propTypes = {
-  translate: PropTypes.func,
+  translate: PropTypes.func, 
   sendLink: PropTypes.func,
   invalid: PropTypes.bool
 };
