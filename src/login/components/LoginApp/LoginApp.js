310b5b6b22ab9009042e7755da581ff6be961219
Nathalie Jonsson <njons@sunet.se>
2021-05-26 11:46:19 +0200

    updates the path at which the login form renders to have a ref

--- src/login/components/LoginApp/LoginApp.js
+++ src/login/components/LoginApp/LoginApp.js
@@ -5,13 +5,18 @@ import { withRouter } from "react-router-dom";
 
 class LoginApp extends Component {
   render() {
+    const url = this.props.location.pathname;
+    const urlCode = url.split("/").reverse()[0];
+
     return (
       <div id="content" className="vertical-content-margin">
-        <Route
-          exact
-          path="/login/"
-          render={(props) => <LoginForm {...props} />}
-        />
+        {url.includes(`login`) && (
+          <Route
+            exact
+            path={`/login/${urlCode}`}
+            render={(props) => <LoginForm {...props} />}
+          />
+        )}
       </div>
     );
   }
