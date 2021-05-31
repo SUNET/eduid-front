237a61705967b79ce89be5f8c31c40f3a14de69a
Eunju Huss <eunju@sunet.se>
2021-05-31 12:26:59 +0200

    Add comments

--- src/login/components/LoginApp/ResetPassword/CountDownTimer.js
+++ src/login/components/LoginApp/ResetPassword/CountDownTimer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-let count = 0, counter = null, minute ="", second = "";
+let count = 0, counter = null, minute = "", second = "";
 
 const timer = () => {
   count = setLocalStorage("count", count - 1);
@@ -10,25 +10,26 @@ const timer = () => {
 
   if (count <= -1) {
      clearInterval(counter),
+     //if count is -1  or below, clearInterval
      elementMinute !== null && elementMinute.classList.add('display-none'),
      elementSecond !== null && elementSecond.classList.add('display-none'),
      elementResendLink !== null && elementResendLink.classList.add('button-active')
-
+     // timer(minute, second) will not be display and the button will be activated
   }else if(count > 0){
     minute = Math.floor(count / 60),
-    second = count % 60,
+    second = count % 60
     elementMinute !== null && elementMinute.classList.remove('display-none'),
     elementSecond !== null && elementSecond.classList.remove('display-none'),
     elementResendLink !== null && elementResendLink.classList.remove('button-active')
-  if(elementMinute){
-    elementMinute.textContent = minute.toString().padStart(2,0)+":";
-  }if(elementSecond)
-    elementSecond.textContent = second.toString().padStart(2,0)
-  }
+    if(elementMinute){
+      elementMinute.textContent = minute.toString().padStart(2,0)+":";
+    }if(elementSecond)
+      elementSecond.textContent = second.toString().padStart(2,0)
+    }
 }
 
 export const clearCountdown = () => { 
-  return window.localStorage.removeItem("count")
+  return window.localStorage.removeItem("count");
 }    
 
 export const getLocalStorage = (key) => {
@@ -48,13 +49,15 @@ export const countDownStart = () =>{
   }
   clearInterval(counter);
   count = getLocalStorage("count") || 300;
+  // 300 = 5 min
   counter = setInterval(timer, 1000);
+  // every countdown step is 1 second 
 }
 
 export const RenderingTimer = (props) => {  
   return (
     <>
-      <a id={`resend-link`} className={count <=- 1 ? "button-active" : ""} onClick={props.sendLink}> {props.translate("resetpw.resend-link-button")} </a>
+      <a id={"resend-link"} className={count <=- 1 ? "button-active" : ""} onClick={props.sendLink}> {props.translate("resetpw.resend-link-button")} </a>
       <span id="minute" className={count <=- 1 ? "display-none" : ""}/>
       <span id="second" className={count <=- 1 ? "display-none" : ""} />
     </>
