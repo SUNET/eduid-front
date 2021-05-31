12afe78f8a122f899cbba81732adcd1c0134493e
Eunju Huss <eunju@sunet.se>
2021-05-31 09:53:56 +0200

    Add if state for checking element is not null and different className depends on count

--- src/login/components/LoginApp/ResetPassword/CountDownTimer.js
+++ src/login/components/LoginApp/ResetPassword/CountDownTimer.js
@@ -1,19 +1,25 @@
+import React from "react";
+
 let count = 0, counter = null, minute ="", second = "";
 
 const timer = () => {
   count = setLocalStorage("count", count - 1);
-  const elementMinute = document.getElementById("minute");
-  const elementSecond = document.getElementById("second");
-  const elementResendLink = document.getElementById("resend-link");
+  const elementMinute = document.querySelector("#minute");
+  const elementSecond = document.querySelector("#second");
+  const elementResendLink = document.querySelector("#resend-link");
 
   if (count <= -1) {
-    clearInterval(counter);
-      elementMinute.classList.add('display-none');
-      elementSecond.classList.add('display-none');
-      elementResendLink.classList.add('button-active');
+     clearInterval(counter),
+     elementMinute !== null && elementMinute.classList.add('display-none'),
+     elementSecond !== null && elementSecond.classList.add('display-none'),
+     elementResendLink !== null && elementResendLink.classList.add('button-active')
+
   }else if(count > 0){
     minute = Math.floor(count / 60),
-    second = count % 60
+    second = count % 60,
+    elementMinute !== null && elementMinute.classList.remove('display-none'),
+    elementSecond !== null && elementSecond.classList.remove('display-none'),
+    elementResendLink !== null && elementResendLink.classList.remove('button-active')
   if(elementMinute){
     elementMinute.textContent = minute.toString().padStart(2,0)+":";
   }if(elementSecond)
@@ -21,11 +27,11 @@ const timer = () => {
   }
 }
 
-const clearCountdown = () => { 
+export const clearCountdown = () => { 
   return window.localStorage.removeItem("count")
 }    
 
-const getLocalStorage = (key) => {
+export const getLocalStorage = (key) => {
   return window.localStorage ? window.localStorage.getItem(key) : '';
 }
 
@@ -37,8 +43,20 @@ const setLocalStorage = (key, val) => {
 } 
 
 export const countDownStart = () =>{
+  if(getLocalStorage("count") <= -1){
     clearCountdown();
-    clearInterval(counter);
-    count = getLocalStorage("count") || 300;
-    counter = setInterval(timer, 1000);
+  }
+  clearInterval(counter);
+  count = getLocalStorage("count") || 300;
+  counter = setInterval(timer, 1000);
+}
+
+export const RenderingTimer = (props) => {  
+  return (
+    <>
+      <a id={`resend-link`} className={count <=- 1 ? "button-active" : ""} onClick={props.sendLink}> {props.translate("resetpw.resend-link-button")} </a>
+      <span id="minute" className={count <=- 1 ? "display-none" : ""}/>
+      <span id="second" className={count <=- 1 ? "display-none" : ""} />
+    </>
+  )
 }
