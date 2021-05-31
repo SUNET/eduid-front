c36300292f6216ff87a15cbe532ba98c9137f409
Nathalie Jonsson <njons@sunet.se>
2021-05-31 16:40:26 +0200

    refactors emptyValueValidation to be generic and work for any input with any name and value

--- src/login/app_utils/validation/emptyValueValidation.js
+++ src/login/app_utils/validation/emptyValueValidation.js
@@ -2,9 +2,14 @@ import { emptyStringPattern } from "./regexPatterns";
 
 export const emptyValueValidation = (values) => {
   const errors = {};
-  const groupName = values.groupName;
-  if (!groupName || emptyStringPattern.test(groupName)) {
-    errors.groupName = "required";
+  let inputValue = "";
+  let inputName = "";
+  if (values !== undefined) {
+    inputValue = Object.values(values)[0];
+    inputName = Object.keys(values)[0];
+  }
+  if (!inputValue || emptyStringPattern.test(inputValue)) {
+    errors[inputName] = "required";
   }
   return errors;
 };
