58e43f6bbe2889cef9bd3a1680eced9ac8aca234
Nathalie Jonsson <njons@sunet.se>
2021-06-02 15:12:24 +0200

    adds saga to post ref to /next

--- src/login/redux/sagas/login/postRefLoginSaga.js
+++ src/login/redux/sagas/login/postRefLoginSaga.js
@@ -1,26 +1,22 @@
 import { call, select, put } from "redux-saga/effects";
 import postRequest from "../postDataRequest";
 import { putCsrfToken } from "../../../../sagas/common";
+import * as actions from "../../actions/postRefLoginActions";
+import { eduidRMAllNotify } from "./../../../../actions/Notifications";
 
 export function* postRefLoginSaga(action) {
-  console.log("welcome to nextLoginStepSaga");
-  // console.log(postNextRequest);
   const state = yield select((state) => state);
   const url = state.config.next_url;
-  console.log("url ", url);
-  // const url = IDP_SERVICE_URL + "/next";
   try {
     const dataToSend = {
       ref: state.login.ref,
       csrf_token: state.config.csrf_token,
     };
-    console.log("dataToSend", dataToSend);
     const nextLoginStepResponse = yield call(postRequest, url, dataToSend);
-    console.log("nextLoginStepResponse", nextLoginStepResponse);
-    // yield put(putCsrfToken(nextLoginStepResponse));
-    // yield put(nextLoginStepResponse);
+    yield put(putCsrfToken(nextLoginStepResponse));
+    yield put(nextLoginStepResponse);
+    yield put(eduidRMAllNotify());
   } catch (error) {
-    // yield put(actions.createInviteFail(error.toString()));
+    yield put(actions.postRefFail(error.toString()));
   }
 }
-
