c49dbf57a07da3927dc5348bc766f88ebb89e92f
Eunju Huss <eunju@sunet.se>
2021-06-02 11:21:06 +0200

    Add postLinkCodeFail

--- src/login/redux/actions/postResetPasswordActions.js
+++ src/login/redux/actions/postResetPasswordActions.js
@@ -2,6 +2,7 @@ export const POST_RESET_PASSWORD = "POST_RESET_PASSWORD";
 export const POST_RESET_PASSWORD_SUCCESS = "POST_RESET_PASSWORD_SUCCESS";
 export const POST_RESET_PASSWORD_FAIL = "POST_RESET_PASSWORD_FAIL";
 export const POST_RESET_PASSWORD_VERIFY_EMAIL = "POST_RESET_PASSWORD_VERIFY_EMAIL";
+export const POST_RESET_PASSWORD_VERIFY_EMAIL_FAIL = "POST_RESET_PASSWORD_VERIFY_EMAIL_FAIL";
 
 export function postEmailLink(email) {
   return {
@@ -26,7 +27,18 @@ export function useLinkCode(code) {
   return {
     type: POST_RESET_PASSWORD_VERIFY_EMAIL,
     payload: {
-      code: code
+      email_code: code
     }
   };
 }
+
+export function postLinkCodeFail(err) {
+  return {
+    type: POST_RESET_PASSWORD_VERIFY_EMAIL_FAIL,
+    error: true,
+    payload: {
+      message: err.toString()
+    }
+  };
+}
+
