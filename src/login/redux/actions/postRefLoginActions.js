9c49c3925fb8bd89d4973daad77ce7d12056d36d
Nathalie Jonsson <njons@sunet.se>
2021-06-02 15:13:50 +0200

    completes actions with SUCCESS and FAIL types

--- src/login/redux/actions/postRefLoginActions.js
+++ src/login/redux/actions/postRefLoginActions.js
@@ -1,7 +1,20 @@
 export const POST_LOGIN_REF_TO_NEXT = "POST_LOGIN_REF_TO_NEXT";
+export const POST_IDP_NEXT_SUCCESS = "POST_IDP_NEXT_SUCCESS";
+export const POST_IDP_NEXT_FAIL = "POST_IDP_NEXT_FAIL";
 
 export function useLoginRef() {
   return {
     type: POST_LOGIN_REF_TO_NEXT,
+  };
+}
+
+export function postRefFail(err) {
+  return {
+    type: POST_IDP_NEXT_FAIL,
+    error: true,
+    payload: {
+      error: err,
+      message: err,
+    },
   };
 }
