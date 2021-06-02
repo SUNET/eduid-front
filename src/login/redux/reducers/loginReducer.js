b44ea0067fc97af4af5a252fb6d7ce8a96333eb0
Nathalie Jonsson <njons@sunet.se>
2021-06-02 10:01:27 +0200

    adds ref to redux store

--- src/login/redux/reducers/loginReducer.js
+++ src/login/redux/reducers/loginReducer.js
@@ -1,9 +1,16 @@
+import * as actions from "../actions/addDataToStoreActions";
+
 const loginData = {
   ref: null,
 };
 
 let loginReducer = (state = loginData, action) => {
   switch (action.type) {
+    case actions.ADD_LOGIN_REF_TO_STORE:
+      return {
+        ...state,
+        ref: action.payload.ref,
+      };
     default:
       return state;
   }
