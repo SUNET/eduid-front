62888f6195f5c21dd547f73d6900c807bc146b16
Nathalie Jonsson <njons@sunet.se>
2021-06-09 12:07:39 +0200

    adds render for all pages controlled by the backend, adds hardcoded override to manually control render during development

--- src/login/components/LoginApp/Login/Login.js
+++ src/login/components/LoginApp/Login/Login.js
@@ -1,15 +1,23 @@
 import React, { Fragment } from "react";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
 import UsernamePw from "./UsernamePw";
+import TermOfUse from "./TermsOfUse";
+import MultiFactorAuth from "./MultiFactorAuth";
 import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
 
 const Login = (props) => {
-  const page = useSelector((state) => state.login.next_page);
+  // uncomment to let backend control render
+  // const nextPage = useSelector((state) => state.login.next_page);
+  const nextPage = "USERNAMEPASSWORD";
   return (
     <Fragment>
-      {page === "USERNAMEPASSWORD" ? (
+      {nextPage === "USERNAMEPASSWORD" ? (
         <UsernamePw {...props} />
+      ) : nextPage === "TOU" ? (
+        <TermOfUse {...props} />
+      ) : nextPage === "MFA" ? (
+        <MultiFactorAuth {...props} />
       ) : (
         <p className="heading">Loading</p>
       )}
