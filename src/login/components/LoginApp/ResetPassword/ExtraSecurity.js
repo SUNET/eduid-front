c5d122b66b572516e2bb19c658b53e987162c77b
Eunju Huss <eunju@sunet.se>
2021-06-14 12:37:43 +0200

    Redirect reset-password without email-code

--- src/login/components/LoginApp/ResetPassword/ExtraSecurity.js
+++ src/login/components/LoginApp/ResetPassword/ExtraSecurity.js
@@ -4,21 +4,20 @@ import { useHistory } from 'react-router-dom';
 import EduIDButton from "../../../../components/EduIDButton";
 function ExtraSecurity(){
   const history = useHistory();
-  const extra_security = history.location.state.extra_security;
-  const [extraSecurity, setExtraSecurity] = useState(extra_security);
+  const [extraSecurity, setExtraSecurity] = useState();
 
   useEffect(()=>{
-    if(extra_security){
-      setExtraSecurity(extra_security)
-    }
-  },[extra_security])
+    if(history.location.state !== undefined){
+      setExtraSecurity(history.location.state.extra_security)
+    }else history.push(`/reset-password/`)
+  },[extraSecurity])
 
   return (
     <>
       <p className="heading">Extra security</p>
       <div id="reset-pass-display">
         <p>Prove that your are the owner of eduID with your extra security. </p>
-        { extraSecurity !== null && extraSecurity.phone_numbers.length > 0 ? 
+        { extraSecurity && extraSecurity.phone_numbers.length > 0 ? 
             extraSecurity.phone_numbers.map(phone => {
             return (
               <EduIDButton
@@ -29,7 +28,7 @@ function ExtraSecurity(){
                 SEND SMS TO {phone.number}
               </EduIDButton>)
           }) : 
-          extraSecurity.tokens && Object.keys(extraSecurity.tokens).length > 0 ? 
+          extraSecurity && Object.keys(extraSecurity.tokens).length > 0 ? 
             Object.values(extraSecurity.tokens).map((security) => {
             return (
               <EduIDButton
