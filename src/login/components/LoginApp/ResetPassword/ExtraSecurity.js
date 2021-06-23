970d379473bf2932d3cead5d5fe346e2162e236c
Eunju Huss <eunju@sunet.se>
2021-06-23 12:19:38 +0200

    Rendering security key first

--- src/login/components/LoginApp/ResetPassword/ExtraSecurity.js
+++ src/login/components/LoginApp/ResetPassword/ExtraSecurity.js
@@ -1,24 +1,7 @@
-import React, { useEffect, useState }  from "react";
+import React, { useEffect, useState, Fragment }  from "react";
 import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
 import { useHistory } from 'react-router-dom';
 import EduIDButton from "../../../../components/EduIDButton";
-
-const SecurityWithSMSButton = ({extraSecurityPhone, translate}) => {
-  return (
-    extraSecurityPhone.map(phone => {
-      return (
-        <EduIDButton
-          className={"settings-button"}
-          id="extra-security" 
-          key={phone.index}
-        > 
-        {translate("resetpw.extra-phone_send_sms")(
-          {phone: phone.number.replace(/^.{10}/g, '**********')})}
-        </EduIDButton>
-      )
-    })
-  )
-};
 
 const SecurityKeyButton = ({extraSecurityKey, translate}) => {
   return (
@@ -35,6 +18,27 @@ const SecurityKeyButton = ({extraSecurityKey, translate}) => {
     })
   )
 };
+
+const SecurityWithSMSButton = ({extraSecurityPhone, translate}) => {
+  return (
+    extraSecurityPhone.map(phone => {
+      return (
+        <Fragment key={phone.index}>
+          <br/>
+          <EduIDButton
+            className={"settings-button"}
+            id="extra-security" 
+            key={phone.index}
+          > 
+          {translate("resetpw.extra-phone_send_sms")(
+            {phone: phone.number.replace(/^.{10}/g, '**********')})}
+          </EduIDButton>
+        </Fragment>
+      )
+    })
+  )
+};
+
 function ExtraSecurity(props){
   const history = useHistory();
   const [extraSecurity, setExtraSecurity] = useState();
@@ -50,13 +54,11 @@ function ExtraSecurity(props){
       <p className="heading">{props.translate("resetpw.extra-security_heading")}</p>
       <div id="reset-pass-display">
         <p>{props.translate("resetpw.extra-security_description")}</p>
-        { extraSecurity && extraSecurity.phone_numbers.length > 0 ? 
-          <>
-            <SecurityWithSMSButton extraSecurityPhone={extraSecurity.phone_numbers} translate={props.translate}/><br />
-          </>: null
-        }
         { extraSecurity && Object.keys(extraSecurity.tokens).length > 0  ?
           <SecurityKeyButton extraSecurityKey={Object.keys(extraSecurity.tokens)} translate={props.translate} /> : null
+        }
+        { extraSecurity && extraSecurity.phone_numbers.length > 0 ? 
+          <SecurityWithSMSButton extraSecurityPhone={extraSecurity.phone_numbers} translate={props.translate}/> : null
         }
         <p className="decription-without-security">{props.translate("resetpw.without_extra_security")}
           <a href={`/reset-password/set-new-password/`}> {props.translate("resetpw.continue_reset_password")}</a> 
