5c5967d9ecf79081efb6e8106b48bd04252ca3a1
Eunju Huss <eunju@sunet.se>
2021-06-23 15:01:35 +0200

    Add mock text for heading and description

--- src/login/components/LoginApp/ResetPassword/SetNewPassword.js
+++ src/login/components/LoginApp/ResetPassword/SetNewPassword.js
@@ -4,7 +4,8 @@ import i18n from "../../../translation/InjectIntl_HOC_factory";
 function SetNewPassword(){
   return (
     <>
-      <p className="heading">Set New Password</p>
+      <p className="heading">Set your new password</p>
+      <p>A suggested password can be selected by clicking the input field. </p>
     </>
   ) 
 }
