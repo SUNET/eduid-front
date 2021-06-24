9e5f707b95956a2d37d8e9952f8d68406b6d6aef
Eunju Huss <eunju@sunet.se>
2021-06-24 14:32:11 +0200

    Add reuseable component for ResetPasswordText

--- src/login/components/LoginApp/ResetPassword/ExtraSecurity.js
+++ src/login/components/LoginApp/ResetPassword/ExtraSecurity.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, Fragment }  from "react";
 import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
 import { useHistory } from 'react-router-dom';
 import EduIDButton from "../../../../components/EduIDButton";
+import ResetPasswordText from "./ResetPasswordText";
 
 const SecurityKeyButton = ({extraSecurityKey, translate, ShowSecurityKey}) => {
   return (
@@ -56,21 +57,19 @@ function ExtraSecurity(props){
   };
 
   return (
-    <>
-      <p className="heading">{props.translate("resetpw.extra-security_heading")}</p>
-      <div id="reset-pass-display">
-        <p>{props.translate("resetpw.extra-security_description")}</p>
-        { extraSecurity && Object.keys(extraSecurity.tokens).length > 0  ?
-          <SecurityKeyButton ShowSecurityKey={ShowSecurityKey} extraSecurityKey={Object.keys(extraSecurity.tokens)} translate={props.translate} /> : null
-        }
-        { extraSecurity && extraSecurity.phone_numbers.length > 0 ? 
-          <SecurityWithSMSButton extraSecurityPhone={extraSecurity.phone_numbers} translate={props.translate}/> : null
-        }
-        <p className="decription-without-security">{props.translate("resetpw.without_extra_security")}
-          <a href={`/reset-password/set-new-password/`}> {props.translate("resetpw.continue_reset_password")}</a> 
-          </p>
-      </div>
-    </>
+    <ResetPasswordText
+      heading={props.translate("resetpw.extra-security_heading")} 
+      description={props.translate("resetpw.extra-security_description")} 
+      linkInfoText={props.translate("resetpw.without_extra_security")}
+      linkText={props.translate("resetpw.continue_reset_password")}
+    > 
+      { extraSecurity && Object.keys(extraSecurity.tokens).length > 0  ?
+        <SecurityKeyButton ShowSecurityKey={ShowSecurityKey} extraSecurityKey={Object.keys(extraSecurity.tokens)} translate={props.translate} /> : null
+      }
+      { extraSecurity && extraSecurity.phone_numbers.length > 0 ? 
+        <SecurityWithSMSButton extraSecurityPhone={extraSecurity.phone_numbers} translate={props.translate}/> : null
+      }
+    </ResetPasswordText>
   ) 
 }
 
