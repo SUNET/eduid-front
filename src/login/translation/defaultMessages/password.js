19e06280d49d1ec18d8099b13b5616ef855ada3d
Eunju Huss <eunju@sunet.se>
2021-06-30 15:57:12 +0200

    Add resetpw.expired-phone-code translation text

--- src/login/translation/defaultMessages/password.js
+++ src/login/translation/defaultMessages/password.js
@@ -341,4 +341,18 @@ export const resetPassword = {
       defaultMessage={`You have recently been sent a verification code. Please wait at least 5 minutes to request a new one.`}
     />
   ),
+
+  "resetpw.send-sms-success": (
+    <FormattedMessage
+      id="resetpw.send-sms-success"
+      defaultMessage={`Phone code has been sent.`}
+    />
+  ),
+
+  "resetpw.expired-phone-code": (
+    <FormattedMessage
+      id="resetpw.expired-phone-code"
+      defaultMessage={`Phone code has expired. Please try again.`}
+    />
+  ),
 };
