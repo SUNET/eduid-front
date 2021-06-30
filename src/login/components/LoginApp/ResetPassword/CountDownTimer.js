60d832be12363a1a13863159c16f7ebf6535dbcd
Eunju Huss <eunju@sunet.se>
2021-06-30 13:04:38 +0200

    Add RenderingResendCodeTimer to prevent send code multiple times

--- src/login/components/LoginApp/ResetPassword/CountDownTimer.js
+++ src/login/components/LoginApp/ResetPassword/CountDownTimer.js
@@ -66,3 +66,14 @@ export const RenderingTimer = (props) => {
     </>
   )
 }
+
+export const RenderingPhoneCodeTimer = (props) => {  
+  const countLocalStorage = getLocalStorage(LOCAL_STORAGE_PERSISTED_COUNT);
+  return (
+    <>
+      <a id={"resend-link"} className={countLocalStorage  <=- 1 ? "button-active" : ""}> {props.translate("resend.button")} </a>
+      <span id="minute" className={countLocalStorage  <=- 1 ? "display-none" : ""}/>
+      <span id="second" className={countLocalStorage  <=- 1 ? "display-none" : ""} />
+    </>
+  )
+}
