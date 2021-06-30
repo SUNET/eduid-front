7a256ef5b52e5981210b07dcd87939d559357bc9
Eunju Huss <eunju@sunet.se>
2021-06-30 13:05:23 +0200

    Add validate for confirmation code

--- src/login/components/LoginApp/ResetPassword/PhoneCodeSent.js
+++ src/login/components/LoginApp/ResetPassword/PhoneCodeSent.js
@@ -3,7 +3,58 @@ import { withRouter } from "react-router-dom";
 import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
 import PropTypes from "prop-types";
 import SuccessIconAnimation from "./SuccessIconAnimation";
-import { RenderingTimer, countDownStart, getLocalStorage, LOCAL_STORAGE_PERSISTED_COUNT } from "./CountDownTimer";
+import { RenderingResendCodeTimer , countDownStart, getLocalStorage, LOCAL_STORAGE_PERSISTED_COUNT } from "./CountDownTimer";
+import { shortCodePattern } from "../../../app_utils/validation/regexPatterns";
+import EduIDButton from "../../../../components/EduIDButton";
+import Form from "reactstrap/lib/Form";
+import CustomInput from "../../Inputs/CustomInput";
+import { Field, reduxForm } from "redux-form";
+import { connect } from 'react-redux';
+
+const validate = (values) => {
+    const value = values.phoneCode;
+    let errors = {};
+    if (!value || !value.trim()) {
+      errors.phoneCode = "required";
+      return errors;
+    }
+  
+    if (!shortCodePattern.test(value.trim())){
+      errors.phoneCode = "confirmation.code_invalid_format";
+      return errors;
+    }
+  };
+
+let PhoneCodeForm = (props) => (
+    <Form id="phone-code-form" role="form">
+      <Field
+         component={CustomInput}
+         componentClass="input"
+         type="text"
+         label="confirmation code"
+         placeholder="Phone confirmation code"
+         id="phone-code"
+         name="phoneCode"
+      />
+      <EduIDButton
+        className="settings-button"
+        id="save-phone-button"
+        disabled={props.invalid}
+      >
+        save
+      </EduIDButton>
+    </Form>
+  );
+    
+  PhoneCodeForm = reduxForm({
+    form: "phone-code-form",
+    validate,
+  })(PhoneCodeForm);
+  
+  PhoneCodeForm = connect(() => ({
+    touchOnChange: true,
+    destroyOnUnmount: false,
+  }))(PhoneCodeForm);
 
 function PhoneCodeSent(props){
 
@@ -17,11 +68,8 @@ function PhoneCodeSent(props){
     <>
       <SuccessIconAnimation />
       <div id="reset-pass-display">
-        <div className="timer">
-          <p>{props.translate("resetpw.resend-link")} 
-            <RenderingTimer {...props}/>
-          </p>
-        </div>
+        <PhoneCodeForm {...props} />
+        <RenderingResendCodeTimer  {...props}/>
       </div>
     </>
   ) 
