390203360e6ee1fc242d2f4dab84e5ca162d6230
Nathalie Jonsson <njons@sunet.se>
2021-07-01 15:39:26 +0200

    splits SecurityKeySelected into separate component with async request to the authenticator

---
+++ src/login/components/LoginApp/Login/SecurityKeySelected.js
@@ -0,0 +1,57 @@
+import React, { Fragment, useEffect } from "react";
+import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRedo, faTimes } from "@fortawesome/free-solid-svg-icons";
+import SecurityKeyGif from "../../../../../img/computer_animation.gif";
+import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
+
+let SecurityKeySelected = ({ translate, setSelected }) => {
+  const webauthn_challenge = useSelector(
+    (state) => state.login.mfa.webauthn_challenge
+  );
+
+  useEffect(() => {
+    async function securityKeyAssertion() {
+      const webauthnAssertion = await navigator.credentials
+        .get(webauthn_challenge)
+        .then()
+        .catch((error) => {
+          console.log("Problem getting MFA credentials:", error);
+        });
+      console.log("asserion from authenticator", webauthnAssertion);
+    }
+    securityKeyAssertion();
+  }, []);
+
+  return (
+    <Fragment>
+      <div className="button-pair selected">
+        <p className="heading">{translate("login.mfa.primary-option.title")}</p>
+        <button
+          className="icon"
+          onClick={() => {
+            setSelected(false);
+          }}
+        >
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
+      </div>
+      <div className="button-pair bottom">
+        <img
+          src={SecurityKeyGif}
+          alt="animation of security key inserted into computer"
+        />
+        <button className="icon" onClick={() => {}}>
+          <FontAwesomeIcon icon={faRedo} />
+        </button>
+      </div>
+    </Fragment>
+  );
+};
+
+SecurityKeySelected.propTypes = {
+  translate: PropTypes.func,
+};
+
+export default InjectIntl(SecurityKeySelected);
