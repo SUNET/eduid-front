4f6ee6a3396b1dd9383502ebce4a03d0fcc95307
Nathalie Jonsson <njons@sunet.se>
2021-07-08 16:49:01 +0200

    component extracts version from the redux store and posts them to the backend, component is informed of the version to render via the version variable

--- src/login/components/LoginApp/Login/TermsOfUse.js
+++ src/login/components/LoginApp/Login/TermsOfUse.js
@@ -6,21 +6,22 @@ import ButtonPrimary from "../../Buttons/ButtonPrimary";
 import PropTypes from "prop-types";
 import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
 
-let TermOfUseText = ({ translate }) => (
+let TermOfUseText = ({ translate, version }) => (
   <div className="tou-text">
     <p className="heading">{translate("login.tou.legal-title")}</p>
-    <ul>{translate("login.tou.2016-v1")}</ul>
+    {version !== null ? (
+      <ul>{translate(`login.tou.version.${version}`)}</ul>
+    ) : null}
     <p className="heading">{translate("login.tou.legal-warning")}</p>
   </div>
 );
+
 let AcceptButton = ({ loading }) => {
   const dispatch = useDispatch();
   return (
     <ButtonPrimary
       type="submit"
       onClick={() => dispatch(updatedTouAccept())}
-      disabled={loading}
-      aria-disabled={loading}
       id="accept-button"
     >
       accept
@@ -30,26 +31,22 @@ let AcceptButton = ({ loading }) => {
 
 let TermOfUse = (props) => {
   const dispatch = useDispatch();
-  const { loading, translate } = props;
-
-  // MOCK: the tou versions will be available from the translations object
-  // pick these from the store
-  const availableTouVersions = ["2016-v1", "2021-v1"];
+  const { translate } = props;
+  const availableTouVersions = useSelector(
+    (state) => state.login.tou.available_versions
+  );
+  const version = useSelector((state) => state.login.tou.version);
   useEffect(() => {
     dispatch(postTouVersions(availableTouVersions));
-  }, []);
-
-  // MOCK: the backend determines what version the user nddes to accept
-  const version = useSelector((state) => state.login.tou.version);
-
+  });
   return (
     <div className="tou">
       <h2 className="heading">
         {translate("login.tou.h2-heading")} ({version})
       </h2>
       <p>{translate("login.tou.paragraph")}</p>
-      <TermOfUseText translate={translate} />
-      <AcceptButton loading={loading} translate={translate} />
+      <TermOfUseText translate={translate} version={version} />
+      <AcceptButton translate={translate} />
     </div>
   );
 };
