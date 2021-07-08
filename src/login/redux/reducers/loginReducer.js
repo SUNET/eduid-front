88c010813a427f32221aa870ae848e633100924e
Nathalie Jonsson <njons@sunet.se>
2021-07-08 16:46:51 +0200

    adds extracetd versions array to redux store

--- src/login/redux/reducers/loginReducer.js
+++ src/login/redux/reducers/loginReducer.js
@@ -2,6 +2,7 @@
 import * as onLoadActions from "../actions/addDataToStoreActions";
 import * as nextPageActions from "../actions/postRefLoginActions";
 import * as usernamePasswordActions from "../actions/postUsernamePasswordActions";
+import * as addDataToStoreActions from "../actions/addDataToStoreActions";
 import * as updatedTouAcceptActions from "../actions/postUpdatedTouAcceptActions";
 import * as postRefForWebauthnChallengeActions from "../actions/postRefForWebauthnChallengeActions";
 import * as postWebauthnFromAuthenticatorActions from "../actions/postWebauthnFromAuthenticatorActions";
@@ -15,7 +16,10 @@ const loginData = {
     webauthn_assertion: null,
     parameters: null,
   },
-  tou: {},
+  tou: {
+    available_versions: null,
+    version: null,
+  },
 };
 
 let loginReducer = (state = loginData, action) => {
@@ -30,8 +34,8 @@ let loginReducer = (state = loginData, action) => {
         action.payload.action === "FINISHED" ? action.payload.parameters : null;
       return {
         ...state,
-        // next_page: "USERNAMEPASSWORD",
-        next_page: action.payload.action,
+        next_page: "TOU",
+        // next_page: action.payload.action,
         post_to: action.payload.target,
         mfa: {
           ...state.mfa,
@@ -56,6 +60,14 @@ let loginReducer = (state = loginData, action) => {
     case usernamePasswordActions.POST_IDP_PW_AUTH_SUCCESS:
       return {
         ...state,
+      };
+    case addDataToStoreActions.ADD_TOU_VERSIONS_TO_STORE:
+      return {
+        ...state,
+        tou: {
+          ...state.tou,
+          available_versions: action.payload.touVersions,
+        },
       };
     case updatedTouAcceptActions.POST_IDP_TOU_SUCCESS:
       return {
