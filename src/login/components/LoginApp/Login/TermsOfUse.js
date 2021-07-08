be4a4b5cfb1b51264219b8c5a0d504989f6b3df0
Nathalie Jonsson <njons@sunet.se>
2021-07-08 15:26:53 +0200

    adds tou text 2016-V1 from TermsOfUse component to translation files

--- src/login/components/LoginApp/Login/TermsOfUse.js
+++ src/login/components/LoginApp/Login/TermsOfUse.js
@@ -6,71 +6,11 @@ import ButtonPrimary from "../../Buttons/ButtonPrimary";
 import PropTypes from "prop-types";
 import InjectIntl from "../../../translation/InjectIntl_HOC_factory";
 
-let TermOfUseText = () => (
+let TermOfUseText = ({ translate }) => (
   <div className="tou-text">
-    <p className="heading">General rules for eduID users:</p>
-    <ul>
-      <p>The following generally applies:</p>
-      <li>
-        <p>
-          that all usage of user accounts follow Sweden&apos;s laws and by-laws,
-        </p>
-      </li>
-      <li>
-        <p>
-          that all personal information that you provide, such as name and
-          contact information shall be truthful,
-        </p>
-      </li>
-      <li>
-        <p>
-          that user accounts, password and codes are individual and shall only
-          be used by the intended individual,
-        </p>
-      </li>
-      <li>
-        <p>
-          that SUNET&apos;s ethical rules regulate the &ldquo;other&ldquo;
-          usage.
-        </p>
-      </li>
-    </ul>
-    <ul>
-      <p>SUNET judges unethical behaviour to be when someone:</p>
-      <li>
-        <p>
-          attempts to gain access to network resources that they do not have the
-          right
-        </p>
-      </li>
-      <li>
-        <p>attempts to conceal their user identity</p>
-      </li>
-      <li>
-        <p>
-          attempts to interfere or disrupt the intended usage of the network
-        </p>
-      </li>
-      <li>
-        <p>
-          clearly wastes available resources (personnel, hardware or software)
-        </p>
-      </li>
-      <li>
-        <p>attempts to disrupt or destroy computer-based information</p>
-      </li>
-      <li>
-        <p>infringes on the privacy of others</p>
-      </li>
-      <li>
-        <p>attempts to insult or offend others</p>
-      </li>
-    </ul>
-    <p className="heading">
-      Any person found violating or suspected of violating these rules can be
-      disabled from eduID.se for investigation. Furthermore, legal action can be
-      taken.
-    </p>
+    <p className="heading">{translate("login.tou.legal-title")}</p>
+    <ul>{translate("login.tou.2016-v1")}</ul>
+    <p className="heading">{translate("login.tou.legal-warning")}</p>
   </div>
 );
 let AcceptButton = ({ loading }) => {
@@ -93,6 +33,7 @@ let TermOfUse = (props) => {
   const { loading, translate } = props;
 
   // MOCK: the tou versions will be available from the translations object
+  // pick these from the store
   const availableTouVersions = ["2016-v1", "2021-v1"];
   useEffect(() => {
     dispatch(postTouVersions(availableTouVersions));
@@ -104,10 +45,10 @@ let TermOfUse = (props) => {
   return (
     <div className="tou">
       <h2 className="heading">
-        {translate("login.tou.h2-heading")}{" "}({version})
+        {translate("login.tou.h2-heading")} ({version})
       </h2>
       <p>{translate("login.tou.paragraph")}</p>
-      <TermOfUseText />
+      <TermOfUseText translate={translate} />
       <AcceptButton loading={loading} translate={translate} />
     </div>
   );
--- src/login/translation/messageIndex.js
+++ src/login/translation/messageIndex.js
@@ -2,7 +2,8 @@ import { defineMessages } from "react-intl";
 
 // import content from other files
 import { generalErrors, specificErrors } from "./defaultMessages/errors";
-import { login } from "./defaultMessages/login";
+import { login, tou } from "./defaultMessages/login";
+// import { tou } from "./defaultMessages/tou";
 import { generalInstructions } from "./defaultMessages/instructions";
 import { changePassword, resetPassword } from "./defaultMessages/password";
 import { register } from "./defaultMessages/register";
@@ -10,7 +11,7 @@ import { generalApp } from "./defaultMessages/generalApp";
 import {
   userData,
   userProfile,
-  userVetting
+  userVetting,
 } from "./defaultMessages/userProfile";
 
 export const formattedMessages = {
@@ -18,13 +19,14 @@ export const formattedMessages = {
   ...generalErrors,
   ...specificErrors,
   ...login,
+  ...tou,
   ...generalInstructions,
   ...changePassword,
   ...register,
   ...resetPassword,
   ...userData,
   ...userProfile,
-  ...userVetting
+  ...userVetting,
 };
 
 // export const formattedMessages = JSON.stringify(formattedMessagesObj);
@@ -32,53 +34,53 @@ export const formattedMessages = {
 export const unformattedMessages = defineMessages({
   "pd.choose-language": {
     id: "pd.choose-language",
-    defaultMessage: `Choose language`
+    defaultMessage: `Choose language`,
   },
   "emails.placeholder": {
     id: "emails.confirm_email_placeholder",
     defaultMessage: `xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx`,
-    description: "Placeholder for email text input"
+    description: "Placeholder for email text input",
   },
-  "emails.confirm_help_text":
-  {
+  "emails.confirm_help_text": {
     id: "emails.confirm_help_text",
-    defaultMessage: "Code is formatted as five groups of characters and numbers, separated by hyphens",
+    defaultMessage:
+      "Code is formatted as five groups of characters and numbers, separated by hyphens",
     description: "Help text for email confirmation code",
   },
   "confirmation.code_invalid_format": {
     id: "confirmation.code_invalid_format",
     defaultMessage: `Invalid confirmation code`,
-    description: "error text for invalid confirmation code"
+    description: "error text for invalid confirmation code",
   },
   "security.description_invalid_format": {
     id: "security.description_invalid_format",
     defaultMessage: `The description is too long`,
-    description: "Error message for max length"
+    description: "Error message for max length",
   },
   "mobile.placeholder": {
     id: "mobile.confirm_mobile_placeholder",
     defaultMessage: `Phone confirmation code`,
-    description: "Placeholder for phone text input"
+    description: "Placeholder for phone text input",
   },
   "security.placeholder": {
     id: "security.confirm_security_placeholder",
     defaultMessage: `Describe your security key`,
-    description: "Placeholder for security key text input"
+    description: "Placeholder for security key text input",
   },
   "security.help_text": {
     id: "security.help_text",
     defaultMessage: `max 50 characters`,
-    description: "Help text for security key max length"
+    description: "Help text for security key max length",
   },
   "register.toLogin": {
     id: "register.toLogin",
     defaultMessage: `If you already have eduID you can log in`,
-    description: "Placeholder for phone text input"
+    description: "Placeholder for phone text input",
   },
   "text.link": {
     id: "text.link",
     defaultMessage: `here.`,
-    description: "Placeholder for phone text input"
+    description: "Placeholder for phone text input",
   },
   "chpass.help-text-newpass": {
     id: "chpass.help-text-newpass",
@@ -89,59 +91,59 @@ export const unformattedMessages = defineMessages({
                 <li>Add special characters, such as &#64; &#36; &#92; &#43; &#95; &#37;</li>
 	            <li>Spaces are ignored</li>
             </ul>`,
-    description: "help text for custom password"
+    description: "help text for custom password",
   },
   "cm.lost_code": {
     id: "cm.lost_code",
     defaultMessage: `Is the code not working?`,
-    description: "Lost code problem description"
+    description: "Lost code problem description",
   },
   "cm.resend_code": {
     id: "cm.resend_code",
     defaultMessage: `Send a new confirmation code`,
-    description: "Lost code problem solution"
+    description: "Lost code problem solution",
   },
   "letter.lost_code": {
     id: "letter.lost_code",
     defaultMessage: `When you click on the "Send code" link a letter with a verification code will be sent to your official post address.`,
-    description: "Text for letter proofing confirm dialog"
+    description: "Text for letter proofing confirm dialog",
   },
   "letter.resend_code": {
     id: "letter.resend_code",
     defaultMessage: `Send code`,
-    description: "Text for letter code resend button"
+    description: "Text for letter code resend button",
   },
   "letter.placeholder": {
     id: "letter.placeholder",
     defaultMessage: `Letter confirmation code`,
-    description: "Placeholder for letter proofing text input"
+    description: "Placeholder for letter proofing text input",
   },
   "pd.given_name": {
     id: "pd.given_name",
-    defaultMessage: `First name`
+    defaultMessage: `First name`,
   },
   "pd.surname": {
     id: "pd.surname",
-    defaultMessage: `Last name`
+    defaultMessage: `Last name`,
   },
   "pd.display_name": {
     id: "pd.display_name",
-    defaultMessage: `Display name`
+    defaultMessage: `Display name`,
   },
   "pd.language": {
     id: "pd.language",
-    defaultMessage: `Language`
+    defaultMessage: `Language`,
   },
   "pd.display_name_input_placeholder": {
     id: "pd.display_name_inputplaceholder",
-    defaultMessage: `First and last name`
+    defaultMessage: `First and last name`,
   },
   "nins.input_placeholder": {
     id: "nins.input_placeholder",
-    defaultMessage: `yyyymmddnnnn`
+    defaultMessage: `yyyymmddnnnn`,
   },
   "phones.input_placeholder": {
     id: "phones.input_placeholder",
-    defaultMessage: `Phone number`
-  }
+    defaultMessage: `Phone number`,
+  },
 });
