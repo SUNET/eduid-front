23f5c2de6c3b2f4859805dcababe129e7d5d7dbb
Nathalie Jonsson <njons@sunet.se>
2021-07-09 16:46:01 +0200

    adds in original index code

--- src/login/app_init/index.js
+++ src/login/app_init/index.js
@@ -21,9 +21,9 @@ import checkTranslationSupport from "../app_utils/browserLang_i18nSupport";
 import { addLocaleData } from "react-intl";
 import en from "react-intl/locale-data/en";
 import sv from "react-intl/locale-data/sv";
-import { updateIntl } from "react-intl-redux";
+// import { updateIntl } from "react-intl-redux";
 
-// utils to check support or compatibility 
+// utils to check support or compatibility
 import polyfillElClosest_EI from "../app_utils/el.closest_IE_polyfill";
 
 /* run all utils and set up the translation */
@@ -31,25 +31,36 @@ checkTranslationSupport();
 polyfillElClosest_EI();
 addLocaleData([...en, ...sv]);
 
-const init_app = function(target, component) {
-  let app, action;
-  action = initContainer;
-  const language = navigator.languages
-    ? navigator.languages[0]
-    : navigator.language || navigator.userLanguage;
-  const supported = AVAILABLE_LANGUAGES.map(lang => lang[0]);
+/* render reactIndex.js */
+const initDomTarget = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={initStore}>
+    <App />
+  </Provider>,
+  initDomTarget,
+  initContainer
+);
 
-  if (supported.includes(language)) {
-    const lang_code = language.substring(0, 2);
-    initStore.dispatch(
-      updateIntl({
-        locale: lang_code,
-        messages: LOCALIZED_MESSAGES[lang_code]
-      })
-    );
-  }
-  app = <Provider store={initStore}>{component}</Provider>;
-  ReactDOM.render(app, target, action);
-};
+// // const init_app = function (target, component) {
+// //   let app, action;
+// //   action = initContainer;
 
-init_app(document.getElementById("root"), <App />);
+// //   const language = navigator.languages
+// //     ? navigator.languages[0]
+// //     : navigator.language || navigator.userLanguage;
+// //   const supported = AVAILABLE_LANGUAGES.map((lang) => lang[0]);
+
+// //   if (supported.includes(language)) {
+// //     const lang_code = language.substring(0, 2);
+// //     initStore.dispatch(
+// //       updateIntl({
+// //         locale: lang_code,
+// //         messages: LOCALIZED_MESSAGES[lang_code],
+// //       })
+// //     );
+// //   }
+// //   app = <Provider store={initStore}>{component}</Provider>;
+// //   ReactDOM.render(app, target, action);
+// // };
+
+// init_app(document.getElementById("root"), <App />);
