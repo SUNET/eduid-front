995d09a7720a62e90d65c70252183b4f6eeb8290
Nathalie Jonsson <njons@sunet.se>
2021-07-09 18:21:16 +0200

    rewrites browser language check to only look for the one preferred by the user browser, check if we have translation and pass only the local translation to the app. if no translation defaults to 'en'

--- src/login/app_utils/browserLang_i18nSupport.js
+++ src/login/app_utils/browserLang_i18nSupport.js
@@ -3,24 +3,25 @@ import { updateIntl } from "react-intl-redux";
 
 // i18n
 const checkTranslationSupport = () => {
-  //check browser language
-  let browserLanguage = navigator.languages
-    ? navigator.languages[0]
-    : navigator.language || navigator.userLanguage;
-  // only use the first two characters to get the langugae code
-  browserLanguage = browserLanguage.substring(0, 2);
+  // check if we have translation for preferred browser language
+  const selectedBrowserLanguage = navigator.language;
+  const translatedLanguages = AVAILABLE_LANGUAGES.map((lang) => lang[0]);
+  const isTranslatedLanguage = translatedLanguages.includes(
+    selectedBrowserLanguage
+  );
+  // check if we have translation for preferred browser language
+  const translationLocale = isTranslatedLanguage
+    ? selectedBrowserLanguage
+    : "en";
 
-  // get the languages we have provided translations for
-  const translatedLanguages = AVAILABLE_LANGUAGES.map(lang => lang[0]);
-  // if translation exists for the browser language pass it to the components
-  if (translatedLanguages.includes(browserLanguage)) {
-    initStore.dispatch(
-      updateIntl({
-        locale: browserLanguage,
-        messages: LOCALIZED_MESSAGES
-      })
-    );
-  }
+  // set translation locale based on preferred browser lang 
+  // pass on only the translation of that locale
+  initStore.dispatch(
+    updateIntl({
+      locale: translationLocale,
+      messages: LOCALIZED_MESSAGES[translationLocale],
+    })
+  );
 };
 
 export default checkTranslationSupport;
