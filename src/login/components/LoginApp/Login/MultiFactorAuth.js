e7096b7c4fa68ef50f7c39f229bdc73d61dbc147
Nathalie Jonsson <njons@sunet.se>
2021-07-09 12:02:13 +0200

    after linting

--- src/login/components/LoginApp/Login/MultiFactorAuth.js
+++ src/login/components/LoginApp/Login/MultiFactorAuth.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { postRefForWebauthnChallenge } from "../../../redux/actions/postRefForWebauthnChallengeActions";
 import SecurityKey from "./SecurityKey";
 import FrejaeID from "./FrejaeID";
