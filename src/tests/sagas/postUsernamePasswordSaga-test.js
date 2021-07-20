672b3372f92ab90c1b17d3d8cc21479836042ea3
Nathalie Jonsson <njons@sunet.se>
2021-07-20 11:19:27 +0200

    cleans up test and adds in test for FAIL response

--- src/tests/sagas/postUsernamePasswordSaga-test.js
+++ src/tests/sagas/postUsernamePasswordSaga-test.js
@@ -1,9 +1,10 @@
 import expect from "expect";
-import { put, call } from "redux-saga/effects";
+import { call } from "redux-saga/effects";
 import { addLocaleData } from "react-intl";
 addLocaleData("react-intl/locale-data/en");
 import postRequest from "../../login/redux/sagas/postDataRequest";
 import { postUsernamePasswordSaga } from "../../login/redux/sagas/login/postUsernamePasswordSaga";
+import { postUsernamePasswordFail } from "../../login/redux/actions/postUsernamePasswordActions";
 
 const fakeState = {
   config: {
@@ -16,20 +17,37 @@ const fakeState = {
   },
 };
 
-describe("initial API call to /next fires", () => {
-  it("postUsernamePasswordSaga posts the expected data", () => {
-    const action = {
-      type: "POST_USERNAME_PASSWORD,",
-      payload: {
-        username: "username",
-        password: "password",
-      },
-    };
+const action = {
+  type: "POST_USERNAME_PASSWORD,",
+  payload: {
+    username: "username",
+    password: "password",
+  },
+};
 
-    const generator = postUsernamePasswordSaga(action);
-    generator.next();
-    generator.next(fakeState);
+const successResponse = {
+  type: "POST_IDP_PW_AUTH_SUCCESS",
+  payload: {
+    csrf_token: "csrf-token",
+    message: "success",
+  },
+};
 
+const failResponse = {
+  type: "POST_IDP_PW_AUTH_FAIL",
+  error: true,
+  payload: {
+    csrf_token: "csrf-token",
+    message: "error",
+  },
+};
+
+describe("API call to /pw_auth behaves as expected on _SUCCESS", () => {
+  const generator = postUsernamePasswordSaga(action);
+  let next = generator.next();
+  next = generator.next(fakeState);
+  expect(next.value.PUT.action.type).toEqual("LOAD_DATA_REQUEST");
+  it("saga posts the expected data", () => {
     const dataToSend = {
       ref: "dummy-ref",
       csrf_token: "csrf-token",
@@ -37,82 +55,51 @@ describe("initial API call to /next fires", () => {
       password: action.payload.password,
     };
     const url = "https://idp.eduid.docker/pw_auth";
-    const resp = generator.next(fakeState).value;
-    expect(resp).toEqual(call(postRequest, url, dataToSend));
+    const apiCall = generator.next(fakeState).value;
+    expect(apiCall).toEqual(call(postRequest, url, dataToSend));
   });
-
-  it("postUsernamePasswordSaga SUCCESS response is followed by 'NEW_CSRF_TOKEN", () => {
-    let action = {
-      type: "POST_USERNAME_PASSWORD,",
-      payload: {
-        username: "username",
-        password: "password",
-      },
-    };
-
-    const generator = postUsernamePasswordSaga(action);
-    let next = generator.next();
-    next = generator.next(fakeState);
-
-    const dataToSend = {
-      ref: "dummy-ref",
-      csrf_token: "csrf-token",
-      username: "username",
-      password: "password",
-    };
-    const url = "https://idp.eduid.docker/pw_auth";
-    const resp = generator.next(fakeState).value;
-    expect(resp).toEqual(call(postRequest, url, dataToSend));
-
-    action = {
-      type: "POST_IDP_PW_AUTH_SUCCESS",
-      payload: {
-        csrf_token: "csrf-token",
-        message: "success",
-      },
-    };
-
-    next = generator.next(action);
+  it("_SUCCESS response is followed by the expected action types", () => {
+    next = generator.next(successResponse);
     expect(next.value.PUT.action.type).toEqual("NEW_CSRF_TOKEN");
     next = generator.next();
-    delete action.payload.csrf_token;
-    expect(next.value).toEqual(put(action));
+    expect(next.value.PUT.action.type).toEqual("POST_IDP_PW_AUTH_SUCCESS");
+    next = generator.next();
+    expect(next.value.PUT.action.type).toEqual("LOAD_DATA_COMPLETE");
+  });
+  it("done after 'LOAD_DATA_COMPLETE'", () => {
+    const done = generator.next().done;
+    expect(done).toEqual(true);
   });
 });
 
-describe("incorrect user details leads to an error response", () => {
-  let action = {
-    type: "POST_USERNAME_PASSWORD,",
-    payload: {
-      username: "username",
-      password: "password",
-    },
-  };
-
+describe("API call to /pw_auth behaves as expected on _FAIL", () => {
   const generator = postUsernamePasswordSaga(action);
   let next = generator.next();
   next = generator.next(fakeState);
-  it("_FAIL response from backend when missing username", () => {
+  expect(next.value.PUT.action.type).toEqual("LOAD_DATA_REQUEST");
+  it("saga posts unexpected data", () => {
     const dataToSend = {
       ref: "dummy-ref",
       csrf_token: "csrf-token",
-      username: "",
-      password: "password",
+      username: "incorrect-username",
+      password: action.payload.password,
     };
     const url = "https://idp.eduid.docker/pw_auth";
-    const resp = generator.next(fakeState).value;
-    expect(resp).not.toEqual(call(postRequest, url, dataToSend));
+    const apiCall = generator.next(fakeState).value;
+    expect(apiCall).not.toEqual(call(postRequest, url, dataToSend));
+  });
 
-    action = {
-      type: "POST_IDP_PW_AUTH_FAIL",
-      error: true,
-      payload: {
-        message: "error",
-      },
-    };
-
-    next = generator.next(action);
+  it("_FAIL response is followed by the expected action types", () => {
+    next = generator.next(failResponse);
+    expect(next.value.PUT.action.type).toEqual("NEW_CSRF_TOKEN");
     next = generator.next();
     expect(next.value.PUT.action.type).toEqual("POST_IDP_PW_AUTH_FAIL");
+    expect(failResponse).toEqual(postUsernamePasswordFail("error"));
+    next = generator.next();
+    expect(next.value.PUT.action.type).toEqual("LOAD_DATA_COMPLETE");
+  });
+  it("done after 'LOAD_DATA_COMPLETE'", () => {
+    const done = generator.next().done;
+    expect(done).toEqual(true);
   });
 });
