49b455282e0907976b3ef483dffd791fd299a18a
Nathalie Jonsson <njons@sunet.se>
2021-07-20 15:33:01 +0200

    updates tests according to recent changes in saga file and adds test for _FAIL response

--- src/tests/sagas/postUpdatedTouAcceptSaga-test.js
+++ src/tests/sagas/postUpdatedTouAcceptSaga-test.js
@@ -1,9 +1,10 @@
 import expect from "expect";
-import { put, call } from "redux-saga/effects";
+import { call } from "redux-saga/effects";
 import { addLocaleData } from "react-intl";
 addLocaleData("react-intl/locale-data/en");
 import postRequest from "../../login/redux/sagas/postDataRequest";
 import { postUpdatedTouAcceptSaga } from "../../login/redux/sagas/login/postUpdatedTouAcceptSaga";
+import { updateTouAcceptFail } from "../../login/redux/actions/postUpdatedTouAcceptActions";
 
 const fakeState = {
   config: {
@@ -14,99 +15,81 @@ const fakeState = {
     next_page: "TOU",
   },
 };
+const url = "https://idp.eduid.docker/tou";
+const action = {
+  type: "POST_UPDATED_TOU_ACCEPT",
+  payload: {
+    user_accepts: "2016-v1",
+  },
+};
 
-describe("API call to /tou fires", () => {
-  it("postUpdatedTouAcceptSaga posts the expected data", () => {
-    const action = {
-      type: "POST_UPDATED_TOU_ACCEPT",
-      payload: {
-        user_accepts: "2016-v1",
-      },
-    };
+const successResponse = {
+  type: "POST_IDP_TOU_SUCCESS",
+  payload: {
+    csrf_token: "csrf-token",
+    message: "success",
+    finished: true,
+  },
+};
 
-    const generator = postUpdatedTouAcceptSaga(action);
-    generator.next(fakeState);
+const failResponse = {
+  type: "POST_IDP_TOU_FAIL",
+  error: true,
+  payload: {
+    csrf_token: "csrf-token",
+    message: "error",
+  },
+};
 
+describe("second API call to /tou behaves as expected on _SUCCESS", () => {
+  const generator = postUpdatedTouAcceptSaga(action);
+  let next = generator.next();
+  it("saga posts the expected data", () => {
     const dataToSend = {
       ref: "dummy-ref",
       csrf_token: "csrf-token",
       user_accepts: action.payload.user_accepts,
     };
-    const url = "https://idp.eduid.docker/tou";
-    const resp = generator.next(fakeState).value;
-    expect(resp).toEqual(call(postRequest, url, dataToSend));
+    const apiCall = generator.next(fakeState).value;
+    expect(apiCall).toEqual(call(postRequest, url, dataToSend));
   });
-
-  it("postUpdatedTouAcceptSaga SUCCESS response is followed by 'NEW_CSRF_TOKEN'", () => {
-    let action = {
-      type: "POST_UPDATED_TOU_ACCEPT",
-      payload: {
-        user_accepts: "2016-v1",
-      },
-    };
-
-    const generator = postUpdatedTouAcceptSaga(action);
-    let next = generator.next(fakeState);
-
-    const dataToSend = {
-      ref: "dummy-ref",
-      csrf_token: "csrf-token",
-      user_accepts: action.payload.user_accepts,
-    };
-    const url = "https://idp.eduid.docker/tou";
-    const resp = generator.next(fakeState).value;
-    expect(resp).toEqual(call(postRequest, url, dataToSend));
-
-    action = {
-      type: "POST_IDP_TOU_SUCCESS",
-      payload: {
-        csrf_token: "csrf-token",
-        message: "success",
-      },
-    };
-
-    // next = generator.next(action);
-    // expect(next.value.PUT.action.type).toEqual("NEW_CSRF_TOKEN");
-    next = generator.next(action);
+  it("_SUCCESS response is followed by the expected action types", () => {
+    next = generator.next(successResponse);
     expect(next.value.PUT.action.type).toEqual("NEW_CSRF_TOKEN");
     next = generator.next();
-    delete action.payload.csrf_token;
-    expect(next.value).toEqual(put(action));
+    expect(next.value.PUT.action.type).toEqual("POST_IDP_TOU_SUCCESS");
+  });
+  it("{finished: true} fires api call to /next loop ", () => {
+    next = generator.next();
+    expect(next.value.PUT.action.type).toEqual("POST_LOGIN_REF_TO_NEXT");
+  });
+  it("done after 'POST_LOGIN_REF_TO_NEXT'", () => {
+    const done = generator.next().done;
+    expect(done).toEqual(true);
   });
 });
 
-describe("incorrect user details leads to an error response", () => {
-  //  it("postUpdatedTouAcceptSaga posts the expected data", () => {
-  let action = {
-    type: "POST_UPDATED_TOU_ACCEPT",
-    payload: {
-      user_accepts: "2016-v1",
-    },
-  };
-
+describe("second API call to /tou behaves as expected on _FAIL", () => {
   const generator = postUpdatedTouAcceptSaga(action);
-  let next = generator.next(fakeState);
-
-  it("_FAIL response from backend when missing user_accepts", () => {
+  let next = generator.next();
+  it("saga posts unexpected data", () => {
     const dataToSend = {
       ref: "dummy-ref",
       csrf_token: "csrf-token",
-      user_accepts: "",
+      user_accepts: "1997-v3",
     };
-    const url = "https://idp.eduid.docker/tou";
-    const resp = generator.next(fakeState).value;
-    expect(resp).not.toEqual(call(postRequest, url, dataToSend));
-
-    action = {
-      type: "POST_IDP_TOU_FAIL",
-      error: true,
-      payload: {
-        message: "error",
-      },
-    };
-
-    next = generator.next(action);
+    const apiCall = generator.next(fakeState).value;
+    expect(apiCall).not.toEqual(call(postRequest, url, dataToSend));
+  });
+  it("_FAIL response is followed by the expected action types", () => {
+    next = generator.next(failResponse);
+    expect(next.value.PUT.action.type).toEqual("NEW_CSRF_TOKEN");
     next = generator.next();
     expect(next.value.PUT.action.type).toEqual("POST_IDP_TOU_FAIL");
+    expect(failResponse).toEqual(updateTouAcceptFail("error"));
   });
+    it("done after 'POST_IDP_TOU_FAIL'", () => {
+      const done = generator.next().done;
+      expect(done).toEqual(true);
+    });
 });
