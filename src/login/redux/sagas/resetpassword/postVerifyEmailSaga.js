9528bdfee649513271920799293a820f4349255a
Eunju Huss <eunju@sunet.se>
2021-08-03 08:36:40 +0200

    Remove tested decodeWebauthChallenge

--- src/login/redux/sagas/resetpassword/postVerifyEmailSaga.js
+++ src/login/redux/sagas/resetpassword/postVerifyEmailSaga.js
@@ -7,7 +7,6 @@ import {
 } from "../../../../sagas/common";
 import { postLinkCodeFail } from "../../actions/postResetPasswordActions";
 import { history } from "../../../components/App/App";
-import * as CBOR from "../../../../sagas/cbor";
 
 export function requestSendLinkCode(config, data) {
   return window
@@ -27,26 +26,12 @@ export function* useLinkCode() {
       email_code: state.resetPassword.email_code,
       csrf_token: state.config.csrf_token
     };
-    const encodedWebauthnChallenge = yield call(requestSendLinkCode, state.config, data);
-    const decodedWebauthnChallenge = yield put(
-      mfaDecodeMiddleware(encodedWebauthnChallenge)
-    );
-    yield put(putCsrfToken(decodedWebauthnChallenge));
-    yield put(decodedWebauthnChallenge);
+    const resp = yield call(requestSendLinkCode, state.config, data);
+    yield put(putCsrfToken(resp));
+    yield put(resp);
       history.push(`/reset-password/`);
     }
   } catch (error) {
     yield* failRequest(error, postLinkCodeFail(error));
   }
 }
-
-window.CBOR = CBOR;
-const mfaDecodeMiddleware = (response) => {
-  if (response.payload.extra_security.tokens && response.payload.extra_security.tokens.webauthn_options !== undefined) {
-    const raw_options = response.payload.extra_security.tokens.webauthn_options;
-    const options = atob(raw_options.replace(/_/g, "/").replace(/-/g, "+"));
-    const byte_options = Uint8Array.from(options, (c) => c.charCodeAt(0));
-    response.payload.extra_security.tokens.webauthn_options = CBOR.decode(byte_options.buffer);
-  }
-  return response;
-};
