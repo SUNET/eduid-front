98c343c2b1e2de8964f9d241cdb6ed5bc1fcbd4b
Nathalie Jonsson <njons@sunet.se>
2021-08-05 15:44:06 +0200

    as input type was set in both <Field> and <Input> I transferred the show/hide password logic into PasswordInput.js

--- src/login/components/Inputs/PasswordInput.js
+++ src/login/components/Inputs/PasswordInput.js
@@ -1,17 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Field } from "redux-form";
 import CustomInput from "./CustomInput";
+import Input from "reactstrap/lib/Input";
 import InjectIntl from "../../translation/InjectIntl_HOC_factory";
 import PropTypes from "prop-types";
 
-let PasswordInput = ({ required, translate }) => {
+let RenderHideButton = ({ setInputType, translate }) => (
+  <button
+    aria-label="hide password"
+    className="show-hide-button"
+    onClick={() => setInputType("password")}
+  >
+    <div className="button-text-container">
+      {translate("nin_hide_last_four_digits")}
+    </div>
+  </button>
+);
+
+let RenderShowButton = ({ setInputType, translate }) => (
+  <button
+    aria-label="show password"
+    className="show-hide-button"
+    onClick={() => setInputType("text")}
+  >
+    {translate("nin_show_last_four_digits")}
+  </button>
+);
+
+export let PasswordInputElement = (props) => {
+  const { input } = props;
+  const [inputType, setInputType] = useState(props.type);
+  return (
+    <div className="password-input">
+      <Input
+        type={inputType}
+        placeholder={props.placeholder}
+        valid={props.valid}
+        invalid={props.invalid}
+        autoComplete={props.autoComplete}
+        aria-label={props.ariaLabel}
+        aria-required={props.required}
+        {...input}
+      />
+      {inputType === "password" ? (
+        <RenderShowButton {...props} setInputType={setInputType} />
+      ) : inputType === "text" ? (
+        <RenderHideButton {...props} setInputType={setInputType} />
+      ) : null}
+    </div>
+  );
+};
+
+let PasswordInput = ({ translate }) => {
   return (
     <Field
       type="password"
       name="current-password"
       component={CustomInput}
       autoComplete="current-password"
-      required={required}
+      required="true"
       label={translate("login.usernamePw.password-input")}
       placeholder={"enter a password"}
       helpBlock={""}
@@ -21,7 +69,6 @@ let PasswordInput = ({ required, translate }) => {
 
 PasswordInput.propTypes = {
   translate: PropTypes.func.isRequired,
-  required: PropTypes.bool,
 };
 
 export default InjectIntl(PasswordInput);
