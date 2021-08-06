115e301ee76c0e0b810e102f05e479a80a86b471
Eunju Huss <eunju@sunet.se>
2021-08-06 13:26:17 +0200

    Add check for response.payload.extra_security.tokens exists

--- src/login/app_utils/helperFunctions/authenticatorAssertion.js
+++ src/login/app_utils/helperFunctions/authenticatorAssertion.js
@@ -3,28 +3,31 @@ import * as CBOR from "../../../sagas/cbor";
 // 1. Ask backend for a challenge
 // decode the challenge to use it
 export const mfaDecodeMiddleware = (response) => {
-  if (response.payload && response.payload.webauthn_options !== undefined) {
-    const raw_options = response.payload.webauthn_options;
-    if (typeof raw_options === "string") {
-      const options = window.atob(
-        raw_options.replace(/_/g, "/").replace(/-/g, "+")
-      );
-      const byte_options = Uint8Array.from(options, (c) => c.charCodeAt(0));
-      response.payload.webauthn_options = CBOR.decode(byte_options.buffer);
+  if (response.payload){
+    if (response.payload.webauthn_options !== undefined) {
+      const raw_options = response.payload.webauthn_options;
+      if (typeof raw_options === "string") {
+        const options = window.atob(
+          raw_options.replace(/_/g, "/").replace(/-/g, "+")
+        );
+        const byte_options = Uint8Array.from(options, (c) => c.charCodeAt(0));
+        response.payload.webauthn_options = CBOR.decode(byte_options.buffer);
+      }
     }
-  }
-  // challenge for reset-password
-  else if (response.payload.extra_security && response.payload.extra_security.tokens.webauthn_options !== undefined) {
-    const raw_options = response.payload.extra_security.tokens.webauthn_options;
-    if (typeof raw_options === "string") {
-      const options = window.atob(
-        raw_options.replace(/_/g, "/").replace(/-/g, "+")
-      );
-      const byte_options = Uint8Array.from(options, (c) => c.charCodeAt(0));
-      response.payload.extra_security.tokens.webauthn_options = CBOR.decode(byte_options.buffer);
+    // challenge for reset-password
+    else if (response.payload.extra_security && response.payload.extra_security.tokens && 
+      response.payload.extra_security.tokens.webauthn_options !== undefined) {
+      const raw_options = response.payload.extra_security.tokens.webauthn_options;
+      if (typeof raw_options === "string") {
+        const options = window.atob(
+          raw_options.replace(/_/g, "/").replace(/-/g, "+")
+        );
+        const byte_options = Uint8Array.from(options, (c) => c.charCodeAt(0));
+        response.payload.extra_security.tokens.webauthn_options = CBOR.decode(byte_options.buffer);
+      }
     }
+    return response;
   }
-  return response;
 };
 
 // 2. Return autheticator assertion to backend for final verification
