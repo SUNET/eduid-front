dd91b49fb30974c5ca4ef510ab54153cb8e8faeb
Eunju Huss <eunju@sunet.se>
2021-08-12 22:11:10 +0200

    Moves variables out of try block and add if/else for displaying error message in correct page

--- src/login/redux/sagas/resetpassword/postExtraSecurityPhoneSaga.js
+++ src/login/redux/sagas/resetpassword/postExtraSecurityPhoneSaga.js
@@ -21,23 +21,30 @@ export function fetchExtraSecurityPhone(config, data) {
 }
 
 export function* requestPhoneCode() {
+  const url = document.location.href;
+  const state = yield select(state => state);
+  const data = {
+    email_code: state.resetPassword.email_code,
+    csrf_token: state.config.csrf_token,
+    phone_index: state.resetPassword.phone.index,
+  };
   try {
-    const state = yield select(state => state);
-    const data = {
-      email_code: state.resetPassword.email_code,
-      csrf_token: state.config.csrf_token,
-      phone_index: state.resetPassword.phone.index,
-    };
     yield put(eduidRMAllNotify());
     const resp = yield call(fetchExtraSecurityPhone, state.config, data);
     yield put(putCsrfToken(resp));
     yield put(resp);
     if (resp.type === "POST_RESET_PASSWORD_EXTRA_SECURITY_PHONE_SUCCESS") {
-      // history.push(`/reset-password/phone-code-sent/${data.email_code}`);
       clearCountdown();
       countDownStart();
+      history.push(`/reset-password/phone-code-sent/${data.email_code}`);
+    }else if(resp.type === "POST_RESET_PASSWORD_EXTRA_SECURITY_PHONE_FAIL"){
+      if(url.includes("extra-security")){
+        history.push(`/reset-password/extra-security/${data.email_code}`);
+      }else if(url.includes("phone-code-sent")){
+        history.push(`/reset-password/phone-code-sent/${data.email_code}`);
     }
-  } catch (error) {
+  };
+ } catch (error) {
     yield* failRequest(error, requestPhoneCodeFail(error));
   }
 }
