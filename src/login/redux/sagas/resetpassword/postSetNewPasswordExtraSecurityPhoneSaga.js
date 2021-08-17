fc500b83e2aa98534adf81b4cec2d60386b407cf
Eunju Huss <eunju@sunet.se>
2021-08-17 11:54:46 +0200

    Add change path to /success, action typs is SUCCESS

--- src/login/redux/sagas/resetpassword/postSetNewPasswordExtraSecurityPhoneSaga.js
+++ src/login/redux/sagas/resetpassword/postSetNewPasswordExtraSecurityPhoneSaga.js
@@ -5,13 +5,14 @@ import {
 } from "../../../../sagas/common";
 import postRequest from "../postDataRequest";
 import { setNewPasswordExtraSecurityPhoneFail } from "../../actions/postResetNewPasswordActions";
+import { history } from "../../../components/App/App";
 
 export function* postSetNewPasswordExtraSecurityPhone() {
   const url = PASSWORD_SERVICE_URL + "/new-password-extra-security-phone/";
   const state = yield select(state => state);
   const data = {
     email_code: state.resetPassword.email_code,
-    phone_code: state.resetPassword.phone_code,
+    phone_code: state.resetPassword.phone.phone_code,
     password: state.resetPassword.suggested_password,
     csrf_token: state.config.csrf_token
   };
@@ -19,6 +20,9 @@ export function* postSetNewPasswordExtraSecurityPhone() {
     const resp = yield call(postRequest, url, data);
     yield put(putCsrfToken(resp));
     yield put(resp);
+    if(resp.type === "POST_RESET_PASSWORD_NEW_PASSWORD_EXTRA_SECURITY_PHONE_SUCCESS"){
+      return history.push(`/reset-password/success`);
+    }
   }
   catch (error) {
     yield* failRequest(error, setNewPasswordExtraSecurityPhoneFail(error));
