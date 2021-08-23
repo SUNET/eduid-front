5df2115d32d13f99d8fd9ba19d513f773eda24c9
Eunju Huss <eunju@sunet.se>
2021-08-23 15:27:07 +0200

    Add path change, if url is set-new-password, add put selecteExtraSecurity freja

--- src/login/redux/sagas/resetpassword/postVerifyEmailSaga.js
+++ src/login/redux/sagas/resetpassword/postVerifyEmailSaga.js
@@ -5,7 +5,7 @@ import {
   failRequest,
   putCsrfToken
 } from "../../../../sagas/common";
-import { postLinkCodeFail } from "../../actions/postResetPasswordActions";
+import { postLinkCodeFail, selectExtraSecurity } from "../../actions/postResetPasswordActions";
 import { history } from "../../../components/App/App";
 import { mfaDecodeMiddlewareForResetPassword } from "../../../app_utils/helperFunctions/authenticatorAssertion";
 
@@ -21,6 +21,7 @@ export function requestSendLinkCode(config, data) {
 
 export function* useLinkCode() {
   const state = yield select(state => state);
+  const url = document.location.href;
   if(state.resetPassword.email_code){
     const data = {
       email_code: state.resetPassword.email_code,
@@ -31,10 +32,12 @@ export function* useLinkCode() {
       const decodedWebauthnChallenge = mfaDecodeMiddlewareForResetPassword(encodedWebauthnChallenge);
       yield put(putCsrfToken(decodedWebauthnChallenge));
       yield put(decodedWebauthnChallenge);
-      if(decodedWebauthnChallenge && decodedWebauthnChallenge.type === "POST_RESET_PASSWORD_VERIFY_EMAIL_SUCCESS")
+      if(url.includes("set-new-password")){
+        return history.push(`/reset-password/set-new-password/${data.email_code}`),
+        yield put(selectExtraSecurity("freja"));
+      }else if(decodedWebauthnChallenge && decodedWebauthnChallenge.type === "POST_RESET_PASSWORD_VERIFY_EMAIL_SUCCESS") {
         return history.push(`/reset-password/extra-security/${data.email_code}`);
-      else
-        return history.push(`/reset-password/email/`);
+      }else return history.push(`/reset-password/email/`);
     }
     catch (error) {
       yield* failRequest(error, postLinkCodeFail(error));
