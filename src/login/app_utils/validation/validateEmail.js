14ee4f72ebc80affe533c0dfdb178583798e5bac
Eunju Huss <eunju@sunet.se>
2021-08-25 13:53:51 +0200

    Add reusable funtion for startValidate

--- src/login/app_utils/validation/validateEmail.js
+++ src/login/app_utils/validation/validateEmail.js
@@ -1,15 +1,21 @@
 export const validate = (values, props) => {
   const errors = {};
-  if (values !== undefined) {
-    let email = values.email;
-    const pattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if(!props.pristine){
-      if (!email) {
-        errors.email = "required";
-      } else if (!pattern.test(email)) {
-        errors.email = "email.invalid_email";
-      }
+  let email = values.email;
+  const pattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  const startValidate = () => {
+    if (!email) {
+      errors.email = "required";
+    } else if (!pattern.test(email)) {
+      errors.email = "email.invalid_email";
     }
-  }
+  };
+
+  if (values !== undefined) { 
+    if(props.form === "usernamePwForm"){
+      if(!props.pristine){
+        startValidate();
+      }
+    }else startValidate();
   return errors;
+  }
 };
