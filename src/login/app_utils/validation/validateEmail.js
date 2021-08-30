d124169fc51b6cccbc95edc6fc6e3cf76dfad2ed
Eunju Huss <eunju@sunet.se>
2021-08-30 08:35:11 +0200

    Replace back validate and Add own validate for login email input

--- src/login/app_utils/validation/validateEmail.js
+++ src/login/app_utils/validation/validateEmail.js
@@ -1,21 +1,29 @@
-export const validate = (values, props) => {
+export const validate = (values) => {
   const errors = {};
-  let email = values.email;
-  const pattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  const startValidate = () => {
+  if (values !== undefined) {
+    let email = values.email;
+    const pattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     if (!email) {
       errors.email = "required";
     } else if (!pattern.test(email)) {
       errors.email = "email.invalid_email";
     }
-  };
+  }
+  return errors;
+};
 
+export const validateEmailOnLogin = (values, props) => {
+  const errors = {};
+  let email = values.email;
+  const pattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
   if (values !== undefined) { 
-    if(props.form === "usernamePwForm"){
-      if(!props.pristine){
-        startValidate();
+    if(!props.pristine){
+      if (!email) {
+        errors.email = "required";
+      } else if (!pattern.test(email)) {
+        errors.email = "email.invalid_email";
       }
-    }else startValidate();
+    }
   return errors;
   }
 };
