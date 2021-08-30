2a2d9bbdd678f896508c3c299fc2aee428dccdc5
Eunju Huss <eunju@sunet.se>
2021-08-30 08:37:26 +0200

    Add email regex pattern and use emailPattern for validate

--- src/login/app_utils/validation/validateEmail.js
+++ src/login/app_utils/validation/validateEmail.js
@@ -1,11 +1,12 @@
+import { emailPattern } from "./regexPatterns";
+
 export const validate = (values) => {
   const errors = {};
   if (values !== undefined) {
     let email = values.email;
-    const pattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     if (!email) {
       errors.email = "required";
-    } else if (!pattern.test(email)) {
+    } else if (! emailPattern.test(email)) {
       errors.email = "email.invalid_email";
     }
   }
@@ -15,12 +16,11 @@ export const validate = (values) => {
 export const validateEmailOnLogin = (values, props) => {
   const errors = {};
   let email = values.email;
-  const pattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
   if (values !== undefined) { 
     if(!props.pristine){
       if (!email) {
         errors.email = "required";
-      } else if (!pattern.test(email)) {
+      } else if (!emailPattern.test(email)) {
         errors.email = "email.invalid_email";
       }
     }
