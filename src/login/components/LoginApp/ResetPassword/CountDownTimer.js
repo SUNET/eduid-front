8041a69c02db65baa524229195ac1b51008f6175
Eunju Huss <eunju@sunet.se>
2021-09-14 12:46:20 +0200

    Add function countRealTime

--- src/login/components/LoginApp/ResetPassword/CountDownTimer.js
+++ src/login/components/LoginApp/ResetPassword/CountDownTimer.js
@@ -77,3 +77,27 @@ export const RenderingResendCodeTimer = (props) => {
     </>
   )
 }
+
+
+export const countRealTime = () => {
+  let countDownDate = getLocalStorage("REALTIME");
+  // Update the count down every 1 second
+  let timer = setInterval(function() {
+  // Get today's date and time
+  let now = new Date().getTime();
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+  // Time calculations for minutes and seconds
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="timer"
+  document.getElementById("timer").innerHTML = minutes + ": " + seconds;
+  // If the count down is over, write some text 
+    if (countDownDate && distance < 0) {
+      document.querySelector("#timer").classList.add('display-none');
+      clearInterval(timer);
+      window.localStorage.removeItem("REALTIME")
+    }
+  }, 1000);
+}
