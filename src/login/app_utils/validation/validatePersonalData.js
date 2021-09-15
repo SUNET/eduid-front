0230acb4496a52a5aa9d5259f7520a19fa4c43bd
Nathalie Jonsson <njons@sunet.se>
2021-09-15 11:48:14 +0200

    moves validation to its own file

--- src/login/app_utils/validation/validatePersonalData.js
+++ src/login/app_utils/validation/validatePersonalData.js
@@ -2,17 +2,29 @@ import { emptyStringPattern } from "./regexPatterns";
 
 const validatePersonalData = (values, props) => {
   const errors = {};
-  ["given_name", "surname", "display_name", "language"].forEach((inputName) => {
-    if (!values[inputName] || emptyStringPattern.test(values[inputName])) {
-      errors[inputName] = "required";
-    }
-    //none of the fields value properties differ from their initial properties will get error message.
-    else if (props.pristine) {
-      errors[inputName] = "value not changed";
-    } else if (values[inputName].trim() === props.initialValues[inputName]) {
-      errors[inputName] = "value not changed";
-    }
-  });
+  if (values !== undefined) {
+    console.log("values", values);
+    console.log("values.language", values.language);
+
+    [("given_name", "surname", "display_name", "language")].forEach(
+      (inputName) => {
+        console.log("!values[inputName]", !values[inputName]);
+        if (!values[inputName] || emptyStringPattern.test(values[inputName])) {
+          console.log("hello");
+          // errors[inputName] = "required";
+        }
+        //none of the fields value properties differ from their initial properties will get error message.
+        else if (props.pristine) {
+          errors[inputName] = "value not changed";
+        } else if (
+          values[inputName].trim() === props.initialValues[inputName]
+        ) {
+          errors[inputName] = "value not changed";
+        }
+      }
+    );
+  }
+  console.log("errors", errors);
   return errors;
 };
 
