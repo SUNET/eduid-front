02554fd2bd885ae4338d0b03a794ddc6bd0a401f
Eunju Huss <eunju@sunet.se>
2021-09-15 09:36:13 +0200

    Change translate text for resetpw.send-sms-success

--- src/login/translation/defaultMessages/password.js
+++ src/login/translation/defaultMessages/password.js
@@ -373,7 +373,7 @@ export const resetPassword = {
   "resetpw.send-sms-success": (
     <FormattedMessage
       id="resetpw.send-sms-success"
-      defaultMessage={`Phone code has been sent.`}
+      defaultMessage={`One time password has been sent to your phone.`}
     />
   ),
 
