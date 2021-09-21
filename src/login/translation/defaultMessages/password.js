e0877e80e7b76a694a3ac42c542aab6a805168b7
Eunju Huss <eunju@sunet.se>
2021-09-21 11:44:47 +0200

    Adjust translate text for resetpw.expired-email-code

--- src/login/translation/defaultMessages/password.js
+++ src/login/translation/defaultMessages/password.js
@@ -338,7 +338,7 @@ export const resetPassword = {
   "resetpw.expired-email-code": (
     <FormattedMessage
       id="resetpw.expired-email-code"
-      defaultMessage={`Email code has expired. Please try again.`}
+      defaultMessage={`Password reset link has expired. Please send link again.`}
     /> 
   ),
   "resetpw.set-new-password-heading": (
