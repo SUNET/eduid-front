8e12ba7faf4fd41bc687d5d0956435edfb53a5da
Nathalie Jonsson <njons@sunet.se>
2021-09-22 16:36:36 +0200

    adds in new recieved invites table for user to accept or reject if any invites

--- src/login/components/GroupManagement/GroupManagement.js
+++ src/login/components/GroupManagement/GroupManagement.js
@@ -1,7 +1,9 @@
 import React, { Fragment } from "react";
+import { useSelector } from "react-redux";
 import InjectIntl from "../../translation/InjectIntl_HOC_factory";
 import CreateGroup from "./Groups/CreateGroupContainer";
 import GroupsParent from "./Groups/GroupsParent";
+import AllInvitesList from "./Invites/AllInvitesList";
 
 const RenderCreateGroupButton = ({ createGroup, handleOpenCreateGroup }) => (
   <Fragment>
@@ -12,6 +14,14 @@ const RenderCreateGroupButton = ({ createGroup, handleOpenCreateGroup }) => (
     )}
   </Fragment>
 );
+
+const RenderInvitesToMe = (props) => {
+  const invites = useSelector((state) => state.invites.invitesToMe);
+  const hasInvites = invites.length > 0 ? true : false;
+  return (
+    <Fragment>{hasInvites ? <AllInvitesList {...props} /> : null}</Fragment>
+  );
+};
 
 const RenderCreateGroupOrGroupData = (props) => {
   if (props.loading) return <p>Loading...</p>;
@@ -30,6 +40,7 @@ const GroupManagement = (props) => (
         Create groups with other eduID users. What the groups are used for is up
         to you and the local services your university provides.
       </p>
+      <RenderInvitesToMe {...props} />
       <RenderCreateGroupOrGroupData {...props} />
     </div>
   </article>
