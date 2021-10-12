e47723d217880afc98292a634f05d40c27a57edc
Eunju Huss <eunju@sunet.se>
2021-10-12 17:05:14 +0200

    Prettier formatting

--- src/login/redux/actions/resetPasswordActions.js
+++ src/login/redux/actions/resetPasswordActions.js
@@ -1,14 +1,17 @@
 import { createAction } from "@reduxjs/toolkit";
 
-export const resetPasswordSagaFail = createAction("RESET_PASSWORD_SAGA_FAIL", function prepare(err) {
-  return {
-    error: true,
-    payload: {
-      message: err.toString(),
-      info: err
-    },
- };
-});
+export const resetPasswordSagaFail = createAction(
+  "RESET_PASSWORD_SAGA_FAIL",
+  function prepare(err) {
+    return {
+      error: true,
+      payload: {
+        message: err.toString(),
+        info: err,
+      },
+    };
+  }
+);
 
 // Action connected to postResetPasswordSaga. Will post the email address to the /(reset-password) endpoint.
 export const requestEmailLink = createAction("requestEmailLink");
@@ -19,45 +22,60 @@ export const saveLinkCode = createAction("saveLinkCode");
 export const useLinkCode = createAction("useLinkCode");
 
 // Action connected to postExtraSecurityPhonSaga. Will post the phone code the user request to the extra-security-phone/ endpoint.
-export const requestPhoneCode = createAction("requestPhoneCode", function prepare(phone) {
-  return {
-    payload: {
-      phone: {
-        index: phone.index,
-        number: phone.number
-      }
-    }
-  };
-});
+export const requestPhoneCode = createAction(
+  "requestPhoneCode",
+  function prepare(phone) {
+    return {
+      payload: {
+        phone: {
+          index: phone.index,
+          number: phone.number,
+        },
+      },
+    };
+  }
+);
 
-export const savePhoneCode = createAction("savePhoneCode", function prepare(code) {
-  return {
-    payload: {
-      phone: {
-        phone_code: code
-      }
-    }
-  };
-});
+export const savePhoneCode = createAction(
+  "savePhoneCode",
+  function prepare(code) {
+    return {
+      payload: {
+        phone: {
+          phone_code: code,
+        },
+      },
+    };
+  }
+);
 
 export const selectExtraSecurity = createAction("selectExtraSecurity");
 
-export const storeNewPassword = createAction("storeNewPassword", function prepare(newPassword) {
-  return {
-    payload: {
-      new_password: newPassword
-    }
-  };
-});
+export const storeNewPassword = createAction(
+  "storeNewPassword",
+  function prepare(newPassword) {
+    return {
+      payload: {
+        new_password: newPassword,
+      },
+    };
+  }
+);
 
-// Action connected to postSetNewPasswordSaga. Will post the new password to the new-password/ endpoint.  
+// Action connected to postSetNewPasswordSaga. Will post the new password to the new-password/ endpoint.
 export const setNewPassword = createAction("setNewPassword");
 
-// Action connected to postSetNewPasswordSecurityPhoneSaga. Will post the new password to the extra-security-phone/ endpoint.   
-export const setNewPasswordExtraSecurityPhone = createAction("setNewPasswordExtraSecurityPhone");
+// Action connected to postSetNewPasswordSecurityPhoneSaga. Will post the new password to the extra-security-phone/ endpoint.
+export const setNewPasswordExtraSecurityPhone = createAction(
+  "setNewPasswordExtraSecurityPhone"
+);
 
-// Action connected to postSetNewPasswordSecurityTokenSaga. Will post the new password to the new-password-extra-security-token/ endpoint.  
-export const setNewPasswordExtraSecurityToken = createAction("setNewPasswordExtraSecurityToken");
+// Action connected to postSetNewPasswordSecurityTokenSaga. Will post the new password to the new-password-extra-security-token/ endpoint.
+export const setNewPasswordExtraSecurityToken = createAction(
+  "setNewPasswordExtraSecurityToken"
+);
 
-// Action connected to postSetNewPasswordExtraSecurityExternalMfaSaga. Will post the new password to the new-password-extra-security-external-mfa/ endpoint.  
-export const setNewPasswordExtraSecurityExternalMfa = createAction("setNewPasswordExtraSecurityExternalMfa");
+// Action connected to postSetNewPasswordExtraSecurityExternalMfaSaga. Will post the new password to the new-password-extra-security-external-mfa/ endpoint.
+export const setNewPasswordExtraSecurityExternalMfa = createAction(
+  "setNewPasswordExtraSecurityExternalMfa"
+);
--- src/login/redux/reducers/resetPasswordReducer.js
+++ src/login/redux/reducers/resetPasswordReducer.js
@@ -3,70 +3,70 @@ import * as getWebauthnActions from "../actions/getWebauthnAssertionActions";
 import { createReducer } from "@reduxjs/toolkit";
 
 const resetPasswordData = {
-    email_address: null,
-    email_code: null,
-    phone: {},
-    webauthn_assertion: null,
-    selected_option: null,
-    new_password: null
+  email_address: null,
+  email_code: null,
+  phone: {},
+  webauthn_assertion: null,
+  selected_option: null,
+  new_password: null,
 };
 
 const resetPasswordReducer = createReducer(resetPasswordData, {
   [resetPasswordActions.savePhoneCode]: (state, action) => {
-    state.phone_code = action.payload
+    state.phone_code = action.payload;
   },
   [resetPasswordActions.requestEmailLink]: (state, action) => {
-    state.email_address = action.payload
+    state.email_address = action.payload;
   },
   [resetPasswordActions.saveLinkCode]: (state, action) => {
-    state.email_code = action.payload
+    state.email_code = action.payload;
   },
   [resetPasswordActions.useLinkCode]: (state, action) => {
     return {
       ...state,
-      ...action.payload
-    }
+      ...action.payload,
+    };
   },
-  "POST_RESET_PASSWORD_VERIFY_EMAIL_SUCCESS": (state, action) => {
+  POST_RESET_PASSWORD_VERIFY_EMAIL_SUCCESS: (state, action) => {
     // Process a successful response from the /verify-email endpoint.
     return {
       ...state,
-      ...action.payload
-    }
+      ...action.payload,
+    };
   },
   [resetPasswordActions.requestPhoneCode]: (state, action) => {
     return {
       ...state,
-      ...action.payload
-    }
+      ...action.payload,
+    };
   },
-  [resetPasswordActions.savePhoneCode] : (state, action) => {
+  [resetPasswordActions.savePhoneCode]: (state, action) => {
     return {
       ...state,
-      ...action.payload
-    }
+      ...action.payload,
+    };
   },
   [getWebauthnActions.GET_WEBAUTHN_ASSERTION]: (state, action) => {
     return {
       ...state,
-      ...action.payload
-    }
+      ...action.payload,
+    };
   },
   [getWebauthnActions.GET_WEBAUTHN_ASSERTION_FAIL]: (state, action) => {
     return {
       ...state,
-      ...action.payload
-    }
+      ...action.payload,
+    };
   },
-  [resetPasswordActions.selectExtraSecurity] : (state, action) => {
-    state.selected_option = action.payload
+  [resetPasswordActions.selectExtraSecurity]: (state, action) => {
+    state.selected_option = action.payload;
   },
   [resetPasswordActions.storeNewPassword]: (state, action) => {
     return {
       ...state,
-      ...action.payload
-    }
+      ...action.payload,
+    };
   },
-})
+});
 
 export default resetPasswordReducer;
