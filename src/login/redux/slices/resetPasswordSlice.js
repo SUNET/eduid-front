26602ee0be36a4c68d1ac68ef64633f38ecbf2e3
Eunju Huss <eunju@sunet.se>
2021-10-13 14:59:22 +0200

    Add comments

--- src/login/redux/slices/resetPasswordSlice.js
+++ src/login/redux/slices/resetPasswordSlice.js
@@ -12,23 +12,28 @@ export const resetPasswordSlice = createSlice({
     new_password: null,
   },
   reducers: {
+    // Common action to signal a caught exception in one of the reset password sagas. Because it ends in _FAIL,
     resetPasswordSagaFail: (state, action) => ({
       ...state,
       ...action.payload,
     }),
+    // API call successfully post data save it to store
     resetPasswordSagaSuccess: (state, action) => ({
       ...state,
       ...action.payload,
     }),
+    // Stor phone_code for API call /new-password-extra-security-phone endpoint.
     savePhoneCode: (state, action) => {
       state.phone.phone_code = action.payload;
     },
+    // Action connected to postResetPasswordSaga. Will post email_address to the state.config.reset_password_url/ endpoint.
     requestEmailLink: (state, action) => {
       state.email_address = action.payload;
     },
     saveLinkCode: (state, action) => {
       state.email_code = action.payload;
     },
+    // Selected_option for correct endpoint
     selectExtraSecurity: (state, action) => {
       state.selected_option = action.payload;
     },
@@ -36,9 +41,11 @@ export const resetPasswordSlice = createSlice({
       ...state,
       ...action.payload,
     }),
+    // Action connected to postExtraSecurityPhoneSaga. Will post phone.index to the /extra-security-phone endpoint.
     requestPhoneCode: (state, action) => {
       state.phone.index = action.payload.index;
     },
+    // Store the result from navigator.credentials.get() in the state, after the user used a webauthn credential.
     getWebauthnAssertion: (state, action) => {
       state.webauthn_assertion = action.payload;
     },
@@ -48,15 +55,19 @@ export const resetPasswordSlice = createSlice({
     storeNewPassword: (state, action) => {
       state.new_password = action.payload;
     },
+    // Action connected to postSetNewPasswordSaga. Will post stored new_password to the /new-password endpoint.
     setNewPassword: (state, action) => ({ ...state, ...action.payload }),
+    // Action connected to postSetNewPasswordExtraSecurityPhoneSaga. Will post stored phone_code, new_password to the /new-password-extra-security-phone endpoint.
     setNewPasswordExtraSecurityPhone: (state, action) => ({
       ...state,
       ...action.payload,
     }),
+    // Action connected to postSetNewPasswordExtraSecurityPhoneSaga. Will post stored phone_code, new_password to the /new-password-extra-security-phone endpoint.
     setNewPasswordExtraSecurityToken: (state, action) => ({
       ...state,
       ...action.payload,
     }),
+    // Action connected to postSetNewPasswordExternalMfaSaga. Will post stored phone_code, new_password to the /new-password-extra-security-external-mfa endpoint.
     setNewPasswordExtraSecurityExternalMfa: (state, action) => ({
       ...state,
       ...action.payload,
