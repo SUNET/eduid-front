275ecd178329b7ed78bf5584a4004bc89f54d402
Eunju Huss <eunju@sunet.se>
2021-10-18 14:15:47 +0200

    Migrate login action to loginSlice

--- src/login/redux/actions/addDataToStoreActions.js
+++ src/login/redux/actions/addDataToStoreActions.js
@@ -1,5 +1,3 @@
-import {createAction} from "@reduxjs/toolkit";
-
 export const ADD_NAVID_TO_STORE = "ADD_NAVID_TO_STORE";
 export const ADD_COOKIE_STATUS_TO_STORE = "ADD_COOKIE_STATUS_TO_STORE";
 
@@ -16,12 +14,3 @@ export const checkCookieStatus = (status) => ({
     hasCookie: status,
   },
 });
-
-// Add the login reference (currently an UUID extracted from the URL), to the store.
-export const addLoginRef = createAction("addLoginRef");
-
-// During app initialisation, we figure out what versions of the TOU we have. This action stores that in the store.
-export const addTouVersions = createAction("addTouVersions");
-
-// Store the result from navigator.credentials.get() in the state, after the user used a webauthn credential.
-export const addWebauthnAssertion = createAction("addWebauthnAssertion");
