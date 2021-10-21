5c866a56fa1ae2acca93ae788fa4d5b0d995f685
Eunju Huss <eunju@sunet.se>
2021-10-21 13:55:32 +0200

    Store decodedResponse.payload data when api call successfully post

--- src/login/redux/sagas/resetpassword/postVerifyEmailSaga.js
+++ src/login/redux/sagas/resetpassword/postVerifyEmailSaga.js
@@ -8,43 +8,27 @@ import { mfaDecodeMiddlewareForResetPassword } from "../../../app_utils/helperFu
 export function* requestLinkCode() {
   const state = yield select((state) => state);
   const url = state.config.reset_password_url + "verify-email/";
-  const locationUrl = document.location.href;
   if (state.resetPassword.email_code) {
     const data = {
       email_code: state.resetPassword.email_code,
       csrf_token: state.config.csrf_token,
     };
     try {
-      const encodedWebauthnChallenge = yield call(postRequest, url, data);
-      const decodedWebauthnChallenge = mfaDecodeMiddlewareForResetPassword(
-        encodedWebauthnChallenge
-      );
-      yield put(putCsrfToken(decodedWebauthnChallenge));
-      yield put(decodedWebauthnChallenge);
-      // if API call successfully post data save it to store
-      if (
-        decodedWebauthnChallenge.type ===
-        "POST_RESET_PASSWORD_VERIFY_EMAIL_SUCCESS"
-      ) {
-        yield put(
-          resetPasswordSlice.actions.resetPasswordVerifyEmailSuccess(
-            decodedWebauthnChallenge.payload
-          )
-        );
+      const response = yield call(postRequest, url, data);
+      yield put(putCsrfToken(response));
+      if (response.error) {
+        // Errors are handled in notifyAndDispatch() (in notify-middleware.js)
+        yield put(response);
+        return history.push(`/reset-password/email`);
       }
-      if (locationUrl.includes("set-new-password")) {
-        return history.push(
-          `/reset-password/set-new-password/${data.email_code}`
-        );
-      } else if (
-        decodedWebauthnChallenge &&
-        decodedWebauthnChallenge.type ===
-          "POST_RESET_PASSWORD_VERIFY_EMAIL_SUCCESS"
-      ) {
-        return history.push(
-          `/reset-password/extra-security/${data.email_code}`
-        );
-      } else return history.push(`/reset-password/email`);
+      const decodedResponse = mfaDecodeMiddlewareForResetPassword(response);
+      // if API call successfully post data save it to store
+      yield put(
+        resetPasswordSlice.actions.resetPasswordVerifyEmailSuccess(
+          decodedResponse.payload
+        )
+      );
+      return history.push(`/reset-password/extra-security/${data.email_code}`);
     } catch (error) {
       yield* failRequest(
         error,
