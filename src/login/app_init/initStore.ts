e1d1321871be3bd7ed48b929a05e82d0ac8af3b9
Fredrik Thulin <fredrik@thulin.net>
2021-11-08 12:46:43 +0100

    fix TypeScript complaints
    no idea if it actually works :)

--- src/login/app_init/initStore.ts
+++ src/login/app_init/initStore.ts
@@ -6,7 +6,7 @@ import { history } from "../components/App/App";
 
 // import all sagas to add to store
 import rootSaga from "../app_config/login-rootSaga";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Reducer } from "@reduxjs/toolkit";
 import initReducer from "./init_reducer";
 import notificationsReducer from "../components/Notifications/Notifications_reducer";
 import { routerReducer } from "react-router-redux";
@@ -21,21 +21,31 @@ import logger from "redux-logger";
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware, logger, notifyAndDispatch, routerMiddleware(history)];
 
+// TODO: Type the notificationsReducer instead of defining a type here
+type NotificationStateType = {
+  messages: unknown[];
+  errors: unknown[];
+};
+
 const initStore = configureStore({
   reducer: {
     config: initReducer,
     app: appReducer,
     login: loginSlice.reducer,
-    notifications: notificationsReducer,
+    notifications: notificationsReducer as Reducer<NotificationStateType>,
     router: routerReducer,
     form: formReducer,
     intl: intlReducer,
     resetPassword: resetPasswordSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), ...middlewares],
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middlewares),
   devTools: process.env.NODE_ENV !== 'production',
 })
-
 sagaMiddleware.run(rootSaga);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof initStore.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof initStore.dispatch
 
 export default initStore;
