5a7b8e66c0f56acf314f5b9e13d13a2abad06b75
Fredrik Thulin <fredrik@thulin.net>
2021-11-09 10:31:00 +0100

    configure babel and jest for typescript

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = function (api) {
   api.cache(false);
-  const presets = ["@babel/preset-env", "@babel/preset-react"];
+  const presets = [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+  ];
   const plugins = [
     [
       "@babel/plugin-transform-runtime",
