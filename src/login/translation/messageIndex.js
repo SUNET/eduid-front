9993f510d7b6595d91aec3986dd9a3554db51f83
Eunju Huss <eunju@sunet.se>
2021-11-11 13:07:28 +0100

    Break lines

--- src/login/translation/messageIndex.js
+++ src/login/translation/messageIndex.js
@@ -93,7 +93,9 @@ export const unformattedMessages = defineMessages({
     id: "chpass.help-text-newpass-tips",
     description: "help text for custom password tips",
     defaultMessage:
-      "Use upper- and lowercase characters, but not at the beginning or end\n Add digits somewhere, but not at the beginning or end\n Add special characters, such as  @ $ \\u005c  + _ %\n Spaces are ignored",
+      "Use upper- and lowercase characters, but not at the beginning or end\n" +
+      "Add digits somewhere, but not at the beginning or end\n" +
+      "Add special characters, such as  @ $ \\u005c  + _ %\n Spaces are ignored",
   },
   "cm.lost_code": {
     id: "cm.lost_code",
