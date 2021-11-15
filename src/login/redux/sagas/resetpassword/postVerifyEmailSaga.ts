313da4b68d0bab3cc357cf8fe798bfa7fedc7549
Eunju Huss <eunju@sunet.se>
2021-11-15 08:49:31 +0100

    Fix conflict after merge

--- src/login/redux/sagas/resetpassword/postVerifyEmailSaga.ts
+++ src/login/redux/sagas/resetpassword/postVerifyEmailSaga.ts
@@ -3,10 +3,24 @@ import { failRequest, putCsrfToken } from "../../../../sagas/common";
 import resetPasswordSlice from "../../slices/resetPasswordSlice";
 import postRequest from "../postDataRequest";
 import { history } from "../../../components/App/App";
-import { mfaDecodeMiddlewareForResetPassword } from "../../../app_utils/helperFunctions/authenticatorAssertion";
+import { LoginRootState } from "../../../app_init/initStore";
+import { PayloadAction } from "@reduxjs/toolkit";
+
+export type extraSecurityParameters = {
+  external_mfa: boolean;
+  phone_numbers?: [];
+  tokens: object;
+};
+
+export type VerifyEmailResponse = {
+  email_address: string;
+  email_code: string;
+  extra_security?: extraSecurityParameters;
+  suggested_password: string;
+};
 
 export function* requestLinkCode() {
-  const state = yield select((state) => state);
+  const state: LoginRootState = yield select((state) => state);
   const url = state.config.reset_password_url + "verify-email/";
   const locationUrl = document.location.href;
   if (state.resetPassword.email_code) {
@@ -15,7 +29,12 @@ export function* requestLinkCode() {
       csrf_token: state.config.csrf_token,
     };
     try {
-      const response = yield call(postRequest, url, data);
+      const response: PayloadAction<
+        VerifyEmailResponse,
+        string,
+        never,
+        boolean
+      > = yield call(postRequest, url, data);
       yield put(putCsrfToken(response));
       if (response.error) {
         // Errors are handled in notifyAndDispatch() (in notify-middleware.js)
@@ -23,9 +42,12 @@ export function* requestLinkCode() {
         history.push(`/reset-password/email`);
         return;
       }
-      const decodedResponse = mfaDecodeMiddlewareForResetPassword(response);
       // if API call successfully post data save it to store
-      yield put(resetPasswordSlice.actions.resetPasswordVerifyEmailSuccess(decodedResponse.payload));
+      yield put(
+        resetPasswordSlice.actions.resetPasswordVerifyEmailSuccess(
+          response.payload
+        )
+      );
       // Completed with frejaeid location changes to set-new-password
       if (locationUrl.includes("set-new-password")) {
         history.push(`/reset-password/set-new-password/${data.email_code}`);
@@ -33,7 +55,10 @@ export function* requestLinkCode() {
         history.push(`/reset-password/extra-security/${data.email_code}`);
       }
     } catch (error) {
-      yield* failRequest(error, resetPasswordSlice.actions.resetPasswordSagaFail(error));
+      yield* failRequest(
+        error,
+        resetPasswordSlice.actions.resetPasswordSagaFail()
+      );
     }
   }
 }
