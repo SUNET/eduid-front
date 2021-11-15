7b1fe9aa6c24e609ef5a267f33d89c528ba98db7
Eunju Huss <eunju@sunet.se>
2021-11-15 14:37:29 +0100

    Add interface for initial state

--- src/login/redux/slices/resetPasswordSlice.ts
+++ src/login/redux/slices/resetPasswordSlice.ts
@@ -1,23 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 // CreateSlice function will return an object with actions and reducer
+import { performAuthentication, webauthnAssertion } from "../../app_utils/helperFunctions/navigatorCredential";
+// Define a type for the slice state
+
+export type ExtraSecurity = { external_mfa: boolean; phone_numbers?: []; tokens: { webauthn_options: string } };
+
+interface ResetPasswordState {
+  email_address?: string;
+  email_code?: string;
+  phone: { index?: string; number?: string; phone_code?: string };
+  webauthn_assertion?: webauthnAssertion;
+  selected_option?: string;
+  new_password?: string;
+  suggested_password?: string;
+  extra_security?: ExtraSecurity;
+}
+
+// Define the initial state using that type
+const initialState: ResetPasswordState = {
+  email_address: undefined,
+  email_code: undefined,
+  phone: { index: undefined, number: undefined, phone_code: undefined },
+  webauthn_assertion: undefined,
+  selected_option: undefined,
+  new_password: undefined,
+  suggested_password: undefined,
+  extra_security: undefined,
+};
 
 export const resetPasswordSlice = createSlice({
   name: "resetPassword",
-  initialState: {
-    email_address: null,
-    email_code: null,
-    phone: {},
-    webauthn_assertion: null,
-    selected_option: null,
-    new_password: null,
-    suggested_password: null,
-    extra_security: {
-      tokens: { webauthn_options: null },
-    },
-  },
+  initialState,
   reducers: {
     // Store phone_code for API call /new-password-extra-security-phone endpoint.
-    savePhoneCode: (state, action) => {
+    savePhoneCode: (state, action: PayloadAction<string>) => {
       state.phone.phone_code = action.payload;
     },
     // Action connected to postResetPasswordSaga. Will post email_address to the state.config.reset_password_url/ endpoint.
@@ -28,11 +44,11 @@ export const resetPasswordSlice = createSlice({
       state.email_code = action.payload;
     },
     // Depending on selectedOption, this will call correct action of new password.
-    selectExtraSecurity: (state, action) => {
+    selectExtraSecurity: (state, action: PayloadAction<string>) => {
       state.selected_option = action.payload;
     },
     // Action connected to postExtraSecurityPhoneSaga. Will post phone.index to the /extra-security-phone endpoint.
-    requestPhoneCode: (state, action) => {
+    requestPhoneCode: (state, action: PayloadAction<{ index: string; number: string }>) => {
       state.phone.index = action.payload.index;
       state.phone.number = action.payload.number;
     },
@@ -42,10 +58,18 @@ export const resetPasswordSlice = createSlice({
     cancelWebauthnAssertion: (state) => {
       state.webauthn_assertion = undefined;
     },
-    storeNewPassword: (state, action) => {
+    storeNewPassword: (state, action: PayloadAction<string>) => {
       state.new_password = action.payload;
     },
-    resetPasswordVerifyEmailSuccess: (state, action) => {
+    resetPasswordVerifyEmailSuccess: (
+      state,
+      action: PayloadAction<{
+        email_address: string;
+        email_code: string;
+        extra_security: ExtraSecurity;
+        suggested_password: string;
+      }>
+    ) => {
       state.email_address = action.payload.email_address;
       state.email_code = action.payload.email_code;
       state.extra_security = action.payload.extra_security;
