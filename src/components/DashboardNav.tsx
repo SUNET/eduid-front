807f8bd701406b1703593b73d4e2c52a69527f6d
Fredrik Thulin <fredrik@thulin.net>
2021-11-26 19:28:33 +0100

    change dynamic FormattedMessage to static ones
    we talked about this, and it seemed a better option to just inform the
    user of the possibilities in the tool-tip, rather than trying to be
    smart and selecting the available ones (and neglecting to inform the
    users about the other options, which would be visible if the user just
    went to the page in question)

--- src/components/DashboardNav.tsx
+++ src/components/DashboardNav.tsx
@@ -18,13 +18,12 @@ function DashboardNav(): JSX.Element {
   const nins = useDashboardAppSelector((state) => state.nins.nins);
   const phones = useDashboardAppSelector((state) => state.phones.phones as PhoneInfo[]);
   const verifiedNin = nins.filter((nin) => nin.verified);
-  const verifiedSweNumber = phones.filter((phone) => phone.verified && phone.number.startsWith("+46"));
-  const unverifiedNumber = phones.filter((phone) => !phone.verified);
+  const verifiedPhones = phones.filter((phone) => phone.verified);
   // depending on languages show different styles
   const selectedLanguage = useDashboardAppSelector((state) => state.intl.locale) as string;
 
   /*
-   * Render on-mouse-over tip at the "Identity" tab telling the user how they might go ahead with proofing their identity
+   * Render on-mouse-over tip at the "Identity" tab nudging the user to proof their identity
    */
   function tips_at_identity(): JSX.Element | undefined {
     if (verifiedNin.length) {
@@ -32,55 +31,39 @@ function DashboardNav(): JSX.Element {
       return undefined;
     }
 
-    const options: JSX.Element[] = [];
-
-    // Freja eID+ is available to everyone
-    options.push(
-      <b>
-        <FormattedMessage defaultMessage="a digital ID-card" description="On-mouse-over tooltip for Identity tab" />
-      </b>
+    return (
+      <NotificationTip
+        position={`settings ${selectedLanguage}`}
+        tipText={
+          <FormattedMessage
+            defaultMessage="Verify your identity to get the most ouf of your eduID"
+            description="Dashboard navigation tooltip"
+          />
+        }
+      />
     );
-
-    options.push(
-      <b>
-        <FormattedMessage defaultMessage="some other option" description="On-mouse-over tooltip for Identity tab" />
-      </b>
-    );
-
-    const tipText = (
-      <span>
-        <FormattedMessage
-          defaultMessage="You can verify your identity with"
-          description="On-mouse-over tooltip for Identity tab"
-        />{" "}
-        {options.map((elem, index) => (
-          <span>
-            {index ? ", " : ""}
-            {elem}
-          </span>
-        ))}
-      </span>
-    );
-
-    return <NotificationTip position={`settings ${selectedLanguage}`} tipText={tipText} />;
   }
 
   /*
    * Render on-mouse-over tip at the "Settings" tab telling the user to confirm any unconfirmed phone numbers
    */
   function tips_at_settings(): JSX.Element | undefined {
-    if (!unverifiedNumber.length) {
-      // no unverified numbers, don't need to tell the user anything
+    if (verifiedPhones.length) {
+      // one or more registered phones, don't need to tell the user anything
       return undefined;
     }
-    const tipText = (
-      <FormattedMessage
-        defaultMessage="<b>Confirm your number</b> to verify your id by phone"
-        description="On-mouse-over tooltip for Settings tab"
+
+    return (
+      <NotificationTip
+        position={`settings ${selectedLanguage}`}
+        tipText={
+          <FormattedMessage
+            defaultMessage="Add and verify your phone number for better security"
+            description="Dashboard navigation tooltip"
+          />
+        }
       />
     );
-
-    return <NotificationTip position={`settings ${selectedLanguage}`} tipText={tipText} />;
   }
 
   const tipsAtSettings = tips_at_settings();
