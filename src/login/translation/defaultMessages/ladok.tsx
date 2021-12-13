46a3387caec1e67e535e21a58e6b16c7dc97b162
Fredrik Thulin <fredrik@thulin.net>
2021-12-13 17:11:16 +0100

    add remaining two possible error messages from the eduid-ladok backend

--- src/login/translation/defaultMessages/ladok.tsx
+++ src/login/translation/defaultMessages/ladok.tsx
@@ -6,7 +6,21 @@ export const apiResponses = {
   "ladok.no-data-for-user": (
     <FormattedMessage
       id="ladok.no-data-for-user"
-      defaultMessage="No information for you found in Ladok with that institution"
+      defaultMessage="No information for you found in Ladok with that institution."
+    />
+  ),
+
+  "ladok.no-verified-nin": (
+    <FormattedMessage
+      id="ladok.no-verified-nin"
+      defaultMessage="You need a verified Swedish national identity number to link your account with Ladok."
+    />
+  ),
+
+  "ladok.missing-university": (
+    <FormattedMessage
+      id="ladok.missing-university"
+      defaultMessage="Unknown higher education institution. Please try again later."
     />
   ),
 };
