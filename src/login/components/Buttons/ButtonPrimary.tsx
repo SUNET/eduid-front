b782b68480856f23afaeed02f680b513ee9a0d44
Fredrik Thulin <fredrik@thulin.net>
2021-12-22 12:16:56 +0100

    simplify

--- src/login/components/Buttons/ButtonPrimary.tsx
+++ src/login/components/Buttons/ButtonPrimary.tsx
@@ -1,20 +1,15 @@
 import React from "react";
-import PropTypes from "prop-types";
 import Button from "reactstrap/lib/Button";
 
 interface ButtonPrimaryProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   id: string;
 }
 
-const ButtonPrimary = ({ id, translate, disabled, onClick, type, children }: ButtonPrimaryProps) => (
-  <Button type={type} id={id} translate={translate} disabled={disabled} onClick={onClick} color="primary">
-    {children}
+// An ordinary <Button> with color="primary", and mandatory id
+const ButtonPrimary = (props: ButtonPrimaryProps) => (
+  <Button {...props} color="primary">
+    {props.children}
   </Button>
 );
-
-ButtonPrimary.propTypes = {
-  onClick: PropTypes.func,
-  id: PropTypes.string.isRequired,
-};
 
 export default ButtonPrimary;
