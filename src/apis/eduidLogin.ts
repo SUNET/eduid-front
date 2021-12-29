786533b342ddb7175b8fed62db0159c05191a82b
Fredrik Thulin <fredrik@thulin.net>
2021-12-29 10:54:14 +0100

    reduce boiler-code

--- src/apis/eduidLogin.ts
+++ src/apis/eduidLogin.ts
@@ -6,6 +6,7 @@ import { createAction, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit"
 import { LoginAppDispatch, LoginRootState } from "login/app_init/initStore";
 import { KeyValues, makeRequest, RequestThunkAPI } from "./common";
 
+/***********************************************************************************/
 export interface LoginAuthnOptions {
   password?: boolean;
   other_device?: boolean;
@@ -16,37 +17,23 @@ export interface LoginAuthnOptions {
 /**
  * @public
  * @function fetchAuthnOptions
- * @desc Redux async thunk to get info about available options for authentication.
+ * @desc     Get info about available options for authentication.
  */
 export const fetchAuthnOptions = createAsyncThunk<
   LoginAuthnOptions,
   { ref: string },
   { dispatch: LoginAppDispatch; state: LoginRootState }
 >("login/api/fetchAuthnOptions", async (args, thunkAPI) => {
-  try {
-    const body: KeyValues = {
-      ref: args.ref,
-    };
+  const body: KeyValues = {
+    ref: args.ref,
+  };
 
-    const response = await makeLoginRequest<LoginAuthnOptions>(thunkAPI, "authn_options", body);
-
-    if (response.error) {
-      // dispatch fail responses so that notification middleware will show them to the user
-      thunkAPI.dispatch(response);
-      return thunkAPI.rejectWithValue(undefined);
-    }
-
-    return response.payload;
-  } catch (error) {
-    if (error instanceof Error) {
-      thunkAPI.dispatch(loginFail(error.toString()));
-      return thunkAPI.rejectWithValue(error.toString());
-    } else {
-      throw error;
-    }
-  }
+  return makeLoginRequest<LoginAuthnOptions>(thunkAPI, "authn_options", body)
+    .then((response) => response.payload)
+    .catch((err) => thunkAPI.rejectWithValue(err));
 });
 
+/***********************************************************************************/
 export interface LoginRequestOtherResponse {
   state_id: string;
   short_code: string;
@@ -58,38 +45,24 @@ export interface LoginRequestOtherResponse {
 /**
  * @public
  * @function requestUseOtherDevice
- * @desc Redux async thunk to request login using another device. Used on device 1 to get a QR code from the backend.
+ * @desc     Request login using another device. Used on device 1 to get a QR code from the backend.
  */
 export const requestUseOtherDevice = createAsyncThunk<
   LoginRequestOtherResponse, // return type
   { ref: string; username?: string }, // args type
   { dispatch: LoginAppDispatch; state: LoginRootState }
 >("login/api/requestOtherDevice", async (args, thunkAPI) => {
-  try {
-    const body: KeyValues = {
-      ref: args.ref,
-      username: args.username,
-    };
+  const body: KeyValues = {
+    ref: args.ref,
+    username: args.username,
+  };
 
-    const response = await makeLoginRequest<LoginRequestOtherResponse>(thunkAPI, "request_other", body);
-
-    if (response.error) {
-      // dispatch fail responses so that notification middleware will show them to the user
-      thunkAPI.dispatch(response);
-      return thunkAPI.rejectWithValue(undefined);
-    }
-
-    return response.payload;
-  } catch (error) {
-    if (error instanceof Error) {
-      thunkAPI.dispatch(loginFail(error.toString()));
-      return thunkAPI.rejectWithValue(error.toString());
-    } else {
-      throw error;
-    }
-  }
+  return makeLoginRequest<LoginRequestOtherResponse>(thunkAPI, "request_other", body)
+    .then((response) => response.payload)
+    .catch((err) => thunkAPI.rejectWithValue(err));
 });
 
+/***********************************************************************************/
 export interface LoginUseOtherResponse {
   login_ref: string;
   expires_in: number;
@@ -98,39 +71,26 @@ export interface LoginUseOtherResponse {
 /**
  * @public
  * @function fetchUseOtherDevice
- * @desc Redux async thunk to request to login on another device (on the device scanning the QR code, device 2).
+ * @desc     Request to login on another device (on the device scanning the QR code, device 2).
  */
 export const fetchUseOtherDevice = createAsyncThunk<
   { state_id: string; data: LoginUseOtherResponse }, // return type
   { state_id: string }, // args type
   { dispatch: LoginAppDispatch; state: LoginRootState }
 >("login/api/useOtherDevice", async (args, thunkAPI) => {
-  try {
-    const body: KeyValues = {
+  const body: KeyValues = {
+    state_id: args.state_id,
+  };
+
+  return makeLoginRequest<LoginUseOtherResponse>(thunkAPI, "use_other", body)
+    .then((response) => ({
       state_id: args.state_id,
-    };
-
-    const response = await makeLoginRequest<LoginUseOtherResponse>(thunkAPI, "use_other", body);
-
-    if (response.error) {
-      // dispatch fail responses so that notification middleware will show them to the user
-      thunkAPI.dispatch(response);
-      return thunkAPI.rejectWithValue(undefined);
-    }
-
-    return { state_id: args.state_id, data: response.payload };
-  } catch (error) {
-    if (error instanceof Error) {
-      thunkAPI.dispatch(loginFail(error.toString()));
-      return thunkAPI.rejectWithValue(error.toString());
-    } else {
-      throw error;
-    }
-  }
+      data: response.payload,
+    }))
+    .catch((err) => thunkAPI.rejectWithValue(err));
 });
 
-export type SAMLParameters = { SAMLResponse: string; RelayState?: string };
-
+/***********************************************************************************/
 export interface LoginNextResponse {
   // The response from the /next API endpoint consists of (in the happy case):
   //   action: what action the backed requires next, or FINISHED
@@ -140,52 +100,57 @@ export interface LoginNextResponse {
   target: string;
   parameters?: SAMLParameters;
 }
+export type SAMLParameters = { SAMLResponse: string; RelayState?: string };
 
 /**
  * @public
  * @function fetchNext
- * @desc Redux async thunk to request the backend to tell us what action to perform next in the login flow.
+ * @desc     Request the backend to tell us what action to perform next in the login flow.
  */
 export const fetchNext = createAsyncThunk<
   LoginNextResponse, // return type
   { ref: string }, // args type
   { dispatch: LoginAppDispatch; state: LoginRootState }
 >("login/api/fetchNext", async (args, thunkAPI) => {
-  try {
-    const body: KeyValues = {
-      ref: args.ref,
-    };
+  const body: KeyValues = {
+    ref: args.ref,
+  };
 
-    const state = thunkAPI.getState();
-
-    const response = await makeRequest<LoginNextResponse>(thunkAPI, state.config.next_url, undefined, body);
-
-    if (response.error) {
-      // dispatch fail responses so that notification middleware will show them to the user
-      thunkAPI.dispatch(response);
-      return thunkAPI.rejectWithValue(undefined);
-    }
-
-    return response.payload;
-  } catch (error) {
-    if (error instanceof Error) {
-      thunkAPI.dispatch(loginFail(error.toString()));
-      return thunkAPI.rejectWithValue(error.toString());
-    } else {
-      throw error;
-    }
-  }
+  // TODO: We also have the full next_url in config, should we remove that?
+  return makeLoginRequest<LoginNextResponse>(thunkAPI, "next", undefined, body)
+    .then((response) => response.payload)
+    .catch((err) => thunkAPI.rejectWithValue(err));
 });
 
-function makeLoginRequest<T>(
+/***********************************************************************************/
+async function makeLoginRequest<T>(
   thunkAPI: RequestThunkAPI,
   endpoint: string,
   body?: KeyValues,
   data?: KeyValues
 ): Promise<PayloadAction<T, string, never, boolean>> {
-  const state = thunkAPI.getState();
+  // Since the whole body of the executor is enclosed in try/catch, this linter warning is excused.
+  // eslint-disable-next-line no-async-promise-executor
+  return new Promise<PayloadAction<T, string, never, boolean>>(async (resolve, reject) => {
+    try {
+      const state = thunkAPI.getState();
 
-  return makeRequest(thunkAPI, state.config.base_url, endpoint, body, data);
+      const response = await makeRequest<T>(thunkAPI, state.config.base_url, endpoint, body, data);
+
+      if (response.error) {
+        // dispatch fail responses so that notification middleware will show them to the user
+        thunkAPI.dispatch(response);
+        reject(response);
+      }
+
+      resolve(response);
+    } catch (error) {
+      if (error instanceof Error) {
+        thunkAPI.dispatch(loginFail(error.toString()));
+      }
+      reject(error);
+    }
+  });
 }
 
 // Fake an error response from the backend. The action ending in _FAIL will make the notification
