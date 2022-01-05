005d582383f9b5205b6510e46c197f8deb963d39
Fredrik Thulin <fredrik@thulin.net>
2022-01-05 15:12:28 +0100

    fix tests

--- src/tests/ChangePassword-test.js
+++ src/tests/ChangePassword-test.js
@@ -89,7 +89,7 @@ describe("API calls", () => {
       const actualActions = store.getActions().map((action) => action.type);
       expect(actualActions).toEqual([
         "chpass/fetchSuggestedPassword/pending",
-        "SECURITY_FAIL",
+        "security_FAIL",
         "chpass/fetchSuggestedPassword/rejected",
       ]);
 
@@ -107,7 +107,7 @@ describe("API calls", () => {
       expect(store.getActions()[1]).toEqual({
         error: true,
         payload: { message: "Error: testing" },
-        type: "SECURITY_FAIL",
+        type: "security_FAIL",
       });
     });
   });
