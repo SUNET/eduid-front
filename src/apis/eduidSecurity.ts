5516ccdddb15acc9ef7ea757303d69dcf9036001
Fredrik Thulin <fredrik@thulin.net>
2022-01-05 15:00:42 +0100

    reduce boiler-plate code in different API calls

--- src/apis/eduidSecurity.ts
+++ src/apis/eduidSecurity.ts
@@ -6,28 +6,9 @@ import { createAction, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit"
 import { DashboardAppDispatch, DashboardRootState } from "../dashboard-init-app";
 import { KeyValues, makeRequest, RequestThunkAPI } from "./common";
 
+/*********************************************************************************************************************/
 export interface SuggestedPasswordResponse {
   suggested_password: string;
-}
-
-export interface ChangePasswordPayload {
-  old_password: string;
-  new_password: string;
-}
-
-export interface ChangePasswordResponse {
-  message?: string;
-}
-
-function makeSecurityRequest<T>(
-  thunkAPI: RequestThunkAPI,
-  endpoint: string,
-  body?: KeyValues,
-  data?: KeyValues
-): Promise<PayloadAction<T, string, never, boolean>> {
-  const state = thunkAPI.getState();
-
-  return makeRequest(thunkAPI, state.config.security_url, endpoint, body, data);
 }
 
 /**
@@ -40,25 +21,20 @@ export const fetchSuggestedPassword = createAsyncThunk<
   undefined,
   { dispatch: DashboardAppDispatch; state: DashboardRootState }
 >("chpass/fetchSuggestedPassword", async (args, thunkAPI) => {
-  try {
-    const response = await makeSecurityRequest<SuggestedPasswordResponse>(thunkAPI, "suggested-password");
-
-    if (response.error) {
-      // dispatch fail responses so that notification middleware will show them to the user
-      thunkAPI.dispatch(response);
-      return thunkAPI.rejectWithValue(undefined);
-    }
-
-    return response.payload.suggested_password;
-  } catch (error) {
-    if (error instanceof Error) {
-      thunkAPI.dispatch(securityFail(error.toString()));
-      return thunkAPI.rejectWithValue(error.toString());
-    } else {
-      throw error;
-    }
-  }
+  return makeSecurityRequest<SuggestedPasswordResponse>(thunkAPI, "suggested-password")
+    .then((response) => response.payload.suggested_password)
+    .catch((err) => thunkAPI.rejectWithValue(err));
 });
+
+/*********************************************************************************************************************/
+export interface ChangePasswordPayload {
+  old_password: string;
+  new_password: string;
+}
+
+export interface ChangePasswordResponse {
+  message?: string;
+}
 
 /**
  * @public
@@ -70,29 +46,51 @@ export const changePassword = createAsyncThunk<
   ChangePasswordPayload,
   { dispatch: DashboardAppDispatch; state: DashboardRootState }
 >("chpass/changePassword", async (args, thunkAPI) => {
-  try {
-    const response = await makeSecurityRequest<ChangePasswordResponse>(thunkAPI, "change-password", args);
-
-    if (response.error) {
-      // dispatch fail responses so that notification middleware will show them to the user
-      thunkAPI.dispatch(response);
-      return thunkAPI.rejectWithValue(undefined);
-    }
-
-    return response.payload;
-  } catch (error) {
-    if (error instanceof Error) {
-      thunkAPI.dispatch(securityFail(error.toString()));
-      return thunkAPI.rejectWithValue(error.toString());
-    } else {
-      throw error;
-    }
-  }
+  return makeSecurityRequest<ChangePasswordResponse>(thunkAPI, "change-password", args)
+    .then((response) => response.payload)
+    .catch((err) => thunkAPI.rejectWithValue(err));
 });
 
+/*********************************************************************************************************************/
+function makeSecurityRequest<T>(
+  thunkAPI: RequestThunkAPI,
+  endpoint: string,
+  body?: KeyValues,
+  data?: KeyValues
+): Promise<PayloadAction<T, string, never, boolean>> {
+  // Since the whole body of the executor is enclosed in try/catch, this linter warning is excused.
+  // eslint-disable-next-line no-async-promise-executor
+  return new Promise<PayloadAction<T, string, never, boolean>>(async (resolve, reject) => {
+    try {
+      const state = thunkAPI.getState();
+
+      const response = await makeRequest<T>(thunkAPI, state.config.security_url, endpoint, body, data);
+
+      if (response.error) {
+        // Dispatch fail responses so that notification middleware will show them to the user.
+        // The current implementation in notify-middleware.js _removes_ error and payload.message from
+        // response, so we clone it first so we can reject the promise with the full error response.
+        const saved = JSON.parse(JSON.stringify(response));
+        thunkAPI.dispatch(response);
+        reject(saved);
+      }
+
+      resolve(response);
+    } catch (error) {
+      if (error instanceof Error) {
+        thunkAPI.dispatch(securityFail(error.toString()));
+        reject(error.toString());
+      } else {
+        reject(error);
+      }
+    }
+  });
+}
+
+/*********************************************************************************************************************/
 // Fake an error response from the backend. The action ending in _FAIL will make the notification
 // middleware picks this error up and shows something to the user.
-export const securityFail = createAction("SECURITY_FAIL", function prepare(message: string) {
+export const securityFail = createAction("security_FAIL", function prepare(message: string) {
   return {
     error: true,
     payload: {
