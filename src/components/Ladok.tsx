9c0c7c90c43fd19c58ac1a892d72240036ece7da
Eunju Huss <eunju@sunet.se>
2022-01-11 13:00:44 +0100

    Move selectAdapter code inside LadokUniversitiesDropdown

--- src/components/Ladok.tsx
+++ src/components/Ladok.tsx
@@ -1,18 +1,10 @@
 import { useDashboardAppDispatch, useDashboardAppSelector } from "dashboard-hooks";
-import React, { useEffect, useState, InputHTMLAttributes } from "react";
+import React, { useEffect, useState } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 import { fetchLadokUniversities, linkUser, unlinkUser } from "../apis/eduidLadok";
 import ReactDOM from "react-dom";
 import { Form, Field } from "react-final-form";
 import Select from "react-select";
-
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  input: { name: string };
-}
-
-const SelectAdapter = ({ input }: InputProps) => {
-  return <Select {...input} />;
-};
 
 const LadokContainer = (): JSX.Element => {
   const isLinked = useDashboardAppSelector((state) => state.ladok.isLinked);
@@ -119,6 +111,8 @@ const LadokUniversitiesDropdown = (): JSX.Element => {
       );
     });
   }
+
+  const SelectAdapter = ({ input, ...rest }: any) => <Select {...input} {...rest} />;
 
   return (
     <React.Fragment>
