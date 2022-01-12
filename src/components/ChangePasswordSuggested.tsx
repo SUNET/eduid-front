8b8ba05b8209687acb7ea3a33afecaeaa180fc72
Eunju Huss <eunju@sunet.se>
2022-01-12 17:41:23 +0100

    Remove unnecessary type for new password

--- src/components/ChangePasswordSuggested.tsx
+++ src/components/ChangePasswordSuggested.tsx
@@ -14,7 +14,6 @@ import { ChangePasswordChildFormProps } from "./ChangePasswordForm";
 interface ChangePasswordSuggestedFormData {
   old?: string;
   suggested?: string;
-  new?: string;
 }
 
 interface FormData {
