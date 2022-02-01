6e7decfe54864a21e52ca0425513407d36c1b293
Fredrik Thulin <fredrik@thulin.net>
2022-02-01 13:49:10 +0100

    tighten up config, and explain thunks to redux-mock-store

--- src/tests/helperFunctions/DashboardTestApp.tsx
+++ src/tests/helperFunctions/DashboardTestApp.tsx
@@ -1,53 +1,49 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { AnyAction, ThunkDispatch } from "@reduxjs/toolkit";
 import { ReduxIntlProvider } from "components/ReduxIntl";
 import { DashboardRootState, dashboardStore } from "dashboard-init-app";
 import { mount, ReactWrapper } from "enzyme";
-import { ChangePasswordState } from "reducers/ChangePassword";
+import React from "react";
+import { DashboardConfig, initialState as DashboardInitialConfig } from "reducers/DashboardConfig";
 import createMockStore, { MockStoreEnhanced } from "redux-mock-store";
 import thunk from "redux-thunk";
-import { DashboardConfig, initialState as DashboardInitialConfig } from "reducers/DashboardConfig";
 
-export const dashboardTestState: DashboardRootState = {
+const bareMinimumTestState: Partial<DashboardRootState> = {
   config: {
     ...DashboardInitialConfig,
     csrf_token: "csrf-token",
   } as DashboardConfig,
-  router: undefined as any,
-  form: undefined as any,
-  intl: { locale: "en", messages: {} },
 
-  chpass: {} as ChangePasswordState,
-  emails: undefined as any,
-  groups: undefined as any,
-  invites: undefined as any,
-  openid_data: undefined as any,
-  lookup_mobile: undefined as any,
-  nins: undefined as any,
-  openid_freja_data: undefined as any,
-  personal_data: undefined as any,
-  phones: undefined as any,
-  letter_proofing: undefined as any,
-  notifications: undefined as any,
-  account_linking: undefined as any,
-  security: undefined as any,
-  eidas_data: undefined as any,
-  ladok: undefined as any,
+  intl: { locale: "en", messages: {} },
 };
+export const dashboardTestState = bareMinimumTestState as DashboardRootState;
 
 export type DashboardStoreType = typeof dashboardStore;
+type DispatchExts = ThunkDispatch<DashboardRootState, void, AnyAction>;
 
-export function fakeStore(state: DashboardRootState = dashboardTestState): MockStoreEnhanced<DashboardRootState> {
+/**
+ * Create a fake redux store object, for use in tests. The store object has a dispatch() and a getState() etc.
+ * @returns
+ */
+export function fakeStore(
+  state: DashboardRootState = dashboardTestState
+): MockStoreEnhanced<DashboardRootState, DispatchExts> {
   const middlewares = [thunk];
-  const store = createMockStore<DashboardRootState>(middlewares);
+
+  const store = createMockStore<DashboardRootState, DispatchExts>(middlewares);
   return store(state);
 }
 
 interface setupComponentArgs {
   component: JSX.Element;
   store?: DashboardStoreType;
-  overrides?: { [key: string]: unknown };
+  overrides?: Partial<DashboardRootState>;
 }
 
+/**
+ * Mount `component' in a ReduxIntlProvider, with a fakeStore (optionally initialised with some overrides).
+ * @returns
+ */
 export function setupComponent({ component, store, overrides }: setupComponentArgs): ReactWrapper {
   if (store === undefined) {
     if (overrides) {
@@ -58,4 +54,12 @@ export function setupComponent({ component, store, overrides }: setupComponentAr
   }
   const wrapper = mount(<ReduxIntlProvider store={store}>{component}</ReduxIntlProvider>);
   return wrapper;
+}
+
+/**
+ * Convenience function to create a correctly typed DashboardRootState with some overrides for use in tests.
+ * @returns
+ */
+export function createTestState(overrides: Partial<DashboardRootState>): DashboardRootState {
+  return { ...dashboardTestState, ...overrides } as DashboardRootState;
 }
