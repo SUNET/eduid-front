93ebb3fdda1dcd892d02e2405ba11e3f5b140fd2
Eunju Huss <eunju@sunet.se>
2022-02-10 16:30:54 +0100

    Migrate to asyncThunk

--- src/sagas/LetterProofing.ts
+++ src/sagas/LetterProofing.ts
@@ -1,49 +1,10 @@
 import { put, select, call } from "redux-saga/effects";
 import { checkStatus, putCsrfToken, failRequest } from "sagas/common";
-import { getRequest, postRequest } from "sagas/ts_common";
+import { postRequest } from "sagas/ts_common";
 import letterProofingSlice from "reducers/LetterProofing";
 import { DashboardRootState } from "dashboard-init-app";
 import { PayloadAction } from "@reduxjs/toolkit";
-
-interface LetterProofingResponse {
-  letter_expired?: boolean;
-  letter_expires?: string;
-  letter_expires_in_day?: number;
-  letter_sent?: string;
-  letter_sent_days_ago?: number;
-  message?: string;
-  code?: string;
-}
-
-export function* sendGetLetterProofing() {
-  try {
-    const state: DashboardRootState = yield select((state) => state),
-      nin = state.nins.nin;
-    const response: PayloadAction<LetterProofingResponse, string, never, boolean> = yield call(
-      fetchGetLetterProofing,
-      state.config,
-      nin
-    );
-    yield put(putCsrfToken(response));
-    if (response.error) {
-      // Errors are handled in notifyAndDispatch() (in notify-middleware.js)
-      yield put(response);
-      return;
-    }
-    yield put(letterProofingSlice.actions.getLetterProofingSuccess(response.payload));
-  } catch (error) {
-    yield* failRequest(error, letterProofingSlice.actions.letterProofingSagaFail);
-  }
-}
-
-export function fetchGetLetterProofing(config: { letter_proofing_url: string }) {
-  return window
-    .fetch(config.letter_proofing_url + "proofing", {
-      ...getRequest,
-    })
-    .then(checkStatus)
-    .then((response) => response.json());
-}
+import { LetterProofingResponse } from "apis/letterProofing";
 
 export function* sendLetterProofing() {
   try {
