44bafd3cf8fa26911fe0c5dc14e99d6bb5497a9d
Eunju Huss <eunju@sunet.se>
2022-02-10 16:26:44 +0100

    Use AsynckThunk for letterProofing

---
+++ src/apis/letterProofing.ts
@@ -0,0 +1,96 @@
+/*
+ * Code and data structures for talking to the letter-proofing backend microservice.
+ */
+
+import { createAction, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { DashboardAppDispatch, DashboardRootState } from "../dashboard-init-app";
+import { KeyValues, makeRequest, RequestThunkAPI } from "./common";
+
+/*********************************************************************************************************************/
+export interface LetterProofingResponse {
+  letter_expired?: boolean;
+  letter_expires?: string;
+  letter_expires_in_day?: number;
+  letter_sent?: string;
+  letter_sent_days_ago?: number;
+  message?: string;
+  code?: string;
+}
+
+/**
+ * @public
+ * @function fetchLetterProofingState
+ * @desc Redux async thunk to get letter proofing state from the backend.
+ */
+export const fetchLetterProofingState = createAsyncThunk<
+  LetterProofingResponse,
+  undefined,
+  { dispatch: DashboardAppDispatch; state: DashboardRootState }
+>("letterProofing/fetchLetterProofingState", async (args, thunkAPI) => {
+  try {
+    const response = await makeLetterProofingRequest<LetterProofingResponse>(thunkAPI, "proofing");
+
+    if (response.error) {
+      // dispatch fail responses so that notification middleware will show them to the user
+      thunkAPI.dispatch(response);
+      return thunkAPI.rejectWithValue(undefined);
+    }
+
+    return response.payload;
+  } catch (error) {
+    if (error instanceof Error) {
+      thunkAPI.dispatch(letterProofingFail(error.toString()));
+      return thunkAPI.rejectWithValue(error.toString());
+    } else {
+      throw error;
+    }
+  }
+});
+
+/*********************************************************************************************************************/
+function makeLetterProofingRequest<T>(
+  thunkAPI: RequestThunkAPI,
+  endpoint: string,
+  body?: KeyValues,
+  data?: KeyValues
+): Promise<PayloadAction<T, string, never, boolean>> {
+  // Since the whole body of the executor is enclosed in try/catch, this linter warning is excused.
+  // eslint-disable-next-line no-async-promise-executor
+  return new Promise<PayloadAction<T, string, never, boolean>>(async (resolve, reject) => {
+    try {
+      const state = thunkAPI.getState();
+
+      const response = await makeRequest<T>(thunkAPI, state.config.letter_proofing_url, endpoint, body, data);
+
+      if (response.error) {
+        // Dispatch fail responses so that notification middleware will show them to the user.
+        // The current implementation in notify-middleware.js _removes_ error and payload.message from
+        // response, so we clone it first so we can reject the promise with the full error response.
+        const saved = JSON.parse(JSON.stringify(response));
+        thunkAPI.dispatch(response);
+        reject(saved);
+      }
+
+      resolve(response);
+    } catch (error) {
+      if (error instanceof Error) {
+        thunkAPI.dispatch(letterProofingFail(error.toString()));
+        reject(error.toString());
+      } else {
+        reject(error);
+      }
+    }
+  });
+}
+
+/*********************************************************************************************************************/
+// Fake an error response from the backend. The action ending in _FAIL will make the notification
+// middleware picks this error up and shows something to the user.
+export const letterProofingFail = createAction("letterProofing_FAIL", function prepare(message: string) {
+  return {
+    error: true,
+    payload: {
+      message,
+    },
+  };
+});
