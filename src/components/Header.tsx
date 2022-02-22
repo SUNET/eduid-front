799165b8530479f8cea1faad99793bed44bbe4e9
Eunju Huss <eunju@sunet.se>
2022-02-22 13:54:52 +0100

    Add temporary types

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,62 +1,61 @@
-import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
+import React, { Fragment } from "react";
 import "../login/styles/index.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-class Header extends Component {
-  render() {
-    const url = location.pathname;
-    let tagline = this.props.translate("banner.tagline");
-    let button = "";
-
-    if (url.includes("register")) {
-      button = (
-        <a href={this.props.dashboard_url}>
-          <button id="login">
-            <FontAwesomeIcon icon={faUser} />
-            {this.props.translate("header.signin")}
-          </button>
-        </a>
-      );
-    } else if (url.includes("profile")) {
-      tagline = (
-        <Fragment>
-          {this.props.translate("dashboard.tagline")} {this.props.email}
-        </Fragment>
-      );
-      button = (
-        <div id="eduid-button">
-          <button id="logout" onClick={this.props.handleLogout}>
-            <FontAwesomeIcon icon={faUser} />
-            {this.props.translate("header.logout")}
-          </button>
-        </div>
-      );
-    } else {
-      button = <div />;
-    }
-
-    return (
-      <section className="banner">
-        <header>
-          <a href={this.props.dashboard_url}>
-            <div id="eduid-logo" />
-          </a>
-          {button}
-        </header>
-        <div className="vertical-content-margin">
-          <h1 className="tagline">{tagline}</h1>
-        </div>
-      </section>
-    );
-  }
+interface HeaderProps {
+  translate: any;
+  button: any;
+  dashboard_url: string;
+  email: string;
+  handleLogout: any;
 }
 
-Header.propTypes = {
-  gotoSignin: PropTypes.func,
-  translate: PropTypes.func,
-  confirmed: PropTypes.string,
+const Header = (props: HeaderProps): JSX.Element => {
+  const url = location.pathname;
+  let tagline = props.translate("banner.tagline");
+  let button: any = "";
+
+  if (url.includes("register")) {
+    button = (
+      <a href={props.dashboard_url}>
+        <button id="login">
+          <FontAwesomeIcon icon={faUser} />
+          {props.translate("header.signin")}
+        </button>
+      </a>
+    );
+  } else if (url.includes("profile")) {
+    tagline = (
+      <Fragment>
+        {props.translate("dashboard.tagline")} {props.email}
+      </Fragment>
+    );
+    button = (
+      <div id="eduid-button">
+        <button id="logout" onClick={props.handleLogout}>
+          <FontAwesomeIcon icon={faUser} />
+          {props.translate("header.logout")}
+        </button>
+      </div>
+    );
+  } else {
+    button = <div />;
+  }
+
+  return (
+    <section className="banner">
+      <header>
+        <a href={props.dashboard_url}>
+          <div id="eduid-logo" />
+        </a>
+        {button}
+      </header>
+      <div className="vertical-content-margin">
+        <h1 className="tagline">{tagline}</h1>
+      </div>
+    </section>
+  );
 };
 
 export default Header;
