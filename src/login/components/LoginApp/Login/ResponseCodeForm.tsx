a7f213750457a9959c78840058670e37b3cd4862
Eunju Huss <eunju@sunet.se>
2022-03-22 09:48:56 +0100

    Short function name

--- src/login/components/LoginApp/Login/ResponseCodeForm.tsx
+++ src/login/components/LoginApp/Login/ResponseCodeForm.tsx
@@ -156,7 +156,7 @@ function CodeField({ num, value, disabled = false, fixed = false, autoFocus = un
     }
   }
 
-  function onlyAllowedNumericalInput(e: React.KeyboardEvent<HTMLFormElement>) {
+  function handleCodeFieldInput(e: React.KeyboardEvent<HTMLFormElement>) {
     if (!isDigit(e.key)) e.preventDefault();
   }
 
@@ -173,7 +173,7 @@ function CodeField({ num, value, disabled = false, fixed = false, autoFocus = un
       autoFocus={autoFocus}
       onKeyUp={handleKeyUp}
       onFocus={(event: FocusEvent<HTMLInputElement>) => event.target.select()}
-      onKeyPress={onlyAllowedNumericalInput}
+      onKeyPress={handleCodeFieldInput}
     />
   );
 }
