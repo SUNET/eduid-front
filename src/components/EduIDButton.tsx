cbd39ee222ef2f788d0ad60cd002ce08b0c6e616
Eunju Huss <eunju@sunet.se>
2022-04-06 11:48:18 +0200

    Fix error, not recognize buttonStyle props

--- src/components/EduIDButton.tsx
+++ src/components/EduIDButton.tsx
@@ -5,10 +5,11 @@ interface EduIDButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>
   id?: string;
   size?: string;
   buttonStyle: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 // depends on props.buttonStyle, button will display as primary, secondary, link or close button
 const EduIDButton = (props: EduIDButtonProps) => (
-  <Button {...props} size={props.size} color={props.buttonStyle}>
+  <Button id={props.id} onClick={props.onClick} size={props.size} color={props.buttonStyle}>
     {props.children}
   </Button>
 );
