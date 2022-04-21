78020f83c6688e075442078ec7fccae5a2e946c8
cartja <carina@sunet.se>
2022-04-21 12:25:14 +0200

    style-realign code verified

--- src/components/CodeVerified.js
+++ src/components/CodeVerified.js
@@ -8,23 +8,25 @@ class CodeVerified extends Component {
       <form key="2" method="GET" action={this.props.dashboard_url} className="vertical-content-margin">
         <div key="0" id="content" className="content">
           <div>
-            <h3 className="register-header">{this.props.translate("finish.registration-complete")}</h3>
-            <p className="lead">{this.props.translate("finish.registration-details")}</p>
+            <h1 className="register-header">{this.props.translate("finish.registration-complete")}</h1>
+            <p className="preamble">{this.props.translate("finish.registration-details")}</p>
             <div id="email-display">
               <label>Email</label>
-              <h3 id="user-email" className="register-header">
+              <div id="user-email" className="register-header display-data">
                 {this.props.email}
-              </h3>
+              </div>
               <label>Password</label>
-              <h3 className="register-header registered-email">
+              <div className="register-header registered-email display-data">
                 <mark id="user-password" className="force-select-all">
                   {this.props.password}
                 </mark>
-              </h3>
+              </div>
             </div>
-            <EduIDButton id="gotit-button" buttonstyle="primary" type="submit">
-              {this.props.translate("finish.got-it")}
-            </EduIDButton>
+            <div className="buttons">
+              <EduIDButton id="gotit-button" buttonstyle="primary" type="submit">
+                {this.props.translate("finish.got-it")}
+              </EduIDButton>
+            </div>
           </div>
         </div>
       </form>
