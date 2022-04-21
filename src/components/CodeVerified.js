9a57e50282913516c261f24d4879651d0cc88976
cartja <carina@sunet.se>
2022-04-21 12:56:05 +0200

    Form and button style code verified

--- src/components/CodeVerified.js
+++ src/components/CodeVerified.js
@@ -11,16 +11,20 @@ class CodeVerified extends Component {
             <h1 className="register-header">{this.props.translate("finish.registration-complete")}</h1>
             <p className="preamble">{this.props.translate("finish.registration-details")}</p>
             <div id="email-display">
-              <label>Email</label>
-              <div id="user-email" className="register-header display-data">
-                {this.props.email}
-              </div>
-              <label>Password</label>
-              <div className="register-header registered-email display-data">
-                <mark id="user-password" className="force-select-all">
-                  {this.props.password}
-                </mark>
-              </div>
+              <fieldset>
+                <label>Email</label>
+                <div id="user-email" className="register-header display-data">
+                  {this.props.email}
+                </div>
+              </fieldset>
+              <fieldset>
+                <label>Password</label>
+                <div className="register-header registered-email display-data">
+                  <mark id="user-password" className="force-select-all">
+                    {this.props.password}
+                  </mark>
+                </div>
+              </fieldset>
             </div>
             <div className="buttons">
               <EduIDButton id="gotit-button" buttonstyle="primary" type="submit">
