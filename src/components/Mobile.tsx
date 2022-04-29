004ff3801173c1afeeb9dfeac922890d036a1984
Eunju Huss <eunju@sunet.se>
2022-04-29 13:01:31 +0200

    Comment out container and add types in component

--- src/components/Mobile.tsx
+++ src/components/Mobile.tsx
@@ -1,18 +1,27 @@
 import EduIDButton from "components/EduIDButton";
 import { translate } from "login/translation";
-import PropTypes from "prop-types";
 import React, { useState } from "react";
-import { useIntl } from "react-intl";
-import { connect } from "react-redux";
-import { Form } from "reactstrap";
-import { Field, reduxForm } from "redux-form";
+import { useIntl, FormattedMessage } from "react-intl";
 import { shortCodePattern } from "../login/app_utils/validation/regexPatterns";
 import TableList from "../login/components/DataTable/DataTable";
 import CustomInput from "../login/components/Inputs/CustomInput";
 import ConfirmModal from "../login/components/Modals/ConfirmModalContainer";
 import "../login/styles/index.scss";
+import { isValid } from "redux-form";
+import {
+  makePrimary,
+  postMobile,
+  startResendMobileCode,
+  startConfirmation,
+  stopConfirmation,
+  startVerify,
+  startRemove,
+} from "actions/Mobile";
+import { clearNotifications } from "reducers/Notifications";
+import { useDashboardAppDispatch, useDashboardAppSelector } from "dashboard-hooks";
+import { Form as FinalForm, Field as FinalField } from "react-final-form";
 
-const validate = (values, props) => {
+const validate = (values: any) => {
   let phone = values.number;
   if (!phone) {
     return { number: "required" };
@@ -22,7 +31,7 @@ const validate = (values, props) => {
     return { number: "phone.e164_format" };
   }
   if (phone.startsWith("0")) {
-    phone = "+" + props.default_country_code + phone.substr(1);
+    phone = "+" + 46 + phone.substr(1);
   }
   const pattern = /^\+[1-9]\d{6,20}$/;
   if (!pattern.test(phone)) {
@@ -30,56 +39,114 @@ const validate = (values, props) => {
   }
 };
 
-let PhoneForm = (props) => {
-  const intl = useIntl();
-  // placeholder can't be an Element, we need to get the actual translated string here
-  const placeholder = intl.formatMessage({
-    id: "placeholder.phone",
-    defaultMessage: "Phone number",
-    description: "placeholder text for phone input",
-  });
+// const PhoneForm = (props: any) => {
+//   const intl = useIntl();
+//   // placeholder can't be an Element, we need to get the actual translated string here
+//   const placeholder = intl.formatMessage({
+//     id: "placeholder.phone",
+//     defaultMessage: "Phone number",
+//     description: "placeholder text for phone input",
+//   });
 
-  return (
-    <Form id="phonesview-form" role="form" onSubmit={props.handleAdd} className="single-input-form">
-      <fieldset id="phone-form" className="tabpane">
-        <Field
-          label={translate("profile.phone_display_title")}
-          component={CustomInput}
-          componentClass="input"
-          type="text"
-          name="number"
-          placeholder={placeholder}
-          helpBlock={translate("phones.input_help_text")}
-        />
-      </fieldset>
-      <EduIDButton id="mobile-button" buttonstyle="primary" disabled={!props.valid_phone} onClick={props.handleAdd}>
-        {translate("mobile.button_add")}
-      </EduIDButton>
-    </Form>
-  );
-};
+//   return (
+//     // <Form id="phonesview-form" role="form" onSubmit={props.handleAdd} className="single-input-form">
+//     //   <fieldset id="phone-form" className="tabpane">
+//     //     <Field
+//     //       label={translate("profile.phone_display_title")}
+//     //       component={CustomInput}
+//     //       componentClass="input"
+//     //       type="text"
+//     //       name="number"
+//     //       placeholder={placeholder}
+//     //       helpBlock={translate("phones.input_help_text")}
+//     //     />
+//     //   </fieldset>
+//     //   <EduIDButton id="mobile-button" buttonstyle="primary" disabled={!props.valid_phone} onClick={props.handleAdd}>
+//     //     {translate("mobile.button_add")}
+//     //   </EduIDButton>
+//     // </Form>
+//   );
+// };
 
-PhoneForm = reduxForm({
-  form: "phones",
-  validate,
-})(PhoneForm);
+// const DecoratedPhoneForm = reduxForm({
+//   form: "phones",
+//   // validate,
+// })(PhoneForm);
 
-PhoneForm = connect((state) => ({
-  initialValues: { number: state.phones.phone },
-  enableReinitialize: true,
-}))(PhoneForm);
+// const FinalePhoneForm = connect((state) => ({
+//   initialValues: { number: "" },
+//   enableReinitialize: true,
+// }))(DecoratedPhoneForm);
 
-function Mobile(props) {
-  const [formClass, setFormClass] = useState("hide");
-  const [addLinkClass, setAddLinkClass] = useState("");
+function Mobile(props: any) {
+  const [showMobileForm, setShowMobileForm] = useState(false);
+  const dispatch = useDashboardAppDispatch();
+  const phones = useDashboardAppSelector((state) => state.phones.phones);
+  const confirming = useDashboardAppSelector((state) => state.phones.confirming);
+  const resending = useDashboardAppSelector((state) => state.phones.resending);
+  // const default_country_code = useDashboardAppSelector((state) => state.config.default_country_code);
 
-  function showEmailForm() {
-    setFormClass("form-content");
-    setAddLinkClass("hide");
+  function handleMobileForm() {
+    setShowMobileForm(true);
     // rendering focus on input, setTimeout for 200 milliseconds to recognize the form
     setTimeout(() => {
-      document.getElementById("number").focus();
+      (document.getElementById("number") as HTMLInputElement).focus();
     }, 200);
+  }
+
+  function handleAdd(event: React.MouseEvent<HTMLElement>) {
+    event.preventDefault();
+    dispatch(postMobile());
+  }
+
+  function handleResend(event: React.MouseEvent<HTMLElement>) {
+    event.preventDefault();
+    dispatch(startResendMobileCode());
+    dispatch(stopConfirmation());
+  }
+
+  function handleStartConfirmation(event: React.MouseEvent<HTMLElement>) {
+    dispatch(clearNotifications());
+    const dataNode = (event.target as HTMLTextAreaElement).closest("tr.number"),
+      data = {
+        identifier: dataNode && dataNode.getAttribute("data-identifier"),
+        phone: dataNode && dataNode.getAttribute("data-object"),
+      };
+
+    dispatch(startConfirmation(data));
+  }
+
+  function handleStopConfirmation() {
+    dispatch(stopConfirmation());
+  }
+
+  function handleConfirm() {
+    const codeValue = document.getElementById("confirmation-code-area");
+    const data = {
+      code: codeValue && (codeValue.querySelector("input") as HTMLInputElement).value.trim(),
+    };
+    dispatch(startVerify(data));
+    dispatch(stopConfirmation());
+  }
+
+  function handleRemove(event: React.MouseEvent<HTMLElement>) {
+    const dataNode = (event.target as HTMLTextAreaElement).closest("tr.number"),
+      data = {
+        phone: dataNode && dataNode.getAttribute("data-object"),
+      };
+    dispatch(startRemove(data));
+  }
+
+  function handleMakePrimary(event: React.MouseEvent<HTMLElement>) {
+    const dataNode = (event.target as HTMLTextAreaElement).closest("tr.number"),
+      data = {
+        phone: dataNode && dataNode.getAttribute("data-object"),
+      };
+    dispatch(makePrimary(data));
+  }
+
+  function handleCancel() {
+    setShowMobileForm(false);
   }
 
   const intl = useIntl();
@@ -96,33 +163,86 @@ function Mobile(props) {
       defaultMessage: "Enter the code sent to {phone}",
       description: "Title for phone code input",
     },
-    { phone: props.confirming }
+    { phone: confirming }
   );
 
   return (
-    <article className="phoneview-form-container" id="phone">
+    <article className="phone-view-form-container" id="phone">
       <div className="intro">
         <h3>{translate("phones.main_title")}</h3>
         <p>{translate("phones.long_description")}</p>
       </div>
       <div id="phone-display">
         <TableList
-          data={props.phones}
-          handleStartConfirmation={props.handleStartConfirmation}
-          handleRemove={props.handleRemove}
-          handleMakePrimary={props.handleMakePrimary}
+          data={phones}
+          handleStartConfirmation={handleStartConfirmation}
+          handleRemove={handleRemove}
+          handleMakePrimary={handleMakePrimary}
         />
-        <div className={formClass}>
-          <PhoneForm {...props} />
-        </div>
-        <EduIDButton
-          id="add-more-button"
-          buttonstyle="link"
-          className={addLinkClass + " lowercase"}
-          onClick={showEmailForm}
-        >
-          {translate("phones.button_add_more")}
-        </EduIDButton>
+        {/* <div className={formClass}> */}
+        {showMobileForm ? (
+          <FinalForm<any>
+            onSubmit={handleAdd}
+            initialValues={{
+              number: "",
+            }}
+            validate={validate}
+            render={({ handleSubmit, pristine, invalid }) => {
+              return (
+                <form onSubmit={handleSubmit}>
+                  <FinalField
+                    label={translate("profile.phone_display_title")}
+                    component={CustomInput}
+                    componentClass="input"
+                    type="text"
+                    name="number"
+                    placeholder={placeholder}
+                    helpBlock={translate("phones.input_help_text")}
+                  />
+                  <div className="flex-buttons">
+                    <EduIDButton id="cancel-adding-mobile" buttonstyle="secondary" onClick={handleCancel}>
+                      <FormattedMessage defaultMessage="Cancel" description="button cancel" />
+                    </EduIDButton>
+                    <EduIDButton
+                      id="add-mobile"
+                      buttonstyle="primary"
+                      type="submit"
+                      disabled={invalid || pristine}
+                      onClick={handleSubmit}
+                    >
+                      <FormattedMessage defaultMessage="Add" description="button add" />
+                    </EduIDButton>
+                  </div>
+                </form>
+              );
+            }}
+          />
+        ) : (
+          //   <Form id="phonesview-form" role="form" onSubmit={handleAdd} className="single-input-form">
+          //   <fieldset id="phone-form" className="tabpane">
+          //     <Field
+          //       label={translate("profile.phone_display_title")}
+          //       component={CustomInput}
+          //       componentClass="input"
+          //       type="text"
+          //       name="number"
+          //       placeholder={placeholder}
+          //       helpBlock={translate("phones.input_help_text")}
+          //     />
+          //   </fieldset>
+          //   <EduIDButton id="mobile-button" buttonstyle="primary"  type="submit"
+          //   disabled={invalid || pristine}
+          //   // disabled={!valid_phone}
+          //   onClick={handleAdd}>
+          //     {translate("mobile.button_add")}
+          //   </EduIDButton>
+          // </Form>
+          <EduIDButton id="add-more-button" buttonstyle="link" className={" lowercase"} onClick={handleMobileForm}>
+            {translate("phones.button_add_more")}
+          </EduIDButton>
+        )}
+
+        {/* </div> */}
       </div>
       <ConfirmModal
         modalId="phoneConfirmDialog"
@@ -132,26 +252,15 @@ function Mobile(props) {
         resendHelp={translate("cm.lost_code")}
         resendText={translate("cm.resend_code")}
         placeholder={placeholder}
-        showModal={Boolean(props.confirming)}
-        closeModal={props.handleStopConfirmation}
-        handleResend={props.handleResend}
-        handleConfirm={props.handleConfirm}
+        showModal={Boolean(confirming)}
+        closeModal={handleStopConfirmation}
+        handleResend={handleResend}
+        handleConfirm={handleConfirm}
         validationPattern={shortCodePattern}
         validationError={"confirmation.code_invalid_format"}
       />
     </article>
   );
 }
-
-Mobile.propTypes = {
-  phones: PropTypes.array,
-  confirming: PropTypes.string,
-  resending: PropTypes.object,
-  handleResend: PropTypes.func,
-  handleAdd: PropTypes.func,
-  handleStartConfirmation: PropTypes.func,
-  handleStopConfirmation: PropTypes.func,
-  handleRemoveMobile: PropTypes.func,
-};
 
 export default Mobile;
