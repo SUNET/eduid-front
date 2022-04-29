134e9774355e1931a40e4b56b10f875fbab526bc
Eunju Huss <eunju@sunet.se>
2022-04-29 13:32:56 +0200

    Use formattedMessages

--- src/components/AccountCreated.tsx
+++ src/components/AccountCreated.tsx
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+import { FormattedMessage } from "react-intl";
 
-class AccountCreated extends Component {
-  render() {
-    return (
-      <div id="content" className="horizontal-content-margin content">
-        <h1 className="register-header">{this.props.translate("created.account-created")}</h1>
-        <div id="email-display">
-          <p className="preamble">{this.props.translate("created.email-label")}</p>
-          <h4 className="register-header registered-email">{this.props.email}</h4>
-        </div>
+function AccountCreated(props: { email: string }) {
+  return (
+    <div className="horizontal-content-margin content">
+      <h1 className="register-header">
+        <FormattedMessage
+          defaultMessage="A link has been sent to your email address."
+          description="Register Account created"
+        />
+      </h1>
+      <div id="email-display">
+        <p className="preamble">
+          <FormattedMessage
+            defaultMessage="Complete registration by clicking the link sent to:"
+            description="Register email label"
+          />
+        </p>
+        <h4 className="register-header registered-email">{props.email}</h4>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-AccountCreated.propTypes = {
-  email: PropTypes.string,
-};
 
 export default AccountCreated;
