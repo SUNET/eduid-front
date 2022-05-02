1a4e5acd76f05202ce817319a7afaf770e5de7f5
Eunju Huss <eunju@sunet.se>
2022-05-02 15:22:30 +0200

    Add eduid phone apis

--- src/apis/eduidPhone.ts
+++ src/apis/eduidPhone.ts
@@ -1,5 +1,5 @@
 /*
- * Code and data structures for talking to the emails backend microservice.
+ * Code and data structures for talking to the eduidPhone backend microservice.
  */
 
 import { createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
@@ -19,8 +19,66 @@ export interface PhonesResponse {
 /*********************************************************************************************************************/
 /**
  * @public
- * @function postNewPhone
+ * @function requestMakePrimaryPhone
  * @desc Redux async thunk to add a new phone.
+ */
+export const requestMakePrimaryPhone = createAsyncThunk<
+  PhonesResponse, // return type
+  { number: string }, // args type
+  { dispatch: DashboardAppDispatch; state: DashboardRootState }
+>("emails/requestMakePrimaryPhone", async (args, thunkAPI) => {
+  const data: KeyValues = {
+    number: args.number,
+  };
+  return makeEmailsRequest<PhonesResponse>(thunkAPI, "primary", data)
+    .then((response) => response.payload)
+    .catch((err) => thunkAPI.rejectWithValue(err));
+});
+
+/*********************************************************************************************************************/
+/**
+ * @public
+ * @function requestVerifyPhone
+ * @desc Redux async thunk to attempt to verify phone using verification code.
+ */
+export const requestVerifyPhone = createAsyncThunk<
+  PhonesResponse, // return type
+  { code: string; number: string }, // args type
+  { dispatch: DashboardAppDispatch; state: DashboardRootState }
+>("emails/requestVerifyPhone", async (args, thunkAPI) => {
+  const data: KeyValues = {
+    number: args.number,
+    code: args.code,
+  };
+  return makeEmailsRequest<PhonesResponse>(thunkAPI, "verify", data)
+    .then((response) => response.payload)
+    .catch((err) => thunkAPI.rejectWithValue(err));
+});
+
+/*********************************************************************************************************************/
+/**
+ * @public
+ * @function requestResendPhoneCode
+ * @desc Redux async thunk to request new code for verification by phone.
+ */
+export const requestResendPhoneCode = createAsyncThunk<
+  PhonesResponse, // return type
+  { number: string }, // args type
+  { dispatch: DashboardAppDispatch; state: DashboardRootState }
+>("emails/requestResendPhoneCode", async (args, thunkAPI) => {
+  const data: KeyValues = {
+    number: args.number,
+  };
+  return makeEmailsRequest<PhonesResponse>(thunkAPI, "resend-code", data)
+    .then((response) => response.payload)
+    .catch((err) => thunkAPI.rejectWithValue(err));
+});
+
+/*********************************************************************************************************************/
+/**
+ * @public
+ * @function postNewPhone
+ * @desc Redux async thunk to add a new phone number.
  */
 export const postNewPhone = createAsyncThunk<
   PhonesResponse, // return type
@@ -33,6 +91,25 @@ export const postNewPhone = createAsyncThunk<
     primary: false,
   };
   return makeEmailsRequest<PhonesResponse>(thunkAPI, "new", data)
+    .then((response) => response.payload)
+    .catch((err) => thunkAPI.rejectWithValue(err));
+});
+
+/*********************************************************************************************************************/
+/**
+ * @public
+ * @function requestRemovePhone
+ * @desc Redux async thunk to remove a phone number.
+ */
+export const requestRemovePhone = createAsyncThunk<
+  PhonesResponse, // return type
+  { number: string }, // args type
+  { dispatch: DashboardAppDispatch; state: DashboardRootState }
+>("emails/requestRemovePhone", async (args, thunkAPI) => {
+  const data: KeyValues = {
+    number: args.number,
+  };
+  return makeEmailsRequest<PhonesResponse>(thunkAPI, "remove", data)
     .then((response) => response.payload)
     .catch((err) => thunkAPI.rejectWithValue(err));
 });
