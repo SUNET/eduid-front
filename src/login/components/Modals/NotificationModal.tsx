061f76455b8595ab8feaad06f66d294229dededf
Eunju Huss <eunju@sunet.se>
2022-05-10 11:21:16 +0200

    Add typs

--- src/login/components/Modals/NotificationModal.tsx
+++ src/login/components/Modals/NotificationModal.tsx
@@ -1,94 +1,57 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
-import i18n from "../../translation/InjectIntl_HOC_factory";
 import EduIDButton from "../../../components/EduIDButton";
+import { FormattedMessage } from "react-intl";
 
-const RenderCloseButton = ({ closeButtonId, closeModal }) => {
-  return (
-    <EduIDButton id={closeButtonId} buttonstyle="close" className="float-right" onClick={closeModal}></EduIDButton>
-  );
-};
-
-const RenderAcceptButton = ({ href, acceptButtonId, acceptModal, acceptButtonText, translate }) => {
-  return href ? (
-    <>
-      <EduIDButton id={acceptButtonId} buttonstyle="primary" href={href}>
-        {acceptButtonText ? acceptButtonText : translate("cm.accept")}
-      </EduIDButton>
-    </>
-  ) : (
-    <>
-      <EduIDButton id={acceptButtonId} buttonstyle="primary" onClick={acceptModal}>
-        {acceptButtonText ? acceptButtonText : translate("cm.accept")}
-      </EduIDButton>
-    </>
-  );
-};
-
-const RenderModalBody = ({ modalId, mainText }) => {
-  return modalId === "register-modal" ? (
-    <ModalBody dangerouslySetInnerHTML={{ __html: mainText }} />
-  ) : (
-    <ModalBody>
-      <>{mainText}</>
-    </ModalBody>
-  );
-};
-
-class NotificationModal extends Component {
-  render() {
-    const {
-      modalId,
-      showModal,
-      title,
-      mainText,
-      acceptButtonId,
-      acceptModal,
-      closeModal,
-      acceptButtonText,
-      closeButtonId,
-      closeButtonText,
-      translate,
-      href,
-    } = this.props;
-
-    return (
-      <div id={modalId} tabIndex="-1" role="dialog" aria-hidden="true" data-backdrop="true">
-        <Modal isOpen={showModal} className={modalId}>
-          <ModalHeader>
-            {title}
-            <RenderCloseButton
-              closeButtonId={closeButtonId}
-              closeModal={closeModal}
-              translate={translate}
-              closeButtonText={closeButtonText}
-            />
-          </ModalHeader>
-          <RenderModalBody modalId={modalId} mainText={mainText} />
-          <ModalFooter>
-            <RenderAcceptButton
-              acceptButtonId={acceptButtonId}
-              acceptModal={acceptModal}
-              acceptButtonText={acceptButtonText}
-              translate={translate}
-              href={href}
-            />
-          </ModalFooter>
-        </Modal>
-      </div>
-    );
-  }
+interface NotificationModalProps {
+  closeModal: React.MouseEventHandler<HTMLButtonElement>;
+  acceptModal: React.MouseEventHandler<HTMLButtonElement>;
+  modalId: string;
+  mainText: React.ReactNode;
+  acceptButtonId?: string;
+  showModal: boolean;
+  href?: any;
+  title: React.ReactNode;
+  closeButtonId?: string;
 }
 
-NotificationModal.propTypes = {
-  modalId: PropTypes.string,
-  title: PropTypes.any,
-  mainText: PropTypes.any,
-  closeModal: PropTypes.func,
-  acceptModal: PropTypes.func,
-  showModal: PropTypes.bool,
-  confirming: PropTypes.bool,
-};
+function NotificationModal(props: NotificationModalProps) {
+  return (
+    <div id={props.modalId} tabIndex={-1} role="dialog" aria-hidden="true" data-backdrop="true">
+      <Modal isOpen={props.showModal} className={props.modalId}>
+        <ModalHeader>
+          {props.title}
+          <EduIDButton
+            id={props.closeButtonId}
+            buttonstyle="close"
+            className="float-right"
+            onClick={props.closeModal}
+          ></EduIDButton>
+        </ModalHeader>
+        {/* {props.modalId !== "register-modal" ? ( */}
+        <ModalBody>{props.mainText}</ModalBody>
+        {/* ) : (
+          <ModalBody dangerouslySetInnerHTML={{ __html: props.mainText }} />
+        )} */}
+        <ModalFooter>
+          {!props.href ? (
+            <EduIDButton id={props.acceptButtonId} buttonstyle="primary" onClick={props.acceptModal}>
+              <FormattedMessage defaultMessage="accept" description="accept button" />
+            </EduIDButton>
+          ) : (
+            <EduIDButton
+              id={props.acceptButtonId}
+              buttonstyle="primary"
+              onClick={props.href}
+              // href={props.href}
+            >
+              <FormattedMessage defaultMessage="accept" description="accept button" />
+            </EduIDButton>
+          )}
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
 
-export default i18n(NotificationModal);
+export default NotificationModal;
