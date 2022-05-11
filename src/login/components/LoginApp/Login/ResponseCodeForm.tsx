419adaa2a71451ffafd7adb8bdc4c67cbc9ee7f8
cartja <carina@sunet.se>
2022-05-11 11:49:04 +0200

    adjusted space for response code error

--- src/login/components/LoginApp/Login/ResponseCodeForm.tsx
+++ src/login/components/LoginApp/Login/ResponseCodeForm.tsx
@@ -82,15 +82,15 @@ function ShortCodeForm(props: FormRenderProps<ResponseCodeValues> & ResponseCode
         <CodeField num={6} disabled={props.inputsDisabled} />
         <CodeField num={7} disabled={props.inputsDisabled} />
         <CodeField num={8} disabled={props.inputsDisabled} />
-
-        {showBadAttempts && (
-          <div>
-            <span className="input-validate-error" id="wrong-code-error">
-              <FormattedMessage defaultMessage="Incorrect code, try again" description="Other Device, device 1" />
-            </span>
-          </div>
-        )}
       </div>
+
+      {showBadAttempts && (
+        <div>
+          <span className="input-validate-error" id="wrong-code-error">
+            <FormattedMessage defaultMessage="Incorrect code, try again" description="Other Device, device 1" />
+          </span>
+        </div>
+      )}
 
       {props.error && <p>{props.error}</p>}
 
