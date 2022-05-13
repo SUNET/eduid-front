eab839c9c88cb6f36529f8b9eff6ad0491332368
Eunju Huss <eunju@sunet.se>
2022-05-13 11:20:26 +0200

    Change to functional component and remove EmailsDisplay container

--- src/components/EmailDisplay.tsx
+++ src/components/EmailDisplay.tsx
@@ -1,33 +1,30 @@
-import React, { Component } from "react";
-// import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-// import "style/base.scss";
-// import "style/DashboardMain.scss";
+import React from "react";
+import { useDashboardAppSelector } from "dashboard-hooks";
+import { FormattedMessage } from "react-intl";
 
-class EmailDisplay extends Component {
-  render() {
-    let userData = "";
-    if (this.props.email.length === 0) {
-      userData = [
-        <div key="0" className="display-data no-data">
-          {this.props.translate("profile.email_display_no_data")}
-        </div>,
-      ];
-    } else {
-      userData = [
-        <div key="0" className="display-data verified">
-          {this.props.email[0].email}
-        </div>,
-      ];
-    }
-    return (
-      <div key="3" className="profile-grid-cell">
-        <label key="0">{this.props.translate("profile.email_display_title")}</label>
-        {/* <div key="1" id="nin-number-container"> */}
-        {userData}
-        {/* </div> */}
+function EmailDisplay(): JSX.Element {
+  const emails = useDashboardAppSelector((state) => state.emails.emails);
+  const primary = emails.filter((email) => email.primary);
+  let userData;
+  // is it possible to happen, no primary email?
+  if (!primary.length) {
+    userData = (
+      <div className="display-data no-data">
+        <FormattedMessage description="profile email display no data" defaultMessage={`no email added`} />
       </div>
     );
+  } else {
+    userData = <div className="display-data verified">{primary[0].email}</div>;
   }
+
+  return (
+    <div className="profile-grid-cell">
+      <label key="0">
+        <FormattedMessage description="profile email display title" defaultMessage={`Email address`} />
+      </label>
+      {userData}
+    </div>
+  );
 }
 
 export default EmailDisplay;
--- src/containers/EmailDisplay.js
+++ src/containers/EmailDisplay.js
@@ -1,18 +1,18 @@
-import { connect } from "react-redux";
-import EmailDisplay from "components/EmailDisplay";
-import i18n from "../login/translation/InjectIntl_HOC_factory";
+// import { connect } from "react-redux";
+// import EmailDisplay from "components/EmailDisplay";
+// import i18n from "../login/translation/InjectIntl_HOC_factory";
 
-const mapStateToProps = (state) => {
-  const emailAddress = state.emails.emails.filter((email) => email.primary);
-  return {
-    email: emailAddress,
-  };
-};
+// const mapStateToProps = (state) => {
+//   const emailAddress = state.emails.emails.filter((email) => email.primary);
+//   return {
+//     email: emailAddress,
+//   };
+// };
 
-const mapDispatchToProps = () => {
-  return {};
-};
+// const mapDispatchToProps = () => {
+//   return {};
+// };
 
-const EmailDisplayContainer = connect(mapStateToProps, mapDispatchToProps)(EmailDisplay);
+// const EmailDisplayContainer = connect(mapStateToProps, mapDispatchToProps)(EmailDisplay);
 
-export default i18n(EmailDisplayContainer);
+// export default i18n(EmailDisplayContainer);
