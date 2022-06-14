7135f8f2ad228f4d4a2e7bc85e4aa65bee23e9d7
Fredrik Thulin <fredrik@thulin.net>
2022-06-14 11:47:35 +0200

    write first new tests for Dashboard using RTL

--- src/tests/DashboardMain-test.tsx
+++ src/tests/DashboardMain-test.tsx
@@ -1,91 +1,35 @@
-const mock = require("jest-mock");
-import React from "react";
-import { ReduxIntlProvider } from "components/ReduxIntl";
-import { shallow, mount } from "enzyme";
-import expect from "expect";
-import { IntlProvider } from "react-intl";
-import Header from "../components/Header";
-import Footer from "../login/components/Footer/Footer";
-import Notifications from "containers/Notifications";
-import { MemoryRouter } from "react-router-dom";
 import { DashboardMain } from "components/DashboardMain";
+import { activeClassName } from "components/DashboardNav";
+import React from "react";
+import { initialState as configInitialState } from "reducers/DashboardConfig";
+import { dashboardTestHistory, render, screen } from "./helperFunctions/DashboardTestApp-rtl";
 
-const fakeStore = (state) => ({
-  default: () => {},
-  dispatch: mock.fn(),
-  subscribe: mock.fn(),
-  getState: () => ({ ...state }),
+test("shows splash screen when not configured", () => {
+  dashboardTestHistory.push("/profile");
+  render(<DashboardMain />, { state: { config: { ...configInitialState, is_app_loaded: false } } });
+
+  expect(screen.getByRole("heading")).toHaveTextContent("eduID Dashboard:");
+
+  expect(screen.getByRole("progressbar")).toBeInTheDocument();
+  expect(screen.getByRole("progressbar")).toHaveClass("spinner");
 });
 
-function setupComponent() {
-  const store = fakeStore({
-    config: {
-      language: "en",
-    },
-    personal_data: {
-      data: {
-        eppn: "test-eppn",
-      },
-    },
-    emails: {
-      emails: [],
-    },
-    nins: {
-      nins: [],
-    },
-    phones: {
-      phones: [],
-    },
-    profile: {
-      pending: [],
-    },
-    notifications: {
-      messages: [],
-      errors: [],
-    },
-    intl: {
-      locale: "en",
-      messages: {},
-    },
-  });
-  const props = {
-    show_sidebar: true,
-    eppn: "eppn-eppn",
-  };
-  const wrapper = mount(
-    <ReduxIntlProvider store={store}>
-      <MemoryRouter>
-        <DashboardMain {...props} />
-      </MemoryRouter>
-    </ReduxIntlProvider>
-  );
-  return {
-    props,
-    wrapper,
-  };
-}
+test("renders Profile page as expected", () => {
+  dashboardTestHistory.push("/profile");
+  render(<DashboardMain />, { state: { config: { ...configInitialState, is_app_loaded: true } } });
 
-describe("Main Component", () => {
-  it("The component does not render 'false' or 'null'", () => {
-    const wrapper = shallow(
-      <IntlProvider locale="en">
-        <DashboardMain />
-      </IntlProvider>
-    );
-    expect(wrapper.isEmptyRender()).toEqual(false);
-  });
+  expect(screen.getByRole("heading")).toHaveTextContent("eduID Dashboard:");
 
-  it("The <Header/> and <Footer/> render", () => {
-    const { wrapper } = setupComponent();
-    const header = wrapper.find(Header);
-    const footer = wrapper.find(Footer);
-    expect(header.exists()).toEqual(true);
-    expect(footer.exists()).toEqual(true);
-  });
+  expect(screen.queryByRole("progressbar")).not.toBeInTheDocument();
 
-  it("The <Notifications/> render", () => {
-    const { wrapper } = setupComponent();
-    const notifications = wrapper.find(Notifications);
-    expect(notifications.exists()).toEqual(true);
-  });
+  const button = screen.getByRole("button", { name: "Log out" });
+  expect(button).toBeEnabled();
+
+  // check that Profile is the active nav link
+  const nav = screen.getByRole("link", { name: "Profile" });
+  expect(nav).toHaveClass(activeClassName);
+
+  // check that another nav link is _not_ active
+  const nav2 = screen.getByRole("link", { name: "Identity" });
+  expect(nav2).not.toHaveClass(activeClassName);
 });
