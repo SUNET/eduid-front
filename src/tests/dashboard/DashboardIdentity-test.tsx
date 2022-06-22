d774c6fe68c7f19fee4468a30d49d7eedd7ebcca
cartja <carina@sunet.se>
2022-06-22 15:23:43 +0200

    Started implementing new tests using rtl

---
+++ src/tests/dashboard/DashboardIdentity-test.tsx
@@ -0,0 +1,12 @@
+import { DashboardMain } from "components/DashboardMain";
+import { activeClassName, dashboardHeading } from "components/DashboardNav";
+import React from "react";
+import { initialState as configInitialState } from "reducers/DashboardConfig";
+import { dashboardTestHistory, render, screen } from "../helperFunctions/DashboardTestApp-rtl";
+
+test("shows identity tab", () => {
+  dashboardTestHistory.push("/profile/verify-identity");
+  render(<DashboardMain />);
+
+  expect(screen.getByRole("heading", { name: "Svenskt personnummer" })).toBeEnabled();
+});
