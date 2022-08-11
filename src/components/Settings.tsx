8957e22c7deb1373fa43b42d6129b24cc1467ea5
Fredrik Thulin <fredrik@thulin.net>
2022-08-11 09:41:56 +0200

    make two straight up components instead of a bunch of Route

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,38 +1,29 @@
-import AccountId from "containers/AccountId";
-import AccountLinkingContainer from "containers/AccountLinking";
-import DeleteAccount from "containers/DeleteAccount";
 import Emails from "components/Emails";
 import Phones from "components/Phones";
-import SecurityContainer from "containers/Security";
-import React, { Fragment } from "react";
-import { Redirect, Route } from "react-router-dom";
+import DeleteAccount from "containers/DeleteAccount";
+import { Fragment } from "react";
 import GroupManagement from "../login/components/GroupManagement/GroupManagement";
 import PersonalDataParent from "../login/components/PersonalData/PersonalDataParent";
 import ChangePasswordDisplay from "./ChangePasswordDisplay";
-import LadokContainer from "./Ladok";
 
-const RenderGroups = () => {
+function RenderGroups(): JSX.Element | null {
   // functionality to be removed when groups feature is released
   const showComponent = false;
-  return (
-    <Fragment>{showComponent && <Route path="/profile/settings/personaldata" component={GroupManagement} />}</Fragment>
-  );
-};
+  if (!showComponent) return null;
 
+  return <GroupManagement />;
+}
+
+/* The Dashboard "Settings" tab */
 export function Settings(): JSX.Element {
   return (
-    <div>
-      <Route exact path="/profile/settings/" component={() => <Redirect to="/profile/settings/personaldata" />} />
-      <Route path="/profile/settings/personaldata" component={PersonalDataParent} />
-      <Route path="/profile/settings/personaldata" component={Emails} />
-      <Route path="/profile/settings/personaldata" component={Phones} />
+    <Fragment>
+      <PersonalDataParent />
+      <Emails />
+      <Phones />
       <RenderGroups />
-      <Route path="/profile/settings/personaldata" component={ChangePasswordDisplay} />
-      <Route path="/profile/settings/personaldata" component={DeleteAccount} />
-      <Route path="/profile/settings/advanced-settings" component={SecurityContainer} />
-      <Route path="/profile/settings/advanced-settings" component={AccountLinkingContainer} />
-      <Route path="/profile/settings/advanced-settings" component={LadokContainer} />
-      <Route path="/profile/settings/advanced-settings" component={AccountId} />
-    </div>
+      <ChangePasswordDisplay />
+      <DeleteAccount />
+    </Fragment>
   );
 }
