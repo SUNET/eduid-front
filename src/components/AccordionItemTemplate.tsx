c368b0b41f2573497dd8e1a09756c023f9ba1338
Fredrik Thulin <fredrik@thulin.net>
2022-08-19 08:12:24 +0200

    re-work to get vetting buttons working in the accordion element

--- src/components/AccordionItemTemplate.tsx
+++ src/components/AccordionItemTemplate.tsx
@@ -1,30 +1,37 @@
 import React from "react";
 import {
   AccordionItem,
-  AccordionItemHeading,
   AccordionItemButton,
-  AccordionItemState,
+  AccordionItemHeading,
   AccordionItemPanel,
+  AccordionItemState,
 } from "react-accessible-accordion";
 
-const AccordionItemTemplate = ({ icon, title, additionalInfo, content }) => {
+interface AccordionItemTemplateProps {
+  icon: React.ReactNode;
+  title: string;
+  additionalInfo: string;
+  children?: React.ReactNode;
+}
+
+function AccordionItemTemplate(props: AccordionItemTemplateProps) {
   return (
     <AccordionItem>
       <AccordionItemHeading>
         <AccordionItemButton>
-          <span className="accordion-icon">{icon}</span>
+          <span className="accordion-icon">{props.icon}</span>
           <span>
-            <h6 className="accordion-title">{title}</h6>
-            <span className="accordion-description">{additionalInfo}</span>
+            <h6 className="accordion-title">{props.title}</h6>
+            <span className="accordion-description">{props.additionalInfo}</span>
           </span>
           <div className="accordion-toggle">
             <AccordionItemState>{({ expanded }) => (expanded ? "❮" : "❯")}</AccordionItemState>
           </div>
         </AccordionItemButton>
       </AccordionItemHeading>
-      <AccordionItemPanel>{content}</AccordionItemPanel>
+      <AccordionItemPanel>{props.children}</AccordionItemPanel>
     </AccordionItem>
   );
-};
+}
 
 export default AccordionItemTemplate;
--- src/components/VerifyIdentity.tsx
+++ src/components/VerifyIdentity.tsx
@@ -1,50 +1,21 @@
-import React, { Fragment } from "react";
-import LetterProofingButton from "components/LetterProofing";
 import Eidas from "components/Eidas";
+import LetterProofingButton from "components/LetterProofing";
 import OpenidConnectContainer from "containers/OpenidConnect";
 import OpenidConnectFrejaContainer from "containers/OpenidConnectFreja";
 import { useDashboardAppSelector } from "dashboard-hooks";
 import LookupMobileProofing from "login/components/LookupMobileProofing/LookupMobileProofing";
-import AddNin from "./AddNin";
+import React, { Fragment } from "react";
+import { Accordion } from "react-accessible-accordion";
+import { CircleFlag } from "react-circle-flags";
 import { FormattedMessage } from "react-intl";
 import AccordionItemTemplate from "./AccordionItemTemplate";
-import { CircleFlag } from "react-circle-flags";
-import { Accordion } from "react-accessible-accordion";
+import AddNin from "./AddNin";
 
 function VerifyIdentity(): JSX.Element | null {
-  // page text depend on nin status (verified or not)
-  let vettingButtons;
-  const nin = useDashboardAppSelector((state) => state.identities.nin);
   const isConfigured = useDashboardAppSelector((state) => state.config.is_configured);
-  const phones = useDashboardAppSelector((state) => state.phones.phones);
-  const hasVerifiedNin = !!nin?.verified;
-  const hasVerifiedSwePhone = phones.some((phone) => phone.verified && phone.number.startsWith("+46"));
 
   if (!isConfigured) {
     return null;
-  }
-
-  // this is where the buttons are generated
-  if (isConfigured && !hasVerifiedNin) {
-    // BUG: used to be 'vettingRegistry(!this.props.valid_nin);' but there is no such prop.
-    //      I guess the intent was to disable the buttons when the user is verified already?
-    const disabled = !undefined;
-    const addedNin = !!nin;
-
-    // proofing via letter requires the user to have added a NIN first
-    const letterProofingDisabled = !addedNin;
-    // proofing via mobile requires the user to have added a NIN first, and have a verified Swedish mobile phone
-    const lookupMobileDisabled = !addedNin || !hasVerifiedSwePhone;
-
-    vettingButtons = (
-      <div id="nins-btn-grid" className="x-adjust">
-        <LetterProofingButton disabled={letterProofingDisabled} />
-        <LookupMobileProofing disabled={lookupMobileDisabled} />
-        <Eidas />
-        <OpenidConnectContainer disabled={disabled} />
-        <OpenidConnectFrejaContainer disabled={disabled} />
-      </div>
-    );
   }
 
   return (
@@ -56,100 +27,115 @@ function VerifyIdentity(): JSX.Element | null {
             defaultMessage={`Connect your identity to your eduID`}
           />
         </h1>
-        <div className="lead">
-          <p>
-            <FormattedMessage
-              description="verify identity unverified description"
-              defaultMessage={`To be able to use eduID you have to prove your identity. Add your national id number
-              and verify it in real life.`}
-            />
-          </p>
-        </div>
+        <VerifyIdentityIntro />
       </div>
 
-      {!hasVerifiedNin ? (
-        <Fragment>
-          <Accordion allowMultipleExpanded allowZeroExpanded>
-            <AccordionItemSe />
-            <AccordionItemEu />
-            <AccordionItemWorld />
-            <AccordionItemNoIcon />
-            <AccordionItemOnlyTitle />
-          </Accordion>
-
-          {/* Move vetting into individual accordion functions? */}
-          {vettingButtons}
-        </Fragment>
-      ) : (
-        <Fragment>
-          <ol className="listed-steps">
-            <li>
-              <h4>
-                <FormattedMessage
-                  description="verify identity verified title"
-                  defaultMessage="Your eduID is ready to use"
-                />
-              </h4>
-              <p className="x-adjust">
-                <FormattedMessage
-                  description="verify identity verified description"
-                  defaultMessage={`The below id number is now connected to this eduID. Use your eduID to log in to
-                  services related to higher education.`}
-                />
-              </p>
-              <AddNin />
-            </li>
-            <li>
-              <h4>
-                <FormattedMessage
-                  description="verify identity improve security heading"
-                  defaultMessage={`Improve your identification`}
-                />
-              </h4>
-              <p className="x-adjust">
-                <FormattedMessage
-                  description="verify identity improve security description"
-                  defaultMessage={`Add a phone number or a security key to your eduID to keep your identity at
-                      password reset under Settings.`}
-                />
-              </p>
-            </li>
-          </ol>
-        </Fragment>
-      )}
+      <Accordion allowMultipleExpanded allowZeroExpanded>
+        <AccordionItemSe />
+        <AccordionItemEu />
+        <AccordionItemWorld />
+        <AccordionItemNoIcon />
+        <AccordionItemOnlyTitle />
+      </Accordion>
     </Fragment>
   );
 }
 
+function VerifyIdentityIntro(): JSX.Element {
+  const identities = useDashboardAppSelector((state) => state.identities);
+
+  if (identities.is_verified) {
+    /* User has a verified identity. Show which one (or ones) it is.
+     *   TODO: Support other types of identities than NINs.
+     */
+    return (
+      <React.Fragment>
+        <h4>
+          <FormattedMessage description="verify identity verified title" defaultMessage="Your eduID is ready to use" />
+        </h4>
+        <p className="x-adjust">
+          <FormattedMessage
+            description="verify identity verified description"
+            defaultMessage={`The below id number is now connected to this eduID.
+            Use your eduID to log in to services related to higher education.`}
+          />
+        </p>
+        <AddNin />
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <div className="lead">
+        <p>
+          <FormattedMessage
+            description="verify identity unverified description"
+            defaultMessage={`To be able to use eduID you have to prove your identity. Add your national id number
+              and verify it in real life.`}
+          />
+        </p>
+      </div>
+    </React.Fragment>
+  );
+}
+
 function AccordionItemSe(): JSX.Element | null {
+  const nin = useDashboardAppSelector((state) => state.identities.nin);
+  const phones = useDashboardAppSelector((state) => state.phones.phones);
+  const hasVerifiedSwePhone = phones.some((phone) => phone.verified && phone.number.startsWith("+46"));
+
+  // this is where the buttons are generated
+  const addedNin = Boolean(nin);
+  const disabled = false;
+
+  // proofing via letter requires the user to have added a NIN first
+  const letterProofingDisabled = !addedNin;
+  // proofing via mobile requires the user to have added a NIN first, and have a verified Swedish mobile phone
+  const lookupMobileDisabled = !addedNin || !hasVerifiedSwePhone;
+
+  /* Show step two ("use one of these options to verify your NIN") only after step 1 (enter your NIN) is complete,
+     and not in case the NIN is already verified. */
+  const showStepTwo = Boolean(nin?.number) && !nin?.verified;
+
   return (
     <AccordionItemTemplate
       icon={<CircleFlag countryCode="se" height="35" />}
       title="Swedish personal ID number"
       additionalInfo=""
-      content={
-        <ol className="listed-steps">
-          <li>
-            <h4>
-              <FormattedMessage description="verify identity add nin heading" defaultMessage="Add your id number" />
-            </h4>
-            <AddNin />
-          </li>
-          <li>
-            <h4>
-              <FormattedMessage description="verify identity connect nin" defaultMessage="Verify your id number" />
-            </h4>
-            <p className="x-adjust">
-              <FormattedMessage
-                description="verify-identity.connect-nin_description"
-                defaultMessage={`Choose a method to verify that you have access to the added id number.
+    >
+      <ol className="listed-steps">
+        <li>
+          <h4>
+            <FormattedMessage description="verify identity add nin heading" defaultMessage="Add your id number" />
+          </h4>
+          <AddNin />
+        </li>
+        {showStepTwo && (
+          <React.Fragment>
+            <li>
+              <h4>
+                <FormattedMessage description="verify identity connect nin" defaultMessage="Verify your id number" />
+              </h4>
+              <p className="x-adjust">
+                <FormattedMessage
+                  description="verify-identity.connect-nin_description"
+                  defaultMessage={`Choose a method to verify that you have access to the added id number.
           If you are unable to use a method you need to try another.`}
-              />
-            </p>
-          </li>
-        </ol>
-      }
-    />
+                />
+              </p>
+            </li>
+            <div id="nins-btn-grid" className="x-adjust">
+              <LetterProofingButton disabled={letterProofingDisabled} />
+              <LookupMobileProofing disabled={lookupMobileDisabled} />
+              <Eidas />
+              <OpenidConnectContainer disabled={disabled} />
+              <OpenidConnectFrejaContainer disabled={disabled} />
+            </div>
+          </React.Fragment>
+        )}
+      </ol>
+    </AccordionItemTemplate>
   );
 }
 
@@ -159,8 +145,9 @@ function AccordionItemEu(): JSX.Element | null {
       icon={<CircleFlag countryCode="european_union" height="35" />}
       title="EU citizen"
       additionalInfo="eIDAS"
-      content={`Lorem ipsum dolor sit amet consectetur adipisicing elit. A, in!`}
-    />
+    >
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A, in!
+    </AccordionItemTemplate>
   );
 }
 
@@ -170,21 +157,27 @@ function AccordionItemWorld(): JSX.Element | null {
       icon={<CircleFlag countryCode="placeholder" height="35" />}
       title="All other countries"
       additionalInfo="Svipe ID"
-      content={`Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
-  quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
-  dolor ut sequi minus iste? Quas?`}
-    />
+    >
+      Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti quam
+      sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos dolor ut sequi minus iste? Quas?
+    </AccordionItemTemplate>
   );
 }
 
 function AccordionItemNoIcon(): JSX.Element | null {
   return (
-    <AccordionItemTemplate icon="" title="No icon Example" additionalInfo="Additional Info" content="Great content" />
+    <AccordionItemTemplate icon="" title="No icon Example" additionalInfo="Additional Info">
+      Great content
+    </AccordionItemTemplate>
   );
 }
 
 function AccordionItemOnlyTitle(): JSX.Element | null {
-  return <AccordionItemTemplate icon="" title="Only title example" additionalInfo="" content="Other great content" />;
+  return (
+    <AccordionItemTemplate icon="" title="Only title example" additionalInfo="">
+      Other great content
+    </AccordionItemTemplate>
+  );
 }
 
 export default VerifyIdentity;
