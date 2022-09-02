cd6e0c097560ba604b38fa6329257e549ded1c85
Eunju Huss <eunju@sunet.se>
2022-09-02 13:22:11 +0200

    Remove display name from validation

--- src/login/app_utils/validation/validatePersonalData.js
+++ src/login/app_utils/validation/validatePersonalData.js
@@ -3,7 +3,7 @@ import { emptyStringPattern } from "./regexPatterns";
 const validatePersonalData = (values, props) => {
   const errors = {};
   if (values !== undefined) {
-    ["given_name", "surname", "display_name", "language"].forEach((inputName) => {
+    ["given_name", "surname", "language"].forEach((inputName) => {
       if (!values[inputName] || emptyStringPattern.test(values[inputName])) {
         errors[inputName] = "required";
       }
