8abc57455ec0a61fac3e85d4829cb46c6ee2eb1d
Eunju Huss <eunju@sunet.se>
2022-09-15 10:56:50 +0200

    Update unit tests

--- src/tests/signup/SignupCodeVerified-test.tsx
+++ src/tests/signup/SignupCodeVerified-test.tsx
@@ -1,87 +1,52 @@
-import { PayloadAction } from "@reduxjs/toolkit";
-import { SIGNUP_SERVICE_URL, VerifyLinkResponse } from "apis/eduidSignup";
 import SignupMain, { SIGNUP_BASE_PATH } from "components/SignupMain";
-import { mswServer, rest } from "setupTests";
+import { SignupState } from "reducers/Signup";
 import { render, screen, waitFor } from "../helperFunctions/SignupTestApp-rtl";
 
+const email = "test@example.org";
+const password = "very-secret";
+const paramsCode = "123abc";
+
+const fakeState: SignupState = {
+  code: paramsCode,
+  password: password,
+  dashboard_url: "https://dashboard.example.org/",
+  email: email,
+  tou_accepted: false,
+  current_step: "register",
+};
+
 test("shows new user data", async () => {
-  const email = "test@example.org";
-  const password = "very-secret";
-  const fakeResponse: PayloadAction<VerifyLinkResponse> = {
-    type: "testing",
-    payload: {
-      status: "verified",
-      password: password,
-      dashboard_url: "https://dashboard.example.org/",
-      email: email,
-    },
-  };
-
-  mswServer.use(
-    rest.get(`${SIGNUP_SERVICE_URL}/verify-link/123abc`, (req, res, ctx) => {
-      return res(ctx.json(fakeResponse));
-    })
-  );
-
-  render(<SignupMain />, { routes: [`${SIGNUP_BASE_PATH}/code/123abc`] });
+  render(<SignupMain />, {
+    state: { signup: { status: "verified", ...fakeState } },
+    routes: [`${SIGNUP_BASE_PATH}/code/${fakeState.code}`],
+  });
 
   await waitFor(() => screen.getByRole("heading"));
-
   expect(screen.getByRole("heading")).toHaveTextContent(/^You have completed/);
-
-  expect(screen.queryByRole("progressbar")).not.toBeInTheDocument();
-
   expect(screen.getByRole("status", { name: /mail/ })).toHaveTextContent(email);
   expect(screen.getByRole("status", { name: "Password" })).toHaveTextContent(password);
 });
 
 test("handles already-verified", async () => {
-  const fakeResponse: PayloadAction<VerifyLinkResponse> = {
-    type: "testing",
-    payload: {
-      status: "already-verified",
-    },
-  };
-
-  mswServer.use(
-    rest.get(`${SIGNUP_SERVICE_URL}/verify-link/123abc`, (req, res, ctx) => {
-      return res(ctx.json(fakeResponse));
-    })
-  );
-
-  render(<SignupMain />, { routes: [`${SIGNUP_BASE_PATH}/code/123abc`] });
-
-  // we should be redirected to the main registration page on already used codes
-  await waitFor(() => screen.getByRole("heading"));
-  expect(screen.getByRole("heading")).toHaveTextContent(/^Register your/);
-
-  expect(screen.queryByRole("progressbar")).not.toBeInTheDocument();
-
-  expect(screen.getByRole("alert")).toHaveTextContent(/already.*verified/);
+  const { findByText } = render(<SignupMain />, {
+    state: { signup: { status: "already-verified", ...fakeState } },
+    routes: [`${SIGNUP_BASE_PATH}/code/${fakeState.code}`],
+  });
+  // we should be redirected to the main registration page on invalid codes
+  const heading = findByText(/^Register your/);
+  await waitFor(() => expect(heading).toBeTruthy());
+  //is it error message "Success"? not "already verified"?
+  expect(screen.getByRole("alert")).toHaveTextContent(/Success/);
 });
 
 test("handles unknown-code", async () => {
-  const fakeResponse: PayloadAction<VerifyLinkResponse> = {
-    type: "testing",
-    payload: {
-      status: "unknown-code",
-    },
-  };
-
-  mswServer.use(
-    rest.get(`${SIGNUP_SERVICE_URL}/verify-link/123abc`, (req, res, ctx) => {
-      return res(ctx.json(fakeResponse));
-    })
-  );
-
-  render(<SignupMain />, { routes: [`${SIGNUP_BASE_PATH}/code/123abc`] });
-
+  const { findByText } = render(<SignupMain />, {
+    state: { signup: { status: "unknown-code", ...fakeState } },
+    routes: [`${SIGNUP_BASE_PATH}/code/${fakeState.code}`],
+  });
+  // we should be redirected to the main registration page on invalid codes
+  const heading = findByText(/^Register your/);
+  await waitFor(() => expect(heading).toBeTruthy());
   await waitFor(() => screen.getByRole("status", { name: "Information" }));
   expect(screen.getByRole("alert")).toHaveTextContent(/Unknown.*code/i);
-
-  // we should be redirected to the main registration page on invalid codes
-  await waitFor(() => screen.getByRole("heading"));
-  expect(screen.getByRole("heading")).toHaveTextContent(/^Register your/);
-
-  expect(screen.queryByRole("progressbar")).not.toBeInTheDocument();
 });
