c3b6a694f9a8ca0f9b8d26929f7384c0868d9a4c
Fredrik Thulin <fredrik@thulin.net>
2022-09-21 10:34:03 +0200

    bugfix: have to remember verify-link response in redux state
    The backend currently only allows fetching this data once, so in order
    to still have it after a language switch causes the component to
    re-render, we have to put it in the redux state instead of in component
    state.

--- src/reducers/Signup.ts
+++ src/reducers/Signup.ts
@@ -1,12 +1,21 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { isFSA } from "apis/common";
-import { fetchTryCaptcha, isTryCaptchaResponse, TryCaptchaNextStep, TryCaptchaResponse } from "apis/eduidSignup";
-import { response } from "msw";
+import {
+  fetchTryCaptcha,
+  fetchVerifyLink,
+  isTryCaptchaResponse,
+  TryCaptchaNextStep,
+  VerifyLinkResponse,
+} from "apis/eduidSignup";
 
 interface SignupState {
   email?: string;
   tou_accepted: boolean;
   current_step: "register" | TryCaptchaNextStep;
+  // Fetching verify-link is a one-shot operation, so we have to store the response in
+  // redux state (rather than in component state) in case switching language causes us
+  // to re-render the component
+  verify_link_response?: VerifyLinkResponse;
 }
 
 // export for use in tests
@@ -45,6 +54,9 @@ export const signupSlice = createSlice({
            */
           state.current_step = action.payload.payload.next;
         }
+      })
+      .addCase(fetchVerifyLink.fulfilled, (state, action) => {
+        state.verify_link_response = action.payload;
       });
   },
 });
