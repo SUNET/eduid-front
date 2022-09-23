42605c30b48fb18b8594aeb7990f34890a7ebcf7
Eunju Huss <eunju@sunet.se>
2022-09-23 10:00:41 +0200

    Update test

--- src/tests/login/LoginResetPassword-test.tsx
+++ src/tests/login/LoginResetPassword-test.tsx
@@ -129,7 +129,7 @@ test("can click 'forgot password' without an e-mail address", async () => {
   });
 
   const sendButton = screen.getByRole("button", { name: /^send/i });
-  expect(sendButton).toBeDisabled();
+  expect(sendButton).toBeEnabled();
 
   const emailInput = screen.getByRole("textbox");
   expect(emailInput).toHaveFocus();
