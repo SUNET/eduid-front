090dc778e2a7256064585acdcd35e79fed7470d8
Eunju Huss <eunju@sunet.se>
2022-10-03 21:10:27 +0200

    Add changing titles depending on url for login app

--- src/login/components/LoginApp/LoginApp.tsx
+++ src/login/components/LoginApp/LoginApp.tsx
@@ -2,8 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./Login/Login";
 import UseOtherDevice2 from "./Login/UseOtherDevice2";
 import ResetPassword from "./ResetPassword/ResetPassword";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 function LoginApp(): JSX.Element {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname.includes("/login/")) document.title = "Log in | eduID";
+    else if (location.pathname.includes("/reset-password/")) {
+      document.title = "Reset Password | eduID";
+    } else document.title = "eduID";
+  }, [location]);
+
   return (
     <div id="content" className="horizontal-content-margin content">
       <Routes>
