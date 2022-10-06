4895eac3f8b90bcbc4c6290743bc5e1d35162da1
Eunju Huss <eunju@sunet.se>
2022-10-06 12:49:58 +0200

    Add intro, lead

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,10 +3,21 @@ import PersonalDataParent from "login/components/PersonalData/PersonalDataParent
 import Emails from "./Emails";
 import Phones from "./Phones";
 import { AccountId } from "./AccountId";
+import { FormattedMessage } from "react-intl";
 
 export default function Profile(): JSX.Element {
   return (
     <Fragment>
+      <div className="intro">
+        <h1>
+          <FormattedMessage description="profile main title" defaultMessage="Profile" />
+        </h1>
+        <div className="lead">
+          <p>
+            <FormattedMessage description="profile description" defaultMessage="profile lead text" />
+          </p>
+        </div>
+      </div>
       <PersonalDataParent />
       <Emails />
       <Phones />
