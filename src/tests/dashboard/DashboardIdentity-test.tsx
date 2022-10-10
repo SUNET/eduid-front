ca1b60d766b667d4a5faa904b7d96665b4e231aa
Eunju Huss <eunju@sunet.se>
2022-10-10 11:06:19 +0200

    Remove dashboardIdentity-test

--- src/tests/dashboard/DashboardIdentity-test.tsx
+++
@@ -1,8 +0,0 @@
-import { DashboardMain } from "components/DashboardMain";
-import { render, screen } from "../helperFunctions/DashboardTestApp-rtl";
-
-test("shows identity tab", () => {
-  render(<DashboardMain />, { routes: ["/profile/verify-identity/"] });
-
-  expect(screen.getByRole("heading", { name: "Swedish personal ID number" })).toBeEnabled();
-});
