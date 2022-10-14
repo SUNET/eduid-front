e75898101ea0188ac21e8633106f2d593b4ee6e6
Eunju Huss <eunju@sunet.se>
2022-10-14 14:32:28 +0200

    Clean up

--- src/dashboard-root-saga.js
+++ src/dashboard-root-saga.js
@@ -2,7 +2,6 @@ import * as headerActions from "actions/Header";
 import * as openidActions from "actions/OpenidConnect";
 import * as openidFrejaActions from "actions/OpenidConnectFreja";
 import * as pdataActions from "actions/PersonalData";
-import * as securityActions from "actions/Security";
 import { lookupMobileProofing } from "apis/eduidLookupMobileProofing";
 import { all, takeEvery, takeLatest } from "redux-saga/effects";
 import { requestLogout } from "sagas/Header";
@@ -10,7 +9,6 @@ import { requestNins } from "sagas/Nins";
 import * as sagasOpenid from "sagas/OpenidConnect";
 import * as sagasOpenidFreja from "sagas/OpenidConnectFreja";
 import { getInitialUserData, requestAllPersonalData } from "sagas/PersonalData";
-import { postDeleteAccount, removeWebauthnToken, requestPasswordChange, verifyWebauthnToken } from "sagas/Security";
 import { confirmLetterCode, postRequestLetter } from "./apis/eduidLetterProofing";
 import * as updateNamesFromSkatteverketActions from "./login/redux/actions/updateNamesFromSkatteverketActions";
 import { postPersonalDataSaga } from "./login/redux/sagas/personalData/postPersonalDataSaga";
@@ -27,8 +25,6 @@ function* rootSaga() {
     takeLatest(openidFrejaActions.GET_OIDC_PROOFING_FREJA_PROOFING, sagasOpenidFreja.requestOpenidFrejaData),
     takeLatest(openidFrejaActions.SHOW_OIDC_FREJA_MODAL, sagasOpenidFreja.checkNINAndShowFrejaModal),
     takeLatest(openidFrejaActions.HIDE_OIDC_FREJA_MODAL, sagasOpenidFreja.closeFrejaModal),
-    takeLatest(securityActions.initiatePasswordChange.type, requestPasswordChange),
-    takeLatest(securityActions.POST_DELETE_ACCOUNT, postDeleteAccount),
     takeEvery(postRequestLetter.fulfilled, requestAllPersonalData),
     takeEvery(confirmLetterCode.fulfilled, requestAllPersonalData),
     takeEvery(lookupMobileProofing.fulfilled, requestAllPersonalData),
@@ -36,8 +32,6 @@ function* rootSaga() {
     takeEvery(openidActions.POST_OIDC_PROOFING_PROOFING_SUCCESS, requestNins),
     takeEvery(openidFrejaActions.POST_OIDC_PROOFING_FREJA_PROOFING_SUCCESS, requestNins),
     takeEvery(headerActions.POST_LOGOUT, requestLogout),
-    // takeLatest(securityActions.POST_WEBAUTHN_REMOVE, removeWebauthnToken),
-    // takeLatest(securityActions.POST_WEBAUTHN_VERIFY, verifyWebauthnToken),
   ]);
 }
 
