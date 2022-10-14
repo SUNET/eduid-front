491b62df1e1f60bedada9b9ed99da8d0d5f33899
Eunju Huss <eunju@sunet.se>
2022-10-14 09:32:56 +0200

    Add createCredential

--- src/login/app_utils/helperFunctions/navigatorCredential.ts
+++ src/login/app_utils/helperFunctions/navigatorCredential.ts
@@ -2,6 +2,12 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import * as CBOR from "../../../sagas/cbor";
 import { safeDecode, safeEncode } from "./base64Utils";
 
+export interface webauthnAttestation {
+  attestationObject: string;
+  clientDataJSON: string;
+  credentialId: string;
+}
+
 // serialised version of a PublicKeyCredential
 export interface webauthnAssertion {
   credentialId: string;
@@ -58,26 +64,26 @@ export const performAuthentication = createAsyncThunk(
   }
 );
 
-export const createAuthentication = createAsyncThunk(
-  "eduid/credentials/createAuthentication",
-  async (webauthn_challenge: any, thunkAPI): Promise<any | undefined> => {
-    const assertion = await navigator.credentials
-      .create(webauthn_challenge)
+export const createCredential = createAsyncThunk(
+  "eduid/credentials/createCredential",
+  async (webauthn_challenge: string, thunkAPI): Promise<webauthnAttestation | undefined> => {
+    const decoded_challenge = decodeChallenge(webauthn_challenge);
+    const credential = await navigator.credentials
+      .create(decoded_challenge)
       .then()
       .catch(() => {
-        // assertion failed / cancelled
-        return thunkAPI.rejectWithValue("Authentication failed, or was cancelled");
+        // create credential failed / cancelled
+        return thunkAPI.rejectWithValue("Create credential failed, or was cancelled");
       });
-    return assertion;
-    // if (assertion instanceof PublicKeyCredential && assertion.response instanceof AuthenticatorAssertionResponse) {
-    //   // encode the assertion into strings that can be stored in the state
-    //   const encoded_assertion: webauthnAssertion = {
-    //     credentialId: safeEncode(assertion.rawId),
-    //     authenticatorData: safeEncode(assertion.response.authenticatorData),
-    //     clientDataJSON: safeEncode(assertion.response.clientDataJSON),
-    //     signature: safeEncode(assertion.response.signature),
-    //   };
-    //   return encoded_assertion;
-    // }
+
+    if (credential instanceof PublicKeyCredential && credential.response instanceof AuthenticatorAttestationResponse) {
+      // encode the credential into strings that can be stored in the state
+      const encoded_credential: webauthnAttestation = {
+        attestationObject: safeEncode(credential.response.attestationObject),
+        clientDataJSON: safeEncode(credential.response.clientDataJSON),
+        credentialId: safeEncode(credential.rawId),
+      };
+      return encoded_credential;
+    }
   }
 );
