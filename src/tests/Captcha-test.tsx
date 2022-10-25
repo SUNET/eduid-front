94a51757918321d3407421aef236c6bb11d6292a
Fredrik Thulin <fredrik@thulin.net>
2022-10-25 09:19:36 +0200

    stop using enzyme

--- src/tests/Captcha-test.tsx
+++ src/tests/Captcha-test.tsx
@@ -1,9 +1,5 @@
 import { Captcha } from "components/Captcha";
-import { shallow } from "enzyme";
-import { rest } from "msw";
-import { IntlProvider } from "react-intl";
-import { mswServer } from "setupTests";
-import { setupComponent } from "./helperFunctions/SignupTestApp";
+import { render, screen } from "./helperFunctions/SignupTestApp-rtl";
 
 function handleCaptchaCancel() {}
 function handleCaptchaCompleted(response: string) {}
@@ -11,37 +7,9 @@ function toggleCaptcha() {}
 
 const args = { handleCaptchaCancel, handleCaptchaCompleted, toggleCaptcha };
 
-describe("Captcha Component", () => {
-  beforeEach(() => {
-    mswServer.use(
-      rest.get("https://www.google.com/recaptcha/api.js", (req, res, ctx) => {
-        return res(ctx.body("dummy-script"));
-      })
-    );
-  });
-  it("The component does not render 'false' or 'null'", () => {
-    const wrapper = shallow(
-      <IntlProvider locale="en">
-        <Captcha {...args} />
-      </IntlProvider>
-    );
-    expect(wrapper.isEmptyRender()).toEqual(false);
-  });
+test("Google ReCaptcha loads", () => {
+  render(<Captcha {...args} />);
 
-  it("The captcha <div> element renders", () => {
-    const fullWrapper = setupComponent({
-      component: <Captcha {...args} />,
-    });
-    const captcha = fullWrapper.find("#captcha");
-    expect(captcha.exists()).toEqual(true);
-  });
-
-  it("Renders the CANCEL button", () => {
-    const fullWrapper = setupComponent({
-      component: <Captcha {...args} />,
-    });
-    const button = fullWrapper.find("EduIDButton");
-    expect(button.exists()).toEqual(true);
-    expect(button.length).toEqual(1);
-  });
+  expect(screen.getByRole("button", { name: /Switch/i })).toBeEnabled();
+  expect(screen.getByRole("button", { name: /Cancel/i })).toBeEnabled();
 });
