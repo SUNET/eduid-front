716b5b447f496089699074d3ea0306bf97883595
Eunju Huss <eunju@sunet.se>
2022-10-27 11:31:57 +0200

    Add specific routes for security zone

--- src/components/DashboardMain.tsx
+++ src/components/DashboardMain.tsx
@@ -4,7 +4,6 @@ import Footer from "login/components/Footer/Footer";
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AdvancedSettings } from "./AdvancedSettings";
-import { SecurityZoneMain } from "./SecurityZoneMain";
 import DashboardNav from "./DashboardNav";
 import { ExternalReturnHandler } from "./ExternalReturnHandler";
 import Header from "./Header";
@@ -12,6 +11,9 @@ import Profile from "./Profile";
 import { Settings } from "./Settings";
 import Splash from "./Splash";
 import VerifyIdentity from "./VerifyIdentity";
+import { Security } from "./Security";
+import { ChangePasswordContainer } from "./ChangePassword";
+import DeleteAccount from "./DeleteAccount";
 
 export function DashboardMain() {
   const emails = useDashboardAppSelector((state) => state.emails.emails);
@@ -38,7 +40,6 @@ export function DashboardMain() {
                 <Route path="/profile/settings/personaldata/" element={<Settings />} />
                 <Route path="/profile/settings/" element={<Navigate to="/profile/settings/personaldata/" />} />
                 <Route path="/profile/verify-identity/" element={<VerifyIdentity />} />
-                <Route path="/profile/security-zone/" element={<SecurityZoneMain />} />
                 <Route path="/profile/ext-return/:app_name/:authn_id" element={<ExternalReturnHandler />} />
                 {/* Navigates for old paths. TODO: redirect in backend server instead */}
                 <Route path="/profile/security/" element={<Navigate to="/profile/settings/" />} />
@@ -48,7 +49,12 @@ export function DashboardMain() {
                 />
                 <Route path="/profile/nins/" element={<Navigate to="/profile/verify-identity/" />} />
                 <Route path="/profile/emails/" element={<Navigate to="/profile/settings/personaldata/" />} />
+
                 <Route path="/profile/" element={<Profile />} />
+                {/* Navigates for security zone */}
+                <Route path="/profile/security-zone/security" element={<Security />} />
+                <Route path="/profile/security-zone/change-password/" element={<ChangePasswordContainer />} />
+                <Route path="/profile/security-zone/delete-account/" element={<DeleteAccount />} />
               </Routes>
             </div>
           </div>
