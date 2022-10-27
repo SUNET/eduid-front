6031d829e22d81d3f7f03b58b91fbf2e1bf2db1b
Eunju Huss <eunju@sunet.se>
2022-10-27 11:30:23 +0200

    Add security zone nav

--- src/components/SecurityZoneMain.tsx
+++ src/components/SecurityZoneMain.tsx
@@ -1,13 +1,51 @@
-import { ChangePasswordContainer } from "./ChangePassword";
-import DeleteAccount from "./DeleteAccount";
-import { Security } from "./Security";
+import { FormattedMessage } from "react-intl";
+import { NavLink } from "react-router-dom";
 
-export function SecurityZoneMain(): JSX.Element {
+export function SecurityZoneIntro(): JSX.Element {
   return (
-    <div className="security-zone">
-      <Security />
-      <ChangePasswordContainer />
-      <DeleteAccount />
+    <div>
+      <h1>
+        <FormattedMessage defaultMessage="Security Zone" description="Security Zone heading" />
+      </h1>
+      <div className="lead">
+        <p tabIndex={0}>
+          <FormattedMessage
+            defaultMessage={`Security zone has a time limit before you need to
+                reauthenticate yourself to continue accessing the security settings.`}
+            description="Security zone description"
+          />
+        </p>
+      </div>
     </div>
+  );
+}
+
+export function SecurityZoneNav(): JSX.Element {
+  return (
+    <nav id="security-zone-nav" className="security-zone-nav">
+      <ul>
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? "active" : undefined)} to="/profile/security-zone/security">
+            <FormattedMessage defaultMessage="Security" description="Security zone tab Security" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : undefined)}
+            to="/profile/security-zone/change-password/"
+          >
+            <FormattedMessage defaultMessage="Change Password" description="Security zone tab Change Password" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : undefined)}
+            to="/profile/security-zone/delete-account/"
+          >
+            <FormattedMessage defaultMessage="Delete Account" description="Security zone tab Delete Account" />
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
