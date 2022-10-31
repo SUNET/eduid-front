2542acc03fa693063ec64c866df6699029396615
Fredrik Thulin <fredrik@thulin.net>
2022-10-31 16:40:36 +0100

    Remove redundant close and retry buttons.
    Chrome and Firefox now has UI components that render these functions
    pointless.

--- src/login/components/LoginApp/Login/SecurityKey.tsx
+++ src/login/components/LoginApp/Login/SecurityKey.tsx
@@ -1,13 +1,9 @@
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { faRedo, faTimes } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { fetchMfaAuth } from "apis/eduidLogin";
 import EduIDButton from "components/EduIDButton";
 import { useAppDispatch, useAppSelector } from "login/app_init/hooks";
 import { performAuthentication } from "login/app_utils/helperFunctions/navigatorCredential";
 import { Fragment, useEffect, useState } from "react";
 import { FormattedMessage } from "react-intl";
-import { clearNotifications } from "reducers/Notifications";
 import SecurityKeyGif from "../../../../../img/computer_animation.gif";
 
 export function SecurityKey(): JSX.Element {
@@ -21,15 +17,15 @@ export function SecurityKey(): JSX.Element {
     <div className="option-wrapper">
       <div className="option">
         {active ? <SecurityKeyActive setActive={setActive} /> : <SecurityKeyInactive setActive={setActive} />}
+        {active && (
+          <p className="help-text">
+            <FormattedMessage
+              description="login mfa primary option hint"
+              defaultMessage="If your Security Key has a button, don't forget to tap it."
+            />
+          </p>
+        )}
       </div>
-      {active && (
-        <p className="help-text">
-          <FormattedMessage
-            description="login mfa primary option hint"
-            defaultMessage="If your security key has a button, don't forget to tap it."
-          />
-        </p>
-      )}
     </div>
   );
 }
@@ -71,7 +67,6 @@ function SecurityKeyInactive({ setActive }: SecurityKeyProps): JSX.Element {
 }
 
 function SecurityKeyActive({ setActive }: SecurityKeyProps): JSX.Element {
-  const [retryToggle, setRetryToggle] = useState(false); // TODO: retry mechanism is probably broken
   const mfa = useAppSelector((state) => state.login.mfa);
   const ref = useAppSelector((state) => state.login.ref);
   const dispatch = useAppDispatch();
@@ -102,53 +97,10 @@ function SecurityKeyActive({ setActive }: SecurityKeyProps): JSX.Element {
             defaultMessage="This Device / Security key"
           />
         </h4>
-        <CloseButton setActive={setActive} />
       </div>
       <div className="button-pair bottom">
-        <img src={SecurityKeyGif} alt="animation of security key inserted into computer" />
-        <RetryButton retryToggle={retryToggle} setRetryToggle={setRetryToggle} />
+        <img src={SecurityKeyGif} alt="animation of Security Key inserted into computer" />
       </div>
     </>
-  );
-}
-
-interface CloseButtonProps {
-  setActive(val: boolean): void;
-}
-
-function CloseButton({ setActive }: CloseButtonProps): JSX.Element {
-  const faTimesCasted = faTimes as IconProp;
-  const dispatch = useAppDispatch();
-  return (
-    <button
-      className="icon"
-      onClick={() => {
-        dispatch(clearNotifications());
-        setActive(false);
-      }}
-    >
-      <FontAwesomeIcon icon={faTimesCasted} />
-    </button>
-  );
-}
-
-interface RetryButtonProps {
-  retryToggle: boolean;
-  setRetryToggle(val: boolean): void;
-}
-
-function RetryButton({ retryToggle, setRetryToggle }: RetryButtonProps): JSX.Element {
-  const faRedoCasted = faRedo as IconProp;
-  const dispatch = useAppDispatch();
-  return (
-    <button
-      className="icon"
-      onClick={() => {
-        setRetryToggle(!retryToggle);
-        dispatch(clearNotifications());
-      }}
-    >
-      <FontAwesomeIcon icon={faRedoCasted} />
-    </button>
   );
 }
