c6eae37d9c60de5440dee9a42c7cafc90f4e3c93
Eunju Huss <eunju@sunet.se>
2022-11-15 13:26:30 +0100

    Clean up

--- src/login/redux/slices/resetPasswordSlice.ts
+++ src/login/redux/slices/resetPasswordSlice.ts
@@ -40,45 +40,14 @@ export const resetPasswordSlice = createSlice({
     savePhoneCode: (state, action: PayloadAction<string>) => {
       state.phone.phone_code = action.payload;
     },
-    saveLinkCode: (state, action: PayloadAction<string>) => {
-      state.email_code = action.payload;
-    },
     // Depending on selectedOption, this will call correct action of new password.
     selectExtraSecurity: (state, action: PayloadAction<string>) => {
       state.selected_option = action.payload;
     },
-    // Action connected to postExtraSecurityPhoneSaga. Will post phone.index to the /extra-security-phone endpoint.
-    requestPhoneCode: (state, action: PayloadAction<{ index: number; number: string }>) => {
-      state.phone.index = action.payload.index;
-      state.phone.number = action.payload.number;
-    },
     storeNewPassword: (state, action: PayloadAction<string>) => {
       state.new_password = action.payload;
     },
-    resetPasswordVerifyEmailSuccess: (
-      state,
-      action: PayloadAction<{
-        email_address: string;
-        email_code: string;
-        extra_security: ExtraSecurityType;
-        suggested_password: string;
-      }>
-    ) => {
-      state.email_address = action.payload.email_address;
-      state.email_code = action.payload.email_code;
-      state.extra_security = action.payload.extra_security;
-      state.suggested_password = action.payload.suggested_password;
-    },
-    // Common action to signal a caught exception in one of the reset password sagas.
-    resetPasswordSagaFail: () => {},
-    // Action connected to postVerifySaga.
-    useLinkCode: () => {},
-    // Action connected to postSetNewPasswordExtraSecurityPhoneSaga. Will post stored phone_code, new_password to the /new-password-extra-security-phone endpoint.
-    setNewPasswordExtraSecurityPhone: () => {},
-    // Action connected to postSetNewPasswordExtraSecurityPhoneSaga. Will post stored phone_code, new_password to the /new-password-extra-security-phone endpoint.
-    setNewPasswordExtraSecurityToken: () => {},
     // Action connected to postSetNewPasswordExternalMfaSaga. Will post stored phone_code, new_password to the /new-password-extra-security-external-mfa endpoint.
-    setNewPasswordExtraSecurityExternalMfa: () => {},
     setGotoUrl: (state, action: PayloadAction<string | undefined>) => {
       // when sagas have completed and want to direct the user to a new route, they set goto_url.
       state.goto_url = action.payload;
@@ -88,6 +57,9 @@ export const resetPasswordSlice = createSlice({
     },
     resetEmailStatus: (state) => {
       state.email_status = undefined;
+    },
+    setPhone: (state, action) => {
+      state.phone = action.payload;
     },
   },
   extraReducers: (builder) => {
