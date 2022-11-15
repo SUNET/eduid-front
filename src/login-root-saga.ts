14b475422ab318e8d47d2e663437592dce5f66d2
Eunju Huss <eunju@sunet.se>
2022-11-15 13:23:47 +0100

    Remove saga

--- src/login-root-saga.ts
+++ src/login-root-saga.ts
@@ -1,21 +1,8 @@
-import { requestPhoneCodeForNewPassword } from "login/redux/sagas/resetpassword/postExtraSecurityPhoneSaga";
-import { postSetNewPasswordExternalMfa } from "login/redux/sagas/resetpassword/postSetNewPasswordExtraSecurityExternalMfaSaga";
-import { postSetNewPasswordExtraSecurityPhone } from "login/redux/sagas/resetpassword/postSetNewPasswordExtraSecurityPhoneSaga";
-import { postSetNewPasswordExtraSecurityToken } from "login/redux/sagas/resetpassword/postSetNewPasswordExtraSecurityTokenSaga";
 import loginSagas from "login/redux/sagas/rootSaga/loginSagas";
-import resetPasswordSlice from "login/redux/slices/resetPasswordSlice";
-import { all, takeLatest } from "redux-saga/effects";
+import { all } from "redux-saga/effects";
 
 function* rootSaga() {
-  yield all([
-    ...loginSagas,
-    takeLatest(resetPasswordSlice.actions.requestPhoneCode, requestPhoneCodeForNewPassword),
-    // TODO: test (security phone request failed, trigger /verify-email to get users extra security) and remove
-    // takeLatest("POST_RESET_PASSWORD_EXTRA_SECURITY_PHONE_FAIL", requestLinkCode),
-    takeLatest(resetPasswordSlice.actions.setNewPasswordExtraSecurityPhone, postSetNewPasswordExtraSecurityPhone),
-    takeLatest(resetPasswordSlice.actions.setNewPasswordExtraSecurityToken, postSetNewPasswordExtraSecurityToken),
-    takeLatest(resetPasswordSlice.actions.setNewPasswordExtraSecurityExternalMfa, postSetNewPasswordExternalMfa),
-  ]);
+  yield all([...loginSagas]);
 }
 
 export default rootSaga;
