ac9749ab6c53c4772ad64b8dfa7ef988561c1125
Eunju Huss <eunju@sunet.se>
2022-11-24 14:04:32 +0100

    Fix conflict

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ export function Header(props: HeaderProps): JSX.Element {
   const dashboard_url = useDashboardAppSelector((state) => state.config.dashboard_url);
   const eduid_site_url = useDashboardAppSelector((state) => state.config.eduid_site_url);
   const token_service_url = useDashboardAppSelector((state) => state.config.token_service_url);
-  //let userName;
+  let userName;
   const [isOpen, setOpen] = useState<boolean>(false);
   const login_url = useDashboardAppSelector((state) => state.config.login_base_url);
   const start_url = dashboard_url || eduid_site_url;
@@ -60,7 +60,7 @@ export function Header(props: HeaderProps): JSX.Element {
       </EduIDButton>
     );
   } else if (props.showLogout) {
-    //userName = <div className="header-user">{props.email}</div>;
+    userName = <div className="header-user">{props.email}</div>;
     button = (
       <EduIDButton buttonstyle="secondary" size="sm" id="logout" onClick={handleLogout} disabled={!login_url}>
         <FormattedMessage defaultMessage="Log out" description="Header logout" />
@@ -83,7 +83,6 @@ export function Header(props: HeaderProps): JSX.Element {
         <a href={start_url} aria-label="eduID start" title="eduID start">
           <div id="eduid-logo" className="eduid-logo" />
         </a>
-        {/* {userName} */}
 
         <Hamburger rounded duration={0.2} label="Menu" size={28} toggled={isOpen} toggle={setOpen} />
 
@@ -92,6 +91,8 @@ export function Header(props: HeaderProps): JSX.Element {
           <div>{button}</div>
         </div>
       </header>
+
+      {userName}
     </section>
   );
 }
