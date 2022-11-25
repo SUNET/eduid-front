7e35729f771f272d2d0ae5d21964a10510abdd70
Eunju Huss <eunju@sunet.se>
2022-11-25 08:43:43 +0100

    Simplify duplicated code

--- src/components/ExternalReturnHandler.tsx
+++ src/components/ExternalReturnHandler.tsx
@@ -1,4 +1,4 @@
-import { eidasGetStatus } from "apis/eduidEidas";
+import { eidasGetStatus, GetStatusResponse } from "apis/eduidEidas";
 import { svipeGetStatus } from "apis/eduidSvipe";
 import { useDashboardAppDispatch } from "dashboard-hooks";
 import { useEffect } from "react";
@@ -16,62 +16,45 @@ export function ExternalReturnHandler() {
   const navigate = useNavigate();
   const params = useParams() as LoginParams;
 
+  function fetchStatus(response: GetStatusResponse) {
+    const status = response;
+    if (status?.method) {
+      // Status has been fetched
+
+      if (status.status) {
+        dispatch(showNotification({ message: status.status, level: status.error ? "error" : "info" }));
+      }
+
+      if (status.frontend_action) {
+        // actionToRoute is a mapping from frontend_action values to where in the Dashboard application
+        // the user should be returned to
+        const actionToRoute: { [key: string]: string } = {
+          eidasVerifyIdentity: "/profile/verify-identity/",
+          eidasVerifyCredential: "/profile/settings/advanced-settings/",
+          svipeidVerifyIdentity: "/profile/verify-identity/",
+        };
+        const _path = actionToRoute[status.frontend_action];
+        if (_path) {
+          navigate(_path);
+          return;
+        }
+      }
+
+      navigate("/profile/"); // GOTO start
+    }
+  }
+
   async function fetchEidasStatus(authn_id: string) {
     const response = await dispatch(eidasGetStatus({ authn_id: authn_id }));
     if (eidasGetStatus.fulfilled.match(response)) {
-      const status = response.payload;
-      if (status?.method) {
-        // Status has been fetched
-
-        if (status.status) {
-          dispatch(showNotification({ message: status.status, level: status.error ? "error" : "info" }));
-        }
-
-        if (status.frontend_action) {
-          // actionToRoute is a mapping from frontend_action values to where in the Dashboard application
-          // the user should be returned to
-          const actionToRoute: { [key: string]: string } = {
-            eidasVerifyIdentity: "/profile/verify-identity/",
-            eidasVerifyCredential: "/profile/settings/advanced-settings/",
-          };
-          const _path = actionToRoute[status.frontend_action];
-          if (_path) {
-            navigate(_path);
-            return;
-          }
-        }
-
-        navigate("/profile/"); // GOTO start
-      }
+      fetchStatus(response.payload);
     }
   }
 
   async function fetchSvipeStatus(authn_id: string) {
     const response = await dispatch(svipeGetStatus({ authn_id: authn_id }));
     if (svipeGetStatus.fulfilled.match(response)) {
-      const status = response.payload;
-      if (status?.method) {
-        // Status has been fetched
-
-        if (status.status) {
-          dispatch(showNotification({ message: status.status, level: status.error ? "error" : "info" }));
-        }
-
-        if (status.frontend_action) {
-          // actionToRoute is a mapping from frontend_action values to where in the Dashboard application
-          // the user should be returned to
-          const actionToRoute: { [key: string]: string } = {
-            svipeidVerifyIdentity: "/profile/verify-identity/",
-          };
-          const _path = actionToRoute[status.frontend_action];
-          if (_path) {
-            navigate(_path);
-            return;
-          }
-        }
-
-        navigate("/profile/"); // GOTO start
-      }
+      fetchStatus(response.payload);
     }
   }
 
