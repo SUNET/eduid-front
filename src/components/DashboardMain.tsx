d51d77c76a7575246498ba0f0c9161d32cf288bd
Eunju Huss <eunju@sunet.se>
2022-12-08 09:17:17 +0100

    No need to inject email to header

--- src/components/DashboardMain.tsx
+++ src/components/DashboardMain.tsx
@@ -1,6 +1,5 @@
 import { AdvancedSettings } from "components/AdvancedSettings";
 import { ChangePasswordContainer } from "components/ChangePassword";
-import DashboardNav from "components/DashboardNav";
 import { ExternalReturnHandler } from "components/ExternalReturnHandler";
 import { Header } from "components/Header";
 import { Notifications } from "components/Notifications";
@@ -14,43 +13,29 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 export function DashboardMain() {
-  const emails = useDashboardAppSelector((state) => state.emails.emails);
   const isLoaded = useDashboardAppSelector((state) => state.config.is_app_loaded);
-
-  let email;
-  if (emails.length >= 1) {
-    email = emails.filter((mail) => mail.primary)[0].email;
-  } else {
-    email = "";
-  }
 
   return (
     <React.StrictMode>
-      <Header email={email} showLogout={true} />
+      <Header showMenu={true} />
       <section id="panel" className="panel">
         <Notifications />
         <Splash showChildren={isLoaded}>
           <div id="content" className="horizontal-content-margin content">
-            <DashboardNav />
-            <div id="text-content">
-              <Routes>
-                <Route path="/profile/settings/advanced-settings/" element={<AdvancedSettings />} />
-                <Route path="/profile/settings/personaldata/" element={<Settings />} />
-                <Route path="/profile/settings/" element={<Navigate to="/profile/settings/personaldata/" />} />
-                <Route path="/profile/verify-identity/" element={<VerifyIdentity />} />
-                <Route path="/profile/chpass/" element={<ChangePasswordContainer />} />
-                <Route path="/profile/ext-return/:app_name/:authn_id" element={<ExternalReturnHandler />} />
-                {/* Navigates for old paths. TODO: redirect in backend server instead */}
-                <Route path="/profile/security/" element={<Navigate to="/profile/settings/" />} />
-                <Route
-                  path="/profile/accountlinking/"
-                  element={<Navigate to="/profile/settings/advanced-settings/" />}
-                />
-                <Route path="/profile/nins/" element={<Navigate to="/profile/verify-identity/" />} />
-                <Route path="/profile/emails/" element={<Navigate to="/profile/settings/personaldata/" />} />
-                <Route path="/profile/" element={<Profile />} />
-              </Routes>
-            </div>
+            <Routes>
+              <Route path="/profile/settings/advanced-settings/" element={<AdvancedSettings />} />
+              <Route path="/profile/settings/personaldata/" element={<Settings />} />
+              <Route path="/profile/settings/" element={<Navigate to="/profile/settings/personaldata/" />} />
+              <Route path="/profile/verify-identity/" element={<VerifyIdentity />} />
+              <Route path="/profile/chpass/" element={<ChangePasswordContainer />} />
+              <Route path="/profile/ext-return/:app_name/:authn_id" element={<ExternalReturnHandler />} />
+              {/* Navigates for old paths. TODO: redirect in backend server instead */}
+              <Route path="/profile/security/" element={<Navigate to="/profile/settings/" />} />
+              <Route path="/profile/accountlinking/" element={<Navigate to="/profile/settings/advanced-settings/" />} />
+              <Route path="/profile/nins/" element={<Navigate to="/profile/verify-identity/" />} />
+              <Route path="/profile/emails/" element={<Navigate to="/profile/settings/personaldata/" />} />
+              <Route path="/profile/" element={<Profile />} />
+            </Routes>
           </div>
         </Splash>
       </section>
