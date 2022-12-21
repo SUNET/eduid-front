a134af42ce5d7138032b75648a501d65052bb53c
Eunju Huss <eunju@sunet.se>
2022-12-21 10:11:36 +0100

    Update test if start page renders username

--- src/tests/DashboardMain-test.tsx
+++ src/tests/DashboardMain-test.tsx
@@ -21,11 +21,13 @@ test("renders Profile page as expected", () => {
     state: {
       ...defaultDashboardTestState,
       config: { ...defaultDashboardTestState.config, login_base_url: "https://example.com/login" },
+      personal_data: {
+        display_name: "test user",
+      },
     },
   });
 
-  expect(screen.getAllByRole("heading")[0]).toHaveTextContent(/Welcome, !/);
-
+  expect(screen.getAllByRole("heading")[0]).toHaveTextContent(/Welcome, test user!/);
   expect(screen.queryByRole("progressbar")).not.toBeInTheDocument();
 
   const button = screen.getByRole("button", { name: "Log out" });
