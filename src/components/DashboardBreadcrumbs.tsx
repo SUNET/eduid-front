f3af41a41de7ca9f55aff8ddf6cbd05175bf525d
Eunju Huss <eunju@sunet.se>
2023-01-30 11:22:48 +0100

    Add aria-label for a link

--- src/components/DashboardBreadcrumbs.tsx
+++ src/components/DashboardBreadcrumbs.tsx
@@ -13,7 +13,7 @@ export function DashboardBreadcrumbs({ pageIcon, currentPage }: DashboardBreadcr
   if (currentPage === "Start") {
     return (
       <nav className="breadcrumb" aria-label="breadcrumb">
-        <Link to="#" className="disabled">
+        <Link to="#" className="disabled" aria-label="disabled link to start page">
           <FontAwesomeIcon icon={faHome as IconProp} />
           <FormattedMessage description="Start" defaultMessage="Start" />
         </Link>
@@ -22,12 +22,12 @@ export function DashboardBreadcrumbs({ pageIcon, currentPage }: DashboardBreadcr
   }
   return (
     <nav className="breadcrumb" aria-label="breadcrumb">
-      <Link key="/profile/" to="/profile/">
+      <Link key="/profile/" to="/profile/" aria-label="go to start page">
         <FontAwesomeIcon icon={faHome as IconProp} />
         <FormattedMessage description="Start" defaultMessage="Start" />
       </Link>
       <span aria-hidden="true">/</span>
-      <Link to="#" className="disabled">
+      <Link to="#" className="disabled" aria-label={`disabled link to ${currentPage}`}>
         <FontAwesomeIcon icon={pageIcon} />
         {currentPage}
       </Link>
