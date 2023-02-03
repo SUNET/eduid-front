91940c8d702f0097b77b01fa992c1e59c9e9fd47
Eunju Huss <eunju@sunet.se>
2023-02-03 14:24:40 +0100

    Clean up

--- src/reducers/PersonalData.ts
+++ src/reducers/PersonalData.ts
@@ -1,14 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { PersonalDataRequest, postPersonalData, requestAllPersonalData } from "apis/eduidPersonalData";
 
-// export for use in tests
 interface PersonalDataState {
   response?: PersonalDataResponse;
 }
 // export for use in tests
 export const initialState: PersonalDataState = {};
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface PersonalDataResponse extends PersonalDataRequest {
   eppn: string;
 }
