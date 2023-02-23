2141ff2354b05ad8053bb5fbe9b6e38c7d83921d
Eunju Huss <eunju@sunet.se>
2023-02-23 09:47:20 +0100

    Add comments to understand the pagination button logic

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -22,46 +22,49 @@ const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage, setCurren
   const [arrOfCurrentButtons, setArrOfCurrentButtons] = useState<any>([]);
 
   useEffect(() => {
-    window.scrollTo({
-      behavior: "smooth",
-      top: 0,
-    });
-  }, [currentPage]);
-
-  useEffect(() => {
     let numberOfPages = [...pageNumbers];
     const dotsInitial = "...";
     const dotsLeft = "...  ";
     const dotsRight = "  ...";
 
-    if (currentPage >= 1 && currentPage <= 3) {
-      numberOfPages = [1, 2, 3, 4, dotsInitial, pageNumbers.length];
-    } else if (currentPage === 4) {
-      const sliced = pageNumbers.slice(0, 5);
-      numberOfPages = [...sliced, dotsInitial, pageNumbers.length];
-    } else if (currentPage > 4 && currentPage < pageNumbers.length - 2) {
-      const sliced1 = pageNumbers.slice(currentPage - 2, currentPage);
-      const sliced2 = pageNumbers.slice(currentPage, currentPage + 1);
-      numberOfPages = [1, dotsLeft, ...sliced1, ...sliced2, dotsRight, pageNumbers.length];
-    } else if (currentPage > pageNumbers.length - 3) {
-      const sliced = pageNumbers.slice(pageNumbers.length - 4);
-      numberOfPages = [1, dotsLeft, ...sliced];
-    } else if (currentPage === dotsInitial) {
-      setCurrentPage(arrOfCurrentButtons[arrOfCurrentButtons.length - 3] + 1);
-    } else if (currentPage === dotsRight) {
-      setCurrentPage(arrOfCurrentButtons[3] + 2);
-    } else if (currentPage === dotsLeft) {
-      setCurrentPage(arrOfCurrentButtons[3] - 2);
+    if (pageNumbers.length > 6) {
+      if (currentPage >= 1 && currentPage <= 3) {
+        // 1, 2, 3 ,4 ... , 10
+        numberOfPages = [1, 2, 3, 4, dotsInitial, pageNumbers.length];
+      } else if (currentPage === 4) {
+        // sliced =[1,2,3,4,5]
+        const sliced = pageNumbers.slice(0, 5);
+        numberOfPages = [...sliced, dotsInitial, pageNumbers.length];
+      } else if (currentPage > 4 && currentPage < pageNumbers.length - 2) {
+        const sliced1 = pageNumbers.slice(currentPage - 2, currentPage);
+        const sliced2 = pageNumbers.slice(currentPage, currentPage + 1);
+        // 1, ... , 4, 5, 6, 7, ... , 10
+        numberOfPages = [1, dotsLeft, ...sliced1, ...sliced2, dotsRight, pageNumbers.length];
+      } else if (currentPage > pageNumbers.length - 3) {
+        const sliced = pageNumbers.slice(pageNumbers.length - 4);
+        numberOfPages = [1, dotsLeft, ...sliced];
+      } else if (currentPage === dotsInitial) {
+        // when click on dotsInitial it will show the next 3 pages
+        setCurrentPage(arrOfCurrentButtons[arrOfCurrentButtons.length - 3] + 1);
+      } else if (currentPage === dotsRight) {
+        // when click on dotsRight it will show the clicked page + 2
+        setCurrentPage(arrOfCurrentButtons[3] + 2);
+      } else if (currentPage === dotsLeft) {
+        // when click on dotsLeft it will show the the clicked page - 2
+        setCurrentPage(arrOfCurrentButtons[3] - 2);
+      }
     }
     setArrOfCurrentButtons(numberOfPages);
-  }, [currentPage]);
+  }, [currentPage, totalPosts]);
+
+  if (!pageNumbers.length) return null;
 
   return (
     <nav>
       <ul className="pagination">
-        <li className={currentPage === 1 ? `disabled page-item` : `page-item`}>
+        <li className="page-item">
           <a
-            className="page-link"
+            className={currentPage === 1 ? `disabled page-link` : `page-link`}
             href="#"
             onClick={() => setCurrentPage((prev: number) => (prev === 1 ? prev : prev - 1))}
           >
@@ -77,9 +80,9 @@ const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage, setCurren
             </a>
           </li>
         ))}
-        <li className={currentPage === pageNumbers.length ? `disabled page-item` : `page-item`}>
+        <li className="page-item">
           <a
-            className="page-link"
+            className={currentPage === pageNumbers.length ? `disabled page-link` : `page-link`}
             href="#"
             onClick={() => setCurrentPage((prev: number) => (prev === pageNumbers.length ? prev : prev + 1))}
           >
