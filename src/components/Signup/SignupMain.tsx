01a8eedfd8812ec0531747233885ce7040f30067
Eunju Huss <eunju@sunet.se>
2023-03-17 13:38:24 +0100

    Add section for signup

--- src/components/Signup/SignupMain.tsx
+++ src/components/Signup/SignupMain.tsx
@@ -1,16 +1,16 @@
-import Footer from "components/Common/Footer";
-import { Header } from "components/Common/Header";
-import { Notifications } from "components/Common/Notifications";
-import { PageNotFound } from "components/Common/PageNotFound";
-import Splash from "components/Common/Splash";
-import { Help } from "components/Help";
-import CodeVerified from "components/Signup/CodeVerified";
-import { useIndexAppSelector as useSignupAppSelector } from "index-hooks";
+import CodeVerified from "components/CodeVerified";
+import { Faq } from "components/Faq";
+import { Header } from "components/Header";
+import { Notifications } from "components/Notifications";
+import Splash from "components/Splash";
+import Footer from "login/components/Footer/Footer";
 import "login/styles/index.scss";
 import React, { useEffect } from "react";
 import { useIntl } from "react-intl";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { useSignupAppSelector } from "signup-hooks";
 import { SignupApp } from "./SignupApp";
+import { SignupGlobalStateProvider } from "./SignupGlobalState";
 
 // export for use in tests
 export const SIGNUP_BASE_PATH = "/register";
@@ -35,17 +35,18 @@ export function SignupMain(): JSX.Element {
         <Notifications />
         <Splash showChildren={isLoaded}>
           <section id="content" className="horizontal-content-margin content">
-            <Routes>
-              <Route path={`${SIGNUP_BASE_PATH}/code/:code`} element={<CodeVerified />} />
-              <Route path={`${SIGNUP_BASE_PATH}/email`} element={<Navigate to={SIGNUP_BASE_PATH} />} />
-              <Route path={SIGNUP_BASE_PATH} element={<SignupApp />} />
-              <Route path="faq" element={<Help />} />
-              <Route path="*" element={<PageNotFound />} />
-            </Routes>
+            <SignupGlobalStateProvider>
+              <Routes>
+                <Route path={`${SIGNUP_BASE_PATH}/code/:code`} element={<CodeVerified />} />
+                <Route path={`${SIGNUP_BASE_PATH}/email`} element={<Navigate to={SIGNUP_BASE_PATH} />} />
+                <Route path={SIGNUP_BASE_PATH} element={<SignupApp />} />
+                <Route path={`${SIGNUP_BASE_PATH}/faq`} element={<Faq />} />
+              </Routes>
+            </SignupGlobalStateProvider>
           </section>
         </Splash>
       </main>
-      <Footer />
+      <Footer faqPath={`${SIGNUP_BASE_PATH}/faq`} />
     </React.Fragment>
   );
 }
