21b29529679140bd99841e634b58ed27241413b1
Eunju Huss <eunju@sunet.se>
2023-04-11 14:31:29 +0200

    Add scroll to top button

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -2,10 +2,11 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
+import { FormattedMessage } from "react-intl";
 import { Button } from "reactstrap";
 
 export default function ScrollToTopButton(): JSX.Element {
-  const [showBtn, setShowBtn] = useState("display-none");
+  const [showBtn, setShowBtn] = useState("none");
 
   // When the user scrolls down 20px from the top of the document, show the button
   window.onscroll = function () {
@@ -16,7 +17,7 @@ export default function ScrollToTopButton(): JSX.Element {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       setShowBtn("scroll-to-top");
     } else {
-      setShowBtn("display-none");
+      setShowBtn("none");
     }
   }
 
@@ -29,6 +30,7 @@ export default function ScrollToTopButton(): JSX.Element {
   return (
     <Button onClick={topFunction} id="scroll-top-button" color="primary" className={showBtn} title="Go to top">
       <FontAwesomeIcon icon={faArrowUp as IconProp} />
+      <FormattedMessage defaultMessage="To Top" description="scroll to top" />
     </Button>
   );
 }
