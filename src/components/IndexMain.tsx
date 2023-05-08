2bd498cc0a12750e701279e780e5ae05093cb0bf
Eunju Huss <eunju@sunet.se>
2023-05-08 11:09:25 +0200

    Create index for all apps

--- src/components/IndexMain.tsx
+++ src/components/IndexMain.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useAppSelector } from "eduid-hooks";
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
@@ -76,6 +77,25 @@ export function IndexMain(): JSX.Element {
             </Routes>
           </section>
         </Splash>
+=======
+import { Header } from "components/Header";
+import { Notifications } from "components/Notifications";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "../../src/login/styles/index.scss";
+import Footer from "../login/components/Footer/Footer";
+import { Index } from "./Index";
+
+export function IndexMain(): JSX.Element {
+  return (
+    <React.StrictMode>
+      <Header />
+      <main id="panel" className="panel">
+        <Notifications />
+        <Routes>
+          <Route path="static/front-build/index.dev.html" element={<Index />} />
+        </Routes>
+>>>>>>> 5cc10eec7 (Create index for all apps)
       </main>
       <Footer />
     </React.StrictMode>
