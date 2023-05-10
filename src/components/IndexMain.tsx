a6b3b63451144ecdea2d572f012b202fab506118
Eunju Huss <eunju@sunet.se>
2023-05-10 15:25:51 +0200

    Add splash

--- src/components/IndexMain.tsx
+++ src/components/IndexMain.tsx
@@ -1,5 +1,6 @@
 import { Header } from "components/Header";
 import { Notifications } from "components/Notifications";
+import { useIndexAppSelector } from "index-hooks";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "../../src/login/styles/index.scss";
@@ -7,23 +8,27 @@ import Footer from "../login/components/Footer/Footer";
 import { Index } from "./Index";
 import { SignupApp } from "./Signup/SignupApp";
 import { SIGNUP_BASE_PATH } from "./Signup/SignupMain";
+import Splash from "./Splash";
 
 export function IndexMain(): JSX.Element {
+  const isLoaded = useIndexAppSelector((state) => state.config.is_configured);
   return (
     <React.StrictMode>
       <Header showLogin={true} />
       <main id="panel" className="panel">
         <Notifications />
-        <section id="content" className="horizontal-content-margin content">
-          <Routes>
-            <Route path="static/front-build/index.dev.html" element={<Index />} />
-            {/* Signup */}
-            <Route path={SIGNUP_BASE_PATH} element={<SignupApp />} />
-            {/* Login */}
-            {/* Dashboard */}
-            {/* Errors*/}
-          </Routes>
-        </section>
+        <Splash showChildren={isLoaded}>
+          <section id="content" className="horizontal-content-margin content">
+            <Routes>
+              <Route path="static/front-build/index.dev.html" element={<Index />} />
+              {/* Signup */}
+              <Route path={SIGNUP_BASE_PATH} element={<SignupApp />} />
+              {/* Login */}
+              {/* Dashboard */}
+              {/* Errors*/}
+            </Routes>
+          </section>
+        </Splash>
       </main>
       <Footer />
     </React.StrictMode>
