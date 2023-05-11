6408a9f0a540317f1c4a46e3afb2f00268e799c6
Eunju Huss <eunju@sunet.se>
2023-05-11 11:33:09 +0200

    configSlice from signup config

--- src/index-store.ts
+++ src/index-store.ts
@@ -1,8 +1,8 @@
 import { signupSlice } from "reducers/Signup";
 import { combineReducers } from "redux";
-import configSlice from "./login/redux/reducers/errorsMainReducer";
 import { intlReducer } from "./reducers/Internationalisation";
 import { notificationsSlice } from "./reducers/Notifications";
+import configSlice from "./reducers/SignupConfig";
 
 const eduIDApp = combineReducers({
   config: configSlice.reducer,
