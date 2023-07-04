8b6464d84eb904bdb490ae70d8f988d6ec000a04
Eunju Huss <eunju@sunet.se>
2023-07-04 10:20:41 +0200

    Remove ids from formattedMessages

--- src/components/ChangePassword.tsx
+++ src/components/ChangePassword.tsx
@@ -27,16 +27,10 @@ function ChangePassword() {
     }
   }, [suggested_password, is_app_loaded]);
 
-  // TODO: Remove ids from FormattedMessage later, when it won't cause a lot of red warnings in the console log
-  //       before this is merged after the holidays.
   return (
     <React.Fragment>
       <h4>
-        <FormattedMessage
-          id="chpass.main_title"
-          defaultMessage="Change your current password"
-          description="Dashboard change password"
-        />
+        <FormattedMessage defaultMessage="Change your current password" description="Dashboard change password" />
       </h4>
       <div id="changePasswordDialog">
         <ChangePasswordForm finish_url={finish_url} />
