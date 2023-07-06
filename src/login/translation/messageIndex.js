d2d85da6cadcd7165dea528c54386f638606922d
Eunju Huss <eunju@sunet.se>
2023-07-06 09:50:37 +0200

    Remove unused, move translations to related files in a format similar to backend

--- src/login/translation/messageIndex.js
+++ src/login/translation/messageIndex.js
@@ -1,28 +1,31 @@
 // import content from other files
-import { generalErrors, specificErrors } from "./defaultMessages/errors";
-import { login } from "./defaultMessages/login";
-import { generalInstructions } from "./defaultMessages/instructions";
-import { changePassword, resetPassword } from "./defaultMessages/password";
-import { register } from "./defaultMessages/register";
-import { generalApp } from "./defaultMessages/generalApp";
-import { userData, userProfile, userVetting } from "./defaultMessages/userProfile";
-import { leftoverMessages } from "./leftovers";
+import * as login from "./defaultMessages/login";
+import * as resetPassword from "./defaultMessages/resetPassword";
 import * as ladok from "./defaultMessages/ladok";
 import * as security from "./defaultMessages/security";
+import * as signup from "./defaultMessages/signup";
+import * as lookupMobileProofing from "./defaultMessages/lookupMobileProofing";
+import * as letterProofing from "./defaultMessages/letterProofing";
+import * as common from "./defaultMessages/common";
+import * as eidas from "./defaultMessages/eidas";
+import * as email from "./defaultMessages/email";
+import * as orcid from "./defaultMessages/orcid";
+import * as phone from "./defaultMessages/phone";
 
 export const formattedMessages = {
-  ...generalApp,
-  ...generalErrors,
-  ...specificErrors,
-  ...login,
-  ...generalInstructions,
-  ...changePassword,
-  ...register,
-  ...resetPassword,
-  ...userData,
-  ...userProfile,
-  ...userVetting,
-  ...leftoverMessages,
+  ...common.proofing,
+  ...common.apiResponse,
+  ...common.validations,
+  ...common.personalData,
+  ...login.apiResponses,
+  ...signup.apiResponses,
   ...ladok.apiResponses,
+  ...eidas.apiResponses,
+  ...email.apiResponses,
+  ...orcid.apiResponses,
+  ...phone.apiResponses,
   ...security.apiResponses,
+  ...letterProofing.apiResponses,
+  ...resetPassword.apiResponses,
+  ...lookupMobileProofing.apiResponses,
 };
