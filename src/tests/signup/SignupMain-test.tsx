a152c734c69befeda7f54d7b9bc781d97dccb7d2
Eunju Huss <eunju@sunet.se>
2023-09-08 15:08:58 +0200

    Remove duplicated signuptest

--- src/tests/signup/SignupMain-test.tsx
+++ src/tests/signup/SignupMain-test.tsx
@@ -14,7 +14,7 @@ import { formatPassword } from "components/Signup/SignupUserCreated";
 
 import { emailPlaceHolder } from "components/Common/EmailInput";
 import { mswServer, rest } from "setupTests";
-import { fireEvent, render, screen, waitFor } from "../helperFunctions/SignupTestApp-rtl";
+import { fireEvent, render, screen, signupTestState, waitFor } from "../helperFunctions/SignupTestApp-rtl";
 
 const emptyState: SignupState = {
   already_signed_up: false,
@@ -390,3 +390,51 @@ async function enterEmailCode(code: string) {
     expect(screen.queryByTestId(codeFormTestId)).not.toBeInTheDocument();
   });
 }
+
+test("show splash screen when not configured", () => {
+  render(<SignupMain />, {
+    state: { config: { ...signupTestState.config, is_configured: false } },
+    routes: [`${SIGNUP_BASE_PATH}/email`],
+  });
+
+  expect(screen.getByRole("heading")).toHaveTextContent(/^Register your email/);
+
+  expect(screen.getByRole("progressbar")).toBeInTheDocument();
+  expect(screen.getByRole("progressbar")).toHaveClass("spinner");
+});
+
+test("renders e-mail form as expected", () => {
+  render(<SignupMain />, { routes: [`${SIGNUP_BASE_PATH}/email`] });
+
+  expect(screen.getByRole("heading")).toHaveTextContent(/^Register your email/);
+
+  expect(screen.queryByRole("progressbar")).not.toBeInTheDocument();
+
+  // there should be no visible form error for example when the page has just loaded
+  expect(screen.queryByRole("alert")).not.toBeInTheDocument();
+
+  const input = screen.getByRole("textbox");
+  expect(input).toHaveFocus();
+  expect(input).toHaveAccessibleName(/^Email address/);
+  expect(input).toHaveProperty("placeholder", "name@example.com");
+
+  const button = screen.getByRole("button", { name: "Create eduID" });
+  expect(button).toBeDisabled();
+
+  fireEvent.change(input, { target: { value: "not-an-email" } });
+  expect(button).toBeDisabled();
+
+  fireEvent.change(input, { target: { value: "test@example.org" } });
+  expect(button).toBeEnabled();
+});
+
+test("redirects from slash", () => {
+  render(<SignupMain />, { routes: [SIGNUP_BASE_PATH] });
+
+  expect(screen.getByRole("heading")).toHaveTextContent(/^Register your email/);
+});
+
+test("renders the register page title", () => {
+  render(<SignupMain />);
+  expect(document.title).toContain("Register");
+});
