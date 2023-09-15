ef27b3e4fd41abfc1e6a60444337d4feda191d8f
Eunju Huss <eunju@sunet.se>
2023-09-15 14:01:03 +0200

    Add useMemo

--- src/components/Signup/SignupGlobalState.tsx
+++ src/components/Signup/SignupGlobalState.tsx
@@ -1,16 +1,15 @@
 import { useInterpret } from "@xstate/react";
 import { createSignupMachine, SignupMachineType } from "machines/SignupMachine";
-import React, { createContext } from "react";
+import React, { createContext, useMemo } from "react";
 import { InterpreterFrom } from "xstate";
 
 export const SignupGlobalStateContext = createContext({ signupService: {} as InterpreterFrom<SignupMachineType> });
 
 export function SignupGlobalStateProvider(props: { children?: React.ReactNode }) {
   const signupService = useInterpret(createSignupMachine());
+  const signupServiceValue = useMemo(() => ({ signupService }), [signupService]);
 
   return (
-    <SignupGlobalStateContext.Provider value={{ signupService: signupService }}>
-      {props.children}
-    </SignupGlobalStateContext.Provider>
+    <SignupGlobalStateContext.Provider value={signupServiceValue}>{props.children}</SignupGlobalStateContext.Provider>
   );
 }
