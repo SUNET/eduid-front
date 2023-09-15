febafaffc2e2ee7afd0e07240fb36475817f7654
Eunju Huss <eunju@sunet.se>
2023-09-15 13:01:06 +0200

    Remove login/ path

--- src/components/Login/LoginMain.tsx
+++ src/components/Login/LoginMain.tsx
@@ -24,7 +24,7 @@ export function LoginMain(): JSX.Element {
           </Splash>
         </ErrorBoundary>
       </main>
-      <Footer helpPath="login/faq" />
+      <Footer helpPath="faq" />
     </React.StrictMode>
   );
 }
