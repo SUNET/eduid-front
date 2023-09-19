7030812a050e0903ff8815c12dd34827031bc8ae
Eunju Huss <eunju@sunet.se>
2023-09-19 09:58:56 +0200

    login.html -> index.html

--- src/entry-points/index.tsx
+++ src/entry-points/index.tsx
@@ -1,4 +1,5 @@
-import { fetchJsConfig } from "apis/eduidJsConfig";
+
+import { requestAllPersonalData } from "apis/eduidPersonalData";
 import { ReduxIntlProvider } from "components/Common/ReduxIntl";
 import { IndexMain } from "components/IndexMain";
 import { ResetPasswordGlobalStateProvider } from "components/ResetPassword/ResetPasswordGlobalState";
@@ -9,19 +10,54 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { setupLanguage } from "translation";
 import "../../src/styles/index.scss";
+
 import { polyfillsInit } from "./polyfills-common";
 import "./public-path";
 
 /* Get configuration */
-const getConfig = function () {
-  eduidStore.dispatch(fetchJsConfig({ url: SIGNUP_CONFIG_URL }));
+
+const getConfig = async function () {
+
+  indexStore.dispatch(fetchJsConfig({ url: SIGNUP_CONFIG_URL }));
+  const result = await indexStore.dispatch(fetchJsConfig({ url: DASHBOARD_CONFIG_URL }));
+  if (fetchJsConfig.fulfilled.match(result)) {
+    const response = await indexStore.dispatch(requestAllPersonalData());
+    console.log("response", response);
+    if (requestAllPersonalData.fulfilled.match(response)) {
+      if (response.payload.language) {
+        indexStore.dispatch(
+          updateIntl({
+            locale: response.payload.language,
+            messages: LOCALIZED_MESSAGES[response.payload.language],
+          })
+        );
+      }
+      indexStore.dispatch(appLoadingSlice.actions.appLoaded());
+    }
+  }
+
+  const params = new URLSearchParams(document.location.search);
+  if (params) {
+    const msg = params.get("msg");
+    if (msg !== null) {
+      if (msg.indexOf(":ERROR:") === 0) {
+        indexStore.dispatch(showNotification({ message: msg.substr(7), level: "error" }));
+      } else {
+        indexStore.dispatch(showNotification({ message: msg, level: "info" }));
+      }
+    }
+
+  const config = await loginStore.dispatch(fetchJsConfig({ url: LOGIN_CONFIG_URL }));
+  if (fetchJsConfig.fulfilled.match(config)) {
+    loginStore.dispatch(appLoadingSlice.actions.appLoaded());
+
 };
 
 /* Initialise common polyfills for missing browser functionality */
 polyfillsInit();
 
 /* Get the language from the browser and initialise locale with the best match */
-setupLanguage(eduidStore.dispatch);
+setupLanguage(loginStore.dispatch);
 
 /* render app */
 const initDomTarget = document.getElementById("root");
@@ -29,6 +65,7 @@ ReactDOM.render(
   <SignupGlobalStateProvider>
     <ResetPasswordGlobalStateProvider>
       <ReduxIntlProvider store={eduidStore}>
+
         <BrowserRouter>
           <IndexMain />
         </BrowserRouter>
