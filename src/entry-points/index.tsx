60e95a5ef800c0f4fa320148503cc1bf8f4915c6
Eunju Huss <eunju@sunet.se>
2023-09-19 14:29:52 +0200

    update rootstate

--- src/entry-points/index.tsx
+++ src/entry-points/index.tsx
@@ -1,19 +1,50 @@
 import { fetchJsConfig } from "apis/eduidJsConfig";
+import { requestAllPersonalData } from "apis/eduidPersonalData";
 import { ReduxIntlProvider } from "components/Common/ReduxIntl";
 import { IndexMain } from "components/IndexMain";
 import { ResetPasswordGlobalStateProvider } from "components/ResetPassword/ResetPasswordGlobalState";
 import { SignupGlobalStateProvider } from "components/Signup/SignupGlobalState";
-import { SIGNUP_CONFIG_URL } from "globals";
+import { DASHBOARD_CONFIG_URL, LOCALIZED_MESSAGES, SIGNUP_CONFIG_URL } from "globals";
 import { indexStore } from "index-init-app";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { appLoadingSlice } from "slices/AppLoading";
+import { updateIntl } from "slices/Internationalisation";
+import { showNotification } from "slices/Notifications";
 import { setupLanguage } from "translation";
 import { polyfillsInit } from "./polyfills-common";
 import "./public-path";
 
 /* Get configuration */
-const getConfig = function () {
+const getConfig = async function () {
   indexStore.dispatch(fetchJsConfig({ url: SIGNUP_CONFIG_URL }));
+  const result = await indexStore.dispatch(fetchJsConfig({ url: DASHBOARD_CONFIG_URL }));
+  if (fetchJsConfig.fulfilled.match(result)) {
+    const response = await indexStore.dispatch(requestAllPersonalData());
+    if (requestAllPersonalData.fulfilled.match(response)) {
+      if (response.payload.language) {
+        indexStore.dispatch(
+          updateIntl({
+            locale: response.payload.language,
+            messages: LOCALIZED_MESSAGES[response.payload.language],
+          })
+        );
+      }
+      indexStore.dispatch(appLoadingSlice.actions.appLoaded());
+    }
+  }
+
+  const params = new URLSearchParams(document.location.search);
+  if (params) {
+    const msg = params.get("msg");
+    if (msg !== null) {
+      if (msg.indexOf(":ERROR:") === 0) {
+        indexStore.dispatch(showNotification({ message: msg.substr(7), level: "error" }));
+      } else {
+        indexStore.dispatch(showNotification({ message: msg, level: "info" }));
+      }
+    }
+  }
 };
 
 /* Initialise common polyfills for missing browser functionality */
--- src/index-store.ts
+++ src/index-store.ts
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
+import accountLinkingSlice from "slices/AccountLinking";
 import { appLoadingSlice } from "slices/AppLoading";
+import chpassSlice from "slices/ChangePassword";
+import emailsSlice from "slices/Emails";
+import identitiesSlice from "slices/Identities";
 import { intlReducer } from "slices/Internationalisation";
+import ladokSlice from "slices/Ladok";
+import letterProofingSlice from "slices/LetterProofing";
 import loginSlice from "slices/Login";
+import lookupMobileProofingSlice from "slices/LookupMobileProofing";
 import { notificationsSlice } from "slices/Notifications";
+import personalDataSlice from "slices/PersonalData";
+import phonesSlice from "slices/Phones";
 import resetPasswordSlice from "slices/ResetPassword";
+import securitySlice from "slices/Security";
 import { signupSlice } from "slices/Signup";
 import configSlice from "slices/SignupConfig";
 
@@ -15,6 +25,16 @@ const eduIDApp = combineReducers({
   login: loginSlice.reducer,
   app: appLoadingSlice.reducer,
   resetPassword: resetPasswordSlice.reducer,
+  chpass: chpassSlice.reducer,
+  emails: emailsSlice.reducer,
+  lookup_mobile: lookupMobileProofingSlice.reducer,
+  identities: identitiesSlice.reducer,
+  personal_data: personalDataSlice.reducer,
+  phones: phonesSlice.reducer,
+  letter_proofing: letterProofingSlice.reducer,
+  account_linking: accountLinkingSlice.reducer,
+  security: securitySlice.reducer,
+  ladok: ladokSlice.reducer,
 });
 
 export default eduIDApp;
