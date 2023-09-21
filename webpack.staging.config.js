0261abae82827946398f21fda02983f7cd52a298
Eunju Huss <eunju@sunet.se>
2023-09-21 14:00:15 +0200

    Remove html-webpack-plugin

--- webpack.staging.config.js
+++ webpack.staging.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const webpackProd = require("./webpack.prod.config");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 //const initialConfigPlugin = require("./src/init-config").initialConfigPlugin;
 
 var webpackStaging = {
@@ -13,22 +12,7 @@ webpackStaging.output = {
   publicPath: "https://www.dev.eduid.se/static/front-build/",
   path: path.join(__dirname, "build"),
 };
-<<<<<<< HEAD
-=======
 
-webpackStaging.plugins = [
-  //  initialConfigPlugin,
-  ...["index"].map((entryName) => {
-    return new HtmlWebpackPlugin({
-      hash: true,
-      template: `./public/${entryName}.html`,
-      filename: `${entryName}.staging.html`,
-      chunks: [`${entryName}`],
-    });
-  }),
-];
-
->>>>>>> ed05e39d2 (Changed  onClick to Link to avoid fetching JS script for help page)
 webpackStaging.devtool = "source-map";
 
 module.exports = webpackStaging;
