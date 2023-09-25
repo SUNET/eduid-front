84d529372a466cd59a13ed75de28f661d1b725c0
Eunju Huss <eunju@sunet.se>
2023-09-25 17:12:06 +0200

    Remove

--- src/hooks.ts
+++
@@ -1,7 +0,0 @@
-// typed variants of useSelector and useDispatch, straight from
-//  https://redux.js.org/usage/usage-with-typescript
-import { LoginAppDispatch, LoginRootState } from "login-init-app";
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-
-export const useAppDispatch = () => useDispatch<LoginAppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<LoginRootState> = useSelector;
