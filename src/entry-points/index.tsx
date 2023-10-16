3287af17c93bab830afbc0e9b415dae2b0eb1bc5
Eunju Huss <eunju@sunet.se>
2023-10-16 14:08:23 +0200

    Save temporary

--- src/entry-points/index.tsx
+++ src/entry-points/index.tsx
@@ -1,20 +1,55 @@
 import { fetchJsConfig } from "apis/eduidJsConfig";
+import { requestAllPersonalData } from "apis/eduidPersonalData";
 import { ReduxIntlProvider } from "components/Common/ReduxIntl";
 import { IndexMain } from "components/IndexMain";
 import { ResetPasswordGlobalStateProvider } from "components/ResetPassword/ResetPasswordGlobalState";
 import { SignupGlobalStateProvider } from "components/Signup/SignupGlobalState";
 import { eduidStore } from "eduid-init-app";
-import { SIGNUP_CONFIG_URL } from "globals";
+import { DASHBOARD_CONFIG_URL, LOCALIZED_MESSAGES } from "globals";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { appLoadingSlice } from "slices/AppLoading";
+import { updateIntl } from "slices/Internationalisation";
+import { showNotification } from "slices/Notifications";
 import { setupLanguage } from "translation";
 import "../../src/styles/index.scss";
 import { polyfillsInit } from "./polyfills-common";
 import "./public-path";
 
 /* Get configuration */
+// const getConfig = async function () {
+//   eduidStore.dispatch(fetchJsConfig({ url: SIGNUP_CONFIG_URL }));
+// };
+
 const getConfig = async function () {
-  eduidStore.dispatch(fetchJsConfig({ url: SIGNUP_CONFIG_URL }));
+  const result = await eduidStore.dispatch(fetchJsConfig({ url: DASHBOARD_CONFIG_URL }));
+  console.log("window.location.href.", window.location.href);
+  if (fetchJsConfig.fulfilled.match(result) && window.location.href.includes("/profile/")) {
+    const response = await eduidStore.dispatch(requestAllPersonalData());
+    if (requestAllPersonalData.fulfilled.match(response)) {
+      if (response.payload.language) {
+        eduidStore.dispatch(
+          updateIntl({
+            locale: response.payload.language,
+            messages: LOCALIZED_MESSAGES[response.payload.language],
+          })
+        );
+      }
+      eduidStore.dispatch(appLoadingSlice.actions.appLoaded());
+    }
+
+    const params = new URLSearchParams(document.location.search);
+    if (params) {
+      const msg = params.get("msg");
+      if (msg !== null) {
+        if (msg.indexOf(":ERROR:") === 0) {
+          eduidStore.dispatch(showNotification({ message: msg.substr(7), level: "error" }));
+        } else {
+          eduidStore.dispatch(showNotification({ message: msg, level: "info" }));
+        }
+      }
+    }
+  }
 };
 
 /* Initialise common polyfills for missing browser functionality */
