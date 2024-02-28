ff07e869b628d277678d0fba173377d228c6ff0d
Eunju Huss <eunju@sunet.se>
2024-02-28 15:07:50 +0100

    Add pageNotFound in route

--- src/components/IndexMain.tsx
+++ src/components/IndexMain.tsx
@@ -1,11 +1,14 @@
+import { GenericError } from "components/Common/GenericError";
 import { useAppSelector } from "eduid-hooks";
 import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "../styles/index.scss";
 import { ExternalReturnHandler } from "./Common/ExternalReturnHandler";
 import Footer from "./Common/Footer";
 import { Header } from "./Common/Header";
 import { Notifications } from "./Common/Notifications";
+import { PageNotFound } from "./Common/PageNotFound";
 import { Settings } from "./Common/Settings";
 import Splash from "./Common/Splash";
 import { AdvancedSettings } from "./Dashboard/AdvancedSettings";
@@ -36,37 +39,40 @@ export function IndexMain(): JSX.Element {
       <Header loginRef={loginRef} />
       <main id="panel" className="panel">
         <Notifications />
-        <Splash showChildren={isLoaded}>
-          <section id="content" className="horizontal-content-margin content">
-            <Routes>
-              <Route path="/" element={<Index />} />
-              {/* Signup */}
-              <Route path={SIGNUP_BASE_PATH} element={<SignupApp />} />
-              {/* Login */}
-              <Route path="/login/ext-return/:app_name/:authn_id" element={<LoginExternalReturnHandler />} />
-              <Route path="/login/other/:state_id" element={<UseOtherDevice2 />} />
-              <Route path="/login/password/:ref" element={<Login />} />
-              <Route path="/login/:ref" element={<Login />} />
-              <Route path="/reset-password/*" element={<ResetPasswordMain />} />
-              {/* Dashboard */}
-              <Route path={advancedSettingsPath} element={<AdvancedSettings />} />
-              <Route path={settingsPath} element={<Settings />} />
-              <Route path="/profile/settings/" element={<Navigate to={settingsPath} />} />
-              <Route path={identityPath} element={<VerifyIdentity />} />
-              <Route path="/profile/chpass/" element={<ChangePasswordContainer />} />
-              <Route path="/profile/ext-return/:app_name/:authn_id" element={<ExternalReturnHandler />} />
-              {/* Navigates for old paths. TODO: redirect in backend server instead */}
-              <Route path="/profile/security/" element={<Navigate to="/profile/settings/" />} />
-              <Route path="/profile/accountlinking/" element={<Navigate to={advancedSettingsPath} />} />
-              <Route path="/profile/nins/" element={<Navigate to={identityPath} />} />
-              <Route path="/profile/emails/" element={<Navigate to={settingsPath} />} />
-              <Route path={startPath} element={<Start />} />
-              {/* Errors*/}
-              <Route path="/errors" element={<Errors />} />
-              <Route path="/faq" element={<Help />} />
-            </Routes>
-          </section>
-        </Splash>
+        <ErrorBoundary FallbackComponent={GenericError}>
+          <Splash showChildren={isLoaded}>
+            <section id="content" className="horizontal-content-margin content">
+              <Routes>
+                <Route path="/" element={<Index />} />
+                {/* Signup */}
+                <Route path={SIGNUP_BASE_PATH} element={<SignupApp />} />
+                {/* Login */}
+                <Route path="/login/ext-return/:app_name/:authn_id" element={<LoginExternalReturnHandler />} />
+                <Route path="/login/other/:state_id" element={<UseOtherDevice2 />} />
+                <Route path="/login/password/:ref" element={<Login />} />
+                <Route path="/login/:ref" element={<Login />} />
+                <Route path="/reset-password/*" element={<ResetPasswordMain />} />
+                {/* Dashboard */}
+                <Route path={advancedSettingsPath} element={<AdvancedSettings />} />
+                <Route path={settingsPath} element={<Settings />} />
+                <Route path="/profile/settings/" element={<Navigate to={settingsPath} />} />
+                <Route path={identityPath} element={<VerifyIdentity />} />
+                <Route path="/profile/chpass/" element={<ChangePasswordContainer />} />
+                <Route path="/profile/ext-return/:app_name/:authn_id" element={<ExternalReturnHandler />} />
+                {/* Navigates for old paths. TODO: redirect in backend server instead */}
+                <Route path="/profile/security/" element={<Navigate to="/profile/settings/" />} />
+                <Route path="/profile/accountlinking/" element={<Navigate to={advancedSettingsPath} />} />
+                <Route path="/profile/nins/" element={<Navigate to={identityPath} />} />
+                <Route path="/profile/emails/" element={<Navigate to={settingsPath} />} />
+                <Route path={startPath} element={<Start />} />
+                {/* Errors*/}
+                <Route path="/errors" element={<Errors />} />
+                <Route path="/faq" element={<Help />} />
+                <Route path="*" element={<PageNotFound />} />
+              </Routes>
+            </section>
+          </Splash>
+        </ErrorBoundary>
       </main>
       <Footer />
     </React.StrictMode>
