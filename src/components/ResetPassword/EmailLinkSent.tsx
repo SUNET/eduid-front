296f8a64e5ae1a55670b356d8c86449606158d31
Eunju Huss <eunju@sunet.se>
2024-04-02 11:02:31 +0900

    Remove resent email code button and timer

--- src/components/ResetPassword/EmailLinkSent.tsx
+++ src/components/ResetPassword/EmailLinkSent.tsx
@@ -1,14 +1,12 @@
-import { requestEmailLink, verifyEmailLink } from "apis/eduidResetPassword";
+import { verifyEmailLink } from "apis/eduidResetPassword";
 import EduIDButton from "components/Common/EduIDButton";
-import { TimeRemainingWrapper } from "components/Common/TimeRemaining";
 import { ResponseCodeForm, ResponseCodeValues } from "components/Login/ResponseCodeForm";
 import { useAppDispatch, useAppSelector } from "eduid-hooks";
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { FormRenderProps } from "react-final-form";
 import { FormattedMessage } from "react-intl";
 import { clearNotifications } from "slices/Notifications";
-import { ExpiresMeter } from "../Login/ExpiresMeter";
-import { GoBackButton } from "./GoBackButton";
+import resetPasswordSlice from "slices/ResetPassword";
 import { ResetPasswordGlobalStateContext } from "./ResetPasswordGlobalState";
 
 interface ResponseCodeButtonsProps {
@@ -17,9 +15,9 @@ interface ResponseCodeButtonsProps {
 
 export function EmailLinkSent(): JSX.Element | null {
   const dispatch = useAppDispatch();
-  const [resendDisabled, setResendDisabled] = useState(true);
   const response = useAppSelector((state) => state.resetPassword.email_response);
   const resetPasswordContext = useContext(ResetPasswordGlobalStateContext);
+  const dashboard_link = useAppSelector((state) => state.config.dashboard_link);
 
   async function handleSubmitCode(values: ResponseCodeValues) {
     const code = values.v.join("");
@@ -42,9 +40,12 @@ export function EmailLinkSent(): JSX.Element | null {
     }
   }
 
-  function handleAbortButtonOnClick(event: React.MouseEvent<HTMLButtonElement>) {
-    event.preventDefault();
-    resetPasswordContext.resetPasswordService.send({ type: "GO_BACK" });
+  function handleAbortButtonOnClick() {
+    if (dashboard_link) {
+      document.location.href = dashboard_link;
+      dispatch(resetPasswordSlice.actions.resetEmailStatus());
+      resetPasswordContext.resetPasswordService.send({ type: "GO_BACK" });
+    }
   }
 
   function ResponseCodeButtons(props: ResponseCodeButtonsProps) {
@@ -88,28 +89,6 @@ export function EmailLinkSent(): JSX.Element | null {
     );
   }
 
-  /**
-   * The user has clicked the button to request that another e-mail should be sent.
-   */
-  function sendEmailOnClick(e: React.MouseEvent<HTMLElement>) {
-    (async () => {
-      e.preventDefault();
-      if (response?.email) {
-        try {
-          const resp = await dispatch(requestEmailLink({ email: response.email }));
-          if (requestEmailLink.rejected.match(resp)) {
-            resetPasswordContext.resetPasswordService.send({ type: "API_FAIL" });
-          }
-        } catch (error) {}
-      }
-      setResendDisabled(true); // disabled button again on use
-    })();
-  }
-
-  function handleTimerReachZero() {
-    setResendDisabled(false); // allow user to request a new e-mail when timer expires
-  }
-
   if (!response) {
     return null;
   }
@@ -139,31 +118,10 @@ export function EmailLinkSent(): JSX.Element | null {
       </p>
       <p>
         <FormattedMessage
-          defaultMessage="If you haven't receive the email code, you can resend it after five minutes, according to the timer next to the Resend button."
+          defaultMessage="If you haven't receive the email code, please cancel the process and restart from the beginning."
           description="Reset Password email link sent"
         />
       </p>
-
-      <div className="buttons">
-        <GoBackButton />
-        <EduIDButton
-          buttonstyle="primary"
-          type="submit"
-          id="send-email-button"
-          onClick={sendEmailOnClick}
-          disabled={resendDisabled}
-        >
-          <FormattedMessage defaultMessage="Resend email code" description="Resend email code button" />
-        </EduIDButton>
-        <TimeRemainingWrapper
-          name="reset-password-email-expires"
-          unique_id={response?.email}
-          value={response?.throttled_seconds}
-          onReachZero={handleTimerReachZero}
-        >
-          <ExpiresMeter showMeter={false} expires_max={response?.throttled_max} />
-        </TimeRemainingWrapper>
-      </div>
       <div className="enter-code">
         <ResponseCodeForm inputsDisabled={false} handleSubmitCode={handleSubmitCode}>
           <ResponseCodeButtons />
