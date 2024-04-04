d3e6ed2a424c7c958d4b73f92e2a836d67beefe6
Eunju Huss <eunju@sunet.se>
2024-04-04 19:04:14 +0900

    Output -> input, use opyToClipboard component

--- src/components/Dashboard/AccountId.tsx
+++ src/components/Dashboard/AccountId.tsx
@@ -1,31 +1,13 @@
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { faCheck, faCopy } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { CopyToClipboard } from "components/Common/CopyToClipboard";
 import { useAppSelector } from "eduid-hooks";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { FormattedMessage } from "react-intl";
 
 const idUserEppn = "user-eppn";
 
 export function AccountId(): JSX.Element {
   const eppn = useAppSelector((state) => state.personal_data.eppn);
-  const [tooltipCopied, setTooltipCopied] = useState(false); // say "Copy to clipboard" or "Copied!" in tooltip
   const ref = useRef<HTMLInputElement>(null);
-
-  function copyToClipboard() {
-    if (ref && ref.current) {
-      ref.current.select();
-      document.execCommand("copy");
-      setTooltipCopied(true);
-      (document.getElementById("icon-copy") as HTMLInputElement).style.display = "none";
-      (document.getElementById("icon-check") as HTMLInputElement).style.display = "inline";
-      setTimeout(() => {
-        (document.getElementById("icon-copy") as HTMLInputElement).style.display = "inline";
-        (document.getElementById("icon-check") as HTMLInputElement).style.display = "none";
-        setTooltipCopied(false);
-      }, 1000);
-    }
-  }
 
   return (
     <article id="uniqueId-container">
@@ -47,21 +29,8 @@ export function AccountId(): JSX.Element {
           </strong>
         </span>
         <div className="display-data">
-          <output name={eppn} id={idUserEppn}>
-            {eppn}
-
-            <button id="clipboard" className="icon-only copybutton" onClick={copyToClipboard}>
-              <FontAwesomeIcon id="icon-copy" icon={faCopy as IconProp} />
-              <FontAwesomeIcon id="icon-check" icon={faCheck as IconProp} />
-              <div className="tool-tip-text" id="tool-tip">
-                {tooltipCopied ? (
-                  <FormattedMessage defaultMessage="Copied!" description="Copied tooltip" />
-                ) : (
-                  <FormattedMessage defaultMessage="Copy to clipboard" description="Copy tooltip" />
-                )}
-              </div>
-            </button>
-          </output>
+          <input readOnly={true} name={eppn} id={idUserEppn} ref={ref} defaultValue={eppn} />
+          <CopyToClipboard ref={ref} />
         </div>
       </div>
     </article>
