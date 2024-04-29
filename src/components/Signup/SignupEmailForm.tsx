6464046a7852e07396bcc1cafb22022fc80e362c
Eunju Huss <eunju@sunet.se>
2024-04-29 19:17:57 +0900

    Add validateEmailForm

--- src/components/Signup/SignupEmailForm.tsx
+++ src/components/Signup/SignupEmailForm.tsx
@@ -4,6 +4,7 @@ import EduIDButton from "components/Common/EduIDButton";
 import EmailInput from "components/Common/EmailInput";
 import { SignupGlobalStateContext } from "components/Signup/SignupGlobalState";
 import { useAppDispatch, useAppSelector } from "eduid-hooks";
+import { validateEmailInForm } from "helperFunctions/validation/validateEmail";
 import { Fragment, useContext, useEffect } from "react";
 import { Field as FinalField, Form as FinalForm, FormRenderProps } from "react-final-form";
 import { FormattedMessage, useIntl } from "react-intl";
@@ -72,6 +73,7 @@ function EmailForm() {
   return (
     <FinalForm<EmailFormData>
       onSubmit={submitEmailForm}
+      validate={validateEmailInForm}
       initialValues={{
         email: "",
       }}
