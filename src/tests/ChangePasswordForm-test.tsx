66a4f92480f7dcf8ff8ebaa3262f17b7ed28d826
Eunju Huss <eunju@sunet.se>
2024-05-20 18:06:09 +0900

    Clean up after removing ChangePasswordForm component

--- src/tests/ChangePasswordForm-test.tsx
+++ src/tests/ChangePasswordForm-test.tsx
@@ -1,33 +1,16 @@
-import { finish_url } from "components/Dashboard/ChangePassword";
-import ChangePasswordForm from "components/Dashboard/ChangePasswordForm";
+import { ChangePassword } from "components/Dashboard/ChangePassword";
+
 import { fireEvent, render, screen, waitFor } from "./helperFunctions/DashboardTestApp-rtl";
 
 test("renders ChangePasswordForm, suggested password value is field in suggested-password-field", () => {
-  render(<ChangePasswordForm finish_url={finish_url} />);
-
-  const oldPasswordInput = screen.getByLabelText(/Current password/i) as HTMLInputElement;
-  expect(oldPasswordInput.value).toBe("");
+  render(<ChangePassword />);
 
   const suggestedPasswordInput = screen.getByLabelText(/Suggested password/i) as HTMLInputElement;
   expect(suggestedPasswordInput.value).toBeDefined();
 });
 
-test("save button will be enabled once current password field is filled", () => {
-  render(<ChangePasswordForm finish_url={finish_url} />);
-
-  const input = screen.getByLabelText(/Current password/i) as HTMLInputElement;
-  // change password save button is initially disabled
-  const savePasswordButton = screen.getByRole("button", { name: /save/i });
-  expect(savePasswordButton).toBeDisabled();
-
-  fireEvent.change(input, { target: { value: "current password" } });
-  expect(input.value).toBe("current password");
-
-  expect(savePasswordButton).toBeEnabled();
-});
-
 test("renders custom password form after clicking do not want a suggested password", async () => {
-  render(<ChangePasswordForm finish_url={finish_url} />);
+  render(<ChangePassword />);
   const customPasswordButton = screen.getByRole("button", { name: /I don't want a suggested password/i });
   expect(customPasswordButton).toBeInTheDocument();
 
