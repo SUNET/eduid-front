889ae058d71f5a94a3a6fc1f4d113b92e6f0f8dd
Eunju Huss <eunju@sunet.se>
2024-05-21 13:35:40 +0900

    Update test due to component changes

--- src/tests/ChangePasswordForm-test.tsx
+++ src/tests/ChangePasswordForm-test.tsx
@@ -1,21 +1,36 @@
 import { ChangePassword } from "components/Dashboard/ChangePassword";
+import { initialState as configInitialState } from "slices/IndexConfig";
 
 import { fireEvent, render, screen, waitFor } from "./helperFunctions/DashboardTestApp-rtl";
 
 test("renders ChangePasswordForm, suggested password value is field in suggested-password-field", () => {
-  render(<ChangePassword />);
-
-  const suggestedPasswordInput = screen.getByLabelText(/Suggested password/i) as HTMLInputElement;
+  render(<ChangePassword />, {
+    state: {
+      config: { ...configInitialState, is_app_loaded: true },
+      chpass: {
+        suggested_password: "test password",
+      },
+    },
+  });
+  expect(screen.getByRole("heading")).toHaveTextContent(/^Change password: Suggested password/);
+  const suggestedPasswordInput = screen.getByLabelText(/Repeat new password/i) as HTMLInputElement;
   expect(suggestedPasswordInput.value).toBeDefined();
 });
 
 test("renders custom password form after clicking do not want a suggested password", async () => {
-  render(<ChangePassword />);
-  const customPasswordButton = screen.getByRole("button", { name: /I don't want a suggested password/i });
+  render(<ChangePassword />, {
+    state: {
+      config: { ...configInitialState, is_app_loaded: true },
+      chpass: {
+        suggested_password: "test password",
+      },
+    },
+  });
+  const customPasswordButton = screen.getByRole("checkbox", { name: /Create a custom password?/i });
   expect(customPasswordButton).toBeInTheDocument();
 
   fireEvent.click(customPasswordButton);
-  expect(screen.getByText(/Suggest a password for me/i)).toBeInTheDocument();
+  expect(customPasswordButton).toBeChecked();
 
   const newPasswordInput = screen.getByLabelText(/Enter new password/i) as HTMLInputElement;
   const repeatNewPasswordInput = screen.getByLabelText(/Repeat new password/i) as HTMLInputElement;
