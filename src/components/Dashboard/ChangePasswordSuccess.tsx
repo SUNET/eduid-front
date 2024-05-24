11e3a6d1ff5e6cece6d06b5ed19c5d55c9d38611
Eunju Huss <eunju@sunet.se>
2024-05-24 14:15:53 +0900

    Add  new ChangePasswordSuccess page

---
+++ src/components/Dashboard/ChangePasswordSuccess.tsx
@@ -0,0 +1,39 @@
+import { ConfirmUserInfo } from "components/Common/ConfirmUserInfo";
+import EduIDButton from "components/Common/EduIDButton";
+import { useAppSelector } from "eduid-hooks";
+import { FormattedMessage } from "react-intl";
+import { useLocation } from "react-router-dom";
+import { finish_url } from "./ChangePassword";
+
+export function ChangePasswordSuccess(): JSX.Element {
+  const emails = useAppSelector((state) => state.emails.emails);
+  const location = useLocation();
+  const password = location.state;
+
+  return (
+    <form method="GET" action={finish_url}>
+      <section className="intro">
+        <h1>
+          <FormattedMessage
+            defaultMessage="Change Password: Completed"
+            description="Change password set new password success heading"
+          />
+        </h1>
+        <div className="lead">
+          <p>
+            <FormattedMessage
+              defaultMessage={`These is your new password for eduID. Make sure to store your password securely for future use`}
+              description="Change password set new password success lead"
+            />
+          </p>
+        </div>
+      </section>
+      <ConfirmUserInfo email_address={emails.filter((mail) => mail.primary)[0].email} new_password={password} />
+      <div className="buttons">
+        <EduIDButton id="change-password-finished" buttonstyle="link" className="normal-case" type="submit">
+          <FormattedMessage defaultMessage="Go to dashboard" description="Go to dashboard" />
+        </EduIDButton>
+      </div>
+    </form>
+  );
+}
--- src/components/IndexMain.tsx
+++ src/components/IndexMain.tsx
@@ -13,6 +13,7 @@ import { Settings } from "./Common/Settings";
 import Splash from "./Common/Splash";
 import { AdvancedSettings } from "./Dashboard/AdvancedSettings";
 import { ChangePassword } from "./Dashboard/ChangePassword";
+import { ChangePasswordSuccess } from "./Dashboard/ChangePasswordSuccess";
 import Start from "./Dashboard/DashboardStart";
 import VerifyIdentity from "./Dashboard/VerifyIdentity";
 import { Help } from "./Help";
@@ -62,6 +63,7 @@ export function IndexMain(): JSX.Element {
                 <Route path="/profile/settings/" element={<Navigate to={settingsPath} />} />
                 <Route path={identityPath} element={<VerifyIdentity />} />
                 <Route path="/profile/chpass/" element={<ChangePassword />} />
+                <Route path="/profile/chpass/success" element={<ChangePasswordSuccess />} />
                 <Route path="/profile/ext-return/:app_name/:authn_id" element={<ExternalReturnHandler />} />
                 {/* Navigates for old paths. TODO: redirect in backend server instead */}
                 <Route path="/profile/security/" element={<Navigate to="/profile/settings/" />} />
