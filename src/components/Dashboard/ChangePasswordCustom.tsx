1da235c3f732dffabf93140619f9dc556168ab56
Eunju Huss <eunju@sunet.se>
2024-05-27 11:46:11 +0900

    Change the form to fina form and add validation to the form to fix it not working properly

--- src/components/Dashboard/ChangePasswordCustom.tsx
+++ src/components/Dashboard/ChangePasswordCustom.tsx
@@ -1,8 +1,8 @@
 import EduIDButton from "components/Common/EduIDButton";
 import NewPasswordInput from "components/Common/NewPasswordInput";
 import PasswordStrengthMeter, { PasswordStrengthData } from "components/Common/PasswordStrengthMeter";
-import { useEffect, useState } from "react";
-import { Field as FinalField } from "react-final-form";
+import { emptyStringPattern } from "helperFunctions/validation/regexPatterns";
+import { Field as FinalField, Form as FinalForm } from "react-final-form";
 import { FormattedMessage, useIntl } from "react-intl";
 import { ChangePasswordChildFormProps } from "./ChangePassword";
 
@@ -10,15 +10,8 @@ import { ChangePasswordChildFormProps } from "./ChangePassword";
 interface ChangePasswordCustomFormProps extends ChangePasswordChildFormProps {}
 
 export default function ChangePasswordCustomForm(props: ChangePasswordCustomFormProps) {
-  const [passwordData, setPasswordData] = useState<PasswordStrengthData>({});
-  const [repeatNewPassword, setRepeatNewPassword] = useState<string>();
   const intl = useIntl();
 
-  useEffect(() => {
-    setRepeatNewPassword(props.formProps.values.custom);
-  }, [props.formProps.values.custom]);
-
-  useEffect(() => {}, [props.formProps.values.custom]);
   const new_password_placeholder = intl.formatMessage({
     id: "placeholder.new_password_placeholder",
     defaultMessage: "enter new password",
@@ -36,113 +29,120 @@ export default function ChangePasswordCustomForm(props: ChangePasswordCustomForm
     // on the current value in the form. We need to trigger validation of the field again at this
     // point, since validation uses this calculated value (and will already have executed when we
     // get here).
-    setPasswordData(value);
     props.formProps.form.change("custom", props.formProps.values.custom);
   }
 
-  function strongEnough(value?: string): string | undefined {
-    // check that the custom password is strong enough, using a score computed in the
-    // PasswordStrengthMeter component.
-    if (!value) {
-      return "required";
-    } else if (passwordData.isTooWeak && passwordData.score !== 4) {
-      return "chpass.low-password-entropy";
+  function validateNewPassword(values: any) {
+    const errors: any = {};
+    if (values !== undefined) {
+      ["custom", "repeat"].forEach((inputName) => {
+        if (!values[inputName] || emptyStringPattern.test(values[inputName])) {
+          errors[inputName] = "required";
+        } else if (values["custom"].replace(/\s/g, "") !== values["repeat"].replace(/\s/g, "")) {
+          // Remove whitespace from both passwords before comparing
+          errors["repeat"] = "chpass.different-repeat";
+        }
+      });
     }
-  }
 
-  function mustMatch(value?: string): string | undefined {
-    // validate that the repeated password is the same as the first one (called 'custom')
-    if (!value) {
-      return "required";
-    }
-    if (value !== repeatNewPassword) {
-      return "chpass.different-repeat";
-    }
+    return errors;
   }
 
   return (
-    <form id="passwords-view-form" onSubmit={props.formProps.handleSubmit}>
-      <fieldset className="password-format">
-        <label>
-          <FormattedMessage
-            defaultMessage="Tip: Choose a strong password"
-            description="help text for custom password label"
-          />
-        </label>
-        <ul id="password-custom-help">
-          {[
-            <FormattedMessage
-              key={1}
-              defaultMessage={`Use upper- and lowercase characters, but not at the beginning or end`}
-              description="help text for custom password tips"
-            />,
-            <FormattedMessage
-              key={2}
-              defaultMessage={`Add digits somewhere, but not at the beginning or end`}
-              description="help text for custom password tips"
-            />,
-            <FormattedMessage
-              key={3}
-              defaultMessage={`Add special characters, such as  @ $ \\ + _ %`}
-              description="help text for custom password tips"
-            />,
-            <FormattedMessage
-              key={4}
-              defaultMessage={`Spaces are ignored`}
-              description="help text for custom password tips"
-            />,
-          ].map((list) => {
-            return <li key={list.key}>{list}</li>;
-          })}
-        </ul>
-      </fieldset>
+    <FinalForm<any>
+      onSubmit={props.formProps.handleSubmit}
+      validate={validateNewPassword}
+      render={(formProps) => {
+        return (
+          <form id="passwords-view-form" onSubmit={formProps.handleSubmit}>
+            <fieldset className="password-format">
+              <label>
+                <FormattedMessage
+                  defaultMessage="Tip: Choose a strong password"
+                  description="help text for custom password label"
+                />
+              </label>
+              <ul id="password-custom-help">
+                {[
+                  <FormattedMessage
+                    key={1}
+                    defaultMessage={`Use upper- and lowercase characters, but not at the beginning or end`}
+                    description="help text for custom password tips"
+                  />,
+                  <FormattedMessage
+                    key={2}
+                    defaultMessage={`Add digits somewhere, but not at the beginning or end`}
+                    description="help text for custom password tips"
+                  />,
+                  <FormattedMessage
+                    key={3}
+                    defaultMessage={`Add special characters, such as  @ $ \\ + _ %`}
+                    description="help text for custom password tips"
+                  />,
+                  <FormattedMessage
+                    key={4}
+                    defaultMessage={`Spaces are ignored`}
+                    description="help text for custom password tips"
+                  />,
+                ].map((list) => {
+                  return <li key={list.key}>{list}</li>;
+                })}
+              </ul>
+            </fieldset>
 
-      <fieldset className="change-password-custom-inputs">
-        <FinalField
-          name="custom"
-          component={NewPasswordInput}
-          componentClass="input"
-          type="password"
-          label={
-            <FormattedMessage defaultMessage="Enter new password" description="chpass form custom password label" />
-          }
-          passwordStrengthMeter={
-            <PasswordStrengthMeter password={props.formProps.values.custom} passStateUp={updatePasswordData} />
-          }
-          id="custom-password-field"
-          validate={strongEnough}
-          autoComplete="new-password"
-          required={true}
-          placeHolder={new_password_placeholder}
-        />
-        <FinalField
-          name="repeat"
-          component={NewPasswordInput}
-          componentClass="input"
-          type="password"
-          id="repeat-password-field"
-          label={
-            <FormattedMessage defaultMessage="Repeat new password" description="chpass form custom password repeat" />
-          }
-          validate={mustMatch}
-          required={true}
-          placeHolder={repeat_new_password_placeholder}
-        />
-      </fieldset>
-      <div id="chpass-form" className="tab-pane buttons">
-        <EduIDButton buttonstyle="secondary" onClick={props.handleCancel}>
-          <FormattedMessage defaultMessage="cancel" description="button cancel" />
-        </EduIDButton>
-        <EduIDButton
-          type="submit"
-          id="chpass-button"
-          buttonstyle="primary"
-          disabled={props.formProps.submitting || props.formProps.invalid}
-          onClick={props.formProps.handleSubmit}
-        >
-          <FormattedMessage defaultMessage="Save" description="button save" />
-        </EduIDButton>
-      </div>
-    </form>
+            <fieldset className="change-password-custom-inputs">
+              <FinalField
+                name="custom"
+                component={NewPasswordInput}
+                componentClass="input"
+                type="password"
+                label={
+                  <FormattedMessage
+                    defaultMessage="Enter new password"
+                    description="chpass form custom password label"
+                  />
+                }
+                passwordStrengthMeter={
+                  <PasswordStrengthMeter password={formProps.values.custom} passStateUp={updatePasswordData} />
+                }
+                id="custom-password-field"
+                autoComplete="new-password"
+                required={true}
+                placeHolder={new_password_placeholder}
+              />
+              <FinalField
+                name="repeat"
+                component={NewPasswordInput}
+                componentClass="input"
+                type="password"
+                id="repeat-password-field"
+                label={
+                  <FormattedMessage
+                    defaultMessage="Repeat new password"
+                    description="chpass form custom password repeat"
+                  />
+                }
+                required={true}
+                placeHolder={repeat_new_password_placeholder}
+              />
+            </fieldset>
+            <div id="chpass-form" className="tab-pane buttons">
+              <EduIDButton buttonstyle="secondary" onClick={props.handleCancel}>
+                <FormattedMessage defaultMessage="cancel" description="button cancel" />
+              </EduIDButton>
+              <EduIDButton
+                type="submit"
+                id="chpass-button"
+                buttonstyle="primary"
+                disabled={formProps.submitting || formProps.invalid}
+                onClick={formProps.handleSubmit}
+              >
+                <FormattedMessage defaultMessage="Save" description="button save" />
+              </EduIDButton>
+            </div>
+          </form>
+        );
+      }}
+    />
   );
 }
