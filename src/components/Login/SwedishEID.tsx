3f7ed6ccdced8de1169316f54b0798aa01795975
Eunju Huss <eunju@sunet.se>
2024-09-26 10:44:06 +0200

    WIP: add react select for mfa options

--- src/components/Login/SwedishEID.tsx
+++ src/components/Login/SwedishEID.tsx
@@ -2,8 +2,9 @@ import { bankIDMfaAuthenticate } from "apis/eduidBankid";
 import { eidasMfaAuthenticate } from "apis/eduidEidas";
 import EduIDButton from "components/Common/EduIDButton";
 import { useAppDispatch, useAppSelector } from "eduid-hooks";
-
+import { Form as FinalForm } from "react-final-form";
 import { FormattedMessage } from "react-intl";
+import Select from "react-select";
 import BankIdFlag from "../../../img/flags/BankID_logo.svg";
 import FrejaFlag from "../../../img/flags/FOvalIndigo.svg";
 
@@ -13,6 +14,11 @@ export function SwedishEID(): JSX.Element {
   const dispatch = useAppDispatch();
   // TODO: when backend is updated to swedish_eid, we should be able to rename this.
   const notAvailable = !authn_options.freja_eidplus;
+
+  const options = [
+    { value: "Bank ID", label: "Bank ID" },
+    { value: "Freja+", label: "Freja+" },
+  ];
 
   async function handleOnClickBankID() {
     const response = await dispatch(
@@ -37,50 +43,80 @@ export function SwedishEID(): JSX.Element {
   }
 
   return (
-    <div className="option-wrapper">
-      <div className="option">
-        <h3>
-          <FormattedMessage defaultMessage={`Swedish eID`} />
-        </h3>
-
-        {notAvailable && (
-          <p className="help-text">
-            <FormattedMessage
-              description="MFA Freja help text"
-              defaultMessage="Requires a confirmed Swedish national identity number."
-            />
-          </p>
-        )}
-
-        <EduIDButton
-          buttonstyle="secondary"
-          type="submit"
-          className="btn-icon"
-          onClick={handleOnClickBankID}
-          id="mfa-bankid"
-          disabled={notAvailable}
-        >
-          <img height="35" alt="BankID" src={BankIdFlag} />
-          <FormattedMessage
-            defaultMessage={`Use my {bankID}`}
-            values={{ bankID: <span className="verbatim">&nbsp;BankID</span> }}
-          />
-        </EduIDButton>
-        <EduIDButton
-          buttonstyle="secondary"
-          type="submit"
-          onClick={handleOnClickFrejaeID}
-          id="mfa-freja"
-          className="btn-icon freja-icon"
-          disabled={notAvailable}
-        >
-          <img height="35" alt="Freja+" src={FrejaFlag} />
-          <FormattedMessage
-            defaultMessage={`Use my {freja_eidplus_verbatim}`}
-            values={{ freja_eidplus_verbatim: <span className="verbatim">&nbsp;Freja+</span> }}
-          />
-        </EduIDButton>
+    <>
+      <div className="option-wrapper">
+        <div className="or-container">
+          <div className="line" />
+          <span>
+            <FormattedMessage defaultMessage="OR" />
+          </span>
+          <div className="line" />
+        </div>
+        <FinalForm
+          onSubmit={() => {}}
+          render={({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <Select
+                  // isDisabled={fetchFailed}
+                  options={options}
+                  // onChange={handleOnChange}
+                  // value={selected}
+                  placeholder={"Select another option"}
+                  isSearchable={false}
+                  className="mfa-select"
+                  classNamePrefix="react-select"
+                />
+              </fieldset>
+            </form>
+          )}
+        />
       </div>
-    </div>
+      <div className="option-wrapper">
+        <div className="option">
+          <h3>
+            <FormattedMessage defaultMessage={`Swedish eID`} />
+          </h3>
+
+          {notAvailable && (
+            <p className="help-text">
+              <FormattedMessage
+                description="MFA Freja help text"
+                defaultMessage="Requires a confirmed Swedish national identity number."
+              />
+            </p>
+          )}
+
+          <EduIDButton
+            buttonstyle="secondary"
+            type="submit"
+            className="btn-icon"
+            onClick={handleOnClickBankID}
+            id="mfa-bankid"
+            disabled={notAvailable}
+          >
+            <img height="35" alt="BankID" src={BankIdFlag} />
+            <FormattedMessage
+              defaultMessage={`Use my {bankID}`}
+              values={{ bankID: <span className="verbatim">&nbsp;BankID</span> }}
+            />
+          </EduIDButton>
+          <EduIDButton
+            buttonstyle="secondary"
+            type="submit"
+            onClick={handleOnClickFrejaeID}
+            id="mfa-freja"
+            className="btn-icon freja-icon"
+            disabled={notAvailable}
+          >
+            <img height="35" alt="Freja+" src={FrejaFlag} />
+            <FormattedMessage
+              defaultMessage={`Use my {freja_eidplus_verbatim}`}
+              values={{ freja_eidplus_verbatim: <span className="verbatim">&nbsp;Freja+</span> }}
+            />
+          </EduIDButton>
+        </div>
+      </div>
+    </>
   );
 }
