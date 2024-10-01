03f6739126dd4130f4a119c75d4daa51645bc553
Eunju Huss <eunju@sunet.se>
2024-10-01 13:23:55 +0200

    Clean up

--- src/components/Login/SwedishEID.tsx
+++ src/components/Login/SwedishEID.tsx
@@ -1,24 +1,25 @@
 import { bankIDMfaAuthenticate } from "apis/eduidBankid";
 import { eidasMfaAuthenticate } from "apis/eduidEidas";
 import { useAppDispatch, useAppSelector } from "eduid-hooks";
-import { useState } from "react";
 import { Form as FinalForm } from "react-final-form";
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 import Select from "react-select";
 import BankIdFlag from "../../../img/flags/BankID_logo.svg";
 import FrejaFlag from "../../../img/flags/FOvalIndigo.svg";
 
-interface SingleValueProps {
-  value: string;
-}
-
 export function SwedishEID(): JSX.Element {
+  const intl = useIntl();
   const authn_options = useAppSelector((state) => state.login.authn_options);
   const ref = useAppSelector((state) => state.login.ref);
   const dispatch = useAppDispatch();
   // TODO: when backend is updated to swedish_eid, we should be able to rename this.
   const notAvailable = !authn_options.freja_eidplus;
-  const [selected, setSelected] = useState();
+
+  const placeholder = intl.formatMessage({
+    id: "placeholder.recovery_option",
+    defaultMessage: "Show recovery options",
+    description: "placeholder text for recovery option",
+  });
 
   const IconWithText = ({ icon, text }: any) => {
     return (
@@ -80,46 +81,41 @@ export function SwedishEID(): JSX.Element {
   }
 
   return (
-    <>
-      <div className="option-wrapper">
-        <div className="or-container">
-          <div className="line" />
-          <span>
-            <FormattedMessage defaultMessage="or use recovery mode" />
-          </span>
-          <div className="line" />
-        </div>
-
-        <FinalForm
-          onSubmit={() => {}}
-          render={({ handleSubmit }) => (
-            <form onSubmit={handleSubmit}>
-              <fieldset>
-                <Select
-                  // isDisabled={fetchFailed}
-                  options={options}
-                  onChange={handleOnChange}
-                  value={selected}
-                  placeholder={"Select recovery option"}
-                  isSearchable={false}
-                  className="mfa-select"
-                  classNamePrefix="react-select"
-                  // menuIsOpen={true}
-                  isDisabled={notAvailable}
-                />
-              </fieldset>
-            </form>
-          )}
-        />
-        {notAvailable && (
-          <p className="help-text">
-            <FormattedMessage
-              description="MFA Freja help text"
-              defaultMessage="Requires a confirmed Swedish national identity number."
-            />
-          </p>
-        )}
+    <div className="option-wrapper">
+      <div className="or-container">
+        <div className="line" />
+        <span>
+          <FormattedMessage defaultMessage="or use recovery mode" />
+        </span>
+        <div className="line" />
       </div>
-    </>
+
+      <FinalForm
+        onSubmit={() => {}}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <fieldset>
+              <Select
+                options={options}
+                onChange={handleOnChange}
+                placeholder={placeholder}
+                isSearchable={false}
+                className="mfa-select"
+                classNamePrefix="react-select"
+                isDisabled={notAvailable}
+              />
+            </fieldset>
+          </form>
+        )}
+      />
+      {notAvailable && (
+        <p className="help-text">
+          <FormattedMessage
+            description="MFA Freja help text"
+            defaultMessage="Requires a confirmed Swedish national identity number."
+          />
+        </p>
+      )}
+    </div>
   );
 }
