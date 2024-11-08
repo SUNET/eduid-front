50ddc679592ceeb655d57dd55f3e0404ea40561e
Eunju Huss <eunju@sunet.se>
2024-11-08 10:32:41 +0100

    Clean up for eduID status overview

--- src/components/Dashboard/Recommendations.tsx
+++ src/components/Dashboard/Recommendations.tsx
@@ -1,33 +1,13 @@
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { faIdCard, faKey, faUser } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { UserIdentities } from "apis/eduidPersonalData";
-import { CredentialType, requestCredentials, RequestCredentialsResponse } from "apis/eduidSecurity";
+import { CredentialType, requestCredentials } from "apis/eduidSecurity";
 import { ACCOUNT_PATH, IDENTITY_PATH, SECURITY_PATH } from "components/IndexMain";
 import { useAppDispatch, useAppSelector } from "eduid-hooks";
 import { useEffect } from "react";
-import { Accordion } from "react-accessible-accordion";
 import { FormattedMessage } from "react-intl";
 import { Link } from "react-router-dom";
-import AccordionItemTemplate from "../Common/AccordionItemTemplate";
 
-/**
- * Recommendation for adding name, security key and phone number and verification of identity
- */
-function RecommendationAddingSecurityKey(props: RequestCredentialsResponse): JSX.Element | null {
-  if (props.credentials.length) {
-    return null;
-  }
-
+function RecommendationAddingSecurityKey(): JSX.Element | null {
   return (
-    <AccordionItemTemplate
-      uuid="recommendation-security-key"
-      icon={<FontAwesomeIcon icon={faKey as IconProp} className="circle-icon" />}
-      title={
-        <FormattedMessage description="accordion item Adding security key" defaultMessage="Add your security key" />
-      }
-      additionalInfo={null}
-    >
+    <>
       <p>
         <FormattedMessage
           description="accordion item security key additional info"
@@ -37,22 +17,13 @@ function RecommendationAddingSecurityKey(props: RequestCredentialsResponse): JSX
       <Link key="advanced-settings" to={SECURITY_PATH}>
         <FormattedMessage defaultMessage="Go to Advanced settings" description="go to Advanced settings" />
       </Link>
-    </AccordionItemTemplate>
+    </>
   );
 }
 
-function RecommendationAddingName(props: { given_name?: string }): JSX.Element | null {
-  if (props.given_name) {
-    return null;
-  }
-
+function RecommendationAddingName(): JSX.Element | null {
   return (
-    <AccordionItemTemplate
-      icon={<FontAwesomeIcon icon={faUser as IconProp} className="circle-icon" />}
-      title={<FormattedMessage description="accordion item Adding name" defaultMessage="Add your name" />}
-      additionalInfo={null}
-      uuid="recommendation-add-name"
-    >
+    <>
       <p>
         <FormattedMessage
           description="accordion item name additionalInfo"
@@ -62,53 +33,23 @@ function RecommendationAddingName(props: { given_name?: string }): JSX.Element |
       <Link key="settings" to={ACCOUNT_PATH}>
         <FormattedMessage defaultMessage="Go to Settings" description="go to settings" />
       </Link>
-    </AccordionItemTemplate>
+    </>
   );
 }
 
-function RecommendationVerifyIdentity(props: { identities: UserIdentities }): JSX.Element | null {
-  let title, description;
-  // if user has swedish nin and it is verified, do not show accordion item
-  if (props.identities.nin?.verified) {
-    return null;
-  }
-  // if user verified with eidas or passport, show accordion item to verify with nin
-  if (props.identities.freja?.verified || props.identities.eidas?.verified) {
-    title = (
-      <FormattedMessage
-        description="accordion item Verification with Swedish national ID number"
-        defaultMessage="Verify your identity with Swedish national ID number"
-      />
-    );
-    description = (
-      <FormattedMessage
-        description="accordion item additional info Verification with Swedish national ID number"
-        defaultMessage={`If you have obtained a Swedish national ID number you are able to verify your identity 
-            with the Swedish national ID number.`}
-      />
-    );
-  } else {
-    title = <FormattedMessage description="accordion item Verification" defaultMessage="Verify your identity" />;
-    description = (
-      <FormattedMessage
-        description="accordion item additional info Verification additional info"
-        defaultMessage="Your identity is not verified. Please verify your identity to get access to more services."
-      />
-    );
-  }
-
+function RecommendationVerifyIdentity(): JSX.Element | null {
   return (
-    <AccordionItemTemplate
-      icon={<FontAwesomeIcon icon={faIdCard as IconProp} className="circle-icon" />}
-      title={title}
-      additionalInfo={null}
-      uuid="recommendation-verify-identity"
-    >
-      <p>{description}</p>
+    <>
+      <p>
+        <FormattedMessage
+          description="accordion item name additionalInfo"
+          defaultMessage="Name can be used to personalise services that you access with your eduID."
+        />
+      </p>
       <Link key="verify-identity" to={IDENTITY_PATH}>
         <FormattedMessage defaultMessage="Go to Identity" description="go to identity" />
       </Link>
-    </AccordionItemTemplate>
+    </>
   );
 }
 
@@ -149,11 +90,9 @@ export function Recommendations(): JSX.Element | null {
           defaultMessage="To get the most out of eduID we recommend that you follow the below recommendations."
         />
       </p>
-      <Accordion allowMultipleExpanded allowZeroExpanded>
-        <RecommendationAddingName given_name={given_name} />
-        {identities && <RecommendationVerifyIdentity identities={identities} />}
-        <RecommendationAddingSecurityKey credentials={tokens} />
-      </Accordion>
+      <RecommendationAddingName />
+      <RecommendationVerifyIdentity />
+      <RecommendationAddingSecurityKey />
     </article>
   );
 }
