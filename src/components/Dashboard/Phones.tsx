7f7b216c9afc04deb489f82f2bc0c43970d519d3
Eunju Huss <eunju@sunet.se>
2024-12-02 13:49:41 +0100

    remove text decoration for link.disabled

--- src/components/Dashboard/Phones.tsx
+++ src/components/Dashboard/Phones.tsx
@@ -264,7 +264,7 @@ function Phones() {
             disabled={true}
             id="phone-number-add-more-button"
             buttonstyle="link"
-            className="lowercase disabled"
+            className="lowercase"
             onClick={handlePhoneForm}
           >
             <FormattedMessage defaultMessage=" + add more" description="button add more" />
