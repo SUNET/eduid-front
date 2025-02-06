c7bef05b92599e49c7e5fe54122e32abc0d89c30
Eunju Huss <eunju@sunet.se>
2025-02-06 09:50:20 +0100

    add dialog element

--- src/components/Common/NotificationModal.tsx
+++ src/components/Common/NotificationModal.tsx
@@ -3,13 +3,13 @@ import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 import EduIDButton from "./EduIDButton";
 
 interface NotificationModalProps {
-  id: string;
-  title: React.ReactNode;
-  mainText: React.ReactNode;
-  showModal: boolean;
-  closeModal: React.MouseEventHandler<HTMLButtonElement>;
-  acceptModal: (event?: React.MouseEvent<HTMLElement>) => void;
-  acceptButtonText: React.ReactNode;
+  readonly id: string;
+  readonly title: React.ReactNode;
+  readonly mainText: React.ReactNode;
+  readonly showModal: boolean;
+  readonly closeModal: React.MouseEventHandler<HTMLButtonElement>;
+  readonly acceptModal: (event?: React.MouseEvent<HTMLElement>) => void;
+  readonly acceptButtonText: React.ReactNode;
 }
 
 function NotificationModal(props: NotificationModalProps) {
@@ -21,25 +21,27 @@ function NotificationModal(props: NotificationModalProps) {
   }
 
   return (
-    <div id={props.id} tabIndex={-1} onKeyDown={handlePress} role="dialog" aria-hidden="true" data-backdrop="true">
-      <Modal isOpen={props.showModal} className={props.id}>
-        <ModalHeader>
-          {props.title}
-          <EduIDButton
-            id={`${props.id}-close-button`}
-            buttonstyle="close"
-            className="float-right"
-            onClick={props.closeModal}
-          ></EduIDButton>
-        </ModalHeader>
-        <ModalBody>{props.mainText}</ModalBody>
-        <ModalFooter>
-          <EduIDButton id={`${props.id}-accept-button`} buttonstyle="primary" onClick={props.acceptModal}>
-            {props.acceptButtonText}
-          </EduIDButton>
-        </ModalFooter>
-      </Modal>
-    </div>
+    <dialog>
+      <div id={props.id} tabIndex={-1} onKeyDown={handlePress} aria-hidden="true" data-backdrop="true">
+        <Modal isOpen={props.showModal} className={props.id}>
+          <ModalHeader>
+            {props.title}
+            <EduIDButton
+              id={`${props.id}-close-button`}
+              buttonstyle="close"
+              className="float-right"
+              onClick={props.closeModal}
+            ></EduIDButton>
+          </ModalHeader>
+          <ModalBody>{props.mainText}</ModalBody>
+          <ModalFooter>
+            <EduIDButton id={`${props.id}-accept-button`} buttonstyle="primary" onClick={props.acceptModal}>
+              {props.acceptButtonText}
+            </EduIDButton>
+          </ModalFooter>
+        </Modal>
+      </div>
+    </dialog>
   );
 }
 
