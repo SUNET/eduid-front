605d4bf00396a4af3ecbd0992baab5fd5cce3488
Eunju Huss <eunju@sunet.se>
2025-02-10 13:16:33 +0100

    Commented out code

--- src/components/Common/MultiFactorAuthentication.tsx
+++ src/components/Common/MultiFactorAuthentication.tsx
@@ -452,7 +452,8 @@ function SecurityKeyTable({
         } else {
           await handleVerifyWebauthnTokenBankID(parsedFrontendState.credential);
         }
-        dispatch(authnSlice.actions.setAuthnFrontendReset());
+        // TODO: clean up
+        // dispatch(authnSlice.actions.setAuthnFrontendReset());
       }
     })();
   }, [authn?.response?.frontend_action]);
