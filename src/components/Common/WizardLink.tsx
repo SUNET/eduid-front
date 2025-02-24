0a07bea2eb6f25a8fbffc024df2412636d83f55b
Eunju Huss <eunju@sunet.se>
2025-02-24 14:53:47 +0100

    link position adjustment

--- src/components/Common/WizardLink.tsx
+++ src/components/Common/WizardLink.tsx
@@ -7,13 +7,13 @@ export function WizardLink({ previousLink, previousText, nextLink, nextText }: a
   return (
     <section className="wizard-link-wrapper">
       {previousLink ? (
-        <Link className="text-link" to={previousLink}>
-          <FontAwesomeIcon icon={faArrowLeft as IconProp} /> {previousText}
+        <Link className="text-link left" to={previousLink}>
+          <FontAwesomeIcon icon={faArrowLeft as IconProp} /> <span>{previousText}</span>
         </Link>
       ) : null}
       {nextLink ? (
-        <Link className="text-link" to={nextLink}>
-          {nextText} <FontAwesomeIcon icon={faArrowRight as IconProp} />
+        <Link className="text-link right" to={nextLink}>
+          <span>{nextText}</span> <FontAwesomeIcon icon={faArrowRight as IconProp} />
         </Link>
       ) : null}
     </section>
