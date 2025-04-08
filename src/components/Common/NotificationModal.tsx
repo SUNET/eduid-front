59153c317c845c7af336136002c786d43b1e75cd
Eunju Huss <eunju@sunet.se>
2025-04-08 11:05:18 +0200

    Add evetn.preventDefault()

--- src/components/Common/NotificationModal.tsx
+++ src/components/Common/NotificationModal.tsx
@@ -7,36 +7,38 @@ interface NotificationModalProps {
   readonly mainText: React.ReactNode;
   readonly showModal: boolean;
   readonly closeModal: React.MouseEventHandler<HTMLButtonElement>;
-  readonly acceptModal: (event?: React.MouseEvent<HTMLElement>) => void;
+  readonly acceptModal?: (event?: React.MouseEvent<HTMLElement>) => void | Promise<void>;
   readonly acceptButtonText: React.ReactNode;
 }
 
 function NotificationModal(props: NotificationModalProps) {
   if (!props.showModal) return null;
-
+  console.log("123");
   return (
     <dialog open={props.showModal}>
-      <div
-        className={props.showModal ? "modal fade show" : "modal"}
-        id={props.id}
-        tabIndex={-1}
-        aria-hidden={!props.showModal}
-      >
+      <div className={props.showModal ? "modal fade show" : "modal"} id={props.id} tabIndex={-1}>
         <div className={`modal-dialog ${props.id}`}>
           <div className={`modal-content ${props.id} `}>
             <div className="modal-header">
               <h5 className="modal-title">{props.title}</h5>
-              <EduIDButton id={`${props.id}-close-button`} buttonstyle="close" onClick={props.closeModal}></EduIDButton>
+              <EduIDButton
+                type="button"
+                id={`${props.id}-close-button`}
+                buttonstyle="close"
+                onClick={props.closeModal}
+              ></EduIDButton>
             </div>
             <div className="modal-body">{props.mainText}</div>
             <div className="modal-footer">
-              <form onSubmit={() => props.acceptModal}>
-                <EduIDButton
-                  type="submit"
-                  id={`${props.id}-accept-button`}
-                  buttonstyle="primary"
-                  onClick={props.acceptModal}
-                >
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  if (props.acceptModal) {
+                    props.acceptModal();
+                  }
+                }}
+              >
+                <EduIDButton type="submit" id={`${props.id}-accept-button`} buttonstyle="primary">
                   {props.acceptButtonText}
                 </EduIDButton>
               </form>
