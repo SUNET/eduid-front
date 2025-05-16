580c24a483f258ffed6daf822b1b4a485c31278e
Lasse Yledahl <ylle@sunet.se>
2025-05-16 16:02:17 +0200

    update typing

--- src/middleware/CsrfTokenMiddleware.ts
+++ src/middleware/CsrfTokenMiddleware.ts
@@ -1,9 +1,10 @@
 import { storeCsrfToken } from "commonConfig";
+import { Middleware } from "redux";
 
 /*
  * This middleware is used to save csrf token in redux store.
  */
-export const csrfTokenMiddleware = (store:any) => (next:any) => (action: any) => {
+export const csrfTokenMiddleware: Middleware = (store) => (next) => (action: any) => {
     if (action?.payload?.payload?.csrf_token !== undefined) {
         const state = store.getState();
         if (action.payload.payload.csrf_token !== state.config.csrf_token) {
