8003e9516fe4d33597e6eb9b475180080786a4ff
Eunju Huss <eunju@sunet.se>
2025-07-31 11:40:55 +0200

    Fix test error by checking if window is defined and matcMedia is a function

--- src/components/Common/ThemeContext.tsx
+++ src/components/Common/ThemeContext.tsx
@@ -6,11 +6,14 @@ interface ThemeContextType {
   theme: Theme;
   toggleTheme: () => void;
 }
-
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
-const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+let systemSettingDark: MediaQueryList | undefined;
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  if (typeof window !== "undefined" && typeof window.matchMedia === "function") {
+    systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+  }
+
   const savedTheme = localStorage.getItem("theme") as Theme | null;
 
   const [theme, setTheme] = useState<Theme>("light");
@@ -18,7 +21,7 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ childre
   useEffect(() => {
     if (savedTheme) {
       setTheme(savedTheme);
-    } else if (systemSettingDark.matches) {
+    } else if (systemSettingDark?.matches) {
       setTheme("dark");
     }
   }, []);
