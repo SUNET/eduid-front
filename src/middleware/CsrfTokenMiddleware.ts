04fcc32029f278f055b22110e8a14774617c9057
Eunju Huss <eunju@sunet.se>
2025-08-28 10:05:30 +0200

    fix error:  Cannot log after tests are done

--- src/middleware/CsrfTokenMiddleware.ts
+++ src/middleware/CsrfTokenMiddleware.ts
@@ -5,15 +5,19 @@ import { Middleware } from "redux";
  * This middleware is used to save csrf token in redux store.
  */
 export const csrfTokenMiddleware: Middleware = (store) => (next) => (action: any) => {
-    if (action?.payload?.payload?.csrf_token !== undefined) {
-        const state = store.getState();
-        if (action.payload.payload.csrf_token !== state.config.csrf_token) {
-            store.dispatch(storeCsrfToken(action.payload.payload.csrf_token));
-        }
-        delete action.payload.payload.csrf_token;
+  if (action?.payload?.payload?.csrf_token !== undefined) {
+    const state = store.getState();
+    if (action.payload.payload.csrf_token !== state.config.csrf_token) {
+      store.dispatch(storeCsrfToken(action.payload.payload.csrf_token));
     }
-    
-    return next(action);
-}
+    const newAction = {
+      ...action,
+      payload: { ...action.payload, payload: { ...action.payload.payload, csrf_token: undefined } },
+    };
+    return next(newAction);
+  }
+
+  return next(action);
+};
 
 export default csrfTokenMiddleware;
