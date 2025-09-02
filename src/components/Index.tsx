0ea52ff71fd237f1cf789ff6a878c68ae5994227
Eunju Huss <eunju@sunet.se>
2025-09-02 12:10:16 +0200

    new version scss

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -18,7 +18,7 @@ export function Index() {
   const dashboard_link = useAppSelector((state) => state.config.dashboard_link);
   const frontend_action = useAppSelector((state) => state.authn?.response?.frontend_action);
   const eduid_site_link = useAppSelector((state) => state.config.eduid_site_link);
-  const [postDeleteAcccount] = securityApi.useLazyPostDeleteAccountQuery()
+  const [postDeleteAcccount] = securityApi.useLazyPostDeleteAccountQuery();
 
   async function redirectToLogin() {
     dispatch(appLoadingSlice.actions.appLoaded());
