371ae65f715a09e90578b2e62b5d244865b7e8c6
Lasse Yledahl <ylle@sunet.se>
2025-09-10 09:18:53 +0200

    remove old polyfill

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,3 @@
-// This ensures you can use `window.fetch()` in your Jest tests.
-require("whatwg-fetch");
-
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
