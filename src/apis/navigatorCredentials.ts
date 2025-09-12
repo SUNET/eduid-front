70518d47ac4558124fd56a7a1552df37459a7d98
Lasse Yledahl <ylle@sunet.se>
2025-09-12 13:14:43 +0200

    small refactor

--- src/apis/navigatorCredentials.ts
+++ src/apis/navigatorCredentials.ts
@@ -2,26 +2,18 @@ import { BaseQueryFn, createApi } from "@reduxjs/toolkit/query/react";
 
 const navigatorCredentialsBaseQuery: BaseQueryFn = async (args) => {
   let errorMessage: string = "";
-  if (args.action === "performAuthentication") {
-    const publicKey = PublicKeyCredential.parseRequestOptionsFromJSON(args.payload);
-    try {
+  try {
+    if (args.action === "performAuthentication") {
+      const publicKey = PublicKeyCredential.parseRequestOptionsFromJSON(args.payload);
       const credential = await navigator.credentials.get({ publicKey });
       if (credential instanceof PublicKeyCredential && credential.response instanceof AuthenticatorAssertionResponse) {
         return { data: credential.toJSON() };
       } else {
         errorMessage = "Unable to obtain credential.";
       }
-    } catch (error) {
-      if (error instanceof DOMException) {
-        errorMessage = error.message;
-      } else {
-        errorMessage = "Unknown error occurred.";
-      }
     }
-  }
-  if (args.action === "createCredential") {
-    const publicKey = PublicKeyCredential.parseCreationOptionsFromJSON(args.payload);
-    try {
+    if (args.action === "createCredential") {
+      const publicKey = PublicKeyCredential.parseCreationOptionsFromJSON(args.payload);
       const credential = await navigator.credentials.create({ publicKey });
       if (
         credential instanceof PublicKeyCredential &&
@@ -31,12 +23,12 @@ const navigatorCredentialsBaseQuery: BaseQueryFn = async (args) => {
       } else {
         errorMessage = "Unable to create credential.";
       }
-    } catch (error) {
-      if (error instanceof DOMException) {
-        errorMessage = error.message;
-      } else {
-        errorMessage = "Unknown error occurred.";
-      }
+    }
+  } catch (error) {
+    if (error instanceof DOMException) {
+      errorMessage = error.message;
+    } else {
+      errorMessage = "Unknown error occurred.";
     }
   }
   return {
