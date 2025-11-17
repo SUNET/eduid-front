51f5305bdb5a5e4b73e5f8c5fef35b362af586a8
Lasse Yledahl <ylle@sunet.se>
2025-11-17 11:46:24 +0100

    add default strings for translation

---
+++ src/translation/defaultMessages/navigatorCredentials.tsx
@@ -0,0 +1,19 @@
+import { FormattedMessage } from "react-intl";
+
+export const credentialErrors = {
+  "Error: dom_error.not_allowed": (
+    <FormattedMessage id="Error: dom_error.not_allowed" defaultMessage={`Access was not allowed.`} />
+  ),
+  "Error: dom_error.unable_to_obtain_credential": (
+    <FormattedMessage
+      id="Error: dom_error.unable_to_obtain_credential"
+      defaultMessage={`Unable to obtain credential.`}
+    />
+  ),
+  "Error: dom_error.unable_to_create_credential": (
+    <FormattedMessage
+      id="Error: dom_error.unable_to_create_credential"
+      defaultMessage={`Unable to create credential.`}
+    />
+  ),
+};
