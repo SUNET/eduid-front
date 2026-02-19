bc503889992da79b4e9074cf5ca70a400d8964c1
Eunju Huss <eunju@sunet.se>
2026-02-19 11:33:42 +0100

    improve ChangePasswordSuccess with null safety and useMemo

--- src/components/Dashboard/ChangePasswordSuccess.tsx
+++ src/components/Dashboard/ChangePasswordSuccess.tsx
@@ -2,6 +2,7 @@ import { ConfirmUserInfo, EmailFieldset } from "components/Common/ConfirmUserInf
 import EduIDButton from "components/Common/EduIDButton";
 import Splash from "components/Common/Splash";
 import { useAppSelector } from "eduid-hooks";
+import { useMemo } from "react";
 import { FormattedMessage } from "react-intl";
 import { useLocation } from "react-router";
 import { finish_url } from "./ChangePassword";
@@ -9,13 +10,12 @@ import { finish_url } from "./ChangePassword";
 export function ChangePasswordSuccess(): React.JSX.Element {
   const emails = useAppSelector((state) => state.emails.emails);
   const location = useLocation();
-  const password = location.state.password;
-  const isSuggested = location.state.isSuggested;
+  const password = location.state?.password ?? "";
+  const isSuggested = location.state?.isSuggested ?? false;
   const is_loaded = useAppSelector((state) => state.config.is_app_loaded);
-  let email = "";
-  if (emails.length) {
-    email = emails?.filter((mail) => mail.primary)[0].email || "";
-  }
+  const primaryEmail = useMemo(() => {
+    return emails.find((mail) => mail.primary)?.email ?? "";
+  }, [emails]);
 
   return (
     <Splash showChildren={is_loaded}>
@@ -30,19 +30,19 @@ export function ChangePasswordSuccess(): React.JSX.Element {
           <div className="lead">
             <p>
               <FormattedMessage
-                defaultMessage={`You have successfully updated your password. Make sure to store your password securely for future use.`}
+                defaultMessage="You have successfully updated your password. Make sure to store your password securely for future use."
                 description="Change password set new password success lead"
               />
             </p>
           </div>
         </section>
 
-        {!isSuggested ? (
-          <div className="email-display">
-            <EmailFieldset email={email} />
-          </div>
+        {isSuggested ? (
+          <ConfirmUserInfo email_address={primaryEmail} new_password={password} />
         ) : (
-          <ConfirmUserInfo email_address={email} new_password={password} />
+          <div className="email-display">
+            <EmailFieldset email={primaryEmail} />
+          </div>
         )}
         <div className="buttons">
           <EduIDButton id="change-password-finished" buttonstyle="link normal-case" type="submit">
