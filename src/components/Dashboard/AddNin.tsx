510e1225ba5e23f3aa2ee9e7d9cf401be0bd336a
Eunju Huss <eunju@sunet.se>
2026-02-19 10:55:08 +0100

    cleanup and state handling

--- src/components/Dashboard/AddNin.tsx
+++ src/components/Dashboard/AddNin.tsx
@@ -2,11 +2,15 @@ import NinDisplay from "components/Common/NinDisplay";
 import { useAppSelector } from "eduid-hooks";
 import NinForm from "./NinForm";
 
-export default function AddNin(props: { name: string }): React.JSX.Element {
+interface AddNinProps {
+  readonly name: string;
+}
+
+export default function AddNin({ name }: AddNinProps): React.JSX.Element {
   const nin = useAppSelector((state) => state.personal_data?.response?.identities?.nin);
   // name props for unique id
   if (nin) {
-    return <NinDisplay name={props.name} nin={nin} allowDelete={true} />;
+    return <NinDisplay name={name} nin={nin} allowDelete={true} />;
   }
   return <NinForm />;
 }
