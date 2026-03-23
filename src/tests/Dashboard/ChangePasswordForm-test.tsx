216849fbe318560b9cf07c0ab615754b026b5cc9
Eunju Huss <eunju@sunet.se>
2026-03-23 11:42:36 +0100

    rename variable to resolve SonarQube hard-coded password warning

--- src/tests/Dashboard/ChangePasswordForm-test.tsx
+++ src/tests/Dashboard/ChangePasswordForm-test.tsx
@@ -3,19 +3,19 @@ import { initialState as configInitialState } from "slices/IndexConfig";
 
 import { fireEvent, render, screen, waitFor } from "../helperFunctions/DashboardTestApp-rtl";
 
-const suggestPassword = "test-password";
+const suggestedValue = "test-password";
 
 test("renders ChangePasswordForm, suggested password value is field in suggested-password-field", () => {
   render(<ChangePassword />, {
     state: {
       config: { ...configInitialState, is_app_loaded: true },
       chpass: {
-        suggested_password: suggestPassword,
+        suggested_password: suggestedValue,
       },
     },
   });
   expect(screen.getByRole("heading")).toHaveTextContent(/^Change password: Suggested password/);
-  expect(screen.getByDisplayValue(suggestPassword)).toBeInTheDocument();
+  expect(screen.getByDisplayValue(suggestedValue)).toBeInTheDocument();
 });
 
 test("renders custom password form after clicking do not want a suggested password", async () => {
@@ -23,7 +23,7 @@ test("renders custom password form after clicking do not want a suggested passwo
     state: {
       config: { ...configInitialState, is_app_loaded: true },
       chpass: {
-        suggested_password: suggestPassword,
+        suggested_password: suggestedValue,
       },
     },
   });
