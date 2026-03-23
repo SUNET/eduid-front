0b124a085e77c0fd54cf3e3be67cf2c7a981b7f4
cartja <carina@sunet.se>
2026-03-23 13:34:04 +0100

    scroll top button aria and title label

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -2,6 +2,7 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons/faArrowUp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
+import { useIntl } from "react-intl";
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
@@ -11,6 +12,12 @@ function topFunction() {
 
 export default function ScrollToTopButton(): React.JSX.Element {
   const [showBtn, setShowBtn] = useState("display-none");
+  const intl = useIntl();
+  //Translated assistive and visual aid for clickable icon
+  const toTopLabel = intl.formatMessage({
+    defaultMessage: "Go to page top",
+    description: "aria-label and title for scroll top button",
+  });
 
   // When the user scrolls down 20px from the top of the document, show the button
   useEffect(() => {
@@ -29,7 +36,13 @@ export default function ScrollToTopButton(): React.JSX.Element {
   }, []);
 
   return (
-    <button onClick={topFunction} id="scroll-top-button" className={`${showBtn} primary`} title="Go to top">
+    <button
+      onClick={topFunction}
+      id="scroll-top-button"
+      className={`${showBtn} primary`}
+      aria-label={toTopLabel}
+      title={toTopLabel}
+    >
       <FontAwesomeIcon icon={faArrowUp as IconProp} />
     </button>
   );
