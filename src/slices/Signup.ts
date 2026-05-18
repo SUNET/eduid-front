db118df37a345ee84f524911a1327542dff9a92e
Eunju Huss <eunju@sunet.se>
2026-05-18 15:09:53 +0200

    Add nextPageTyps

--- src/slices/Signup.ts
+++ src/slices/Signup.ts
@@ -4,6 +4,7 @@ import { isFSA } from "apis/helpers/typeGuards";
 
 type NextPageTypes =
   | "SIGNUP_START"
+  | "SIGNUP_EXTERNAL_MFA"
   | "SIGNUP_EMAIL_FORM"
   | "PROCESS_CAPTCHA"
   | "SIGNUP_CAPTCHA"
