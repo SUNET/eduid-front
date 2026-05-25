f524b77e5b9bde630a143d6c337365ce622db105
Eunju Huss <eunju@sunet.se>
2026-05-25 14:57:35 +0200

    Update total step

--- src/components/Signup/SignupStepIndicator.tsx
+++ src/components/Signup/SignupStepIndicator.tsx
@@ -1,4 +1,3 @@
-import { useAppSelector } from "eduid-hooks";
 import { Fragment } from "react/jsx-runtime";
 
 interface SignupStepIndicatorProps {
@@ -8,16 +7,13 @@ interface SignupStepIndicatorProps {
 
 export function SignupStepIndicator({
   currentStep,
-  totalSteps,
+  totalSteps = 6,
 }: Readonly<SignupStepIndicatorProps>): React.JSX.Element {
-  const external_mfa = useAppSelector((state) => state.signup.state?.external_mfa);
-  const steps = totalSteps ?? (external_mfa ? 6 : 7);
-
   return (
     <Fragment>
       <hr className="border-line border-line-lesser" />
       <section className="step-indicator">
-        {Array.from({ length: steps }, (_, i) => {
+        {Array.from({ length: totalSteps }, (_, i) => {
           const step = i + 1;
           let className = "";
           if (step < currentStep) className = "completed";
