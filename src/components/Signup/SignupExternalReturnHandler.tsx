74eee610d0035e4f1e3a945e2ca9730ef4530452
Eunju Huss <eunju@sunet.se>
2026-06-03 14:38:16 +0200

    Clean up

--- src/components/Signup/SignupExternalReturnHandler.tsx
+++ src/components/Signup/SignupExternalReturnHandler.tsx
@@ -1,36 +1,3 @@
-// import { signupApi } from "apis/eduidSignup";
-// import { useAppSelector } from "eduid-hooks";
-// import { useCallback, useEffect } from "react";
-// import { useNavigate, useParams } from "react-router";
-
-// interface SignupCallbackParams {
-//   app_name?: string;
-//   authn_id?: string;
-// }
-
-// export function SignupExternalReturnHandler() {
-//   const navigate = useNavigate();
-//   const params = useParams() as SignupCallbackParams;
-//   const [externalMfaRegister] = signupApi.useLazyExternalMfaRegisterQuery();
-//   const is_configured = useAppSelector((state) => state.config.is_configured);
-
-//   const handleCallback = useCallback(async () => {
-//     if (!params.authn_id || !params.app_name || !is_configured) return;
-
-//     await externalMfaRegister({
-//       app_name: params.app_name,
-//       authn_id: params.authn_id,
-//     });
-//     navigate("/register");
-//   }, [params, externalMfaRegister, navigate, is_configured]);
-
-//   useEffect(() => {
-//     handleCallback().catch(console.error);
-//   }, [handleCallback]);
-
-//   return null;
-// }
-
 import { bankIDApi } from "apis/eduidBankid";
 import { eidasApi, GetStatusResponse } from "apis/eduidEidas";
 import { frejaeIDApi } from "apis/eduidFrejaeID";
