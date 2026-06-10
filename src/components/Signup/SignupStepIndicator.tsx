57f1c7c3f72380b287b60ddce08a0143f305bfa0
Eunju Huss <eunju@sunet.se>
2026-06-10 10:40:08 +0200

    add translation

--- src/components/Signup/SignupStepIndicator.tsx
+++ src/components/Signup/SignupStepIndicator.tsx
@@ -1,15 +1,23 @@
+import { FormattedMessage } from "react-intl";
 import { Fragment } from "react/jsx-runtime";
 
 interface SignupStepIndicatorProps {
   currentStep: number;
   totalSteps?: number;
-  labels?: string[];
+  labels?: React.ReactNode[];
 }
 
 export function SignupStepIndicator({
   currentStep,
   totalSteps = 5,
-  labels = ["Registration method", "Confirm/Accept", "Verify email address", "Sign-in method", "Completed"],
+
+  labels = [
+    <FormattedMessage key="step1" description="signup step1" defaultMessage="Registration method" />,
+    <FormattedMessage key="step2" description="signup step2" defaultMessage="Confirm/Accept" />,
+    <FormattedMessage key="step3" description="signup step3" defaultMessage="Verify email address" />,
+    <FormattedMessage key="step4" description="signup step4" defaultMessage="Sign-in method" />,
+    <FormattedMessage key="step5" description="signup step5" defaultMessage="Completed" />,
+  ],
 }: Readonly<SignupStepIndicatorProps>): React.JSX.Element {
   return (
     <Fragment>
