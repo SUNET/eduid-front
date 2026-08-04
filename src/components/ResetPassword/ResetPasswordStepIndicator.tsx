bc1e2950dd877a9c692338f24bf58009ccbaf23e
cartja <carina@sunet.se>
2026-08-04 13:28:11 +0200

    tweaks to reset pw steps copy

--- src/components/ResetPassword/ResetPasswordStepIndicator.tsx
+++ src/components/ResetPassword/ResetPasswordStepIndicator.tsx
@@ -2,11 +2,11 @@ import { StepIndicator } from "components/Common/StepIndicator";
 import { FormattedMessage } from "react-intl";
 
 const RESETPW_LABELS = [
-  <FormattedMessage key="step1" description="resetpw step1" defaultMessage="Enter the email address" />,
-  <FormattedMessage key="step2" description="resetpw step2" defaultMessage="Captcha" />,
+  <FormattedMessage key="step1" description="resetpw step1" defaultMessage="Enter email address" />,
+  <FormattedMessage key="step2" description="resetpw step2" defaultMessage="Confirm Captcha" />,
   <FormattedMessage key="step3" description="resetpw step3" defaultMessage="Verify email address" />,
   <FormattedMessage key="step4" description="resetpw step4" defaultMessage="Verification method" />,
-  <FormattedMessage key="step5" description="resetpw step5" defaultMessage="Set a password" />,
+  <FormattedMessage key="step5" description="resetpw step5" defaultMessage="Set password" />,
   <FormattedMessage key="step6" description="resetpw step6" defaultMessage="Completed" />,
 ];
 
